4e1d99ae49395f6a5c49a4d0cd63f70a3bb5ec2e
Owen Winkler <epithet@gmail.com>
2006-10-11 20:55:58 +0000

    Minor database class fixes/feature additions.  Fixed query in posts class for SQLite compatibility.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@19 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/habari_db.php
+++ classes/habari_db.php
@@ -16,6 +16,7 @@ class habari_db
 	private $dbh;  // Database handle
 	private $pdostatement;  // PDOStatement handle
 	private $errors = array(); // Array of SQL errors 
+	public $queryok; // Boolean on last query success 
 
 	/**
 	 * function __construct
@@ -45,9 +46,11 @@ class habari_db
 		if($this->pdostatement) {
 			$this->pdostatement->setFetchMode(PDO::FETCH_OBJ);
 			if($this->pdostatement->execute($args)) {
+				$this->queryok = true;
 				return true;
 			}
 		}
+		$this->queryok = false;
 		$this->errors[] = $this->dbh->errorInfo();
 		return false;
 	}
@@ -62,6 +65,15 @@ class habari_db
 	}
 
 	/**
+	 * function get_last_error
+	 * Returns only the last error info
+	 */	  	 	
+	public function get_last_error()
+	{
+		return end($this->errors);
+	}
+
+	/**
 	 * function get_results
 	 * Execute a query and return the results as an array of objects
 	 * @param string The query to execute
@@ -70,8 +82,10 @@ class habari_db
 	public function get_results()
 	{
 		$args = func_get_args();
-		if(call_user_func_array(array(&$this, query), $args))
+		if(count($args) != 0) call_user_func_array(array(&$this, 'query'), $args);
+		if($this->queryok) {
 			return $this->pdostatement->fetchAll();
+		}
 		else
 			return false;
 	}
@@ -100,49 +114,48 @@ class habari_db
 		return call_user_func_array(array(&$this, query), $values);
 	}	 	 	 	 	
 
-public function install_habari() {
-/**
- * function install_habari
- * Installs base tables and starter data.
- */
- 	global $db;
- 
-	// Create the table
-	if ( $db->query("CREATE TABLE habari__posts 
-		(slug VARCHAR(255) NOT NULL PRIMARY KEY, 
-		title VARCHAR(255), 
-		guid VARCHAR(255) NOT NULL, 
-		content LONGTEXT, 
-		author VARCHAR(255) NOT NULL, 
-		status VARCHAR(50) NOT NULL, 
-		pubdate TIMESTAMP, 
-		updated TIMESTAMP);")) {
-			echo "Created Table<br />";
+	public function install_habari() {
+	/**
+	 * function install_habari
+	 * Installs base tables and starter data.
+	 */
+	 
+		// Create the table
+		if ( $this->query("CREATE TABLE habari__posts 
+			(slug VARCHAR(255) NOT NULL PRIMARY KEY, 
+			title VARCHAR(255), 
+			guid VARCHAR(255) NOT NULL, 
+			content LONGTEXT, 
+			author VARCHAR(255) NOT NULL, 
+			status VARCHAR(50) NOT NULL, 
+			pubdate TIMESTAMP, 
+			updated TIMESTAMP);")) {
+				echo "Created Table<br />";
 		} else {
-			print_r( $db->get_errors() );
+			print_r( $db->get_last_error() );
 		}
+		
+		// Insert records
 	
-	// Insert records
-
-	if($db->query("INSERT INTO habari__posts (slug, title, guid, content, author, status, pubdate, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-		'first-post', 'First Post', 'tag:localhost/first-post/1935076', 'This is my first post', 'owen', 'publish', '2006-10-04 17:17:00', '2006-10-04 17:17:00')) echo "Inserted Record 1<br/>";
-
-	if($db->query("INSERT INTO habari__posts (slug, title, guid, content, author, status, pubdate, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-		'second-post', 'Second Post', 'tag:localhost/second-post/5987120', 'This is my second post', 'owen', 'publish', '2006-10-04 17:18:00', '2006-10-04 17:18:00')) echo "Inserted Record 2<br/>";
-
-	if($db->insert('habari__posts', array (
-		'slug'=>'third-post',
-		'title'=>'Third Post',
-		'guid'=>'tag:localhost/third-post/4981704',
-		'content'=>'This is my third post',
-		'author'=>'owen',
-		'status'=>'publish',
-		'pubdate'=>'2006-10-04 17:19:00',
-		'updated'=>'2006-10-04 17:18:00'
-	))) echo "Inserted Record 3<br/>";
-
-	// Output any errors
-	echo "Errors:<pre>" . print_r($db->get_errors(), 1) . "</pre>";
+		if($this->query("INSERT INTO habari__posts (slug, title, guid, content, author, status, pubdate, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+			'first-post', 'First Post', 'tag:localhost/first-post/1935076', 'This is my first post', 'owen', 'publish', '2006-10-04 17:17:00', '2006-10-04 17:17:00')) echo "Inserted Record 1<br/>";
+	
+		if($this->query("INSERT INTO habari__posts (slug, title, guid, content, author, status, pubdate, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+			'second-post', 'Second Post', 'tag:localhost/second-post/5987120', 'This is my second post', 'owen', 'publish', '2006-10-04 17:18:00', '2006-10-04 17:18:00')) echo "Inserted Record 2<br/>";
+	
+		if($this->insert('habari__posts', array (
+			'slug'=>'third-post',
+			'title'=>'Third Post',
+			'guid'=>'tag:localhost/third-post/4981704',
+			'content'=>'This is my third post',
+			'author'=>'owen',
+			'status'=>'publish',
+			'pubdate'=>'2006-10-04 17:19:00',
+			'updated'=>'2006-10-04 17:18:00'
+		))) echo "Inserted Record 3<br/>";
+	
+		// Output any errors
+		echo "Errors:<pre>" . print_r($this->get_errors(), 1) . "</pre>";
 	}
 }
 ?>
--- classes/posts.php
+++ classes/posts.php
@@ -9,7 +9,7 @@
 class posts {
 	function retrieve() {
 		global $db;
-		$query = $db->get_results( "SELECT slug, title, guid, content, author, status, pubdate, updated from habari__posts ORDER BY 'pubdate' DESC" );
+		$query = $db->get_results( "SELECT slug, title, guid, content, author, status, pubdate, updated from habari__posts ORDER BY pubdate DESC" );
 			if ( is_array( $query ) ) {
 				return $query;
 			} else {
