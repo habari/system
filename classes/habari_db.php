d362bb9e57ab9eeb46fe720d4bc3b620445c9088
Chris J Davis <chrisjdavis@contrib.habariproject.org>
2006-10-11 15:46:21 +0000

    Trying to add some files for test installs of Habari
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@8 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ classes/habari_db.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Habari DB Class
+ *
+ * Requires PHP 5.0.4 or later
+ * @package Habari
+ */
+
+/**
+ * class habari_db
+ * The database class.
+ * Connects to the database and provides access to data 
+ */  
+class habari_db
+{
+	private $dbh;  // Database handle
+	private $recordset;  // PDOStatement handle
+	private $errors = array(); // Array of SQL errors 
+
+	/**
+	 * function __construct
+	 * Connects to a PDO database.	 
+	 * @param string A PDO connection string
+	 * @param string The database username
+	 * @param string The database password
+	 */	 	 	
+	public function __construct($connection_string, $user, $pass) 
+	{
+			$this->dbh = new PDO($connection_string, $user, $pass);
+	}
+
+	/**
+	 * function query
+	 * Executes a query and returns success
+	 * @param string The query to execute
+	 * @params mixed Arguments to pass for prepared statements
+	 */	 	 	 	 	
+	public function query()
+	{
+		// Grab query from first parameter
+		$args = func_get_args();
+		$query = array_shift($args);
+
+		$this->recordset = $this->dbh->prepare($query);
+		if($this->recordset) {
+			$this->recordset->setFetchMode(PDO::FETCH_OBJ);
+			if($this->recordset->execute($args)) {
+				return true;
+			}
+		}
+		$this->errors[] = $this->dbh->errorInfo();
+		return false;
+	}
+	
+	/**
+	 * function get_errors
+	 * Returns error data gathered from database connection
+	 */	  	 	
+	public function get_errors()
+	{
+		return $this->errors;
+	}
+
+	/**
+	 * function get_results
+	 * Execute a query and return the results as an array of objects
+	 * @param string The query to execute
+	 * @params mixed Arguments to pass for prepared statements
+	 */	 	 	 	 
+	public function get_results()
+	{
+		$args = func_get_args();
+		call_user_func_array(array(&$this, query), $args);
+		return $this->recordset->fetchAll();
+	}
+	
+	/**
+	 * function insert
+	 * Inserts into the specified table values associated to the key fields
+	 * @param string The table name
+	 * @param array An associative array of fields and values to insert
+	 */
+	public function insert($table, $fieldvalues)
+	{
+		ksort($fieldvalues);
+
+		$query = "INSERT INTO {$table} (";
+		$comma = '';
+		
+		foreach($fieldvalues as $field => $value) {
+			$query .= $comma . $field;
+			$comma = ', ';
+			$values[] = $value;
+		}
+		$query .= ') VALUES (' . trim(str_repeat('?,', count($fieldvalues)), ',') . ');';
+
+		array_unshift($values, $query);
+		return call_user_func_array(array(&$this, query), $values);
+	}	 	 	 	 	
+
+public function install_habari() {
+/**
+ * function install_habari
+ * Installs base tables and starter data.
+ */
+	// Create the table
+	if ( $db->query("CREATE TABLE habari__posts 
+		(slug VARCHAR(255) NOT NULL PRIMARY KEY, 
+		title VARCHAR(255), 
+		guid VARCHAR(255) NOT NULL, 
+		content LONGTEXT, 
+		author VARCHAR(255) NOT NULL, 
+		status VARCHAR(50) NOT NULL, 
+		pubdate TIMESTAMP, 
+		updated TIMESTAMP);")) {
+			echo "Created Table<br />";
+		} else {
+			print_r( $db->get_errors() );
+		}
+	
+	// Insert records
+
+	if($db->query("INSERT INTO habari__posts (slug, title, guid, content, author, status, pubdate, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+		'first-post', 'First Post', 'tag:localhost/first-post/1935076', 'This is my first post', 'owen', 'publish', '2006-10-04 17:17:00', '2006-10-04 17:17:00')) echo "Inserted Record 1<br/>";
+
+	if($db->query("INSERT INTO habari__posts (slug, title, guid, content, author, status, pubdate, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+		'second-post', 'Second Post', 'tag:localhost/second-post/5987120', 'This is my second post', 'owen', 'publish', '2006-10-04 17:18:00', '2006-10-04 17:18:00')) echo "Inserted Record 2<br/>";
+
+	if($db->insert('habari__posts', array (
+		'slug'=>'third-post',
+		'title'=>'Third Post',
+		'guid'=>'tag:localhost/third-post/4981704',
+		'content'=>'This is my third post',
+		'author'=>'owen',
+		'status'=>'publish',
+		'pubdate'=>'2006-10-04 17:19:00',
+		'updated'=>'2006-10-04 17:18:00'
+	))) echo "Inserted Record 3<br/>";
+
+	// Output any errors
+	echo "Errors:<pre>" . print_r($db->get_errors(), 1) . "</pre>";
+	}
+}
+?>
