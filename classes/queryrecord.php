62708f6182751a6e9fe0dd558d5d12601b5c4a47
Owen Winkler <epithet@gmail.com>
2006-10-17 19:28:34 +0000

    Additional phpdocs comments, added delete method to Post ad QueryRecord.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@36 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/queryrecord.php
+++ classes/queryrecord.php
@@ -11,8 +11,13 @@ class QueryRecord
 {
 	protected $fields = array();  // Holds field values from db
 	protected $newfields = array(); // Holds updated field values to commit to db
-	private $loaded = false;
+	private $loaded = false;  // Set to true after the constructor executes, is false when PDO fills data fields
 	
+	/**
+	 * constructor __construct
+	 * Constructor for the QueryRecord class.
+	 * @param array an associative array of initial field values.
+	 **/	 	 	 	
 	public function __construct($paramarray = array())
 	{
 		$this->loaded = true;
@@ -24,11 +29,24 @@ class QueryRecord
 		);
 	}
 	
+	/**
+	 * function __get
+	 * Handles getting virtual properties for this class
+	 * @param string Name of the property
+	 * @return mixed The set value
+	 **/	 
 	public function __get($name)
 	{
 		return isset($this->newfields[$name]) ? $this->newfields[$name] : $this->fields[$name];
 	}
 	
+	/**
+	 * function __set
+	 * Handles setting virtual properties for this class
+	 * @param string Name of the property
+	 * @param mixed Value to set it to	 
+	 * @return mixed The set value 
+	 **/	 
 	public function __set($name, $value)
 	{
 		if($this->loaded) {
@@ -40,18 +58,45 @@ class QueryRecord
 		return $this->__get($name);
 	}
 	
+	/**
+	 * function insert
+	 * Inserts this record's fields as a new row
+	 * @param string Table to update
+	 * @return boolean True on success, false if not 
+	 **/	 
 	public function insert($table)
 	{
 		global $db;
 
-		$db->insert($table, array_merge($this->fields, $this->newfields));
+		return $db->insert($table, array_merge($this->fields, $this->newfields));
 	}
-	
+
+	/**
+	 * function update
+	 * Updates this record's fields using the new data
+	 * @param string Table to update
+	 * @param array An associative array of field data to match	 	 	 		
+	 * @return boolean True on success, false if not 
+	 **/	 
 	public function update($table, $updatekeyfields)
 	{
 		global $db;
 		
-		$db->update($table, array_merge($this->fields, $this->newfields), $updatekeyfields);
+		return $db->update($table, array_merge($this->fields, $this->newfields), $updatekeyfields);
+	}
+	
+	/**
+	 * function delete
+	 * Deletes a record based on the match array
+	 * @param string Table to delete from
+	 * @param array An associative array of field data to match	 	 	 		
+	 * @return boolean True on success, false if not 
+	 **/	 
+	public function delete($table, $updatekeyfields)
+	{
+		global $db;
+		
+		return $db->delete($table, $updatekeyfields);
 	}
 
 }
