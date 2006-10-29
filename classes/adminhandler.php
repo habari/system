888a811cc712c24390ed4ccf7508f2a50a116077
Scott Merrill <skippy@contrib.habariproject.org>
2006-10-29 02:43:05 +0000

    Adding skeletal dashboard.  The AdminHandler constructor checks to confirm that the accessing user is logged in.  This needs to be modified eventually to check for administrative privilege.  Shuffled some of the rules in URLParser init_rules, purely for organization.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@81 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/adminhandler.php
+++ classes/adminhandler.php
@@ -8,6 +8,19 @@
 
 class AdminHandler extends ActionHandler
 {
+	/**
+	* constructor __construct
+	* verify that the page is being accessed by an admin
+	* @param string The action that was in the URLParser rule
+	* @param array An associative array of settings found in the URL by the URLParser
+	*/
+	public function __construct( $action, $settings )
+	{
+		if (! user::identify() ) {
+			die ("Tricksey, eh?");
+		}
+		parent::__construct( $action, $settings );
+	}
 
 	/**
 	 * Description
@@ -15,6 +28,14 @@ class AdminHandler extends ActionHandler
 	 * @param array Settings array from the URLParser
 	 **/
 	public function wooga( $settings ) {
+	}
+
+	/**
+	* function dashboard
+	* display an overview of current blog stats
+	*/
+	public function dashboard() {
+		echo "Hiya! Welcome to your dashboard.";
 	}
 }
 
