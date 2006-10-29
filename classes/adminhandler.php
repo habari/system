ed4b7a93bdc9e88c8edbe1a1f3df822020a1c961
Rich Bowen <rbowen@contrib.habariproject.org>
2006-10-29 01:54:45 +0000

    AdminHandler class. There. We're practically done.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@77 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ classes/adminhandler.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Habari AdminHandler Class
+ *
+ * @package Habari
+ */
+
+class AdminHandler extends ActionHandler
+{
+
+	/**
+	 * Description
+	 *
+	 * @param array Settings array from the URLParser
+	 **/
+	public function wooga( $settings ) {
+	}
+}
+
+?>
