0bba92d77198462ae29958a65b9126e91d1d53e8
Chris J Davis <chrisjdavis@contrib.habariproject.org>
2006-10-29 21:43:06 +0000

    adding the beginnings of the admin area from a style perspective.  Feel free to complain.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@86 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ admin/footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+	<p>
+		powered by <a href="http://code.google.com/p/habari/" title="Habari">Habari</a> <?php echo Options::o()->version; ?>.
+	</p>
+</div>
+</body>
+</html>
---
+++ admin/options.php
@@ -0,0 +1,4 @@
+<div id="content-area">
+	<h3>Habari Options</h3>
+	<p>Below are the options currently set on your site. You can also create new options if you need them.</p>
+</div>
