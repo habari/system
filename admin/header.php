5850a065b0753f493e223cb79860a60e6f4fd387
Owen Winkler <ringmaster@contrib.habariproject.org>
2006-11-01 17:59:52 +0000

    Removed login redirect from admin header which is now done in the handler.  Changed links to use new URL::out() to echo admin urls.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@112 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/header.php
+++ admin/header.php
@@ -1,10 +1,3 @@
-<?php $user = User::identify(); 
-	if ( '' == $user ) {
-		$login = Options::get('base_url') . 'login';
-		header( "Location: " . $login );
-		exit;
-	} else { 
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
@@ -14,15 +7,14 @@
 <body>
 <div id="page">
 	<div id="header">
-		<h1>Welcome back <?php echo User::identify()->nickname; ?> &raquo; <a href="<?php Options::out('base_url'); ?>" title="View <?php Options::out('blog_title'); ?>">view site</a> + <a href="<?php echo Options::out('base_url'); ?>admin/user" title="<?php Options::out('base_url'); ?>admin/profile">edit your profile</a> + <a href="<?php Options::out('base_url'); ?>logout" title="logout of Habari">logout</a></h1>
+		<h1>Welcome back <?php echo User::identify()->nickname; ?> &raquo; <a href="<?php Options::out('base_url'); ?>" title="View <?php Options::out('title'); ?>">view site</a> + <a href="<?php echo URL::out('admin', 'page=user'); ?>" title="<?php URL::out('admin', 'page=profile'); ?>">edit your profile</a> + <a href="<?php URL::out('logout'); ?>" title="logout of Habari">logout</a></h1>
 	</div>
 	<div id="menu">
 		<ul id="menu-items">
 			<li id="current-item"><a href="<?php Options::out('base_url'); ?>admin/" title="Overview of your site">Overview</a></li>
-			<li><a href="<?php Options::out('base_url'); ?>admin/content" title="Edit the content of your site">Content</a></li>
-			<li><a href="<?php Options::out('base_url'); ?>admin/options" title="edit your site options">Options</a></li>
-			<li><a href="<?php Options::out('base_url'); ?>admin/themes" title="manage your sites themes">Themes</a></li>
-			<li><a href="<?php Options::out('base_url'); ?>admin/plugins" title="edit your sites plugins">Plugins</a></li>
+			<li><a href="<?php URL::out('admin', 'page=content'); ?>" title="Edit the content of your site">Content</a></li>
+			<li><a href="<?php URL::out('admin', 'page=options'); ?>" title="edit your site options">Options</a></li>
+			<li><a href="<?php URL::out('admin', 'page=themes'); ?>" title="manage your sites themes">Themes</a></li>
+			<li><a href="<?php URL::out('admin', 'page=plugins'); ?>" title="edit your sites plugins">Plugins</a></li>
 		</ul>
 	</div>
-<?php } ?>
