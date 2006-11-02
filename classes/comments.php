8317537c05bd66829246793ee0079a9a8bb3d818
Scott Merrill <skippy@contrib.habariproject.org>
2006-11-02 04:04:23 +0000

    Checking in work on comments.php class: by_email(), by_name(), by_url(), and by_ip() all return comments that have  the specified value. by_slug() returns all comments attached to a specific post by_status() returns all comments of the specified status
    I considered a by_date() method, but decided against it for now.
    
    I have added the comments table to schema.mysql.php.
    
    The Post class now supports $post->comments to return the comments 
    attached to the current post.
    
    I'm sure some tidying up is necessary on the Comments class, and a 
    corresponding Comment class is needed now to handle individual comments.
    
    
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@115 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/comments.php
+++ classes/comments.php
@@ -1,33 +1,190 @@
 <?php
 /**
- * Habari Comment Creation & Retrieval Class
+ * Habari Comment Class
  *
  * Requires PHP 5.0.4 or later
  * @package Habari
  */
 
-// Needs to be sorted out once we have a schema
+class Comments extends QueryRecord
+{
 
-class Comments {
+	static function default_fields()
+	{
+		return  array(
+				'id' => '',
+				'post_slug' => '',
+				'name' => '',
+				'email' => '',
+				'url' => '',
+				'ip' => '',
+				'content' => '',
+				'status' => '',
+				'date' => ''
+				);
+	}
 
 	/**
-	 * function retrieve
-	 * Returns requested comments
-	 * @return array An array of Comment objects, one for each query result
-	 **/	 	  
-	static function retrieve() {
+	* constructor __construct
+	* Constructor for the Comments class
+	* @param array an associative array of initial Comment field values
+	**/
+	public function __construct( $paramarray = array() )
+	{
+		// Defaults
+		$this->fields = array_merge( self::default_fields(), $this->fields );
+		parent::__construct( $paramarray );
+	}
+
+	/* static function get
+	* Returns a single comment
+	* @param int a comment's ID
+	* @return a Comment object
+	*/
+	static function get( $ID = 0 )
+	{
+		if ( ! $ID )
+		{
+			return false;
+		}
 		global $db;
-		$query = $db->get_results( "SELECT "stuff" from habari__comments ORDER BY 'pubdate' DESC" );
-			if ( is_array( $query ) ) {
-				return $query;
-			} else {
-				return array();
+		$comment = $db->get_row("SELECT * FROM habari__posts WHERE id = ?", array( $ID ) );
+		return $comment;
+	}
+
+	/**
+	 * function do_query
+	 * Returns requested comments
+	 * @param array An associated array of parameters, or a querystring
+	 * @return array An array of Comment objects, one for each query result
+	 *
+	 * <code>
+	 * $comments = comments::do_query( array ("author" => "skippy" ) );
+	 * $comments = comments::do_query( array ("slug" => "first-post", "status" => "1", "orderby" => "date ASC" ) );
+	 * </code>
+	 *
+	 **/	 	  
+	static function do_query( $paramarray )
+	{
+		global $db;
+
+		$orderby = "date ASC";
+		foreach ($paramarray as $key => $value)
+		{
+			if ('orderby' == $key)
+			{
+				$orderby = $value;
+				continue;
 			}
+			// only accept those keys that correspond to
+			// table columns
+			if ( ! array_key_exists ( $key, self::default_fields() ) )
+			{
+				continue;
+			}
+			$where[] = "$key = ?";
+			$params[] = $value;
+		}
+
+		$sql = "SELECT * from habari__comments WHERE " . implode( ' AND ', $where ) . " ORDER BY $orderby";
+		$query = $db->get_results($sql, $params);
+		if ( is_array( $query ) ) {
+			return $query;
+		} else {
+			return array();
+		}
 	}
 	
 	static function create() {
 		global $db;
 		// insert posts!
 	}
+
+	/**
+	* function by_email
+	* selects all comments from a given email address
+	* @param string an email address
+	* @return array an array of Comment objects written by that email address
+	*/
+	public function by_email($email = '')
+	{
+		if ( ! $email )
+		{
+			return array();
+		}
+		return self::do_query( array ( "email" => $email ) );
+	}
+
+	/**
+	* function by_name
+	* selects all comments from a given name
+	* @param string a name
+	* @return array an array of Comment objects written by the given name
+	*/
+	public function by_name ($name = '')
+	{
+		if ( ! $name )
+		{
+			return array();
+		}
+		return self::do_query( array ( "name" => $name ) );
+	}
+
+	/**
+	* function by_ip
+	* selects all comments from a given IP address
+	* @param string an IP address
+	* @return array an array of Comment objects written from the given IP
+	*/
+	public function by_ip ( $ip = '' )
+	{
+		if ( ! $ip ) 
+		{
+			return false;
+		}
+		return self::do_query( array ( "ip" => $ip ) );
+	}
+
+	/**
+	* function by_url
+	* select all comments from an author's URL
+	* @param string a URL
+	* @return array array an array of Comment objects with the same URL
+	*/
+	public function by_url ( $url = '' )
+	{
+		if ( ! $url )
+		{
+			return false;
+		}
+		return self::do_query( array( "url" => $url ) );
+	}
+
+	/**
+	* function by_slug
+	* select all comments for a given post slug
+	* @param string a post slug
+	* @return array array an array of Comment objects for the given post
+	*/
+	public function by_slug ( $slug = '' )
+	{
+		if ( ! $slug )
+		{
+			return false;
+		}
+		return self::do_query( array( "post_slug" => $slug ) );
+	}
+
+	/**
+	* function by_status
+	* select all comments of a given status
+	* @param int a status value
+	* @return array array an array of Comment objects with the same status
+	*/
+	public function by_status ( $status = 0 )
+	{
+		return self::do_query( array( "status" => $status ) );
+	}
+
 }
 ?>
