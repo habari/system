fc33d8639aa80493104ae5f525c279d01e20dbb2
Scott Merrill <skippy@contrib.habariproject.org>
2006-11-05 13:51:25 +0000

    adding a stub /admin/content page, which displays posts and comments. This is proof-of-concept, and is being checked in with the express intention of having it modified, or at least starting a discussion.  It does not paginate.
    Also modified comments.php so that Comments::get() will return all 
    comments, by use of the "WHERE 1=1" trick used in Posts::get().
    
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@137 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ admin/content.php
@@ -0,0 +1,48 @@
+<div id="content-area">
+	<h1>Habari Content</h1>
+	<?php 
+	switch( URL::o()->settings['result'] ) {
+	case 'success':
+		_e('<p>Your options have been updated.</p>');
+		break;
+	}
+	?>
+<div style="width: 45%; float: left; border-right: 1px solid #000;">
+<strong>Posts</strong>
+<ul>
+<?php
+	foreach (Posts::get() as $post)
+	{
+		echo '<li>';
+		echo '<a href="' . $post->permalink . '">' . $post->title . '</a><br />';
+		echo 'By ' . $post->author . ' on ' . $post->pubdate;
+		if ($post->updated > $post->pubdate)
+		{
+			echo ' (updated: ' . $post->updated . ')';
+		}
+		echo '<br />';
+		echo $post->content;
+		echo '</li>';
+	}
+?>
+</ul>
+</div>
+<div style="width: 45%; float: left; margin-left: 2px;">
+<strong>Comments</strong>
+<ul>
+<?php
+	foreach (Comments::get() as $comment)
+	{
+		echo '<li>';
+		echo '<a href="mailto:' . $comment->email . '">' . $comment->name . '</a> on <a href="' . URL::get( 'post', array( 'slug' => $comment->post_slug ) ) . '">' . $comment->post_slug . '</a><br />';
+		echo $comment->date . ' from <a href="http://ws.arin.net/cgi-bin/whois.pl?queryinput=' . $comment->ip . '">' . $comment->ip . '</a><br />';
+		echo $comment->content;
+		echo '</li>';
+	}
+?>
+</ul>
+</div>
+<div style="clear: both;"></div>
+<p> </p>
+
+</div>
