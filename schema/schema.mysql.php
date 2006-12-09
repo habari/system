ea3ee73af4574cba25dcea6ee31296417e4efa67
Scott Merrill <skippy@contrib.habariproject.org>
2006-12-09 03:42:25 +0000

    updating mysql schema to use new table prefix support.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@177 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/schema.mysql.php
+++ schema/schema.mysql.php
@@ -1,6 +1,6 @@
 <?php
 $queries = array(
-'CREATE TABLE habari__posts ( 
+'CREATE TABLE ' . DB::o()->posts . ' ( 
 	id smallint AUTO_INCREMENT NOT NULL UNIQUE,
 	slug VARCHAR(255) NOT NULL PRIMARY KEY, 
 	title VARCHAR(255), 
@@ -11,24 +11,24 @@ $queries = array(
 	pubdate TIMESTAMP, 
 	updated TIMESTAMP
 	);',
-'CREATE TABLE habari__options (
+'CREATE TABLE ' . DB::o()->options . ' (
 	name varchar(50) PRIMARY KEY NOT NULL UNIQUE,
 	type integer DEFAULT 0,
 	value text
 	);',
-'CREATE TABLE habari__users (
+'CREATE TABLE ' . DB::o()->users . ' (
 	id smallint AUTO_INCREMENT NOT NULL UNIQUE,
 	username varchar(20) PRIMARY KEY NOT NULL UNIQUE,
 	email varchar(30) NOT NULL,
 	password varchar(40) NOT NULL
 	);',
-'CREATE TABLE habari__tags (
+'CREATE TABLE ' . DB::o()->tags . ' (
 	slug varchar(255) NOT NULL,
 	tag varchar(30) NOT NULL,
 	KEY slug (slug),
 	KEY tag (tag)
 );',
-'CREATE TABLE habari__comments (
+'CREATE TABLE ' . DB::o()->comments . ' (
 	id int AUTO_INCREMENT NOT NULL UNIQUE,
 	post_slug varchar(255) NOT NULL,
 	name varchar(255),
