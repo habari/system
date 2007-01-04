611cb67e31e92e30e1e5323bfc32cb01d4d9e695
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-01-04 14:49:26 +0000

    PHPDoc update.  Checking out some of the features, now that I've bothered to run a parser on it.  Pretty neat!
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@233 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/actionhandler.php
+++ classes/actionhandler.php
@@ -1,23 +1,27 @@
 <?php
 
 /**
- * Habari ActionHandler Class
- *
- * Requires PHP 5.0.4 or later
+ * class ActionHandler
+ *  
+ * A base class dispatcher for URL-based actions.
+ * The class name specified in a URL rule should be an extension of this class,
+ * and should implement a function named the same as the action value of the
+ * rule.
+ *  
  * @package Habari
- * 
- * A base class to derive Handlers from.
- * Handlers are used by the URL to dispatch actions found in the request URL   
- */
-
+ * @see URL::init_rules()
+ * @version $Id$ 
+ **/  
 class ActionHandler
 {
 
 	/**
 	 * function __construct
+	 * 	 
 	 * Constructor for ActionHandler and derived classes
 	 * Attempts to find a method in the object that matches the requested action,
 	 * and call it with the settings that are provided.
+	 * 	 
 	 * @param string The action that was in the URL rule
 	 * @param array An associative array of settings found in the URL by the URL
 	 **/	 	 	 	 
