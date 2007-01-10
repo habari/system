38a7bfec82d414890418d09f33e90da17030a5f8
Jay Pipes <jaypipes@contrib.habariproject.org>
2007-01-10 03:44:41 +0000

    Added themes table, indexing, cleaned up the tags to post relationship, and created relationships on integer columns instead of VARCHAR(255)
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@278 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/schema.mysql.php
+++ schema/schema.mysql.php
@@ -1,78 +1,120 @@
 <?php
 $queries = array(
 'CREATE TABLE ' . DB::o()->posts . ' ( 
-	id smallint AUTO_INCREMENT NOT NULL UNIQUE,
-	slug VARCHAR(255) NOT NULL PRIMARY KEY,
-	content_type smallint,
-	title VARCHAR(255), 
-	guid VARCHAR(255) NOT NULL, 
-	content LONGTEXT, 
-	user_id smallint, 
-	status smallint,
-	pubdate TIMESTAMP, 
-	updated TIMESTAMP
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT
+,	slug VARCHAR(255) NOT NULL
+,	content_type SMALLINT UNSIGNED NOT NULL
+,	title VARCHAR(255) NOT NULL
+,	guid VARCHAR(255) NOT NULL
+,	content LONGTEXT NOT NULL
+,	user_id SMALLINT UNSIGNED NOT NULL
+,	status SMALLINT UNSIGNED NOT NULL
+,	pubdate DATETIME NOT NULL 
+,	updated TIMESTAMP NOT NULL
+, PRIMARY KEY (id)
+, UNIQUE INDEX (slug(80))
 	);',
 'CREATE TABLE ' . DB::o()->postinfo . ' ( 
-	slug VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	post_id INT UNSIGNED NOT NULL
+,	name VARCHAR(50) NOT NULL
+,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
+,	value TEXT
+, PRIMARY KEY (post_id, name)
 	);',
 'CREATE TABLE ' . DB::o()->posttype . ' ( 
-	name VARCHAR(255) NOT NULL PRIMARY KEY,
-	type smallint DEFAULT 0
+	name VARCHAR(255) NOT NULL 
+,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
+, PRIMARY KEY (name)
 	);',
 'INSERT INTO ' . DB::o()->posttype . ' VALUES
 		(\'entry\', 0),
 		(\'page\', 1);',
 'CREATE TABLE ' . DB::o()->poststatus . ' ( 
-	name VARCHAR(255) NOT NULL PRIMARY KEY,
-	type smallint DEFAULT 0
+	name VARCHAR(255) NOT NULL 
+,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
+, PRIMARY KEY (name)
 	);',
 'INSERT INTO ' . DB::o()->poststatus . ' VALUES
 		(\'draft\', 0),
 		(\'published\', 1), 
 		(\'private\', 1);',
 'CREATE TABLE ' . DB::o()->options . ' (
-	name varchar(50) PRIMARY KEY NOT NULL UNIQUE,
-	type integer DEFAULT 0,
-	value text
+	name VARCHAR(50) NOT NULL
+,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
+,	value TEXT
+, PRIMARY KEY (name)
 	);',
 'CREATE TABLE ' . DB::o()->users . ' (
-	id smallint AUTO_INCREMENT NOT NULL UNIQUE,
-	username varchar(20) PRIMARY KEY NOT NULL UNIQUE,
-	email varchar(30) NOT NULL,
-	password varchar(40) NOT NULL
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
+,	username VARCHAR(20) NOT NULL
+,	email VARCHAR(30) NOT NULL
+,	password VARCHAR(40) NOT NULL
+, PRIMARY KEY (id)
+, UNIQUE INDEX (username)
 	);',
 'CREATE TABLE ' . DB::o()->userinfo . ' ( 
-	user_id VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	user_id SMALLINT UNSIGNED NOT NULL
+,	name VARCHAR(50) NOT NULL
+,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
+,	value TEXT
+, PRIMARY KEY (user_id, name)
 	);',
 'CREATE TABLE ' . DB::o()->tags . ' (
-	slug varchar(255) NOT NULL,
-	tag varchar(30) NOT NULL,
-	KEY slug (slug),
-	KEY tag (tag)
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT
+, tag_text VARCHAR(50) NOT NULL
+, PRIMARY KEY (id)
+, UNIQUE INDEX (tag_text)	
+);',
+'CREATE TABLE ' . DB::o()->tag2post . ' (
+  tag_id INT UNSIGNED NOT NULL
+, post_id INT UNSIGNED NOT NULL
+, PRIMARY KEY (tag_id, post_id)
+, INDEX (post_id)
+);',
+'CREATE TABLE ' . DB::o()->themes . ' (
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
+, name VARCHAR(80) NOT NULL
+, version VARCHAR(10) NOT NULL
+, template_engine VARCHAR(40) NOT NULL
+, theme_dir VARCHAR(255) NOT NULL
+, is_active TINYINT UNSIGNED NOT NULL DEFAULT 0
+, PRIMARY KEY (id)
+);',
+'INSERT INTO ' . DB::o()->themes . ' (
+  id
+, name
+, version
+, template_engine
+, theme_dir
+, is_active
+) VALUES (
+  NULL
+, \'k2\'
+, \'1.0\'
+, \'rawphpengine\'
+, \'k2\'
+, 1
 );',
 'CREATE TABLE ' . DB::o()->comments . ' (
-	id int AUTO_INCREMENT NOT NULL UNIQUE,
-	post_slug varchar(255) NOT NULL,
-	name varchar(255),
-	email varchar(255),
-	url varchar(255),
-	ip varchar(255),
-	content text,
-	status int,
-	date TIMESTAMP,
-	type int
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT
+,	post_id INT UNSIGNED NOT NULL
+,	name VARCHAR(100) NOT NULL
+,	email VARCHAR(100) NOT NULL
+,	url VARCHAR(255) NULL
+,	ip INT UNSIGNED NOT NULL
+,	content TEXT
+,	status TINYINT UNSIGNED NOT NULL
+,	date TIMESTAMP NOT NULL
+,	type SMALLINT UNSIGNED NOT NULL
+, PRIMARY KEY (id)
+, INDEX (post_id)
 );',
 'CREATE TABLE ' . DB::o()->commentinfo . ' ( 
-	comment_id VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	comment_id INT UNSIGNED NOT NULL
+,	name VARCHAR(50) NOT NULL
+,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
+,	value TEXT NULL
+, PRIMARY KEY (comment_id, name)
 	);',
 );
 ?>
