9e8832027d67e750dd42a29cd7506616641eac5a
Matthias Bauer <moeffju@contrib.habariproject.org>
2007-01-18 12:25:15 +0000

    - adding option defaults for pingback_send and comments_require_id
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@347 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/options.php
+++ classes/options.php
@@ -94,40 +94,46 @@ class Options
 	 **/
 	public function __get($name)
 	{
-		if(!isset($this->options[$name])) {
-			$result = DB::get_row('SELECT value, type FROM ' . DB::o()->options . ' WHERE name = ?', array($name));
+		if ( ! isset( $this->options[$name] ) ) {
+			$result= DB::get_row( 'SELECT value, type FROM ' . DB::o()->options . ' WHERE name = ?', array( $name ) );
 			if ( Error::is_error( $result ) ) {
 				$result->out();
 				die();
 			}
 			else if ( is_object( $result ) ) {
-				if($result->type == 1) {
-					$this->options[$name] = unserialize($result->value);
+				if ( $result->type == 1 ) {
+					$this->options[$name]= unserialize( $result->value );
 				}
 				else {
-					$this->options[$name] = $result->value;
+					$this->options[$name]= $result->value;
 				}
 			} else {
 				// Return some default values here
-				switch($name) {
-					case 'pagination': return 10;
-					case 'host_url': {
+				switch ( $name ) {
+					case 'pagination':
+						return 10;
+					case 'host_url':
 						// If we're running on a port other than 80, add the port number
 						// to the value returned from host_url
 						$port= 80; // Default in case not set.
-						if ( isset($_SERVER['SERVER_PORT']) ) {
+						if ( isset( $_SERVER['SERVER_PORT'] ) ) {
 							$port= $_SERVER['SERVER_PORT'];
 						}
 						$portpart = "";
 						if ( $port != 80 ) {
 							$portpart= ":$port";
 						}
+						// use Utils::glue_url?
 						return "http://" . $this->hostname . $portpart . $this->base_url;
-					}
+					case 'comments_require_id':
+						return FALSE;
+					case 'pingback_send':
+						return FALSE;
 				}
-				return null;
+				return NULL;
 			}
 		}
+		
 		return $this->options[$name];
 	}
 	
