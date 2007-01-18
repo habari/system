395543a8c5d2f9ca42901ee490ffb0fb335164f3
Matthias Bauer <moeffju@contrib.habariproject.org>
2007-01-18 04:30:19 +0000

    - RPC Client - Pingback client - changes in post.php to send pingbacks (req cleanup) - fixes issue #117 http://code.google.com/p/habari/issues/detail?id=117
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@341 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ classes/rpcclient.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * XMLRPC Client
+ */
+class RPCClient {
+    private $url;
+    private $method;
+    private $params;
+    private $request_body;
+    private $result= FALSE;
+
+	/**
+	 * @param string URL
+	 * @param string method to call
+	 * @param string method's arguments
+	 */
+	function __construct( $url, $method, $params )
+	{
+		$this->url= $url;
+		$this->method= $method;
+		$this->params= $params;
+		
+		$this->request_body= xmlrpc_encode_request( $method, $params );
+	}
+
+	/**
+	 * Execute the request. Populates result field.
+	 */
+	public function execute()
+	{
+		$rr= new RemoteRequest( $this->url, 'POST' );
+		$rr->add_header( 'Content-Type: text/xml' );
+		$rr->set_body( $this->request_body );
+		
+		// should throw an error on failure
+		$rr->execute();
+		// in that case, we should never get here
+		
+		$this->result= xmlrpc_decode($rr->get_response_body());
+	}
+	
+	/**
+	 * Return the (decoded) result of the request, or FALSE if the result was invalid. 
+	 */
+	public function get_result()
+	{
+		return $this->result;
+	}
+}
+
+?>
