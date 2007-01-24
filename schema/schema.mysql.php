510d043eba5bf2b4e7047dc4cda9192d19999b98
Thimal Jayasooriya <lairmail@contrib.habariproject.org>
2007-01-24 12:15:25 +0000

    1. made posts.post_id the primary key and changed type to integer 2. changed info table foreign key references to int (from earlier varchars) 3. added PK declarations to info tables 4. changed datatype of columns to UPPERCASE throughout the schema for consistency
    schema incorporates elements of patch in issue #92 and has been tested against the r402 codebase
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@403 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/schema.mysql.php
+++ schema/schema.mysql.php
@@ -1,78 +1,86 @@
 <?php
 $queries = array(
 'CREATE TABLE ' . DB::o()->posts . ' ( 
-	id smallint AUTO_INCREMENT NOT NULL UNIQUE,
-	slug VARCHAR(255) NOT NULL PRIMARY KEY,
-	content_type smallint,
-	title VARCHAR(255), 
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	slug VARCHAR(255) NOT NULL,
+	content_type SMALLINT UNSIGNED NOT NULL,
+	title VARCHAR(255) NOT NULL, 
 	guid VARCHAR(255) NOT NULL, 
 	content LONGTEXT, 
-	user_id smallint, 
-	status smallint,
+	user_id SMALLINT, 
+	status SMALLINT UNSIGNED NOT NULL,
 	pubdate TIMESTAMP, 
-	updated TIMESTAMP
+	updated TIMESTAMP,
+	PRIMARY KEY (id),
+	UNIQUE INDEX (slug)
 	);',
 'CREATE TABLE ' . DB::o()->postinfo . ' ( 
-	slug VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	post_id INT UNSIGNED NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	type SMALLINT DEFAULT 0,
+	value TEXT,
+	PRIMARY KEY (post_id, name)
 	);',
 'CREATE TABLE ' . DB::o()->posttype . ' ( 
 	name VARCHAR(255) NOT NULL PRIMARY KEY,
-	type smallint DEFAULT 0
+	type SMALLINT DEFAULT 0
 	);',
 'INSERT INTO ' . DB::o()->posttype . ' VALUES
 		(\'entry\', 0),
 		(\'page\', 1);',
 'CREATE TABLE ' . DB::o()->poststatus . ' ( 
 	name VARCHAR(255) NOT NULL PRIMARY KEY,
-	type smallint DEFAULT 0
+	type SMALLINT DEFAULT 0
 	);',
 'INSERT INTO ' . DB::o()->poststatus . ' VALUES
 		(\'draft\', 0),
 		(\'published\', 1), 
 		(\'private\', 1);',
 'CREATE TABLE ' . DB::o()->options . ' (
-	name varchar(50) PRIMARY KEY NOT NULL UNIQUE,
-	type integer DEFAULT 0,
-	value text
+	name VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE,
+	type INTEGER DEFAULT 0,
+	value TEXT
 	);',
 'CREATE TABLE ' . DB::o()->users . ' (
-	id smallint AUTO_INCREMENT NOT NULL UNIQUE,
-	username varchar(20) PRIMARY KEY NOT NULL UNIQUE,
-	email varchar(30) NOT NULL,
-	password varchar(40) NOT NULL
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	username VARCHAR(20) NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	password VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id), 
+	UNIQUE INDEX (username)
 	);',
 'CREATE TABLE ' . DB::o()->userinfo . ' ( 
-	user_id VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	user_id INT UNSIGNED NOT NULL, 
+	name VARCHAR(50) NOT NULL,
+	type SMALLINT DEFAULT 0,
+	value TEXT,
+	PRIMARY KEY (user_id, name)
 	);',
 'CREATE TABLE ' . DB::o()->tags . ' (
-	slug varchar(255) NOT NULL,
-	tag varchar(30) NOT NULL,
+	slug VARCHAR(255) NOT NULL,
+	tag VARCHAR(30) NOT NULL,
 	KEY slug (slug),
 	KEY tag (tag)
 );',
 'CREATE TABLE ' . DB::o()->comments . ' (
-	id int AUTO_INCREMENT NOT NULL UNIQUE,
-	post_slug varchar(255) NOT NULL,
-	name varchar(255),
-	email varchar(255),
-	url varchar(255),
-	ip varchar(255),
-	content text,
-	status int,
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	post_slug VARCHAR(255) NOT NULL,
+	name VARCHAR(255),
+	email VARCHAR(255),
+	url VARCHAR(255),
+	ip VARCHAR(255),
+	content TEXT,
+	status INT,
 	date TIMESTAMP,
-	type int
+	type INT,
+	PRIMARY KEY (id)
 );',
 'CREATE TABLE ' . DB::o()->commentinfo . ' ( 
-	comment_id VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	comment_id INT UNSIGNED NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	type SMALLINT DEFAULT 0,
+	value TEXT,
+	PRIMARY KEY (comment_id, name)
 	);',
 );
 ?>
