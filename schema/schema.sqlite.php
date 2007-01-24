0dfeb72047a9222359be80c3bf30d7852a0aa9c5
Thimal Jayasooriya <lairmail@contrib.habariproject.org>
2007-01-24 12:19:13 +0000

    updates to sqlite schema to reflect r403 mysql schema changes.
    also removed some redundant "auto_increment not null unique" primary key declarations - "integer primary key" is implicitly auto incremented and primary keys are unique by definition.
    
    schema changes work with a test SQLite 3.3.6 database
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@404 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/schema.sqlite.php
+++ schema/schema.sqlite.php
@@ -2,7 +2,7 @@
 $queries = array(
 'BEGIN',
 'CREATE TABLE ' . DB::o()->posts . ' ( 
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+	id INTEGER PRIMARY KEY,
 	slug VARCHAR(255) NOT NULL UNIQUE, 
 	content_type INTEGER,
 	title VARCHAR(255), 
@@ -14,62 +14,65 @@ $queries = array(
 	updated TIMESTAMP
 	);',
 'CREATE TABLE ' . DB::o()->postinfo . ' ( 
-	slug VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	post_id INTEGER NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	type SMALLINT DEFAULT 0,
+	value TEXT,
+	PRIMARY KEY (post_id, name)
 	);',
 'CREATE TABLE ' . DB::o()->posttype . ' ( 
 	name VARCHAR(255) NOT NULL PRIMARY KEY,
-	type smallint DEFAULT 0
+	type SMALLINT DEFAULT 0
 	);',
 'INSERT INTO ' . DB::o()->posttype . ' VALUES (\'entry\', 0);',
 'INSERT INTO ' . DB::o()->posttype . ' VALUES (\'page\', 1);',
 'CREATE TABLE ' . DB::o()->poststatus . ' ( 
 	name VARCHAR(255) NOT NULL PRIMARY KEY,
-	type smallint DEFAULT 0
+	type SMALLINT DEFAULT 0
 	);',
 'INSERT INTO ' . DB::o()->poststatus . ' VALUES (\'draft\', 0);',
 'INSERT INTO ' . DB::o()->poststatus . ' VALUES (\'published\', 1);',
 'INSERT INTO ' . DB::o()->poststatus . ' VALUES (\'private\', 1);',
 'CREATE TABLE ' . DB::o()->options . ' (
-	name varchar(50) PRIMARY KEY NOT NULL UNIQUE,
-	type integer DEFAULT 0,
-	value text
+	name VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE,
+	type INTEGER DEFAULT 0,
+	value TEXT
 	);',
 'CREATE TABLE ' . DB::o()->users . ' (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
-	username varchar(20) NOT NULL UNIQUE,
-	email varchar(30) NOT NULL,
-	password varchar(40) NOT NULL
+	id INTEGER PRIMARY KEY,
+	username VARCHAR(20) NOT NULL UNIQUE,
+	email VARCHAR(30) NOT NULL,
+	password VARCHAR(40) NOT NULL
 	);',
 'CREATE TABLE ' . DB::o()->userinfo . ' ( 
-	user_id VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	user_id INTEGER NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	type SMALLINT DEFAULT 0,
+	value TEXT,
+	PRIMARY KEY (user_id, name)
 	);',
 'CREATE TABLE ' . DB::o()->tags . ' (
-	slug varchar(255) NOT NULL,
-	tag varchar(30) NOT NULL
+	slug VARCHAR(255) NOT NULL,
+	tag VARCHAR(30) NOT NULL
 );',
 'CREATE TABLE ' . DB::o()->comments . ' (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
-	post_slug varchar(255) NOT NULL,
-	name varchar(255),
-	email varchar(255),
-	url varchar(255),
-	ip varchar(255),
-	content text,
-	status int,
+	id INTEGER PRIMARY KEY,
+	post_slug VARCHAR(255) NOT NULL,
+	name VARCHAR(255),
+	email VARCHAR(255),
+	url VARCHAR(255),
+	ip VARCHAR(255),
+	content TEXT,
+	status INT,
 	date TIMESTAMP,
-	type int
+	type INT
 );',
 'CREATE TABLE ' . DB::o()->commentinfo . ' ( 
-	comment_id VARCHAR(255) NOT NULL,
-	name varchar(50) NOT NULL,
-	type smallint DEFAULT 0,
-	value text
+	comment_id INTEGER NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	type SMALLINT DEFAULT 0,
+	value TEXT,
+	PRIMARY KEY (comment_id, name)
 	);',
 'COMMIT'
 );
