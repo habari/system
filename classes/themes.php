2ff3f2bd0474851ffb1a20c7b6e0f25ad33c3057
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-01-28 05:53:01 +0000

    Prep themes for override class.
    The UserThemeHandler class will execute it's Theme instance's methods in place of its own methods in an effort to allow themes to specify their own data gathering routines.  To facilitate this, some of the logic in the constructor for Theme (finding the default theme from the database or using the parameters passed in to create a specific theme) has been moved into Themes::create(), which returns an instance of Theme.  This will allow Themes::create() to return an instance of a Theme descendant for those themes that want to collect more data for display (by implementing CustomTheme::act_display_posts() for example) than what Theme provides.
    
    There is currently not a method to load such a Theme-descendant class, which should be added.
    
    Also included: Moved the logic for selecting a set of posts by date from UserThemeHandler into Posts, so that you can select posts by date via the Posts object API.
    
    All of these changes should be transparent if you're looking only at output, with the exception that day-, month-, and year-based URLs should now return a valid set of posts.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@445 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/themes.php
+++ classes/themes.php
@@ -29,6 +29,73 @@ class Themes
           	WHERE is_active=1';
 		return DB::get_row( $query, array(), 'QueryRecord' );
 	}
+	
+	/**
+	 * Returns a named Theme descendant.
+	 * If no parameter is supplied, then 
+	 * load the active theme from the database.
+	 * 
+	 * If no theme option is set, a fatal error is thrown
+	 * 
+	 * @param name            ( optional ) override the default theme lookup
+	 * @param template_engine ( optional ) specify a template engine
+	 * @param theme_dir       ( optional ) specify a theme directory
+	 **/
+	public function create()
+	{
+		if ( func_num_args() > 0 ) {
+			/* 
+			 * A theme name ( or more information ) was supplied.  This happens when we
+			 * want to use a pre-installed theme ( for instance, the
+			 * installer theme. )
+			 */
+			if ( func_num_args() >= 2 ) {
+				/* we load template engine from specified args, not DB */
+				$themedata = new QueryRecord();
+				$themedata->name= func_get_arg( 0 );
+				$themedata->template_engine= func_get_arg( 1 );
+				$themedata->theme_dir = $themedata->name;
+				$themedata->version = 0;
+				if( func_num_args() == 3 ) {
+					$themedata->theme_dir= func_get_arg( 2 );
+				}
+				else {
+					$themedata->theme_dir= HABARI_PATH . '/user/themes/' . $themedata->theme_dir . '/';
+				}
+			}
+			else {
+				/* lookup in DB for template engine info. */
+				$themedata= self::get_by_name( func_get_arg( 0 ) );
+				if ( empty( $themedata ) ) {
+					die( 'Theme not installed.' );
+				}
+				$themedata->theme_dir= HABARI_PATH . '/user/themes/' . $themedata->theme_dir . '/';
+			}
+		}
+		else {
+			// Grab the theme from the database
+			$themedata= self::get_active();
+			if ( empty( $themedata ) ) {
+				die( 'Theme not installed.' );
+			}
+			$themedata->theme_dir= HABARI_PATH . '/user/themes/' . $themedata->theme_dir . '/';
+		}
+		
+		$classname = 'Theme';
+		/**
+		 * @todo Should we include_once a theme's theme.php file here?
+		 **/
+		/*		 		
+		if( file_exists( $themedata->theme_dir . 'theme.php' ) ) {
+			include_once( $themedata->theme_dir . 'theme.php' );
+		}
+		*/
+		/**
+		 * If a theme provides a descendant class, it should be used instead of Theme.		 
+		 **/		 		
+		return new $classname($themedata);
+		
+	}
 }
 
 ?>
