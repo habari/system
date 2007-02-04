3a7250e2611239121db459f99e4b6662e3f8efef
Owen Winkler <epithet@gmail.com>
2007-02-04 01:40:20 +0000

    Removed remnants of rewrite_rules_args from the RewriteRules class. Allow plugins to add new rules via filter 'rewrite_rules'.  Create a new RewriteRule object, insert it into the array passed to the plugin filter, return the array --> Instant new rule.
    Better yet, you could use this method to detect if the rule that your plugin requires is already listed in the database, and if not, create the rule and add it.
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@489 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/rewriterules.php
+++ classes/rewriterules.php
@@ -5,22 +5,53 @@
  */      
 class RewriteRules extends ArrayObject {
 	
+	/**
+	 * Return the active rewrite rules, both in the database and applied by plugins
+	 * 
+	 * @return array Array of RewriteRule objects for active rewrite rules
+	 **/	  	 	 	
 	static public function get_active()
 	{
 		$sql= "
-			SELECT rr.rule_id, rr.name, rr.parse_regex, rr.build_str, rr.handler, rr.action, COUNT( * ) as num_args
+			SELECT rr.rule_id, rr.name, rr.parse_regex, rr.build_str, rr.handler, rr.action, rr.priority
 			FROM " . DB::table( 'rewrite_rules' ) . " AS rr
-			LEFT JOIN " . DB::table( 'rewrite_rule_args' ) . " AS rra
-			ON rr.rule_id= rra.rule_id
 			WHERE rr.is_active= 1
-			GROUP BY rr.rule_id, rr.name, rr.parse_regex, rr.build_str, rr.handler, rr.action, rr.priority
 			ORDER BY rr.priority";
 		$db_rules= DB::get_results( $sql, array(), 'RewriteRule' );
+
+		$db_rules= Plugins::filter('rewrite_rules', $db_rules);
+		
+		usort($db_rules, array('RewriteRules', 'sort_rules'));
 
 		$c = __CLASS__;
 		return new $c ( $db_rules );
 	}
 	
+	/**
+	 * Helper function for sorting rewrite rules by priority.
+	 * 
+	 * Required because plugins would insert their rules at the end of the array,
+	 * which would allow any other rule (including the one that executes by default
+	 * when no other rules work) to execute first.
+	 *
+	 * @param RewriteRule $rulea A rule to compare
+	 * @param RewriteRule $ruleb A rule to compare
+	 * @return integer The standard usort() result values, -1, 0, 1 	 	 	  
+	 **/	 	 	 
+	public function sort_rules($rulea, $ruleb)
+	{
+		if( $rulea->priority == $ruleb->priority ) {
+			return 0;
+		}
+		return ($rulea->priority < $ruleb->priority) ? -1 : 1;
+	}
+	
+	/**
+	 * Get a RewriteRule by its name
+	 * 
+	 * @param $name The name of the rule
+	 * @return RewriteRule The rule requested
+	 **/	 	 	 	 	
 	public function by_name( $name )
 	{
 		foreach($this as $rule) {
