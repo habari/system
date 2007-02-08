303e297b15702a04c7cced5e8a5df026ef3ef04a
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-02-08 22:52:35 +0000

    Schema changes from issue 211, discussed here: http://groups.google.com/group/habari-dev/browse_thread/thread/24c228d32bd5dc5f
    The MySQL and SQLite schemas are both correct for the database types they are associated to.  The SQLite schema is an actual SQLite schema, not a Postgres one.  The Postgres schema is yet to be added as a separate file and tested.
    
    The installer will not currently install on SQLite without some tweaks, which are still being ironed out.
    
    The Post class has been altered slightly to continue to work with additional fields in the posts table that require values that we don't yet provide.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@496 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/schema.mysql.sql
+++ schema/schema.mysql.sql
@@ -5,6 +5,7 @@ CREATE TABLE {$prefix}posts (
 ,	title VARCHAR(255) NOT NULL
 ,	guid VARCHAR(255) NOT NULL
 ,	content LONGTEXT NOT NULL
+, cached_content LONGTEXT NOT NULL
 ,	user_id SMALLINT UNSIGNED NOT NULL
 ,	status SMALLINT UNSIGNED NOT NULL
 ,	pubdate DATETIME NOT NULL 
@@ -15,7 +16,7 @@ CREATE TABLE {$prefix}posts (
 
 CREATE TABLE  {$prefix}postinfo  ( 
 	post_id INT UNSIGNED NOT NULL
-,	name VARCHAR(50) NOT NULL
+,	name VARCHAR(255) NOT NULL
 ,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
 ,	value TEXT
 , PRIMARY KEY (post_id, name)
@@ -43,7 +44,7 @@ INSERT INTO  {$prefix}poststatus VALUES
 ("private", 1);
 
 CREATE TABLE  {$prefix}options (
-	name VARCHAR(50) NOT NULL
+	name VARCHAR(255) NOT NULL
 ,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
 ,	value TEXT
 , PRIMARY KEY (name)
@@ -51,16 +52,16 @@ CREATE TABLE  {$prefix}options (
 
 CREATE TABLE  {$prefix}users (
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
-,	username VARCHAR(20) NOT NULL
-,	email VARCHAR(30) NOT NULL
-,	password VARCHAR(40) NOT NULL
+,	username VARCHAR(255) NOT NULL
+,	email VARCHAR(255) NOT NULL
+,	password VARCHAR(255) NOT NULL
 , PRIMARY KEY (id)
 , UNIQUE INDEX (username)
 );
 
 CREATE TABLE  {$prefix}userinfo ( 
 	user_id SMALLINT UNSIGNED NOT NULL
-,	name VARCHAR(50) NOT NULL
+,	name VARCHAR(255) NOT NULL
 ,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
 ,	value TEXT
 , PRIMARY KEY (user_id, name)
@@ -68,7 +69,8 @@ CREATE TABLE  {$prefix}userinfo (
 
 CREATE TABLE  {$prefix}tags (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT
-, tag_text VARCHAR(50) NOT NULL
+, tag_text VARCHAR(255) NOT NULL
+, tag_slug VARCHAR(255) NOT NULL
 , PRIMARY KEY (id)
 , UNIQUE INDEX (tag_text)	
 );
@@ -82,9 +84,9 @@ CREATE TABLE  {$prefix}tag2post (
 
 CREATE TABLE  {$prefix}themes (
   id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
-, name VARCHAR(80) NOT NULL
-, version VARCHAR(10) NOT NULL
-, template_engine VARCHAR(40) NOT NULL
+, name VARCHAR(255) NOT NULL
+, version VARCHAR(255) NOT NULL
+, template_engine VARCHAR(255) NOT NULL
 , theme_dir VARCHAR(255) NOT NULL
 , is_active TINYINT UNSIGNED NOT NULL DEFAULT 0
 , PRIMARY KEY (id)
@@ -109,8 +111,8 @@ INSERT INTO  {$prefix}themes (
 CREATE TABLE  {$prefix}comments (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT
 ,	post_id INT UNSIGNED NOT NULL
-,	name VARCHAR(100) NOT NULL
-,	email VARCHAR(100) NOT NULL
+,	name VARCHAR(255) NOT NULL
+,	email VARCHAR(255) NOT NULL
 ,	url VARCHAR(255) NULL
 ,	ip INT UNSIGNED NOT NULL
 ,	content TEXT
@@ -123,7 +125,7 @@ CREATE TABLE  {$prefix}comments (
 
 CREATE TABLE  {$prefix}commentinfo ( 
 	comment_id INT UNSIGNED NOT NULL
-,	name VARCHAR(50) NOT NULL
+,	name VARCHAR(255) NOT NULL
 ,	type SMALLINT UNSIGNED NOT NULL DEFAULT 0
 ,	value TEXT NULL
 , PRIMARY KEY (comment_id, name)
@@ -131,25 +133,16 @@ CREATE TABLE  {$prefix}commentinfo (
 
 CREATE TABLE {$prefix}rewrite_rules (
   rule_id INT UNSIGNED NOT NULL AUTO_INCREMENT
-, name VARCHAR(100) NOT NULL
-, parse_regex VARCHAR(200) NOT NULL
-, build_str VARCHAR(200) NOT NULL
-, handler VARCHAR(100) NOT NULL
-, action VARCHAR(100) NOT NULL
+, name VARCHAR(255) NOT NULL
+, parse_regex VARCHAR(255) NOT NULL
+, build_str VARCHAR(255) NOT NULL
+, handler VARCHAR(255) NOT NULL
+, action VARCHAR(255) NOT NULL
 , priority SMALLINT UNSIGNED NOT NULL
 , is_active TINYINT UNSIGNED NOT NULL DEFAULT 0
-, is_system TINYINT UNSIGNED NOT NULL DEFAULT 0
+, rule_class TINYINT UNSIGNED NOT NULL DEFAULT 0
 , description TEXT NULL
 , PRIMARY KEY (rule_id)
-, UNIQUE INDEX (name)
-);
-
-CREATE TABLE {$prefix}rewrite_rule_args (
-  rule_id INT UNSIGNED NOT NULL
-, arg_index TINYINT UNSIGNED NOT NULL
-, name VARCHAR(30) NOT NULL
-, PRIMARY KEY (rule_id, arg_index)
-, UNIQUE INDEX (rule_id, name)
 );
 
 INSERT INTO {$prefix}rewrite_rules
@@ -157,93 +150,45 @@ INSERT INTO {$prefix}rewrite_rules
 VALUES (NULL, 'display_posts_at_page','/^page\\/([\\d]+)[\\/]{0,1}$/i','page/{$page}'
 ,'UserThemeHandler','display_posts',1,'Displays posts.  Page (of post) parameter is passed in URL');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'page', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'display_posts_by_date','/([1,2]{1}[\\d]{3})\\/([\\d]{2})\\/([\\d]{2})[\\/]{0,1}$/','{$year}/{$month}/{$day}'
 ,'UserThemeHandler','display_posts',2,'Displays posts for a specific date.');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'year', 0);
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'month', 1);
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'day', 2);
 
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'display_posts_by_month','/([1,2]{1}[\\d]{3})\\/([\\d]{2})[\\/]{0,1}$/','{$year}/{$month}'
 ,'UserThemeHandler','display_posts',3,'Displays posts for a specific month.');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'year', 0);
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'month', 1);
-
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'display_posts_by_year','/([1,2]{1}[\\d]{3})[\\/]{0,1}$/','{$year}'
 ,'UserThemeHandler','display_posts',4,'Displays posts for a specific year.');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'year', 0);
 
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'display_feed_by_type','/^feed\\/(atom|rs[sd])[\\/]{0,1}$/i','feed/{$feed_type}'
 ,'FeedHandler','display_feed',5,'Return feed per specified feed type');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'feed_type', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'display_posts_by_tag','/^tag\\/([^\\/]*)[\\/]{0,1}$/i','tag/{$tag}'
 ,'UserThemeHandler','display_posts',5,'Return posts matching specified tag');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'tag', 0);
 
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'admin','/^admin[\\/]*([^\\/]*)[\\/]{0,1}$/i','admin/{$page}'
 ,'AdminHandler','admin',6,'An admin action');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'page', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'user','/^user\\/([^\\/]*)[\\/]{0,1}$/i','user/{$page}'
 ,'UserHandler','{$page}',7,'A user action or display, for instance the login screen');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'page', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'display_posts_by_slug','/([^\\/]+)[\\/]{0,1}$/i','{$slug}'
 ,'UserThemeHandler','display_posts',99,'Return posts matching specified slug');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'slug', 0);
 
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
@@ -265,10 +210,6 @@ INSERT INTO {$prefix}rewrite_rules
 VALUES (NULL, 'collection','/^atom\\/(.+)[\\/]{0,1}$/i','atom/{$index}'
 ,'AtomHandler','collection',1,'Atom collection');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'index', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'search','/^search$/i','search'
@@ -278,9 +219,5 @@ INSERT INTO {$prefix}rewrite_rules
 (rule_id, name, parse_regex, build_str, handler, action, priority, description)
 VALUES (NULL, 'comment','/^([0-9]+)\\/feedback[\\/]{0,1}$/i','{$id}/feedback'
 ,'FeedbackHandler','add_comment',8,'Adds a comment to a post');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (LAST_INSERT_ID(), 'id', 0);
 
 UPDATE {$prefix}rewrite_rules SET is_active=1;
--- schema/schema.sqlite.sql
+++ schema/schema.sqlite.sql
@@ -1,22 +1,21 @@
-CREATE SEQUENCE {$prefix}posts_pkey_seq;
 CREATE TABLE {$prefix}posts ( 
-	id INTEGER NOT NULL DEFAULT nextval('{$prefix}posts_pkey_seq'::regclass)
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
 ,	slug VARCHAR(255) NOT NULL
 ,	content_type SMALLINT NOT NULL
 ,	title VARCHAR(255) NOT NULL
 ,	guid VARCHAR(255) NOT NULL
 ,	content TEXT NOT NULL
+,	cached_content LONGTEXT NOT NULL
 ,	user_id SMALLINT NOT NULL
 ,	status SMALLINT NOT NULL
 ,	pubdate TIMESTAMP NOT NULL 
 ,	updated TIMESTAMP NOT NULL
-, PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX slug ON {$prefix}posts(slug);
 
 CREATE TABLE {$prefix}postinfo  ( 
 	post_id INTEGER NOT NULL
-,	name VARCHAR(50) NOT NULL
+,	name VARCHAR(255) NOT NULL
 ,	type SMALLINT NOT NULL DEFAULT 0
 ,	value TEXT
 , PRIMARY KEY (post_id, name)
@@ -28,9 +27,8 @@ CREATE TABLE  {$prefix}posttype (
 , PRIMARY KEY (name)
 );
 
-INSERT INTO  {$prefix}posttype VALUES
-('entry', 0),
-('page', 1);
+INSERT INTO  {$prefix}posttype VALUES('entry', 0);
+INSERT INTO  {$prefix}posttype VALUES('page', 1);
 
 CREATE TABLE  {$prefix}poststatus ( 
 	name VARCHAR(255) NOT NULL 
@@ -38,42 +36,37 @@ CREATE TABLE  {$prefix}poststatus (
 , PRIMARY KEY (name)
 );
 
-INSERT INTO  {$prefix}poststatus VALUES
-('draft', 0),
-('published', 1), 
-('private', 2);
+INSERT INTO  {$prefix}poststatus VALUES ('draft', 0);
+INSERT INTO  {$prefix}poststatus VALUES ('published', 1); 
+INSERT INTO  {$prefix}poststatus VALUES ('private', 2);
 
 CREATE TABLE  {$prefix}options (
-	name VARCHAR(50) NOT NULL
+	name VARCHAR(255) NOT NULL
 ,	type SMALLINT NOT NULL DEFAULT 0
 ,	value TEXT
 , PRIMARY KEY (name)
 );
 
-CREATE SEQUENCE {$prefix}users_pkey_seq;
 CREATE TABLE  {$prefix}users (
-	id SMALLINT NOT NULL DEFAULT nextval('{$prefix}users_pkey_seq'::regclass)
-,	username VARCHAR(20) NOT NULL
-,	email VARCHAR(30) NOT NULL
-,	password VARCHAR(40) NOT NULL
-, PRIMARY KEY (id)
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+,	username VARCHAR(255) NOT NULL
+,	email VARCHAR(255) NOT NULL
+,	password VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX username ON {$prefix}users(username);
 
 CREATE TABLE  {$prefix}userinfo ( 
-	user_id SMALLINT NOT NULL
-,	name VARCHAR(50) NOT NULL
+	user_id INTEGER NOT NULL
+,	name VARCHAR(255) NOT NULL
 ,	type SMALLINT NOT NULL DEFAULT 0
 ,	value TEXT
 , PRIMARY KEY (user_id, name)
 );
 
-CREATE SEQUENCE {$prefix}tags_pkey_seq;
 CREATE TABLE  {$prefix}tags (
-	id INTEGER NOT NULL DEFAULT nextval('{$prefix}tags_pkey_seq'::regclass)
-,	username VARCHAR(20) NOT NULL
-, tag_text VARCHAR(50) NOT NULL
-, PRIMARY KEY (id)
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL 
+,	tag_text VARCHAR(255) NOT NULL
+, tag_slug VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX tag_text ON {$prefix}tags(tag_text);
 
@@ -84,15 +77,13 @@ CREATE TABLE  {$prefix}tag2post (
 );
 CREATE INDEX tag2post_post_id ON {$prefix}tag2post(post_id);
 
-CREATE SEQUENCE {$prefix}themes_pkey_seq;
 CREATE TABLE  {$prefix}themes (
-	id SMALLINT NOT NULL DEFAULT nextval('{$prefix}themes_pkey_seq'::regclass)
-, name VARCHAR(80) NOT NULL
-, version VARCHAR(10) NOT NULL
-, template_engine VARCHAR(40) NOT NULL
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL 
+, name VARCHAR(255) NOT NULL
+, version VARCHAR(255) NOT NULL
+, template_engine VARCHAR(255) NOT NULL
 , theme_dir VARCHAR(255) NOT NULL
 , is_active SMALLINT NOT NULL DEFAULT 0
-, PRIMARY KEY (id)
 );
 
 INSERT INTO  {$prefix}themes (
@@ -109,146 +100,85 @@ INSERT INTO  {$prefix}themes (
 , 1
 );
 
-CREATE SEQUENCE {$prefix}comments_pkey_seq;
 CREATE TABLE  {$prefix}comments (
-	id INTEGER NOT NULL DEFAULT nextval('{$prefix}comments_pkey_seq'::regclass)
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
 ,	post_id INTEGER NOT NULL
-,	name VARCHAR(100) NOT NULL
-,	email VARCHAR(100) NOT NULL
+,	name VARCHAR(255) NOT NULL
+,	email VARCHAR(255) NOT NULL
 ,	url VARCHAR(255) NULL
 ,	ip INTEGER NOT NULL
 ,	content TEXT
 ,	status SMALLINT NOT NULL
 ,	date TIMESTAMP NOT NULL
 ,	type SMALLINT NOT NULL
-, PRIMARY KEY (id)
 );
 CREATE INDEX comments_post_id ON {$prefix}comments(post_id);
 
 CREATE TABLE  {$prefix}commentinfo ( 
 	comment_id INTEGER NOT NULL
-,	name VARCHAR(50) NOT NULL
+,	name VARCHAR(255) NOT NULL
 ,	type SMALLINT NOT NULL DEFAULT 0
 ,	value TEXT NULL
 , PRIMARY KEY (comment_id, name)
 );
 
-CREATE SEQUENCE {$prefix}rewrite_rules_pkey_seq;
 CREATE TABLE {$prefix}rewrite_rules (
-	rule_id INTEGER NOT NULL DEFAULT nextval('{$prefix}rewrite_rules_pkey_seq'::regclass)
-, name VARCHAR(100) NOT NULL
-, parse_regex VARCHAR(200) NOT NULL
-, build_str VARCHAR(200) NOT NULL
-, handler VARCHAR(100) NOT NULL
-, action VARCHAR(100) NOT NULL
+	rule_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL 
+, name VARCHAR(255) NOT NULL
+, parse_regex VARCHAR(255) NOT NULL
+, build_str VARCHAR(255) NOT NULL
+, handler VARCHAR(255) NOT NULL
+, action VARCHAR(255) NOT NULL
 , priority SMALLINT NOT NULL
 , is_active SMALLINT NOT NULL DEFAULT 0
 , is_system SMALLINT NOT NULL DEFAULT 0
 , description TEXT NULL
-, PRIMARY KEY (rule_id)
 );
-CREATE UNIQUE INDEX rewrite_rules_name ON {$prefix}rewrite_rules(name);
-
-CREATE TABLE {$prefix}rewrite_rule_args (
-  rule_id INTEGER NOT NULL
-, arg_index SMALLINT NOT NULL
-, name VARCHAR(30) NOT NULL
-, PRIMARY KEY (rule_id, arg_index)
-);
-CREATE UNIQUE INDEX rewrite_rule_args_rule_id_name ON {$prefix}rewrite_rule_args(rule_id, name);
 
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_at_page','/^page\\/([\\d]+)[\\/]{0,1}$/i','page/{$page}'
+VALUES ('display_posts_at_page','/^page\/([\\d]+)[\/]{0,1}$/i','page/{$page}'
 ,'UserThemeHandler','display_posts',1,'Displays posts.  Page (of post) parameter is passed in URL');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'page', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_date','/([1,2]{1}[\\d]{3})\\/([\\d]{2})\\/([\\d]{2})[\\/]{0,1}$/','{$year}/{$month}/{$day}'
+VALUES ('display_posts_by_date','/([1,2]{1}[\d]{3})\/([\d]{2})\/([\d]{2})[\/]{0,1}$/','{$year}/{$month}/{$day}'
 ,'UserThemeHandler','display_posts',2,'Displays posts for a specific date.');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'year', 0);
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'month', 1);
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'day', 2);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_month','/([1,2]{1}[\\d]{3})\\/([\\d]{2})[\\/]{0,1}$/','{$year}/{$month}'
+VALUES ('display_posts_by_month','/([1,2]{1}[\d]{3})\/([\d]{2})[\/]{0,1}$/','{$year}/{$month}'
 ,'UserThemeHandler','display_posts',3,'Displays posts for a specific month.');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'year', 0);
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'month', 1);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_year','/([1,2]{1}[\\d]{3})[\\/]{0,1}$/','{$year}'
+VALUES ('display_posts_by_year','/([1,2]{1}[\d]{3})[\/]{0,1}$/','{$year}'
 ,'UserThemeHandler','display_posts',4,'Displays posts for a specific year.');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'year', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_feed_by_type','/^feed\\/(atom|rs[sd])[\\/]{0,1}$/i','feed/{$feed_type}'
+VALUES ('display_feed_by_type','/^feed\/(atom|rs[sd])[\/]{0,1}$/i','feed/{$feed_type}'
 ,'FeedHandler','display_feed',5,'Return feed per specified feed type');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'feed_type', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_tag','/^tag\\/([^\\/]*)[\\/]{0,1}$/i','tag/{$tag}'
+VALUES ('display_posts_by_tag','/^tag\/([^\/]*)[\/]{0,1}$/i','tag/{$tag}'
 ,'UserThemeHandler','display_posts',5,'Return posts matching specified tag');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'tag', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('admin','/^admin[\\/]*([^\\/]*)[\\/]{0,1}$/i','admin/{$page}'
+VALUES ('admin','/^admin[\/]*([^\/]*)[\/]{0,1}$/i','admin/{$page}'
 ,'AdminHandler','admin',6,'An admin action');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'page', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('user','/^user\\/([^\\/]*)[\\/]{0,1}$/i','user/{$page}'
+VALUES ('user','/^user\/([^\/]*)[\/]{0,1}$/i','user/{$page}'
 ,'UserHandler','{$page}',7,'A user action or display, for instance the login screen');
 
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'page', 0);
-
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_slug','/([^\\/]+)[\\/]{0,1}$/i','{$slug}'
+VALUES ('display_posts_by_slug','/([^\/]+)[\/]{0,1}$/i','{$slug}'
 ,'UserThemeHandler','display_posts',99,'Return posts matching specified slug');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'slug', 0);
 
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
@@ -267,12 +197,8 @@ VALUES ('introspection','/^atom$/i','atom'
 
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('collection','/^atom\\/(.+)[\\/]{0,1}$/i','atom/{$index}'
+VALUES ('collection','/^atom\/(.+)[\/]{0,1}$/i','atom/{$index}'
 ,'AtomHandler','collection',1,'Atom collection');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'index', 0);
 
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
@@ -281,11 +207,7 @@ VALUES ('search','/^search$/i','search'
 
 INSERT INTO {$prefix}rewrite_rules
 (name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('comment','/^([0-9]+)\\/feedback[\\/]{0,1}$/i','{$id}/feedback'
+VALUES ('comment','/^([0-9]+)\/feedback[\/]{0,1}$/i','{$id}/feedback'
 ,'FeedbackHandler','add_comment',8,'Adds a comment to a post');
-
-INSERT INTO {$prefix}rewrite_rule_args
-(rule_id, name, arg_index)
-VALUES (currval('{$prefix}rewrite_rules_pkey_seq'), 'id', 0);
 
 UPDATE {$prefix}rewrite_rules SET is_active=1;
