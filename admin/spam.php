cefd6d1e4ab5375a520849cae64a46a39e1433b6
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-03-08 12:34:56 +0000

    Radio buttons should now work like radio buttons on comment moderation pages with this commit.
    Modified the Comment class to correctly reference tables and delete the comment to which the instance points, rather than any specific id (which is better done through Comments::delete_these()).
    
    Modified the Comments class to indicate static versus dynamic functions.  Note that the previously named Comments::delete() is now Comments::delete_these(), a static function.  This is because an instance of the Comments class represents a set of comments.  If you call $comments->delete(), you should expect it to do the same thing as $comment->delete(), which is to delete the comments held by the instance.  In other words, while the static function Comments::delete_these() accepts a parameter of which comments to delete, Comments::delete() should not accept a parameter and should delete the comments already held in the instance.  Comments::delete() does not exist in this commit.
    
    Chris' system/admin/comments.php is included in this commit with changes to the radio button style.  This page is not currently referenced in the menu, but is accessible via domain.com/admin/comments .  We should discuss whittling down the number of comment moderation pages so that it's perhaps less confusing which page you need to look at to get the information you want.  Maybe an Ajax-powered switch at the top of the page?
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@561 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/spam.php
+++ admin/spam.php
@@ -4,31 +4,32 @@
 		<h1>Comments Marked as Spam</h1>
 		<?php if( Comments::count_total( Comment::STATUS_SPAM ) ) { ?>
 		<p>Below you will find comments awaiting moderation.</p>
-	<form method="post" name="spam">
-	<p class="submit"><input type="submit" name="remove_spam" value="Update!" /> <input type="checkbox" name="mass_spam_delete" id="mass_spam_delete" value="mass_spam_delete">Delete 'em all</p>
+	<form method="post" name="spam" action="<?php URL::out( 'admin', array( 'page' => 'moderate', 'result' => 'success' ) ); ?>">
+	<p class="submit"><input type="submit" name="submit" value="Update!" /> <input type="checkbox" name="mass_spam_delete" id="mass_spam_delete" value="1">Delete 'em all</p>
 	<ul id="waiting">
 		<?php foreach( Comments::get( array( 'status' => Comment::STATUS_SPAM, 'limit' => 30, 'orderby' => 'date DESC' ) ) as $comment ){ ?>
 		<li>
 			Comment by <?php echo $comment->name;?> on <a href="<?php URL::get( 'post', array( 'slug' => $comment->post->slug ) ); ?>"><?php echo $comment->post->slug; ?></a>
-			<br /><small>(Commented created on <?php echo $comment->date; ?>)</small>
+			<br><small>(Commented created on <?php echo $comment->date; ?>)</small>
 			<p><?php echo $comment->content; ?></p>
 			<span class="manage">
-				<p>Action: 
+				<p>Action:
 				<label>
-					<input type="checkbox" name="spam_approve[<?php echo $comment->id; ?>]" id="spam_approve-<?php echo $comment->id; ?>" value="<?php echo $comment->id; ?>">Approve
+					<input type="radio" name="moderate[<?php echo $comment->id; ?>]" id="approve-<?php echo $comment->id; ?>" value="approve">Approve
 				</label>
 				<label>
-				<input type="checkbox" name="spam_delete[<?php echo $comment->id; ?>]" id="spam_delete-<?php echo $comment->id; ?>" value="<?php echo $comment->id; ?>">Delete
+					<input type="radio" name="moderate[<?php echo $comment->id; ?>]" id="delete-<?php echo $comment->id; ?>" value="delete">Delete
 				</label>
 				<label>
-				<input type="checkbox" name="spam_ignore" id="spam-<?php echo $comment->id; ?>" value="" checked="checked">Ignore
+					<input type="radio" name="moderate[<?php echo $comment->id; ?>]" id="ignore-<?php echo $comment->id; ?>" value="spam" checked="checked">Leave as Spam
 				</label>
 				</p>
-			</span><br />
+			</span><br>
 		</li>
 		<?php }	?>
 	</ul>
-	<p class="submit"><input type="submit" name="remove_spam" value="Update!" /> <input type="checkbox" name="mass_delete" id="mass_spam_delete" value="mass_spam_delete">Delete 'em all</p>
+	<input type="hidden" name="returnpage" value="spam" >
+	<p class="submit"><input type="submit" name="submit" value="Update!" > <input type="checkbox" name="mass_spam_delete" id="mass_spam_delete2" value="1">Delete 'em all</p>
 	</form>
 	<?php } else { ?>
 		<p>You are currently spam free!</p>
--- classes/adminhandler.php
+++ classes/adminhandler.php
@@ -29,19 +29,19 @@ class AdminHandler extends ActionHandler
 	/**
 	* function admin
 	* Figures out what admin page to show, and displays it to the user.
-	* Calls post_{page}() function for post requests to the specific page.	
+	* Calls post_{page}() function for post requests to the specific page.
 	* @param array An associative array of this->handler_vars found in the URL by the URL
 	*/
 	public function act_admin()
 	{
-		$page= ( isset( $this->handler_vars['page']) && ! empty($this->handler_vars['page']) ) 
-          ? $this->handler_vars['page'] 
+		$page= ( isset( $this->handler_vars['page']) && ! empty($this->handler_vars['page']) )
+          ? $this->handler_vars['page']
           : 'dashboard';
 		switch( $_SERVER['REQUEST_METHOD'] ) {
 			case 'POST':
 				// Handle POSTs to the admin pages
 				$fn= 'post_' . $page;
-				if ( method_exists( $this, $fn ) ) { 
+				if ( method_exists( $this, $fn ) ) {
 					$this->$fn();
 					//call_user_func( array(&$this, $fn), $this->handler_vars);
 				}
@@ -86,8 +86,8 @@ class AdminHandler extends ActionHandler
 	 * function post_options
 	 * Handles post requests from the options admin page.
 	 * Sets all of the set options.
-	 * @param array An associative array of content found in the url, $_POST array, and $_GET array 
-	 **/	 	 	 	
+	 * @param array An associative array of content found in the url, $_POST array, and $_GET array
+	 **/
 	public function post_options()
 	{
 		foreach ( $_POST as $option => $value ) {
@@ -97,11 +97,11 @@ class AdminHandler extends ActionHandler
 		}
 		Utils::redirect( URL::get('admin', 'page=options&result=success') );
 	}
-	
+
 	/**
 	* function post_dashboard
 	* Handles post requests from the dashboard.
-	* @param array An associative array of content found in the url, $_POST array, and $_GET array 
+	* @param array An associative array of content found in the url, $_POST array, and $_GET array
 	*/
 	public function post_dashboard()
 	{
@@ -113,7 +113,7 @@ class AdminHandler extends ActionHandler
 	* function post_publish
 	* Handles post requests from the publish page.
 	* Adds a post to the site, if the post content is not NULL.
-	* @param array An associative array of content found in the url, $_POST array, and $_GET array 
+	* @param array An associative array of content found in the url, $_POST array, and $_GET array
 	*/
 	public function post_publish()
 	{
@@ -138,7 +138,7 @@ class AdminHandler extends ActionHandler
 				$post= Post::create( $postdata );
 			}
 			Utils::redirect( Utils::de_amp( URL::get( 'admin', 'page=publish&result=success&slug=' . $post->slug ) ) );
-		} 
+		}
 		else {
 			// do something intelligent here
 			_e('Danger, Will Robinson!  Danger!');
@@ -157,7 +157,7 @@ class AdminHandler extends ActionHandler
 		$results= array( 'page' => 'user', );;
 		$currentuser= User::identify();
 		$user= $currentuser;
-		
+
 		if ( $currentuser->id != $this->handler_vars['user_id'] ) {
 			// user is editing someone else's profile
 			// load that user account
@@ -226,7 +226,7 @@ class AdminHandler extends ActionHandler
 			{
 				$error.= 'Please supply a user name!<br />';
 			}
-			if ( ! isset( $this->handler_vars['email'] ) || 
+			if ( ! isset( $this->handler_vars['email'] ) ||
 				( '' == $this->handler_vars['username'] ) ||
 				( ! strstr($this->handler_vars['email'], '@') ) )
 			{
@@ -257,7 +257,7 @@ class AdminHandler extends ActionHandler
 				}
 			}
 		}
-	}	
+	}
 
 	/**
 	 * function post_import
@@ -271,26 +271,26 @@ class AdminHandler extends ActionHandler
 		 * This function needs to validate the import form fields,
 		 * and then forward the import information on to an import function
 		 * rather than doing the import right here.
-		 **/		  
-	
+		 **/
+
 		$db_connection= array(
 		'connection_string' => $this->handler_vars['connection'],  // MySQL Connection string
 		'username' => $this->handler_vars['username'],  // MySQL username
 		'password' => $this->handler_vars['password'],  // MySQL password
 		'prefix'	=>	$this->handler_vars['prefix'], // Prefix for your WP tables
 		);
-		
+
 		// Connect to the database or fail informatively
 		try {
 			$wpdb= new DatabaseConnection();
 			$wpdb->connect( $db_connection['connection_string'], $db_connection['username'], $db_connection['password'], $db_connection['prefix'] );
 		}
 		catch( Exception $e) {
-			die( 'Could not connect to database using the supplied credentials.  Please check config.php for the correct values. Further information follows: ' .  $e->getMessage() );		
+			die( 'Could not connect to database using the supplied credentials.  Please check config.php for the correct values. Further information follows: ' .  $e->getMessage() );
 		}
-		
+
 		echo '<h1>Import your content into ' . Options::get('title') . '</h1>';
-		
+
 		$posts= $wpdb->get_results("
 			SELECT
 				post_content as content,
@@ -303,20 +303,20 @@ class AdminHandler extends ActionHandler
 				post_modified as updated,
 				(post_status= 'publish') as status,
 				(post_type= 'page') as content_type
-			FROM {$db_connection['prefix']}posts 
+			FROM {$db_connection['prefix']}posts
 			", array(), 'Post');
-		
+
 		$post_map= array();
 		foreach( $posts as $post ) {
-		
-			$tags= $wpdb->get_column( 
+
+			$tags= $wpdb->get_column(
 				"SELECT category_nicename
 				FROM {$db_connection['prefix']}post2cat
-				INNER JOIN {$db_connection['prefix']}categories 
+				INNER JOIN {$db_connection['prefix']}categories
 				ON ({$db_connection['prefix']}categories.cat_ID= {$db_connection['prefix']}post2cat.category_id)
-				WHERE post_id= {$post->id}" 
+				WHERE post_id= {$post->id}"
 			);
-		
+
 			$p= new Post( $post->to_array() );
 			$p->slug= $post->slug;
 			$p->guid= $p->guid; // Looks fishy, but actually causes the guid to be set.
@@ -324,8 +324,8 @@ class AdminHandler extends ActionHandler
 			$p->insert();
 			$post_map[$p->slug]= $p->id;
 		}
-		
-		$comments= $wpdb->get_results("SELECT 
+
+		$comments= $wpdb->get_results("SELECT
 										comment_content as content,
 										comment_author as name,
 										comment_author_email as email,
@@ -334,20 +334,20 @@ class AdminHandler extends ActionHandler
 									 	comment_approved as status,
 										comment_date as date,
 										comment_type as type,
-										post_name as post_slug 
+										post_name as post_slug
 										FROM {$db_connection['prefix']}comments
 										INNER JOIN
 										{$db_connection['prefix']}posts on ({$db_connection['prefix']}posts.ID= {$db_connection['prefix']}comments.comment_post_ID)
-										", 
+										",
 										array(), 'Comment');
-		
+
 		foreach( $comments as $comment ) {
 			switch( $comment->type ) {
 				case 'pingback': $comment->type= Comment::PINGBACK; break;
 				case 'trackback': $comment->type= Comment::TRACKBACK; break;
 				default: $comment->type= Comment::COMMENT;
 			}
-			
+
 			$carray= $comment->to_array();
 			if ($carray['ip'] == '') {
 				$carray['ip']= 0;
@@ -360,71 +360,110 @@ class AdminHandler extends ActionHandler
 				$carray['status']= Comment::STATUS_APPROVED;
 				break;
 			case 'spam':
-				$carray['status']= Comment::STATUS_SPAM; 
+				$carray['status']= Comment::STATUS_SPAM;
 				break;
-			} 
+			}
 			if ( !isset($post_map[$carray['post_slug']]) ) {
 				Utils::debug($carray);
 			}
 			else {
 			$carray['post_id']= $post_map[$carray['post_slug']];
 			unset($carray['post_slug']);
-				
+
 			$c= new Comment( $carray );
 			//Utils::debug( $c );
 			$c->insert();
 		}
 		}
 		echo '<p>All done, your content has been imported.</p>';
-		
+
 		// Redirect back to a URL with a notice?
 	}
-	
+
 	/**
 	 * function post_moderate
 	 * Handles the submission of the comment moderation form.
 	 * @param array An array of information found in the post array
+	 * @todo Separate delete from "delete until purge"
 	 **/
 	function post_moderate()
 	{
-		if ( isset( $_POST['mass_delete'] ) ) {
-			Comment::mass_delete();
-		}
-		else if ( isset( $_POST['delete'] ) && is_array( $_POST['delete'] ) ) {
-			foreach( $_POST['delete'] as $destroy ) {
-				Comment::delete( $destroy );
+		// Setting these mass_delete options prevents any other processing.  Desired?
+		if ( isset($_POST['mass_spam_delete']) ) {
+			$comments= Comments::by_status(Comment::STATUS_SPAM);
+			// Delete all comments that have the spam status.
+			foreach($comments as $comment) {
+				$comment->delete();
 			}
 		}
-		else if ( isset( $_POST['approve'] ) && is_array( $_POST['approve'] ) ) {
-			foreach( $_POST['approve'] as $promote ) {
-				$this->id = $promote;
-				$this->fields = array('id' => $promote);
-				$this->newfields = array('status' => 1);
-				$this->unsetfields = array();
-				Comment::update();
+		elseif ( isset($_POST['mass_delete']) ) {
+			$comments= Comments::by_status(Comment::STATUS_UNAPPROVED);
+			// Delete all comments that are unapproved.
+			foreach($comments as $comment) {
+				$comment->delete();
 			}
 		}
-		else { continue; }
+		else {
+			// Process each comment according to its setting in the form.
+			if( isset( $_POST['moderate'] ) ) {
+				foreach( $_POST['moderate'] as $commentid => $status ) {
+					switch ( $status ){
+					case 'delete':
+						// This comment was marked for deletion
+						Comments::delete($commentid);
+						break;
+					case 'spam':
+						// This comment was marked as spam
+						$comment= Comment::get($commentid);
+						$comment->status = Comment::STATUS_SPAM;
+						$comment->update();
+						break;
+					case 'approve':
+						// This comment was marked for approval
+						$comment= Comment::get($commentid);
+						$comment->status = Comment::STATUS_APPROVED;
+						$comment->update();
+						break;
+					case 'unapprove':
+						// This comment was marked for unapproval
+						$comment= Comment::get($commentid);
+						$comment->status = Comment::STATUS_UNAPPROVED;
+						$comment->update();
+						break;
+					}
+				}
+			}
+		}
 
-		Utils::redirect( URL::get( 'admin', array( 'page' => 'moderate', 'result' => 'success' ) ) );
+		// Get the return page, making sure it's one of the valid pages.
+		if(
+				isset( $_POST['returnpage'] ) &&
+			 	in_array( $_POST['returnpage'], array('comments', 'moderate', 'spam'))
+			) {
+			$returnpage= $_POST['returnpage'];
+		}
+		else {
+			$returnpage= 'moderate';
+		}
+		Utils::redirect( URL::get( 'admin', array( 'page' => $returnpage, 'result' => 'success' ) ) );
 	}
 
 	/**
 	 * Helper function which automatically assigns all handler_vars
 	 * into the theme and displays a theme template
-	 * 
+	 *
 	 * @param template_name Name of template to display (note: not the filename)
 	 */
 	protected function display( $template_name )
 	{
-		/* 
+		/*
 		 * Assign internal variables into the theme (and therefore into the theme's template
 		 * engine.  See Theme::assign().
 		 */
 		foreach ( $this->handler_vars as $key => $value ) {
 			$this->theme->assign( $key, $value );
 		}
-		
+
 		$this->theme->display( $template_name );
 	}
 }
