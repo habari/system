b0bb87fa14aacb6c0473bbf07ed86c47a5468bdc
Owen Winkler <epithet@gmail.com>
2007-03-26 18:15:31 +0000

    Initial XMLRPC client.  Can be used to ping update services, among other XMLRPC functions.
    Currently does not handle error responses, and only implements the client, not the server.
    
    XMLRPCDate is a class used to hold a date.  This is used to differentiate a DateTime datatype when passing such a value via the client.
    
    XMLRPCBinary is a class used to hold binary data.  This is used to differentiate a binary datatype when passing such a value via the client.  You can load this value form a file using: $xmlrpcbinary->load_from_file($filename);
    
    
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@599 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ classes/xmlrpcdate.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * XMLRPC Date type
+ * Used to hold dates for transmission in XMLRPC calls.
+ *
+ * @package Habari
+ */
+
+class XMLRPCDate
+{
+	private $rpcdate;
+	
+	public function __set($name, $value)
+	{
+		switch($name) {
+		case 'date':
+			if(is_numeric($value)) {
+				$this->rpcdate = $value;
+			}
+			else {
+				$this->rpcdate = strtotime($value); 
+			}
+		}
+	}
+	
+	public function __get($name)
+	{
+		switch($name) {
+		case 'date':
+			return $this->rpcdate;
+		}
+	}
+	
+	public function __construct($date = null)
+	{
+		if(isset($date)) {
+			$this->date = $date;
+		}
+	}
+}
+
+?>
---
+++ classes/xmlrpcstruct.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * XMLRPC Struct type
+ * Used to hold struct types (objects) that are returned in XMLRPC requests.
+ *
+ * @package Habari
+ */
+
+class XMLRPCStruct
+{
+	private $fields = array();
+	
+	/**
+	 * Property setter for the XMLRPCStruct class.
+	 * This allows the following:
+	 * <code>
+	 * $struct = new XMLRPCStruct();
+	 * $struct->foo = 'bar'; // This is done by __set() and assigns 'bar' into $this->fields['foo']
+	 * </code>
+	 * 
+	 * @param string $name The name of the property on this object to set
+	 * @param mixed $value The value to set in the property
+	 * @return 
+	 */
+	public function __set($name, $value)
+	{
+		$this->fields[$name] = $value;
+	}
+	
+	/**
+	 * Property getter for the XMLRPCStruct class.
+	 * Returns the value of $this->fields for the specified porperty name.
+	 * 
+	 * @param string $name The name of the property
+	 * @return mixed The value stored for the requested property
+	 */
+	public function __get($name)
+	{
+		return $this->fields[$name];
+	}
+	
+	/**
+	 * Get the list of properties that this object contains.
+	 * 
+	 * @return array List of object properties, as stored in $this->fields.
+	 */
+	public function get_fields()
+	{
+		return array_keys($this->fields);
+	}
+	
+	/**
+	 * Constructor for XMLRPCStruct
+	 * 
+	 * @param array $fields Default field values to set into properties.
+	 */
+	public function __construct($fields = array())
+	{
+		$this->fields = $fields;
+	}
+}
+
+?>
