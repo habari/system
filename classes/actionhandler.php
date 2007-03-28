a3c7a2f441e3a73246170e967849e32339246605
Matthias Bauer <moeffju@contrib.habariproject.org>
2007-03-28 12:14:05 +0000

    Coding style fixes and cleanup for ActionHandler.
    Add plugin hooks for before_ and after_ actions. Fixes issue #266, props mattread.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@612 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/actionhandler.php
+++ classes/actionhandler.php
@@ -1,43 +1,56 @@
 <?php
+
 /**
  * A base class handler for URL-based actions.
  *  
  * @package Habari
  **/  
 class ActionHandler {
-  public $action= '';               // string name of action
-  public $handler_vars= array();    // internal array of handler variables (state info)
+	public $action= '';               // string name of action
+	public $handler_vars= array();    // internal array of handler variables (state info)
 
 	/**
 	 * All handlers must implement act() to conform to handler API.
-   * This is the default implementation of act(), which attempts
-   * to call a class member method of $this->act_$action().  Any
-   * subclass is welcome to override this default implementation.
-   *
-	 * @param   action  the action that was in the URL rule
-   * @return  bool    did the action succeed?
+	 * This is the default implementation of act(), which attempts
+	 * to call a class member method of $this->act_$action().  Any
+	 * subclass is welcome to override this default implementation.
+	 *
+	 * @param string $action the action that was in the URL rule
 	 */	 	 	 	 
 	public function act($action) {
-    $this->action= $action;
-    $action_method= 'act_' . $action;
-    $before_action_method= 'before_' . $action_method;
-    $after_action_method= 'after_' . $action_method;
-    if (method_exists($this, $action_method)) {
-      if (method_exists($this, $before_action_method))
-        $this->$before_action_method();
-      $this->$action_method();
-      if (method_exists($this, $after_action_method))
-        $this->$after_action_method();
-    }
+		$this->action= $action;
+		
+		$action_method= 'act_' . $action;
+		$before_action_method= 'before_' . $action_method;
+		$after_action_method= 'after_' . $action_method;
+		
+		if (method_exists($this, $action_method)) {
+			if (method_exists($this, $before_action_method)) {
+				$this->$before_action_method();
+			}
+			Plugins::act( $before_action_method );
+			
+			$this->$action_method();
+			
+			Plugins::act( $after_action_method );
+			if (method_exists($this, $after_action_method)) {
+				$this->$after_action_method();
+			}
+		}
 	}
 
-  /**
-   * Helper method to convert calls to $handler->my_action()
-   * to $handler->act('my_action');
-   */
-  public function __call($function, $args) {
-    $this->handler_vars= array_merge($this->handler_vars, $args);
-    $this->act($function);
-  }
+	/**
+	 * Helper method to convert calls to $handler->my_action()
+	 * to $handler->act('my_action');
+	 * 
+	 * @param string $function function name
+	 * @param array $args function arguments
+	 */
+	public function __call($function, $args) {
+		$this->handler_vars= array_merge($this->handler_vars, $args);
+		return $this->act($function);
+	}
+	
 }
+
 ?>
