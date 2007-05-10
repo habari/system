8534edc63401a8491b5aaae95303c017cce6b309
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-05-10 23:53:24 +0000

    Added hook to Post to allow post data to be passed in whole to plugin sink.
    Added Format::apply_with_hook_params() to Format class for applying formatters that also accept all plugin hook data as parameters, not just the first parameter.
    
    Added Format::more(), which when used with apply_with_hook_params() allows a theme to display the first part of a post (prior to <!--more--> in the post content) on pages displaying more than one post, and the second part of a post only on pages that display that post alone.
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@655 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/format.php
+++ classes/format.php
@@ -11,10 +11,9 @@ class Format
 	static private $formatters = null;
 	
 	/**
-	 * function use
-	 * Called to register a format function to a plugin hook.
-	 * @param string A function name that exists in a Format class
-	 * @param string A plugin hook to apply that Format function to as a filter
+	 * Called to register a format function to a plugin hook, only passing the hook's first parameter to the Format function.
+	 * @param string $format A function name that exists in a Format class
+	 * @param string $onwhat A plugin hook to apply that Format function to as a filter
 	 **/	 	 	 	 	
 	static function apply($format, $onwhat)
 	{
@@ -24,19 +23,47 @@ class Format
 	
 		foreach(self::$formatters as $formatobj) {
 			if( method_exists($formatobj, $format) ) {
-				if( func_num_args() > 2 ) {
-					$index = array_search($formatobj, self::$formatters);
-					$func = '$o = Format::by_index(' . $index . ');return $o->' . $format . '($a, ';
-					$args = func_get_args();
+				$index = array_search($formatobj, self::$formatters);
+				$func = '$o = Format::by_index(' . $index . ');return $o->' . $format . '($a';
+				$args = func_get_args();
+				if( count($args) > 2) {
+					$func.= ', ';
 					$args = array_map(create_function('$a', 'return "\'{$a}\'";'), array_slice($args, 2));
-					$func .= implode(', ', $args) . ');';
-					$lambda = create_function('$a', $func);
-					Plugins::register( $lambda, 'filter', $onwhat);
+					$func .= implode(', ', $args);
 				}
-				else {
-					Plugins::register( array($formatobj, $format), 'filter', $onwhat);
+				$func .= ');';
+				$lambda = create_function('$a', $func);
+				Plugins::register( $lambda, 'filter', $onwhat);
+				break;  // We only look for one matching format function to apply.
+			}
+		}
+	}
+
+	/**
+	 * Called to register a format function to a plugin hook, and passes all of the hook's parameters to the Format function. 
+	 * @param string $format A function name that exists in a Format class
+	 * @param string $onwhat A plugin hook to apply that Format function to as a filter
+	 **/	 	 	 	 	
+	static function apply_with_hook_params($format, $onwhat)
+	{
+		if( self::$formatters == null ) {
+			self::load_all();
+		}
+	
+		foreach(self::$formatters as $formatobj) {
+			if( method_exists($formatobj, $format) ) {
+				$index = array_search($formatobj, self::$formatters);
+				$func = '$o= Format::by_index(' . $index . '); $args= func_get_args(); return call_user_func_array(array($o, "' . $format . '"), array_merge($args';
+				$args = func_get_args();
+				if( count($args) > 2) {
+					$func.= ', array( ';
+					$args = array_map(create_function('$a', 'return "\'{$a}\'";'), array_slice($args, 2));
+					$func .= implode(', ', $args) . ')';
 				}
-				break;
+				$func .= '));';
+				$lambda = create_function('$a', $func);
+				Plugins::register( $lambda, 'filter', $onwhat);
+				break;  // We only look for one matching format function to apply.
 			}
 		}
 	}
@@ -45,6 +72,8 @@ class Format
 	 * function by_index
 	 * Returns an indexed formatter object, for use by lambda functions created
 	 * to supply additional parameters to plugin filters.
+	 * @param integer $index The index of the formatter object to return.
+	 * @return Format The formatter object requested
 	 **/
 	static function by_index($index)
 	{
@@ -64,7 +93,8 @@ class Format
 				self::$formatters[] = new $class();
 			}
 		}
-		self::$formatters = array_reverse(self::$formatters, true);
+		self::$formatters = array_merge( self::$formatters, Plugins::get_by_interface( 'FormatPlugin' ) );
+		self::$formatters = array_reverse( self::$formatters, true );
 	}
 
 	/** DEFAULT FORMAT FUNCTIONS **/
@@ -73,7 +103,7 @@ class Format
 	 * function autop
 	 * Converts non-HTML paragraphs separated with 2 line breaks into HTML paragraphs 
 	 * while preserving any internal HTML
-	 * @param string The string to apply the formatting
+	 * @param string $value The string to apply the formatting
 	 * @returns string The formatted string
 	 **/	 	 	  	 	
 	public function autop($value)
@@ -101,9 +131,9 @@ class Format
 	 * function tag_and_list
 	 * Formatting function (should be in Format class?)
 	 * Turns an array of tag names into an HTML-linked list with command and an "and".
-	 * @param array An array of tag names
-	 * @param string Text to put between each element
-	 * @param string Text to put between the next to last element and the last element
+	 * @param array $array An array of tag names
+	 * @param string $between Text to put between each element
+	 * @param string $between_last Text to put between the next to last element and the last element
 	 * @return string HTML links with specified separators.
 	 **/	 	 	 	 	  
 	public function tag_and_list($array, $between = ', ', $between_last = ' and ')
@@ -149,13 +179,12 @@ class Format
 	/**
 	 * Returns a shortened version of whatever is passed in.
 	 * @param string $value A string to shorten
+	 * @param integer $count Maximum words to display [100]
+	 * @param integer $maxparagraphs Maximum paragraphs to display [1]	 	 
 	 * @return string The string, shortened
 	 **/	 	 	 	
-	function summarize( $text )
+	function summarize( $text, $count= 100, $maxparagraphs= 1 )
 	{
-		$count= 100;  // The number of words to display
-		$maxparagraphs= 1;  // The maximum number of paragraphs to display
-	
 		preg_match_all( '/<script.*?<\/script.*?>/', $text, $scripts );
 		preg_replace( '/<script.*?<\/script.*?>/', '', $text );
 	
@@ -221,6 +250,44 @@ class Format
 		return $output;
 	}
 
+	/**
+	 * Returns a truncated version of post content when the post isn't being displayed on its own.
+	 * Posts are split either at the comment <!--more--> or at the specified maximums.
+	 * Use only after applying autop or other paragrpah styling methods.	 	 
+	 * Apply to posts using:
+	 * <code>Format::apply_with_hook_params( 'more', 'post_content_out' );</code>	 
+	 * @param string $content The post content
+	 * @param Post $post The Post object of the post
+	 * @param string $more_text The text to use in the "read more" link.
+	 * @param integer $max_words null or the maximum number of words to use before showing the more link
+	 * @param integer $max_paragraphs null or the maximum number of paragraphs to use before showing the more link	 
+	 * @return string The post content, suitable for display
+	 **/	 	 	 	
+	public function more($content, $post, $more_text = 'Read More &raquo;', $max_words = null, $max_paragraphs = null) 
+	{
+		// There should be a more readable way than this to detect if this post is being displayed by itself:
+    if(isset(Controller::get_handler()->handler_vars['slug'])) {
+    	return $content;
+    }
+    else {
+    	$matches= preg_split( '/<!--\s*more\s*-->/is', $content, 2, PREG_SPLIT_NO_EMPTY );
+    	if(count($matches) > 1) {
+    		return reset($matches) . ' <a href="' . $post->permalink . '">' . $more_text . '</a>';
+    	}
+    	elseif (isset($max_words) || isset($max_paragraphs)) {
+    		$max_words = empty($max_words) ? 9999999 : intval($max_words);
+				$max_paragraphs = empty($max_paragraphs) ? 9999999 : intval($max_paragraphs); 
+				$summary = Format::summarize($content, $max_words, $max_paragraphs);
+				if($summary == $content) {
+					return $content;
+				}
+				else {
+					return $summary . ' <a href="' . $post->permalink . '">' . $more_text . '</a>';
+				}
+    	}
+    }
+		return $content;
+	} 
 	
 }
 ?>
