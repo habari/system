69040d3fe1ecd5b77479947cefc3b784da6feeb8
Matthias Bauer <moeffju@contrib.habariproject.org>
2007-05-17 20:44:42 +0000

    - Fix compile errors in abstract class Cache. Basic Cache should be doable now.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@661 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/cache.php
+++ classes/cache.php
@@ -1,9 +1,16 @@
 <?php
 /**
+ * @package Habari
+ * 
+ * Contains the abstract Cache class.
+ */
+
+/**
  * Habari Cache Class
  *
- * Requires PHP 5.1 or later
  * Base class for caching computationally expensive or bandwidth intensive data
+ * 
+ * Requires PHP 5.1 or later
  * 
  * @package Habari
  */
@@ -12,7 +19,7 @@ abstract class Cache
 	protected $_cache_data;
 	protected $_cache_expiry;	
 
-	public __construct()
+	public function __construct()
 	{
 		$this->_cache_data = array();
 		$this->_cache_expiry = array();		
@@ -20,14 +27,13 @@ abstract class Cache
 	
 	/**
 	 * Method to load a cached record. Should be overridden by child
-	 * classes depending on the storage mechanism for the cache
-	 * ie: a DBCache would load cached records with SQL, a DiskCache from files
+	 * classes depending on the storage mechanism for the cache,
+	 * i.e.: a DBCache would load cached records with SQL, a DiskCache from files
 	 *
-	 * @param $name name of the cached record
-	 * @param optional $force forces a reload from source instead of returning previously fetched data
+	 * @param $name string name of the cached record
+	 * @param $force boolean (optional) force a reload from source instead of returning previously fetched data
 	 * @return boolean TRUE if cached item is available, FALSE otherwise
 	 **/
-
 	abstract protected function _load( $name, $force = false ) { }
 
 	/**
@@ -36,32 +42,35 @@ abstract class Cache
 	 * ie: a DBCache would store cached records within a database, a DiskCache into files
 	 *
 	 * Method should be idempotent, ie:, can be called multiple times for the same cached object
+	 * 
+	 * @param $name string name for the record
+	 * @param $value mixed the record to store
+	 * @param $expiry int time until expiration of the record, in seconds
 	 * @return boolean TRUE if cached item is stored successfully, FALSE otherwise
 	 **/
-
-	abstract protected function _store ( $name, $value, $expiry ) { }
+	abstract protected function _store( $name, $value, $expiry ) { }
 
 	/**
 	 * Renews cached item expiry time. Should be overridden by child
-	 * classes depending on the storage mechanism for the cache
-	 * ie: a DBCache would store cached records within a database, a DiskCache into files
+	 * classes depending on the storage mechanism for the cache,
+	 * i.e.: a DBCache would store cached records within a database, a DiskCache into files
 	 *
+	 * @param $name string name for the record
+	 * @param $expiry int time until expiration of the record, in seconds
 	 * @return boolean TRUE if cached item is renewed successfully, FALSE otherwise
 	 **/
-
-	abstract protected function _renew ( $name, $expiry ) { }
+	abstract protected function _renew( $name, $expiry ) { }
 
 	/**
 	 * Has cached record with $name expired?
 	 * 
-	 * @param $name name of the cached item
+	 * @param $name string name of the cached item
 	 * @return boolean TRUE if cached item is stale, FALSE if still valid
 	 **/
-
 	public function is_expired( $name )
 	{
 		$this->_load( $name );
-		if ( $this->_cache_expiry[$name] > time() ) {
+		if( $this->_cache_expiry[$name] > time() ) {
 			return false;
 		}
 		return true;
@@ -70,10 +79,9 @@ abstract class Cache
 	/**
 	 * Is record with $name in the cache?
 	 * 
- 	 * @param $name name of the cached item
+ 	 * @param $name string name of the cached item
 	 * @return boolean TRUE if item is cached, FALSE if not
 	 **/
-
 	public function is_cached( $name )
 	{
 		$this->_load( $name );
@@ -83,14 +91,14 @@ abstract class Cache
 	/**
 	 * Get cached item with $name
 	 * 
- 	 * @param $name name of the cached item
-	 * @param optional $force forces return of an item, even if it is expired
+ 	 * @param $name string name of the cached item
+	 * @param $force boolean (optional) forces return of an item, even if it is expired
 	 * @return mixed cached item if available or null on failure. Will not return expired items unless force is set to true
 	 **/
-	public function __get ( $name, $force = false )	
+	public function __get( $name, $force= false )	
 	{
-		if ( $this->_is_cached( $name ) ) {			
-			if ( $this->_is_expired( $name ) && $force == false ) {
+		if( $this->_is_cached( $name ) ) {			
+			if( $this->_is_expired( $name ) && $force == false ) {
 				return null;
 			}
 			return $this->_cache_data[$name];
@@ -102,13 +110,16 @@ abstract class Cache
 	/**
 	 * Caches item with $name, $value and with the expiry offset $expiry
 	 * 
- 	 * @param string $name name of the item to be cached
-	 * @param mixed $value Item to be cached.
-	 * @param int $expiry Time (in seconds) that cached item will remain valid. Offset from time(). Defaults to 1 hour
-	 * @return returns true on success, false on failure
+ 	 * @param $name string name of the item to be cached
+	 * @param $value mixed Item to be cached.
+	 * @param $expiry int Time (in seconds) that cached item will remain valid. Offset from time(). Defaults to 1 hour.
+	 * @return boolean TRUE on success, FALSE on failure
 	 **/
-	public function __set ( $name, $value, $expiry = 3600 )
+	public function __set( $name, $value, $expiry = 3600 )
 	{
 		return $this->_store( $name, $value, $expiry );		
 	}
+
 }
+
+?>
