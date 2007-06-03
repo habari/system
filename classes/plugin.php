7cdefb8b493b3a38fe4d332e5d725997df06631b
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-06-03 04:10:38 +0000

    Adding plugin ids for each plugin to specify plugins by some string value that is not a path.  (Useful for an upcoming commit.)
    Adding xmlrpc_ prefix to plugin functions for registering XMLRPC hooks that have dots in the function name.  For example, to hook the system.listMethods function, use a function name in the plugin of 'xmlrpc_system__listMethods'.  Note the '__' in place of the dot.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@692 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/plugin.php
+++ classes/plugin.php
@@ -11,6 +11,8 @@
 
 abstract class Plugin
 {
+	private $_class_name= null;
+
 	/**
 	 * Plugin constructor.
 	 * Plugins should not define their own constructors, because they are instantiated
@@ -20,27 +22,38 @@ abstract class Plugin
 	final public function __construct(){}	 
 
 	/**
-	 * function get_file
 	 * Gets the filename that contains the plugin
 	 * @return string The filename of the file that contains the plugin class.	 
 	 **/
 	final public function get_file()
 	{
-		$class = new ReflectionClass( get_class( $this ) );
-		return $class->getFileName();
+		if(empty($this->_class_name)) {
+			$class = new ReflectionClass( get_class( $this ) );
+			$this->_class_name= $class->getFileName(); 
+		}
+		return $this->_class_name;
 	}	 	 	
 
 	/**
-	 * abstract function info
 	 * Returns information about this plugin
 	 * @return array An associative array of information about this plugin
 	 **/
 	abstract public function info();
+	
+	/**
+	 * Returns a unique id for this plugin
+	 * @return string A plugin id
+	 */
+	final public function plugin_id()
+	{
+		return sprintf('%x', crc32($this->get_file()));
+	}
 
 	/**
-	 * function load
 	 * Called when a plugin is loaded to register its actions and filters.	 
 	 * Registers all of this plugins action_ and filter_ functions with the Plugins dispatcher
+	 * Registers xmlrpc_ functions with the Plugins dispatcher, and turns '__' into '.'
+	 * for the purposes of matching dotted XMLRPC requests. 
 	 **/	 	 	 		
 	public function load()
 	{
@@ -52,12 +65,19 @@ abstract class Plugin
 		foreach ( get_class_methods( $this ) as $fn ) {
 			// make sure the method name is of the form
 			// action_foo or filter_foo
-			if ( ( 0 !== strpos( $fn, 'action_' ) ) && ( 0 !== strpos( $fn, 'filter_' ) ) ) {
+			if ( 
+				( 0 !== strpos( $fn, 'action_' ) ) 
+				&& ( 0 !== strpos( $fn, 'filter_' ) ) 
+				&& ( 0 !== strpos( $fn, 'xmlrpc_' ) ) 
+			) {
 				continue;
 			}	
 			$priority = isset($priorities[$fn]) ? $priorities[$fn] : 8;
 			$type = substr( $fn, 0, strpos( $fn, '_' ) );
 			$hook = substr( $fn, strpos( $fn, '_' ) + 1 );
+			if ( 0 !== strpos( $fn, 'xmlrpc_' ) ) {
+				$hook = str_replace('__', '.', $hook);
+			}
 			Plugins::register( array($this, $fn), $type, $hook, $priority );
 		}
 	}
