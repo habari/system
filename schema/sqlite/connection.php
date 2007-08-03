3e3019c45532af5c2d59d5ab1ae7870407f7b2d6
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-08-03 22:21:06 +0000

    Moved DB engine includes out of __autoload() and into the DatabaseConnection class.  (issue 352, thanks skippy) Applied new SQLite schema.  (issue 352, thanks geoffers) Truncated SQLiteConnection::dbdelta() to simply execute all queries in the schema.  Upgrades will come later.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@727 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/sqlite/connection.php
+++ schema/sqlite/connection.php
@@ -49,170 +49,47 @@ class SQLiteConnection extends DatabaseConnection
 	 * @return  string			translated SQL string
 	 *** FIXME: SQLite diffing is horribly terribly broken. There is varying support for alter table and mucking with columns
 	 */
-	function dbdelta( $queries, $execute = true, $silent = true ) 
+	function dbdelta( $queries, $execute = true, $silent = true, $doinserts= false ) 
 	{
 		if( !is_array($queries) ) {
 			$queries = explode( ';', $queries );
 			if('' == $queries[count($queries) - 1]) array_pop($queries);
 		}
 
-			$cqueries = array();
-			$iqueries = array();
-			$for_update = array();
+		$cqueries = array();
+		$iqueries = array();
+		$for_update = array();
 
-			foreach($queries as $qry) {
-				if(preg_match("|CREATE TABLE ([^ ]*)|", $qry, $matches)) {
-					$cqueries[strtolower($matches[1])] = $qry;
-					$for_update[$matches[1]] = 'Created table '.$matches[1];
-				}
-				else if(preg_match("|CREATE DATABASE ([^ ]*)|", $qry, $matches)) { 
-					// should not usually apply to SQLite unless there is some weird link database hackery going on
-					array_unshift($cqueries, $qry);
-				}
-				else if(preg_match("|INSERT INTO ([^ ]*)|", $qry, $matches)) {
-					$iqueries[] = $qry;
-				}
-				else if(preg_match("|UPDATE ([^ ]*)|", $qry, $matches)) {
-					$iqueries[] = $qry;
-				}
-				else {
-					// Unrecognized query type
+		foreach($queries as $qry) {
+			if(preg_match("|CREATE TABLE ([^ ]*)|", $qry, $matches)) {
+				$cqueries[strtolower($matches[1])] = $qry;
+				$for_update[$matches[1]] = 'Created table '.$matches[1];
+			}
+			else if(preg_match("|INSERT INTO ([^ ]*)|", $qry, $matches)) {
+				$iqueries[] = $qry;
+			}
+			else if(preg_match("|UPDATE ([^ ]*)|", $qry, $matches)) {
+				$iqueries[] = $qry;
+			}
+			else {
+				// Unrecognized query type
+			}
+		}
+		
+		if( $execute ) {
+			$allqueries = $cqueries;
+			if( $doinserts ) {
+				$allqueries = array_merge($allqueries, $iqueries);
+			}
+			foreach($allqueries as $query) {
+				if(!$this->query($query)) {
+					$this->get_error(true);
+					return false;
 				}
 			}
+		}
 
-			if($tables = $this->get_column('SELECT name FROM sqlite_master WHERE type = "table"')) { 
-				// support for .tables would have been lovely but unfortunately, it doesnt work
-				foreach($tables as $table) {
-					if( array_key_exists(strtolower($table), $cqueries) ) {
-						unset($cfields);
-						unset($indices);
-						preg_match("|\((.*)\)|ms", $cqueries[strtolower($table)], $match2);
-						$qryline = trim($match2[1]);
-
-						$flds = explode("\n", $qryline);
-						foreach($flds as $fld) {
-							preg_match("|^([^ ]*)|", trim($fld), $fvals);
-							$fieldname = $fvals[1];
-							$validfield = true;
-							switch(strtolower($fieldname))
-							{
-							case '':
-							case 'primary':
-							case 'index':
-							case 'fulltext':
-							case 'unique':
-							case 'key':
-								$validfield = false;
-								$indices[] = trim(trim($fld), ", \n");
-								break;
-							}
-							$fld = trim($fld);
-							if($validfield) {
-								$cfields[strtolower($fieldname)] = trim($fld, ", \n");
-							}
-						}
-						$tablefields = $this->get_results("DESCRIBE {$table};");
-						foreach((array)$tablefields as $tablefield) {
-							if(array_key_exists(strtolower($tablefield->Field), $cfields)) {
-								preg_match("|".$tablefield->Field." ([^ ]*( unsigned)?)|i", $cfields[strtolower($tablefield->Field)], $matches);
-								$fieldtype = $matches[1];
-								if($tablefield->Type != $fieldtype) {
-									$cqueries[] = "ALTER TABLE {$table} CHANGE COLUMN {$tablefield->Field} " . $cfields[strtolower($tablefield->Field)];
-									$for_update[$table.'.'.$tablefield->Field] = "Changed type of {$table}.{$tablefield->Field} from {$tablefield->Type} to {$fieldtype}";
-								}
-								if(preg_match("| DEFAULT '(.*)'|i", $cfields[strtolower($tablefield->Field)], $matches)) {
-									$default_value = $matches[1];
-									if($tablefield->Default != $default_value)
-									{
-										$cqueries[] = "ALTER TABLE {$table} ALTER COLUMN {$tablefield->Field} SET DEFAULT '{$default_value}'";
-										$for_update[$table.'.'.$tablefield->Field] = "Changed default value of {$table}.{$tablefield->Field} from {$tablefield->Default} to {$default_value}";
-									}
-								}
-								unset($cfields[strtolower($tablefield->Field)]);
-							}
-							else {
-								// This field exists in the table, but not in the creation queries?
-							}
-						}
-						foreach($cfields as $fieldname => $fielddef) {
-							$cqueries[] = "ALTER TABLE {$table} ADD COLUMN $fielddef";
-							$for_update[$table.'.'.$fieldname] = 'Added column '.$table.'.'.$fieldname;
-						}
-						$tableindices = $this->get_results("SHOW INDEX FROM {$table};");
-
-						if($tableindices) {
-							unset($index_ary);
-							foreach($tableindices as $tableindex) {
-								$keyname = $tableindex->Key_name;
-								$index_ary[$keyname]['columns'][] = array('fieldname' => $tableindex->Column_name, 'subpart' => $tableindex->Sub_part);
-								$index_ary[$keyname]['unique'] = ($tableindex->Non_unique == 0)?true:false;
-							}
-							foreach($index_ary as $index_name => $index_data) {
-								$index_string = '';
-								if($index_name == 'PRIMARY') {
-									$index_string .= 'PRIMARY ';
-								}
-								else if($index_data['unique']) {
-									$index_string .= 'UNIQUE ';
-								}
-								$index_string .= 'KEY ';
-								if($index_name != 'PRIMARY') {
-									$index_string .= $index_name;
-								}
-								$index_columns = '';
-								foreach($index_data['columns'] as $column_data) {
-									if($index_columns != '') $index_columns .= ',';
-									$index_columns .= $column_data['fieldname'];
-									if($column_data['subpart'] != '') {
-										$index_columns .= '('.$column_data['subpart'].')';
-									}
-								}
-								$index_string .= ' ('.$index_columns.')';
-
-								if(!(($aindex = array_search($index_string, $indices)) === false)) {
-									unset($indices[$aindex]);
-								}
-							}
-						}
-						foreach($indices as $index) {
-							$cqueries[] = "ALTER TABLE {$table} ADD $index";
-							$for_update[$table.'.'.$fieldname] = 'Added index '.$table.' '.$index;
-						}
-						unset($cqueries[strtolower($table)]);
-						unset($for_update[strtolower($table)]);
-					} else {
-					}
-				}
-			}
-
-			$allqueries = array_merge($cqueries, $iqueries);
-			if($execute) {
-				foreach($allqueries as $query) {
-					if(!$this->query($query))
-					{
-						$this->get_error(true);
-						return false;
-					}
-				}
-			}
-
-			if(!$silent)
-			{
-				if(count($for_update) > 0)
-				{
-					echo "<ul>\n";
-					foreach($for_update as $upgrade)
-					{
-						echo "<li>{$upgrade}</li>\n";
-					}
-					echo "</ul>\n";
-				}
-				else
-				{
-				echo "<ul><li>No Upgrades</li></ul>";
-				}
-			}
-			return $for_update;
+		return $allqueries;
 	}
 
 }
--- schema/sqlite/schema.sql
+++ schema/sqlite/schema.sql
@@ -1,226 +1,193 @@
 CREATE TABLE {$prefix}posts (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-,	slug VARCHAR(255) NOT NULL
-,	content_type SMALLINT NOT NULL
-,	title VARCHAR(255) NOT NULL
-,	guid VARCHAR(255) NOT NULL
-,	content TEXT NOT NULL
-,	cached_content LONGTEXT NOT NULL
-,	user_id SMALLINT NOT NULL
-,	status SMALLINT NOT NULL
-,	pubdate TIMESTAMP NOT NULL
-,	updated TIMESTAMP NOT NULL
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	slug VARCHAR(255) NOT NULL,
+	content_type SMALLINT UNSIGNED NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	guid VARCHAR(255) NOT NULL,
+	content TEXT NOT NULL,
+	cached_content LONGTEXT NOT NULL,
+	user_id SMALLINT UNSIGNED NOT NULL,
+	status SMALLINT UNSIGNED NOT NULL,
+	pubdate DATETIME NOT NULL,
+	updated TIMESTAMP NOT NULL
 );
 CREATE UNIQUE INDEX slug ON {$prefix}posts(slug);
 
 CREATE TABLE {$prefix}postinfo  (
-	post_id INTEGER NOT NULL
-,	name VARCHAR(255) NOT NULL
-,	type SMALLINT NOT NULL DEFAULT 0
-,	value TEXT
-, PRIMARY KEY (post_id, name)
+	post_id INTEGER UNSIGNED NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	type SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+	value TEXT,
+  PRIMARY KEY (post_id, name)
 );
 
-CREATE TABLE  {$prefix}posttype (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-,	name VARCHAR(255) NOT NULL
-);
-
-INSERT INTO  {$prefix}posttype (name) VALUES('entry');
-INSERT INTO  {$prefix}posttype (name) VALUES('page');
-
-CREATE TABLE  {$prefix}poststatus (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-,	name VARCHAR(255) NOT NULL
-);
-
-INSERT INTO  {$prefix}poststatus (name) VALUES ('deleted');
-INSERT INTO  {$prefix}poststatus (name) VALUES ('draft');
-INSERT INTO  {$prefix}poststatus (name) VALUES ('published');
-INSERT INTO  {$prefix}poststatus (name) VALUES ('private');
-
-CREATE TABLE  {$prefix}options (
+CREATE TABLE {$prefix}posttype (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	name VARCHAR(255) NOT NULL
-,	type SMALLINT NOT NULL DEFAULT 0
-,	value TEXT
-, PRIMARY KEY (name)
 );
 
-CREATE TABLE  {$prefix}users (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-,	username VARCHAR(255) NOT NULL
-,	email VARCHAR(255) NOT NULL
-,	password VARCHAR(255) NOT NULL
+INSERT INTO {$prefix}posttype (name) VALUES('entry');
+INSERT INTO {$prefix}posttype (name) VALUES('page');
+
+CREATE TABLE {$prefix}poststatus (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO {$prefix}poststatus (name) VALUES ('deleted');
+INSERT INTO {$prefix}poststatus (name) VALUES ('draft');
+INSERT INTO {$prefix}poststatus (name) VALUES ('published');
+INSERT INTO {$prefix}poststatus (name) VALUES ('private');
+
+CREATE TABLE {$prefix}options (
+	name VARCHAR(255) NOT NULL,
+	type SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+	value TEXT,
+  PRIMARY KEY (name)
+);
+
+CREATE TABLE {$prefix}users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX username ON {$prefix}users(username);
 
-CREATE TABLE  {$prefix}userinfo (
-	user_id INTEGER NOT NULL
-,	name VARCHAR(255) NOT NULL
-,	type SMALLINT NOT NULL DEFAULT 0
-,	value TEXT
-, PRIMARY KEY (user_id, name)
+CREATE TABLE {$prefix}userinfo (
+	user_id SMALLINT UNSIGNED NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	type SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+	value TEXT,
+  PRIMARY KEY (user_id, name)
 );
 
-CREATE TABLE  {$prefix}tags (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-,	tag_text VARCHAR(255) NOT NULL
-, tag_slug VARCHAR(255) NOT NULL
+CREATE TABLE {$prefix}tags (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	tag_text VARCHAR(255) NOT NULL,
+	tag_slug VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX tag_text ON {$prefix}tags(tag_text);
 
-CREATE TABLE  {$prefix}tag2post (
-  tag_id INTEGER NOT NULL
-, post_id INTEGER NOT NULL
-, PRIMARY KEY (tag_id, post_id)
+CREATE TABLE {$prefix}tag2post (
+	tag_id INTEGER UNSIGNED NOT NULL,
+	post_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY (tag_id, post_id)
 );
 CREATE INDEX tag2post_post_id ON {$prefix}tag2post(post_id);
 
-CREATE TABLE  {$prefix}themes (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-, name VARCHAR(255) NOT NULL
-, version VARCHAR(255) NOT NULL
-, template_engine VARCHAR(255) NOT NULL
-, theme_dir VARCHAR(255) NOT NULL
-, is_active SMALLINT NOT NULL DEFAULT 0
+CREATE TABLE {$prefix}themes (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	version VARCHAR(255) NOT NULL,
+	template_engine VARCHAR(255) NOT NULL,
+	theme_dir VARCHAR(255) NOT NULL,
+	is_active TINYINT UNSIGNED NOT NULL DEFAULT 0
 );
 
-INSERT INTO  {$prefix}themes (
-  name
-, version
-, template_engine
-, theme_dir
-, is_active
+INSERT INTO {$prefix}themes (
+	name,
+	version,
+	template_engine,
+	theme_dir,
+	is_active
 ) VALUES (
-  'k2'
-, '1.0'
-, 'rawphpengine'
-, 'k2'
-, 1
+	'k2',
+	'1.0',
+	'rawphpengine',
+	'k2',
+	1
 );
 
-CREATE TABLE  {$prefix}comments (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-,	post_id INTEGER NOT NULL
-,	name VARCHAR(255) NOT NULL
-,	email VARCHAR(255) NOT NULL
-,	url VARCHAR(255) NULL
-,	ip INTEGER NOT NULL
-,	content TEXT
-,	status SMALLINT NOT NULL
-,	date TIMESTAMP NOT NULL
-,	type SMALLINT NOT NULL
+CREATE TABLE {$prefix}comments (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	post_id INTEGER UNSIGNED NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	url VARCHAR(255) NULL,
+	ip INTEGER UNSIGNED NOT NULL,
+	content TEXT,
+	status TINYINT UNSIGNED NOT NULL,
+	date TIMESTAMP NOT NULL,
+	type SMALLINT UNSIGNED NOT NULL
 );
 CREATE INDEX comments_post_id ON {$prefix}comments(post_id);
 
-CREATE TABLE  {$prefix}commentinfo (
-	comment_id INTEGER NOT NULL
-,	name VARCHAR(255) NOT NULL
-,	type SMALLINT NOT NULL DEFAULT 0
-,	value TEXT NULL
-, PRIMARY KEY (comment_id, name)
+CREATE TABLE {$prefix}commentinfo (
+	comment_id INTEGER UNSIGNED NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	type SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+	value TEXT NULL,
+  PRIMARY KEY (comment_id, name)
 );
 
 CREATE TABLE {$prefix}rewrite_rules (
-  rule_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-, name VARCHAR(255) NOT NULL
-, parse_regex VARCHAR(255) NOT NULL
-, build_str VARCHAR(255) NOT NULL
-, handler VARCHAR(255) NOT NULL
-, action VARCHAR(255) NOT NULL
-, priority SMALLINT NOT NULL
-, is_active SMALLINT NOT NULL DEFAULT 0
-, rule_class SMALLINT UNSIGNED NOT NULL DEFAULT 0
-, description TEXT NULL
+	rule_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	parse_regex VARCHAR(255) NOT NULL,
+	build_str VARCHAR(255) NOT NULL,
+	handler VARCHAR(255) NOT NULL,
+	action VARCHAR(255) NOT NULL,
+	priority SMALLINT UNSIGNED NOT NULL,
+	is_active TINYINT UNSIGNED NOT NULL DEFAULT 0,
+	rule_class TINYINT UNSIGNED NOT NULL DEFAULT 0,
+	description TEXT NULL
 );
 
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_at_page','/^page\/([\\d]+)[\/]{0,1}$/i','page/{$page}'
-,'UserThemeHandler','display_posts',1,'Displays posts.  Page (of post) parameter is passed in URL');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('display_posts_by_date', '%^(?P<year>[1,2]{1}[\d]{3})(?:/(?P<month>[\d]{2}))?(?:/(?P<day>[\d]{2}))?(?:/page/(?P<page>\d+))?/?$%i', '{$year}/({$month}/)({$day}/)(page/{$page}/)', 'UserThemeHandler', 'display_date', 2, 1, 1, 'Displays posts for a specific date.');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('display_feed_by_type', '/^feed\/(?P<feed_type>atom|rs[sd])[\/]?$/i', 'feed/{$feed_type}', 'FeedHandler', 'display_feed', 5, 1, 0, 'Return feed per specified feed type');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('admin', '/^admin[\/]*(?P<page>[^\/]*)[\/]?$/i', 'admin/({$page})', 'AdminHandler', 'admin', 6, 1, 0, 'An admin action');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('userprofile', '/^admin\/(?P<page>user)\/(?P<user>[^\/]+)\/?$/', 'admin/{$page}/{$user}', 'AdminHandler', 'admin', 4, 1, 1, 'The profile page for a specific user');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('user', '/^user\/(?P<page>[^\/]*)[\/]?$/i', 'user/{$page}', 'UserHandler', '{$page}', 7, 1, 0, 'A user action or display, for instance the login screen');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('display_posts_by_slug', '%^(?P<slug>[^/]+)(?:/page/(?P<page>\d+))?/?$%i', '{$slug}(/page/{$page}/)', 'UserThemeHandler', 'display_post', 99, 1, 1, 'Return post matching specified slug');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('display_entry_by_slug', '%^(?P<slug>[^/]+)(?:/page/(?P<page>\d+))?/?$%i', '{$slug}(/page/{$page}/)', 'UserThemeHandler', 'display_post', 100, 1, 1, 'Return entry matching specified slug');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('display_page_by_slug', '%^(?P<slug>[^/]+)(?:/page/(?P<page>\d+))?/?$%i', '{$slug}(/page/{$page}/)', 'UserThemeHandler', 'display_post', 100, 1, 1, 'Return page matching specified slug');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('index_page', '/^(?:|page\/(?P<page>\d+)[\/]?)$/', '(page/{$page})', 'UserThemeHandler', 'display_home', 1000, 1, 1, 'Homepage (index) display');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('rsd', '/^rsd$/i', 'rsd', 'AtomHandler', 'rsd', 1, 1, 0, 'RSD output');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('introspection', '/^atom$/i', 'atom', 'AtomHandler', 'introspection', 1, 1, 0, 'Atom introspection');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('collection', '/^atom\/(?P<index>.+)[\/]?$/i', 'atom/{$index}', 'AtomHandler', 'collection', 1, 1, 0, 'Atom collection');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('search', '/^search(?:\/(?P<criteria>[^\/]+))?(?:\/page\/(?P<page>\d+))?$/i', 'search(/{$criteria})(/page/{$page}/)', 'UserThemeHandler', 'search', 8, 1, 1, 'Searches posts');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('comment', '/^(?P<id>[0-9]+)\/feedback[\/]?$/i', '{$id}/feedback', 'FeedbackHandler', 'add_comment', 8, 1, 0, 'Adds a comment to a post');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('ajax', '/^ajax\/(?P<context>[^\/]+)[\/]?$/i', 'ajax/{$context}', 'AjaxHandler', 'ajax', 8, 1, 0, 'Ajax handling');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('auth_ajax', '/^auth_ajax\/(?P<context>[^\/]+)[\/]?$/i', 'auth_ajax/{$context}', 'AjaxHandler', 'auth_ajax', 8, 1, 0, 'Authenticated ajax handling');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('entry', '/(?P<slug>[^\/]+)\/atom$/i', '{$slug}/atom', 'AtomHandler', 'entry', 8, 1, 0, 'Atom Publishing Protocol');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('entry_comments', '/^(?P<slug>[^\/]+)\/atom\/comments$/i', '{$slug}/atom/comments', 'AtomHandler', 'entry_comments', 8, 1, 0, 'Entry comments');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('comments', '/^atom\/comments$/i', 'atom/comments', 'AtomHandler', 'comments', 7, 1, 0, 'Entries comments');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('tag_collection', '/^tag\/(?P<tag>[^\/]+)\/atom$/i', 'tag/{$tag}/atom', 'AtomHandler', 'tag_collection', 8, 1, 0, 'Atom Tag Collection');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('display_posts_by_tag', '%^tag/(?P<tag>[^/]*)(?:/page/(?P<page>\d+))?[/]?$%i', 'tag/{$tag}(/page/{$page}/)', 'UserThemeHandler', 'display_tag', 5, 1, 1, 'Return posts matching specified tag');
+INSERT INTO {$prefix}rewrite_rules (name, parse_regex, build_str, handler, action, priority, is_active, rule_class, description) VALUES ('xmlrpc', '%^xmlrpc/?%i', 'xmlrpc', 'XMLRPCServer', 'xmlrpc_call', 8, 1, 0, 'Handle incoming XMLRPC requests.');
 
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_date','/([1,2]{1}[\d]{3})\/([\d]{2})\/([\d]{2})[\/]{0,1}$/','{$year}/{$month}/{$day}'
-,'UserThemeHandler','display_posts',2,'Displays posts for a specific date.');
+CREATE TABLE {$prefix}crontab (
+	cron_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	callback VARCHAR(255) NOT NULL,
+	last_run VARCHAR(255) NOT NULL,
+	next_run VARCHAR(255) NOT NULL,
+	increment VARCHAR(255) NOT NULL,
+	start_time VARCHAR(255) NOT NULL,
+	end_time VARCHAR(255) NOT NULL,
+	result VARCHAR(255) NOT NULL,
+	notify VARCHAR(255) NOT NULL,
+	cron_class TINYINT UNSIGNED NOT NULL DEFAULT 0,
+	description TEXT NULL
+);
 
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_month','/([1,2]{1}[\d]{3})\/([\d]{2})[\/]{0,1}$/','{$year}/{$month}'
-,'UserThemeHandler','display_posts',3,'Displays posts for a specific month.');
+CREATE TABLE {$prefix}log (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	user_id INTEGER NULL DEFAULT NULL,
+	type_id INTEGER NOT NULL,
+	severity_id TINYINT NOT NULL,
+	message VARCHAR(255) NOT NULL,
+	data BLOB NULL DEFAULT NULL,
+	timestamp DATETIME NOT NULL
+);
 
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_year','/([1,2]{1}[\d]{3})[\/]{0,1}$/','{$year}'
-,'UserThemeHandler','display_posts',4,'Displays posts for a specific year.');
+CREATE TABLE {$prefix}log_types (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	module VARCHAR(100) NOT NULL,
+	type VARCHAR(100) NOT NULL
+);
+CREATE UNIQUE INDEX module_type ON {$prefix}log_types(module, type);
 
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_feed_by_type','/^feed\/(atom|rs[sd])[\/]{0,1}$/i','feed/{$feed_type}'
-,'FeedHandler','display_feed',5,'Return feed per specified feed type');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_tag','/^tag\/([^\/]*)[\/]{0,1}$/i','tag/{$tag}'
-,'UserThemeHandler','display_posts',5,'Return posts matching specified tag');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('admin','/^admin[\/]*([^\/]*)[\/]{0,1}$/i','admin/{$page}'
-,'AdminHandler','admin',6,'An admin action');
-
-INSERT INTO {$prefix}rewrite_rules
-(rule_id, name, parse_regex, build_str, handler, action, priority, description)
-VALUES (NULL, 'userprofile', '/^admin\/(user)\/([^\/]+)\/{0,1}$/i', 'admin/{$page}/{$user}', 'AdminHandler', 'admin', 4, 'The profile page for a specific user');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('user','/^user\/([^\/]*)[\/]{0,1}$/i','user/{$page}'
-,'UserHandler','{$page}',7,'A user action or display, for instance the login screen');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('display_posts_by_slug','/([^\/]+)[\/]{0,1}$/i','{$slug}'
-,'UserThemeHandler','display_posts',99,'Return posts matching specified slug');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('index_page','//',''
-,'UserThemeHandler','display_posts',1000,'Homepage (index) display');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('rsd','/^rsd$/i','rsd'
-,'AtomHandler','rsd',1,'RSD output');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('introspection','/^atom$/i','atom'
-,'AtomHandler','introspection',1,'Atom introspection');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('collection','/^atom\/(.+)[\/]{0,1}$/i','atom/{$index}'
-,'AtomHandler','collection',1,'Atom collection');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('search','/^search$/i','search'
-,'UserThemeHandler','search',8,'Searches posts');
-
-INSERT INTO {$prefix}rewrite_rules
-(name, parse_regex, build_str, handler, action, priority, description)
-VALUES ('comment','/^([0-9]+)\/feedback[\/]{0,1}$/i','{$id}/feedback'
-,'FeedbackHandler','add_comment',8,'Adds a comment to a post');
-
-INSERT INTO {$prefix}rewrite_rules
-(rule_id, name, parse_regex, build_str, handler, action, priority, description)
-VALUES (NULL, 'ajax','/^ajax\/([^\/]+)[\/]{0,1}$/i','ajax/{$context}'
-,'AjaxHandler','ajax',8,'Ajax handling');
-
-INSERT INTO {$prefix}rewrite_rules
-(rule_id, name, parse_regex, build_str, handler, action, priority, description)
-VALUES (NULL, 'auth_ajax','/^auth_ajax\/([^\/]+)[\/]{0,1}$/i','auth_ajax/{$context}'
-,'AjaxHandler','auth_ajax',8,'Authenticated ajax handling');
-
-UPDATE {$prefix}rewrite_rules SET is_active=1;
+INSERT INTO {$prefix}log_types (module, type) VALUES ('habari', 'default');
+INSERT INTO {$prefix}log_types (module, type) VALUES ('habari', 'authentication');
