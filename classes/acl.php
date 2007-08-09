6c2b31c7bc72ec603132bc6021ede7717db28f81
Owen Winkler <epithet@gmail.com>
2007-08-09 17:14:01 +0000

    It would be nice if I shared the acl.php from my /user/classes directory, wouldn't it?
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@745 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/acl.php
+++ classes/acl.php
@@ -1,100 +1,174 @@
 <?php
 
-/**
- * Habari ACL (Access Control List) Class
- * 
- * @package habari
- * @version 2007 beta
- *
- */
-
-
+/** 
+ * Access Control List class
+ *  
+ * The default Habari ACL class implements groups, and group permissions
+ * Users are assigned to one or more groups.
+ * Groups are assigned one or more permissions.
+ * Membership in any group that grants a permission
+ * means you have that permission.  Membership in any group that denies
+ * that permission denies the user that permission, even if another group
+ * grants that permission.
+ *     
+ * @package Habari
+ **/
+ 
 class ACL {
-	private $user_id;
-	private static $permission_cache= null;
+	/**
+	 * An array of users assigned to a specific group id.
+	 * Both the group and the user are id integers, not string values.
+	 * The user arrays have both the key and the value set to the user_id. 	 	 
+	 * For example:
+	 * <code>
+	 * self::$groups= array(
+	 * 	1 => array(1 => 1),
+	 * 	2 => array(1 => 1, 2 => 2),
+	 * );
+	 * </code>
+	 **/	 
+	private static $groups= array();
 
 	/**
-	 * Constructor for ACL class.
-	 * One instance per user to contain their permissions.
-	 */
-	public function __construct( $user_id )
+	 * An array of group IDs with group names.
+	 * For example:
+	 * <code>
+	 * self::$group_names= array(
+	 * 	1 => 'administrators',
+	 * 	2 => 'guests',
+	 * );
+	 * </code>	 
+	 **/	 	 
+	private static $group_names= array();
+
+	/**
+	 * An array of permssions assigned to a group.
+	 * The group and permission are both id integers, not string values.
+	 * The key of each permission array is the permission id.  
+	 * The value of the permission id is boolean on whether to grant or deny that permission.
+	 * For example:
+	 * <code>
+	 * self::$group_permissions= array(
+	 * 	1 => array( 1 => true, 2 => true),
+	 * 	2 => array( 2 => false),
+	 * );
+	 * </code>
+	 **/	 	 	 	 	 	 	 	 	 	 	
+	private static $group_permissions= array();
+
+	/**
+	 * An array in which specific user permissions are cached as they are built from the other structures.
+	 **/	  		
+	private static $user_permissions= array();
+	
+	/**
+	 * An array of permissions, with the permission id as the key and the string value as the value.
+	 **/	 	
+	private static $permissions= array();
+	
+	/**
+	 * How to handle a permission request for a permission that is not in the permission list.
+	 * For example, if you request $user->can('some non-existant permission') then this value is returned.
+	 * It's true at the moment because that allows access to all features for upgrading users.
+	 * @todo Decide if this is a setting we need or want to change, or perhaps it should be an option.
+	 **/	 	 	 	 	
+	const ACCESS_NONEXISTANT_PERMISSION = true;
+
+	/**
+	 * Load all group and permission data from the DB
+	 * __static() class members are called by __autoload()	 
+	**/
+	public static function __static()
 	{
-		$this->user_id= $user_id;
+		$results= DB::get_results( 'SELECT * FROM ' . DB::table('groups') );
+		foreach ($results as $group) {
+			self::$group_names[$group->id]= $group->name;
+		}
+		$results= DB::get_results( 'SELECT * FROM ' . DB::table('permissions'));
+		foreach ( $results as $permission ) {
+			self::$permissions[$permission->id] = $permission->name;
+		}
+		$results= DB::get_results( 'SELECT * FROM ' . DB::table('users_groups') );
+		foreach ( $results as $group ) {
+			self::$groups[$group->group_id][$group->user_id]= $group->user_id;
+		}
+		$results= DB::get_results( 'SELECT * FROM ' . DB::table('groups_permissions'));
+		foreach ( $results as $permission ) {
+			self::$group_permissions[$permission->group_id][$permission->permission_id]= !$permission->denied;
+		}
+	}
+
+	/**
+	 * Determine whether the specified user is a member of the specified group
+	 * @param int A user  ID
+	 * @param int A group ID
+	 * @return bool True if the user is in the group, otherwise false
+	**/
+	public static function user_in_group( $user_id, $group_id )
+	{
+		return isset(self::$groups[self::group_id( $group_id )][$user_id]);
+	}
+
+	/**
+	 * Return an array of the groups to which this user belongs
+	 * @param int A user ID
+	 * @return array An array of group IDs
+	**/
+	public static function user_group_list( $user_id )
+	{
+		$user_id= intval($user_id);
+		return array_keys( array_filter( self::$groups, create_function('$a', 'return isset($a[' . $user_id . ']);') ) );
+	}
+
+	/**
+	 * Determine whether a group can perform a specific action
+	 * @param int A group ID
+	 * @param string An action
+	 * @return bool Whether the group can perform the action
+	**/
+	public static function group_can( $group_id, $action )
+	{
+		return isset(self::$group_permissions[self::group_id( $group_id )][$action]) && self::$group_permissions[self::group_id( $group_id )][$action];
 	}
 	
 	/**
-	 * Grant a permission to this user
-	 * @param string $permission The permission to grant
-	 * @param string $context optional The context to which the permission applies
-	 */
-	public function grant( $permission, $context= '' )
+	 * Return the id of a group, provided the name
+	 * @param string $group_id The name of the group
+	 * @return int The group id
+	 **/
+	public static function group_id( $group_id )
 	{
-		// Set permission in cache and commit to DB
-	}
+		if(!is_numeric($group_id)) {
+			$group_id = array_search($group_id, self::$group_names);
+		}
+		return $group_id;
+	}	 	 	 	
 
 	/**
-	 * Explicitly deny a permission to this user
-	 * @param string $permission The permission to deny
-	 * @param string $context optional The context to which the permission applies
-	 */
-	public function deny( $permission, $context= '' )
+	 * Determine whether a user can perform a specific action
+	 * @param int A user ID
+	 * @param string An action
+	 * @return bool Whether the user can perform the action
+	**/
+	public static function user_can( $user_id, $action )
 	{
-		// Set permission in cache and commit to DB
-	}
-
-	/**
-	 * Remove a permission from this user, whether granted or denied
-	 * @param string $permission The permission to remove
-	 * @param string $context optional The context to which the permission applies
-	 */
-	public function remove( $permission, $context= '' )
-	{
-		// Set permission in cache and commit to DB
-	}
-	
-	/**
-	 * 
-	 * @param string $permission ...
-	 * @param string $context (default '') ...
-	 * @return boolean Whether the current user has the $permission permission
-	 */
-	public function can( $permission, $context= '')
-	{
-		ACL::build_cache();
-		if ( !array_key_exists( self::permission_cache, $this->user_id ) ) {
-			return false;  // User doesn't have any permissions
+		if(($permission_id = array_search($action, self::$permissions)) === false) {
+			return ACL::ACCESS_NONEXISTANT_PERMISSION;
 		}
-		$permissions= self::permission_cache[$this->user_id];
-		// boolean $permissions[$permission][$context]
-		if ( !array_key_exists( $permissions, $permission ) ) {
-			return false; // permission does not exist
+		if(!isset(self::$user_permissions[$user_id])) {
+			self::$user_permissions[$user_id] = array();
+			foreach( self::user_group_list( $user_id ) as $group_id )	{
+				foreach( self::$group_permissions[$group_id] as $action => $grant ) {
+					if(isset(self::$user_permissions[$user_id][$permission_id])) {
+						self::$user_permissions[$user_id][$permission_id]= self::$user_permissions[$user_id][$permission_id] && $grant;
+					}
+					else {
+						self::$user_permissions[$user_id][$permission_id]= $grant;
+					}
+				}
+			}
 		}
-		elseif ( !array_key_exists( $permissions[$permission], $context ) ) {
-			return false;  // User doesn't have this permission
-		}
-		return $permissions[$permission][$context];  // true if granted, false if denied
+		return isset(self::$user_permissions[$user_id][$permission_id]) && self::$user_permissions[$user_id][$permission_id];
 	}
-	
-	/**
-	 * @param int $user_id The ID of the user to check
-	 * @param string $permission ...
-	 * @param string $context (default '') ...
-	 * @return boolean Whether the given user has the $permission permission
-	 */
-	public static function user_can( $user_id, $permission, $context= '')
-	{
-		$user_acl= new ACL( $user_id ); 
-		return $user_acl->can( $permission, $context ); 
-	}
-
-	public static function build_cache()
-	{
-		if ( empty( self::permission_cache ) ) {
-			self::permission_cache= DB::do_something();
-		}
-	
-	}
-
 }
-
 ?>
