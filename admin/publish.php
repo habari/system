419c276cad8abff48a8be5d05c67a2e00419511a
Owen Winkler <epithet@gmail.com>
2007-08-30 02:05:41 +0000

    Several more admin changes.  Much of what's currently going on in here will need to be heavily revised to be more dynamic. i18n sprinkled throughout. Overrode the annoying justification that was causing three words to be spread evenly across the page width. Added Page listings to the content page. Added plugin hook for stats block on Dashboard, "statistics_summary". Corrected links to point to actual stuff (like the manual and support mailing list). Theme's theme.xml must now exist (not just the subdirectory in themes) for it to be listed in the admin.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@793 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/publish.php
+++ admin/publish.php
@@ -5,7 +5,7 @@
   if ( isset( $result ) ) {
    switch( $result ) {
     case 'success':
-     _e('<p class="update">Your post has been saved.</p>');
+     echo '<p class="update">' . _t('Your post has been saved.') . '</p>';
      break;
    }
   }
@@ -21,20 +21,20 @@
    ?>
   <form name="create-content" id="create-content" method="post" action="<?php URL::out( 'admin', 'page=publish' ); ?>">
    <div class="dashboard-block c3 publish">
-    <h4>Title</h4>
+    <h4><?php _e('Title'); ?></h4>
     <input type="text" name="title" id="title" size="100%" value="<?php echo $post->title; ?>">
     
-    <h4>Content</h4>
+    <h4><?php _e('Content'); ?></h4>
     <textarea name="content" id="content" rows="20"><?php echo htmlspecialchars( $post->content ); ?></textarea>
     
-    <h4>Tags</h4>
-    <p>Seperate tags with a comma, space seperated words become multi-world tags. <input type="text" name="tags" id="tags" value="<?php echo ( !empty( $tags ) ) ? $tags : ''; ?>"></p>
+    <h4><?php _e('Tags'); ?></h4>
+    <p><?php _e('Tags (comma separated):'); ?> <input type="text" name="tags" id="tags" value="<?php echo ( !empty( $tags ) ) ? $tags : ''; ?>"></p>
     
-    <h4>Meta Information</h4>
-    <p>Publish Date: <input type="text" name="pubdate" id="pubdate" value="<?php echo $post->pubdate; ?>"></p>
-    <p>Content Address: <input type="text" name="newslug" id="newslug" value="<?php echo $post->slug; ?>"></p>
+    <h4><?php _e('Meta Information'); ?></h4>
+    <p><?php _e('Publish Date:'); ?> <input type="text" name="pubdate" id="pubdate" value="<?php echo $post->pubdate; ?>"></p>
+    <p><?php _e('Content Address:'); ?> <input type="text" name="newslug" id="newslug" value="<?php echo $post->slug; ?>"></p>
     
-    <h4>Page Settings</h4>
+    <h4><?php _e('Entry Settings'); ?></h4>
     <ul>
      <?php
      $statuses= Post::list_post_statuses();
@@ -45,19 +45,19 @@
       }
 					$post_status= Post::status( $name );
      ?>
-     <li><label><input type="radio" name="status" id="<?php echo $name; ?>" value="<?php echo $post_status; ?>" <?php echo ( $post->status == $post_status ) ? 'checked' : ''; ?> > <?php echo ucwords( $name ); ?></label></li>
+     <li><label><input type="radio" name="status" id="<?php echo $name; ?>" value="<?php echo $post_status; ?>" <?php echo ( $post->status == $post_status ) ? 'checked' : ''; ?> > <?php _e( ucwords( $name ) ); ?></label></li>
      <?php
      }
      ?>
     </ul>
     
-    <h4>Comments</h4>
+    <h4><?php _e('Comments'); ?></h4>
     <ul>
-     <li><label><input type="radio" name="comments_disabled" id="comments_enabled" value="0" <?php echo ( $post->info->comments_disabled == 0 ) ? 'checked' : ''; ?>> Allow comments</label></li>
-     <li><label><input type="radio" name="comments_disabled" id="comments_disabled" value="1" <?php echo ( $post->info->comments_disabled == 1 ) ? 'checked' : ''; ?>> No comments</label></li>
+     <li><label><input type="radio" name="comments_disabled" id="comments_enabled" value="0" <?php echo ( $post->info->comments_disabled == 0 ) ? 'checked' : ''; ?>> <?php _e('Allow comments'); ?></label></li>
+     <li><label><input type="radio" name="comments_disabled" id="comments_disabled" value="1" <?php echo ( $post->info->comments_disabled == 1 ) ? 'checked' : ''; ?>> <?php _e('No comments'); ?></label></li>
     </ul>
     <input type="hidden" name="content_type" value="<?php echo $content_type; ?>">
-    <p><input type="submit" name="submit" id="submit" value="Save!"></p>
+    <p><input type="submit" name="submit" id="submit" value="<?php _e('Save!'); ?>"></p>
    </div>
    <?php if ( $post->slug != '' ) { ?>
    <input type="hidden" name="slug" id="slug" value="<?php echo $post->slug; ?>">
