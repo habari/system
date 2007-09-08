4b73554f24ad44048458cd5e0f67a50e1c0ee19f
Scott Merrill <skippy@contrib.habariproject.org>
2007-09-08 13:58:16 +0000

    Use Site::out_url() for the Help link.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@832 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/footer.php
+++ admin/footer.php
@@ -4,7 +4,7 @@
 		<div class="column span-8 first"><a href="http://code.google.com/p/habari/" title="Habari">Habari</a><small> - Spread the News</small></div>
 		
 		<div class="column prepend-10 span-6 last">
-			<a href="<?php Site::get_url('habari')?>/manual/index.html" onclick="popUp(this.href);return false;" title="The Habari Help Center">The Help Center</a> | 
+			<a href="<?php Site::out_url('habari')?>/manual/index.html" onclick="popUp(this.href);return false;" title="The Habari Help Center">The Help Center</a> | 
 			<a href="http://groups.google.com/group/habari-users" title="Support Forums">Support Forums</a>
 		</div>
 		
