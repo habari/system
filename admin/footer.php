c02901b120163b4dcdf7fa3386261ffac4d3fa0d
Andrew da Silva <freakerz@contrib.habariproject.org>
2007-09-17 20:51:41 +0000

    Fixed the admin footer links, added one to the wiki.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@853 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/footer.php
+++ admin/footer.php
@@ -1,11 +1,12 @@
 <hr />
 <div class="container" id="footer">
 	<div class="column span-24">
-		<div class="column span-8 first"><a href="http://code.google.com/p/habari/" title="Habari">Habari</a><small> - Spread the News</small></div>
+		<div class="column span-8 first"><a href="http://habariproject.org/" title="Habari Official Website">Habari</a><small> - Spread the News</small></div>
 		
-		<div class="column prepend-10 span-6 last">
-			<a href="<?php Site::out_url('habari')?>/manual/index.html" onclick="popUp(this.href);return false;" title="The Habari Help Center">The Help Center</a> | 
-			<a href="http://groups.google.com/group/habari-users" title="Support Forums">Support Forums</a>
+		<div class="column prepend-11 span-5 last">
+			<a href="<?php Site::out_url('habari')?>/manual/index.html" onclick="popUp(this.href);return false;" title="Read this first">User Manual</a> |
+			<a href="http://groups.google.com/group/habari-users" title="Ask questions to the community">Support</a> |
+			<a href="http://wiki.habariproject.org/" title="Various documentation">Wiki</a>
 		</div>
 		
 	</div>
