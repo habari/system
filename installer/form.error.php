59507466affc36543b24a5b2c867d8efdff2ec49
Andrew da Silva <freakerz@contrib.habariproject.org>
2007-09-26 14:09:15 +0000

    This file is not used anymore. We output form errors in the db_setup.php
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@942 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- installer/form.error.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (
-  isset($form_errors) && 
-  count($form_errors) > 0 && 
-  isset($form_errors[$error_id])) {
-?>
- <div class="error"><?php echo $form_errors[$error_id];?></div>
-<?php }?>
