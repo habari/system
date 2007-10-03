9ec3e40378aeaeada65f75eb94890a3f776f16b6
Andrew da Silva <freakerz@contrib.habariproject.org>
2007-10-03 03:52:22 +0000

    Converting .post to .ajax so we can bind a function to ajax errors.
    Added _very_ simple error output, an alert().
    
    Since these errors are rare, the output can be revised later.
    
    Thanks Michael!
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@949 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- installer/script.js
+++ installer/script.js
@@ -16,75 +16,95 @@ function setDatabaseType(el)
 function checkDBCredentials()
 {
 	if ( ( $('#databasetype').val() == 'mysql' ) && ( $('#databasehost').val() != '' ) && ( $('#databaseuser').val() != '' ) && ( $('#databasename').val() != '' ) ) {
-		$.post('ajax/check_mysql_credentials',{ // Ask InstallHandler::ajax_check_mysql_credentials to check the credentials
+		$.ajax({ 
+			type: 'POST',
+			url: 'ajax/check_mysql_credentials',
+			data: { // Ask InstallHandler::ajax_check_mysql_credentials to check the credentials
 				ajax_action: 'check_mysql_credentials',
 				host: $('#databasehost').val(),
 				database: $('#databasename').val(),
 				user: $('#databaseuser').val(),
 				pass: $('#databasepass').val()
-			}, function(xml) {
+			},
+			success: function(xml) {
 				switch($('status',xml).text()) {
-					case '0': // Show warning, fade the borders and hide the next step
-						$('id',xml).each(function(id) {
-							ido= $('id',xml).get(id);
-							warningtext= $('message',xml).text();
-							$('#siteconfiguration').children('.options').fadeOut();
-							$('#siteconfiguration').removeClass('ready').removeClass('done');
-							$('#install').children('.options').fadeOut();
-							$('#install').removeClass('ready').removeClass('done');
-							$($(ido).text()).parents('.installstep').removeClass('done');
-							$($(ido).text()).parents('.inputfield').removeClass('invalid').removeClass('valid').addClass('invalid');
-							$($(ido).text()).parents('.inputfield').find('.warning:hidden').html(warningtext).fadeIn();
-							});
-						break;
-					case '1': // Hide the warnings, highlight the borders and show the next step
-						ida= new Array( '#databasename', '#databasehost', '#databasepass', '#databaseuser' );
-						$(ida).each(function(id) {
-							ido= $(ida).get(id);
-							$(ido).parents('.inputfield').removeClass('invalid').addClass('valid');
-							$(ido).parents('.inputfield').find('.warning:visible').fadeOut();
-							$(ido).parents('.installstep').addClass('done')
-							});
-						$('#siteconfiguration').children('.options').fadeIn();
-						$('#siteconfiguration').addClass('ready');
-						break;
+				case '0': // Show warning, fade the borders and hide the next step
+					$('id',xml).each(function(id) {
+					ido= $('id',xml).get(id);
+					warningtext= $('message',xml).text();
+					$('#siteconfiguration').children('.options').fadeOut();
+					$('#siteconfiguration').removeClass('ready').removeClass('done');
+					$('#install').children('.options').fadeOut();
+					$('#install').removeClass('ready').removeClass('done');
+					$($(ido).text()).parents('.installstep').removeClass('done');
+					$($(ido).text()).parents('.inputfield').removeClass('invalid').removeClass('valid').addClass('invalid');
+					$($(ido).text()).parents('.inputfield').find('.warning:hidden').html(warningtext).fadeIn();
+					});
+					break;
+				case '1': // Hide the warnings, highlight the borders and show the next step
+					ida= new Array( '#databasename', '#databasehost', '#databasepass', '#databaseuser' );
+					$(ida).each(function(id) {
+					ido= $(ida).get(id);
+					$(ido).parents('.inputfield').removeClass('invalid').addClass('valid');
+					$(ido).parents('.inputfield').find('.warning:visible').fadeOut();
+					$(ido).parents('.installstep').addClass('done')
+					});
+					$('#siteconfiguration').children('.options').fadeIn();
+					$('#siteconfiguration').addClass('ready');
+					break;
 				}
-			}
-		);
+			},
+			error: function(msg, status, err) {
+				alert("ERROR:\n"+
+				"\nStatus: "+status+
+				"\nError: "+err+
+				"\n\n"+msg.responseText);
+				},
+		});
 	}
 	else if ( ( $('#databasetype').val() == 'sqlite' ) && ( $('#databasefile').val() != '' ) ) {
-		$.post('ajax/check_sqlite_credentials',{ // Ask InstallHandler::ajax_check_sqlite_credentials to check the credentials
+		$.ajax({
+			type: 'POST',
+			url: 'ajax/check_sqlite_credentials',
+			data: { // Ask InstallHandler::ajax_check_mysql_credentials to check the credentials
 				ajax_action: 'check_sqlite_credentials',
 				file: $('#databasefile').val(),
-			}, function(xml) {
+			},
+			success: function(xml) {
 				switch($('status',xml).text()) {
-					case '0': // Show warning, fade the borders and hide the next step
-						$('id',xml).each(function(id) {
-							ido= $('id',xml).get(id);
-							warningtext= $('message',xml).text();
-							$('#siteconfiguration').children('.options').fadeOut();
-							$('#siteconfiguration').removeClass('ready').removeClass('done');
-							$('#install').children('.options').fadeOut();
-							$('#install').removeClass('ready').removeClass('done');
-							$($(ido).text()).parents('.installstep').removeClass('done');
-							$($(ido).text()).parents('.inputfield').removeClass('invalid').removeClass('valid').addClass('invalid');
-							$($(ido).text()).parents('.inputfield').find('.warning:hidden').html(warningtext).fadeIn();
-							});
-						break;
-					case '1': // Hide the warnings, highlight the borders and show the next step
-						ida= new Array( '#databasefile' );
-						$(ida).each(function(id) {
-							ido= $(ida).get(id);
-							$(ido).parents('.inputfield').removeClass('invalid').addClass('valid');
-							$(ido).parents('.inputfield').find('.warning:visible').fadeOut();
-							$(ido).parents('.installstep').addClass('done')
-							});
-						$('#siteconfiguration').children('.options').fadeIn();
-						$('#siteconfiguration').addClass('ready');
-						break;
+				case '0': // Show warning, fade the borders and hide the next step
+					$('id',xml).each(function(id) {
+					ido= $('id',xml).get(id);
+					warningtext= $('message',xml).text();
+					$('#siteconfiguration').children('.options').fadeOut();
+					$('#siteconfiguration').removeClass('ready').removeClass('done');
+					$('#install').children('.options').fadeOut();
+					$('#install').removeClass('ready').removeClass('done');
+					$($(ido).text()).parents('.installstep').removeClass('done');
+					$($(ido).text()).parents('.inputfield').removeClass('invalid').removeClass('valid').addClass('invalid');
+					$($(ido).text()).parents('.inputfield').find('.warning:hidden').html(warningtext).fadeIn();
+					});
+					break;
+				case '1': // Hide the warnings, highlight the borders and show the next step
+					ida= new Array( '#databasefile' );
+					$(ida).each(function(id) {
+					ido= $(ida).get(id);
+						$(ido).parents('.inputfield').removeClass('invalid').addClass('valid');
+						$(ido).parents('.inputfield').find('.warning:visible').fadeOut();
+						$(ido).parents('.installstep').addClass('done')
+					});
+					$('#siteconfiguration').children('.options').fadeIn();
+					$('#siteconfiguration').addClass('ready');
+					break;
 				}
-			}
-		);
+			},
+			error: function(msg, status, err) {
+				alert("ERROR:\n"+
+				"\nStatus: "+status+
+				"\nError: "+err+
+				"\n\n"+msg.responseText);
+				},
+		});
 	}
 	else {
 		$('.installstep:first').removeClass('done');
