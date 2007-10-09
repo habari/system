b39880c03d95c389690ef04b674b075486085f63
Andrew da Silva <freakerz@contrib.habariproject.org>
2007-10-09 04:42:11 +0000

    - Code Formatting. - Fixed a bug. A foreach() was assigning $action, therefore, the "Plugin : Action" when you clicked a "FormUI" link was not good, renamed the variables used by the foreach().
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@982 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/plugins.php
+++ admin/plugins.php
@@ -1,4 +1,4 @@
-<?php include('header.php');?>
+<?php include( 'header.php' ); ?>
 <div class="container">
 <hr>
 	<div class="dashboard-block span-24 first last" id="welcome">
@@ -10,27 +10,27 @@
 			$listok= true;
 			$all_plugins= Plugins::list_all();
 			$active_plugins= Plugins::get_active();
-			if(Plugins::changed_since_last_activation()) {
-				$request= new RemoteRequest(URL::get('admin', array('page'=>'loadplugins')), 'POST', 300);
-				$request->add_header(array('Cookie'=>$_SERVER['HTTP_COOKIE']));
+			if ( Plugins::changed_since_last_activation() ) {
+				$request= new RemoteRequest( URL::get( 'admin', array( 'page' => 'loadplugins' ) ), 'POST', 300 );
+				$request->add_header( array( 'Cookie' => $_SERVER['HTTP_COOKIE'] ) );
 				$result= $request->execute();
-				if(!$request->executed() || preg_match('%^http/1\.\d 500%i', $request->get_response_headers())) {
+				if ( !$request->executed() || preg_match( '%^http/1\.\d 500%i', $request->get_response_headers() ) ) {
 					$listok= false;
 		?>
-			<p><?php _e('The plugin list has changed since the last activation.  Please wait while Habari loads the plugin list.'); ?></p>
+			<p><?php _e( 'The plugin list has changed since the last activation.  Please wait while Habari loads the plugin list.' ); ?></p>
 		<?php
 				}
 			}
 			Options::clear_cache();
 			$failed_plugins= Options::get('failed_plugins');
-			if(is_array($failed_plugins)) {
-				$all_plugins= array_diff($all_plugins, $failed_plugins);
+			if ( is_array( $failed_plugins ) ) {
+				$all_plugins= array_diff( $all_plugins, $failed_plugins );
 			} 
-			if($listok) :
-				if(count($failed_plugins) > 0) {
+			if ( $listok ) :
+				if ( count( $failed_plugins ) > 0 ) {
 					echo '<div class="warning">';
-					foreach($failed_plugins as $failed) {
-						echo '<p>' . sprintf(_t('Attempted to load the plugin file "%s", but it failed with syntax errors.'), basename($failed)) . '</p>';
+					foreach( $failed_plugins as $failed ) {
+						echo '<p>' . sprintf( _t( 'Attempted to load the plugin file "%s", but it failed with syntax errors.' ), basename( $failed ) ) . '</p>';
 					}
 					echo '</div>';
 				}
@@ -47,11 +47,10 @@
 			<tbody>
 			<?php
 			foreach ( $all_plugins as $file ) :
-				$verb= _t('Activate');
+				$verb= _t( 'Activate' );
 				$plugin_id= Plugins::id_from_file( $file );
-				if ( array_key_exists( $plugin_id, $active_plugins ) )
-				{
-					$verb= _t('Deactivate');
+				if ( array_key_exists( $plugin_id, $active_plugins ) ) {
+					$verb= _t( 'Deactivate' );
 					$plugin= $active_plugins[$plugin_id];
 					$active= true;
 				}
@@ -59,15 +58,15 @@
 				{
 					// instantiate this plugin
 					// in order to get its info()
-					include_once($file);
-					$plugin= Plugins::load($file);
+					include_once( $file );
+					$plugin= Plugins::load( $file );
 					$active= false;
 				}
-				if(isset($plugin->info->url)) {
-					$url = "<p><a href=\"{$plugin->info->url}\" title=\"Visit " . str_replace('"', '\\"', $plugin->info->name) . "\">{$plugin->info->author}</a></p>";
+				if (isset( $plugin->info->url ) ) {
+					$url= "<p><a href=\"{$plugin->info->url}\" title=\"Visit " . str_replace('"', '\\"', $plugin->info->name) . "\">{$plugin->info->author}</a></p>";
 				}
 				else {
-					$url = $plugin->info->author;
+					$url= $plugin->info->author;
 				}
 			?>
 				<tr>
@@ -81,15 +80,15 @@
 					<input type='hidden' name='action' value='<?php echo $active ? 'Deactivate' : 'Activate'; ?>'>
 					<p><button name='submit'><?php echo $verb; ?></button></p>
 					<?php 
-					if ($active) {
-						$actions= array();
-						$actions= Plugins::filter('plugin_config', $actions, $plugin->plugin_id);
-						foreach($actions as $action => $caption) {
-							if(is_numeric($action)) {
-								$action = $caption;
+					if ( $active ) {
+						$plugin_actions= array();
+						$plugin_actions= Plugins::filter( 'plugin_config', $plugin_actions, $plugin->plugin_id );
+						foreach( $plugin_actions as $plugin_action => $plugin_action_caption ) {
+							if ( is_numeric( $plugin_action ) ) {
+								$plugin_action = $plugin_action_caption;
 							}
 							?>
-							<p><a href="<?php URL::out( 'admin', 'page=plugins&configure=' . $plugin->plugin_id . '&action=' . $action ); ?>"><?php echo $caption; ?></a></p>
+							<p><a href="<?php URL::out( 'admin', 'page=plugins&configure=' . $plugin->plugin_id . '&action=' . $plugin_action ); ?>"><?php echo $plugin_action_caption; ?></a></p>
 							<?php
 						}
 					}
@@ -97,18 +96,18 @@
 					</form>
 					</td>
 				</tr>
-			<?php endforeach; ?>
+			<?php } ?>
 			</tbody>
 		</table>
-		<?php endif; ?>
+		<?php } ?>
 	</div>
-	<?php if( isset( $this->engine_vars['configure'] ) && ( $configure = $this->engine_vars['configure'] ) ): ?>
+	<?php if ( isset( $this->engine_vars['configure'] ) ) { ?>
 	<div class="dashboard-block span-24 first last" id="plugin_options">
-		<h2><?php echo $active_plugins[$configure]->info->name; echo ' : '; echo $action; ?></h2>
+		<h2><?php echo $active_plugins[$configure]->info->name; ?> : <? echo $action; ?></h2>
 		<?php
-			Plugins::act('plugin_ui', $this->engine_vars['configure'], $this->engine_vars['action']);
+			Plugins::act( 'plugin_ui', $configure, $action );
 		?>
 	</div>
-	<?php endif; ?>
+	<?php } ?>
 </div>
-<?php include('footer.php');?>
+<?php include( 'footer.php' ); ?>
