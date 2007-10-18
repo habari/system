12497082011d8745a5395a491f14b8cb4d44e2c7
Andrew da Silva <freakerz@contrib.habariproject.org>
2007-10-18 22:54:24 +0000

    Convert the Controller class from spaces to tabs.
    Little code format modifications.
    
    Next are the comments.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1029 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/controller.php
+++ classes/controller.php
@@ -7,119 +7,118 @@
  * @package Habari 
  */
 class Controller extends Singleton {
-  public $base_url= '';        // base url for site
-  private $stub= '';            // stub supplied by rewriter
-  private $action= '';          // action name (string)
-  private $handler= null;       // the action handler object
+	public $base_url= '';        // base url for site
+	private $stub= '';            // stub supplied by rewriter
+	private $action= '';          // action name (string)
+	private $handler= null;       // the action handler object
 
-  /**
-   * Enables singleton working properly
-   * 
-   * @see singleton.php
-   */
-  static protected function instance() {
-    return parent::instance(get_class());
-  }
+	/**
+	 * Enables singleton working properly
+	 * 
+	 * @see singleton.php
+	 */
+	static protected function instance() {
+		return parent::instance(get_class());
+	}
 
-  /**
-   * Returns the base URL
-   *
-   * @return string base URL
-   */
-  public function get_base_url() {
-    return Controller::instance()->base_url;
-  }
+	/**
+	 * Returns the base URL
+	 *
+	 * @return string base URL
+	 */
+	public function get_base_url() {
+		return Controller::instance()->base_url;
+	}
 
-  /**
-   * Returns the stub in its entirety
-   *
-   * @return  string  the URL incoming stub
-   */
-  public function get_stub() {
-    return Controller::instance()->stub;
-  }
+	/**
+	 * Returns the stub in its entirety
+	 *
+	 * @return  string  the URL incoming stub
+	 */
+	public function get_stub() {
+		return Controller::instance()->stub;
+	}
 
-  /**
-   * Returns the action
-   *
-   * @return  string name of action
-   */
-  public function get_action() {
-    return Controller::instance()->action;
-  }
-  
-  /**
-   * Returns the action handler
-   * 
-   * @return  object  handler object
-   */
-  public function get_handler() {
-    return Controller::instance()->handler;
-  }
-  
-  /**
-   * A convenience method for returning a handler variable (handler_var).
-   * This includes parameters set on the url, and fields submitted by POST.
-   * The alternative to this, while possible to write, is just too long.
-   * @param string $name The name of the variable to return.
-   * @return mixed The value of that variable in the handler
-   **/	  	 	   
-  public static function get_var( $name )
-  {
-  	return isset( Controller::instance()->handler->handler_vars[ $name ] ) ? Controller::instance()->handler->handler_vars[ $name ] : NULL;
-  }
+	/**
+	 * Returns the action
+	 *
+	 * @return  string name of action
+	 */
+	public function get_action() {
+		return Controller::instance()->action;
+	}
 
-  /**
-   * Parses the requested URL.  Automatically 
-   * translates URLs coming in from mod_rewrite and parses
-   * out any action and parameters in the slug.
-   */
-  static public function parse_request() {
-    /* Local scope variable caching */
-    $controller= Controller::instance();
+	/**
+	 * Returns the action handler
+	 * 
+	 * @return  object  handler object
+	 */
+	public function get_handler() {
+		return Controller::instance()->handler;
+	}
 
-    /* Grab the base URL from the Site class */
-    $controller->base_url= Site::get_path('base', true);
+	/**
+	 * A convenience method for returning a handler variable (handler_var).
+	 * This includes parameters set on the url, and fields submitted by POST.
+	 * The alternative to this, while possible to write, is just too long.
+	 * @param string $name The name of the variable to return.
+	 * @return mixed The value of that variable in the handler
+	 */	  	 	   
+	public static function get_var( $name ) {
+		return isset( Controller::instance()->handler->handler_vars[ $name ] ) ? Controller::instance()->handler->handler_vars[ $name ] : NULL;
+	}
 
-    /* Start with the entire URL coming from web server... */
-    $start_url= ( isset($_SERVER['REQUEST_URI']) 
-      ? $_SERVER['REQUEST_URI'] 
-      : $_SERVER['SCRIPT_NAME'] . 
-        ( isset($_SERVER['PATH_INFO']) 
-        ? $_SERVER['PATH_INFO'] 
-        : '') . 
-        ( (isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'] != '')) 
-          ? '?' . $_SERVER['QUERY_STRING'] 
-          : ''));
-    
-    /* Strip out the base URL from the requested URL */
-    /* but only if the base URL isn't / */
-    if ( '/' != $controller->base_url)
-	    $start_url= str_replace($controller->base_url, '', $start_url);
-    
-    /* Trim off any leading or trailing slashes */
-    $start_url= trim($start_url, '/');
+	/**
+	 * Parses the requested URL.  Automatically 
+	 * translates URLs coming in from mod_rewrite and parses
+	 * out any action and parameters in the slug.
+	 */
+	static public function parse_request() {
+		/* Local scope variable caching */
+		$controller= Controller::instance();
 
-    /* Remove the querystring from the URL */
-    if ( strpos($start_url, '?') !== FALSE )
-      list($start_url, $query_string)= explode('?', $start_url);
+		/* Grab the base URL from the Site class */
+		$controller->base_url= Site::get_path('base', true);
 
-    /* Return $_GET values to their proper place */
-    if( !empty($query_string) )
-        parse_str($query_string, $_GET);
+		/* Start with the entire URL coming from web server... */
+		$start_url= ( isset($_SERVER['REQUEST_URI']) 
+					? $_SERVER['REQUEST_URI'] 
+					: $_SERVER['SCRIPT_NAME'] . 
+						( isset($_SERVER['PATH_INFO']) 
+						? $_SERVER['PATH_INFO'] 
+						: '') . 
+							( (isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'] != '')) 
+							? '?' . $_SERVER['QUERY_STRING'] 
+							: ''));
 
-    /* Undo what magic_quotes_gpc might have wrought */
-    Utils::revert_magic_quotes_gpc();
+		/* Strip out the base URL from the requested URL */
+		/* but only if the base URL isn't / */
+		if ( '/' != $controller->base_url)
+			$start_url= str_replace($controller->base_url, '', $start_url);
 
-    $controller->stub= $start_url;
+		/* Trim off any leading or trailing slashes */
+		$start_url= trim($start_url, '/');
 
-    /* Grab the URL filtering rules from DB */
-    $matched_rule= URL::parse($controller->stub);
+		/* Remove the querystring from the URL */
+		if ( strpos($start_url, '?') !== FALSE )
+			list($start_url, $query_string)= explode('?', $start_url);
 
-    if ($matched_rule === FALSE) {
-    	// Create a rule to handle a 404 and force it to dispatch
-    	$matched_rule = new RewriteRule(
-	    	array( 
+		/* Return $_GET values to their proper place */
+		if( !empty($query_string) )
+			parse_str($query_string, $_GET);
+
+		/* Undo what magic_quotes_gpc might have wrought */
+		Utils::revert_magic_quotes_gpc();
+
+		$controller->stub= $start_url;
+
+		/* Grab the URL filtering rules from DB */
+		$matched_rule= URL::parse($controller->stub);
+
+		if ($matched_rule === FALSE) {
+			// Create a rule to handle a 404 and force it to dispatch
+			$matched_rule = new RewriteRule(
+					array( 
 					'name' => '404', 
 					'parse_regex' => '', 
 					'build_str' => '', 
@@ -129,31 +128,31 @@ class Controller extends Singleton {
 					'description' => 'Displays an error page when a URL is not matched.', 
 					'is_active' => 1, 
 					'rule_class' => RewriteRule::RULE_SYSTEM 
-				)
-			);
-    }
-    
-    /* OK, we have a matching rule.  Set the action and create a handler */
-    $controller->action= $matched_rule->action;
-    $controller->handler= new $matched_rule->handler();
-    
-    /* Insert the regexed submatches as the named parameters */
-    $controller->handler->handler_vars['entire_match']= $matched_rule->entire_match; // The entire matched string is returned at index 0
-    foreach ($matched_rule->named_arg_values as $named_arg_key=>$named_arg_value)
-      $controller->handler->handler_vars[$named_arg_key]= $named_arg_value;
+					)
+				);
+		}
 
-    /* Also, we musn't forget to add the GET and POST vars into the action's settings array */
-    $controller->handler->handler_vars= array_merge($controller->handler->handler_vars, $_GET, $_POST);
-    return true;
-  }
+		/* OK, we have a matching rule.  Set the action and create a handler */
+		$controller->action= $matched_rule->action;
+		$controller->handler= new $matched_rule->handler();
 
-  /**
-   * Handle the requested action by firing off the matched handler action(s)
-   */
-  public function dispatch_request() {
-    /* OK, set the wheels in motion... */
-    Controller::instance()->handler->act(Controller::instance()->action);
-  }
+		/* Insert the regexed submatches as the named parameters */
+		$controller->handler->handler_vars['entire_match']= $matched_rule->entire_match; // The entire matched string is returned at index 0
+		foreach ($matched_rule->named_arg_values as $named_arg_key=>$named_arg_value)
+			$controller->handler->handler_vars[$named_arg_key]= $named_arg_value;
+
+		/* Also, we musn't forget to add the GET and POST vars into the action's settings array */
+		$controller->handler->handler_vars= array_merge($controller->handler->handler_vars, $_GET, $_POST);
+		return true;
+	}
+
+	/**
+	 * Handle the requested action by firing off the matched handler action(s)
+	 */
+	public function dispatch_request() {
+		/* OK, set the wheels in motion... */
+		Controller::instance()->handler->act(Controller::instance()->action);
+	}
 }
 
 ?>
