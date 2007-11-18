4f828582aa3e4bb8b43beec25fa7db47090c3ccd
Owen Winkler <epithet@gmail.com>
2007-11-18 04:30:05 +0000

    Changed the footer text and graphic background.  Fixes 483.  Thanks robin.adr.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1085 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/footer.php
+++ admin/footer.php
@@ -1,12 +1,13 @@
 
-<div class="container" id="footer">
-		
-		<div id="footer-main">
-			<p><a href="<?php URL::out('user', 'page=logout'); ?>" title="logout of Habari">Logout</a> |<a href="<?php Site::out_url('habari')?>/manual/index.html" onclick="popUp(this.href);return false;" title="Read this first">User Manual</a>  | 
-			<a href="http://groups.google.com/group/habari-users" title="Ask questions to the community">Support</a>  | 
-			<a href="http://wiki.habariproject.org/" title="Various documentation">Wiki</a></p>
-		</div>
+<div id="footer">
+	<p class="left"><a href="<?php Site::out_url( 'habari' ); ?>/manual/index.html" onclick="popUp(this.href); return false;" title="Read the user manual">Manual</a> - 
+		<a href="http://wiki.habariproject.org/" title="Read the Habari wiki">Wiki</a> - 
+		<a href="http://groups.google.com/group/habari-users" title="Ask the community">Mailing List</a>
+	</p>
+	
+	<p class="right"><a href="<?php URL::out( 'user', 'page=logout' ); ?>" title="Logout of Habari">Logout &rarr;</a></p>
 </div>
+
 <?php
 	Plugins::act( 'admin_footer', $this );
 	Stack::out( 'admin_footer_javascript', ' <script src="%s" type="text/javascript"></script>'."\r\n" );
