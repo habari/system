77faaf1f84f3673da3c917db3b3422e71a982975
Chris J Davis <chrisjdavis@contrib.habariproject.org>
2007-11-23 17:05:07 +0000

    Adding some style to the default login form.
    For now, we are using the same visual elements as the
    installer, which I happen to think are quite nice. Feel free
    to do something else if you see fit.
    
    
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1128 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/login.php
+++ admin/login.php
@@ -1,36 +1,42 @@
-<div class="content">
-	<div id="primary">
-		<div id="primarycontent" class="hfeed">
-			<?php
-if ( isset( $error ) ) {
-?>
-<p>That login is incorrect.</p>
-<?php
+<link href="<?php Site::out_url( 'habari' ); ?>/system/installer/style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+body {
+	text-align: center;
+	min-width: 600px;
+	background: #f1f1f1;
+	font-family: Verdana;
 }
-if ( isset($user) ) {
-?>
-<p>You are logged in as <?php echo $user->username; ?>.</p>
-<p>Want to <a href="<?php Site::out_url( 'habari' ); ?>/user/logout">log out</a>?</p>
-<?php
+
+.submit
+{
+	width: auto !important;
+	position: relative;
 }
-else {
-?>
-<form method="post" action="<?php URL::out( 'user', array( 'page' => 'login' ) ); ?>">
-	<p>
-		<label for="habari_username"><small><strong>Name:</strong></small></label>
-		<input type="text" size="25" name="habari_username" id="habari_username">
-	</p>
-	<p>
-		<label for="habari_password"><small><strong>Password:</strong></small></label>
-		<input type="password" size="25" name="habari_password" id="habari_password">
-	</p>
-	<p>
-		<input type="submit" value="GO!">
-	</p>
-</form>
-<?php
-}
-?>
+
+</style>
+	<div id="wrapper">
+		<div id="masthead">
+			<h1>Habari</h1>
+		</div>
+		<form method="post" action="<?php URL::out( 'user', array( 'page' => 'login' ) ); ?>">
+		<div class="installstep ready done" id="databasesetup">
+			<h2>Login to <?php Options::out( 'title' ); ?></h2>
+			<div class="options">
+				<div class="inputfield">
+					<p>
+						<label for="habari_username">Name:</label>
+						<input type="text" size="25" name="habari_username" id="habari_username">
+					</p>
+					<p>
+						<label for="habari_password">Password:</label>
+						<input type="password" size="25" name="habari_password" id="habari_password">
+					</p>
+					<p>
+						<input class="submit" type="submit" value="GO!">
+					</p>
+				</div>
+			</div>			
+			<div class="bottom"></div>
 		</div>
 	</div>
 </div>
