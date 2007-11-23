fcb37d2124e00384022f1ca70b1cd4e55baed4fe
Owen Winkler <epithet@gmail.com>
2007-11-23 16:29:38 +0000

    Added system login page.  If the current theme is lacking a login template, the one in system/admin is used.
    Looking for HTML/CSS folks to improve the login page.
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1127 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ admin/login.php
@@ -0,0 +1,36 @@
+<div class="content">
+	<div id="primary">
+		<div id="primarycontent" class="hfeed">
+			<?php
+if ( isset( $error ) ) {
+?>
+<p>That login is incorrect.</p>
+<?php
+}
+if ( isset($user) ) {
+?>
+<p>You are logged in as <?php echo $user->username; ?>.</p>
+<p>Want to <a href="<?php Site::out_url( 'habari' ); ?>/user/logout">log out</a>?</p>
+<?php
+}
+else {
+?>
+<form method="post" action="<?php URL::out( 'user', array( 'page' => 'login' ) ); ?>">
+	<p>
+		<label for="habari_username"><small><strong>Name:</strong></small></label>
+		<input type="text" size="25" name="habari_username" id="habari_username">
+	</p>
+	<p>
+		<label for="habari_password"><small><strong>Password:</strong></small></label>
+		<input type="password" size="25" name="habari_password" id="habari_password">
+	</p>
+	<p>
+		<input type="submit" value="GO!">
+	</p>
+</form>
+<?php
+}
+?>
+		</div>
+	</div>
+</div>
