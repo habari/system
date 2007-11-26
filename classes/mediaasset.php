19290b4ae314e7a0e4f132d5761a94dd61745bc5
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-11-26 05:11:46 +0000

    Media classes.  Pass 1, incomplete functionality, but with code comments.  Please review and help complete.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1151 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ classes/mediaasset.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * MediaAsset represents a file or directory in the media system
+ *
+ */
+
+class MediaAsset
+{
+	protected $path;
+	protected $is_dir;
+	protected $content = null;
+
+	/**
+	 * MediaAsset constructor
+	 *
+	 * @param string $path The path of the asset
+	 * @param boolean $is_dir true if the asset is a directory
+	 */
+	public function __construct($path, $is_dir = false)
+	{
+		$this->path = $path;
+		$this->is_dir = $is_dir;
+	}
+
+	/**
+	 * Return the content of the asset
+	 *
+	 * @return mixed The asset content
+	 */
+	public function get()
+	{
+		if(empty($this->content)) {
+			$this->content = Media::get($this->path);
+		}
+		return $this->content;
+	}
+
+	/**
+	 * Set the content of this asset
+	 *
+	 * @param mixed $content The asset content
+	 */
+	public function set($content)
+	{
+		$this->content = $content;
+	}
+
+	/**
+	 * Get the url where this asset can be viewed
+	 *
+	 * @param array $options Qualities of the URL to return (such as 'thumbnail' or 'size')
+	 * @return string The URL of the asset
+	 */
+	public function url($options = array())
+	{
+		if(empty($this->url)) {
+			$this->url = Media::url($this->path, $options);
+		}
+		return $this->url;
+	}
+
+	/**
+	 * Retrieve attributes about this asset
+	 *
+	 * @param string $name The name of the property to retrieve
+	 * @return mixed The value requested
+	 */
+	public function __get($name)
+	{
+		switch($name) {
+			case 'url':
+				return $this->url();
+			case 'content':
+				return $this->get($content);
+			case 'is_dir':
+				return $this->is_dir;
+			case 'thumbnail_url':
+				return $this->url(array('size'=>'thumbnail'));
+		}
+	}
+
+}
+
+?>
---
+++ classes/mediasilo.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * MediaSilo interface
+ *
+ * @version $Id$
+ * @copyright 2007
+ */
+
+interface MediaSilo
+{
+	const PERM_READ = 1;
+	const PERM_WRITE = 2;
+
+	/**
+	 * Return basic information about this silo
+	 *   name- The name of the silo, used as the root directory for media in this silo
+	 **/
+	public function silo_info();
+	
+	/**
+	 * Return directory contents for the silo path
+	 * @param string $path The path to retrieve the contents of
+	 * @return array An array of MediaAssets describing the contents of the directory
+	 **/
+	public function silo_dir( $path );
+	
+	/**
+	 * Get the file from the specified path
+	 * @param string $path The path of the file to retrieve
+	 * @param array $qualities Qualities that specify the version of the file to retrieve.
+	 * @return MediaAsset The requested asset
+	 **/
+	public function silo_get( $path, $qualities = null );
+	
+	/**
+	 * Get the direct URL of the file of the specified path
+	 * @param string $path The path of the file to retrieve
+	 * @param array $qualities Qualities that specify the version of the file to retrieve.
+	 * @return string The requested url
+	 **/
+	public function silo_url( $path, $qualities = null );
+
+	/**
+	 * Create a new asset instance for the specified path
+	 * @param string $path The path of the new file to create
+	 * @return MediaAsset The requested asset
+	 **/
+	public function silo_new( $path );
+
+	/**
+	 * Store the specified media at the specified path
+	 * @param string $path The path of the file to retrieve
+	 * @param MediaAsset The asset to store
+	 **/  
+	public function silo_put( $path, $filedata );
+	
+	/**
+	 * Delete the file at the specified path
+	 * @param string $path The path of the file to retrieve
+	 **/ 
+	public function silo_delete( $path );
+	
+	/**
+	 * Retrieve a set of highlights from this silo
+	 * This would include things like recently uploaded assets, or top downloads
+	 * @return array An array of MediaAssets to highlihgt from this silo
+	 **/
+	public function silo_highlights();
+	
+	/**
+	 * Retrieve the permissions for the current user to access the specified path
+	 * @param string $path The path to retrieve permissions for
+	 * @return array An array of permissions constants (MediaSilo::PERM_READ, MediaSilo::PERM_WRITE)
+	 **/
+	public function silo_permissions( $path );
+}
+
+?>
