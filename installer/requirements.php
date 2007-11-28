4f9e47ff2d9d1f037de2f39e70d001b04e44f710
Rich Bowen <rbowen@contrib.habariproject.org>
2007-11-28 00:24:11 +0000

    I suppose it's possible that this is *too* verbose, but at least it is more correct. If someone wishes to wordsmith it a little, you're welcome to it.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1153 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- installer/requirements.php
+++ installer/requirements.php
@@ -17,9 +17,16 @@
           <h2>Writeable directory needed...</h2>
           <?php if ($PHP_OS != 'WIN') {?>
             <p class="instructions">
-              Before you can install habari, you first need to make the install directory
-              "writeable", so that the installation script can write your configuration 
-              information properly. This is a simple process, outlined in the steps below.
+              Before you can install habari, you first need to make the install
+              directory writeable by php, so that the installation script can 
+              write your configuration information properly. The exact proces of
+              doing this will vary depending on the configuration of your web 
+              server and the ownership of the directory.
+            </p>
+            <p>
+              If your webserver is part of the group which owns the
+              directory, you'll need to add group write permissions to
+              the directory. The procedure for this is as follows:
             </p>
             <ol>
               <li>
@@ -28,13 +35,30 @@
               </li>
               <li>
               Change the <em>mode</em> (permissions) of the current directory:
-                <pre><strong>$></strong> chmod g+w .</pre>
+                <pre><strong>$></strong> chmod g+w .</pre><br />
+                <pre><strong>$></strong> chmod g+x .</pre>
                 <p class="note">
                   <em>Note</em>: You may need to use <strong>sudo</strong> and enter
-                  an administrator password on certain distributions.
+                  an administrator password if you do not own the
+                  directory.
                 </p>
               </li>
             </ol>
+            <p>
+              If the webserver is not part of the group which owns the
+              directory, you will need to <strong>temporarily</strong>
+              grant world write permissions to the directory:
+            </p>
+            <ol>
+            <li>
+                <pre><strong>$></strong> chmod o+w .</pre><br />
+                <pre><strong>$></strong> chmod o+x .</pre>
+            </li>
+            </ol>
+            <p>
+              <strong>Be sure to remove the write permissions on the directory
+              as soon as the installation is completed.</strong>
+            </p>
           <?php } else {?>
             <strong>@todo Windows instructions</strong>
           <?php }?>
