d3d20b38a81dddc0be05120f6c40fd47ce27e6a5
Owen Winkler <ringmaster@contrib.habariproject.org>
2007-11-30 03:58:38 +0000

    Added some array-based criteria to post selection.  You can now provide arrays to some of the Posts::get() calls, like Posts::get(array('tag'=>array('tag1', 'tag2'))) to select posts from either tag.
    Providing an array of posts as the 'posts' array element to the theme's act_display() will prevent the default Posts::get() call from executing and instead use the provided posts.  For example, from inside your theme's custom theme class, you'd create a function act_display_home(), and in there call:  return $this->act_display( array( 'posts' => Posts::get(...) ) );  This allows you to do whatever crazy specific queries you need inside of your theme functions without having to pass criteria on to act_display().  It also makes the prior feature easier to test.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1159 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/theme.php
+++ classes/theme.php
@@ -4,9 +4,9 @@
  *
  * @package Habari
  *
- * The Theme class is the behind-the-scenes representation of 
+ * The Theme class is the behind-the-scenes representation of
  * of a set of UI files that compose the visual theme of the blog
- *   
+ *
  */
 class Theme
 {
@@ -15,10 +15,10 @@ class Theme
 	public $template_engine= null;
 	public $theme_dir= null;
 	public $config_vars= array();
-	
+
 	/**
 	 * We build the Post filters by analyzing the handler_var
-		* data which is assigned to the handler ( by the Controller and 
+		* data which is assigned to the handler ( by the Controller and
 		* also, optionally, by the Theme )
 		*/
 	public $valid_filters= array(
@@ -36,19 +36,19 @@ class Theme
 		'nolimit',
 		'fetch_fn',
 	);
-	
+
 	/**
 	 * Constructor for theme
-	 * 
-	 * If no parameter is supplied, then the constructor 
+	 *
+	 * If no parameter is supplied, then the constructor
 	 * Loads the active theme from the database.
-	 * 
+	 *
 	 * If no theme option is set, a fatal error is thrown
-	 * 
+	 *
 	 * @param name            ( optional ) override the default theme lookup
 	 * @param template_engine ( optional ) specify a template engine
 	 * @param theme_dir       ( optional ) specify a theme directory
-	 */	 	 	 	 	
+	 */
 	public function __construct($themedata)
 	{
 		$this->name= $themedata->name;
@@ -62,7 +62,7 @@ class Theme
 	/**
 	 * Loads a theme's metadata from an XML file in theme's
 	 * directory.
-	 * 
+	 *
 	 * @param theme Name of theme to retrieve metadata about
 	 */
 	public function info( $theme )
@@ -81,20 +81,20 @@ class Theme
 				// Assigns based on wether or not it's a valid named element.
 				foreach ( $theme_data->theme->children() as $key => $value ) {
 					$key= strtolower( $key );
-					if ( in_array( $key, $valid_named_elements ) ) { 
+					if ( in_array( $key, $valid_named_elements ) ) {
 						$this->$key= $value;
 					}
-					else { 
+					else {
 						$this->config_vars[$key]= $value;
 					}
 				}
 			}
 		}
-	} 
+	}
 
 	/**
 	 * Assign the default variables that would be used in every template
-	 */	
+	 */
 	public function add_template_vars()
 	{
 		$handler= Controller::get_handler();
@@ -102,7 +102,7 @@ class Theme
 			Plugins::act('add_template_vars', $this, Controller::get_handler()->handler_vars);
 		}
 	}
-	
+
 	/**
 	 * Find the first template that matches from the list provided and display it
 	 * @param array $template_list The list of templates to search for
@@ -117,15 +117,15 @@ class Theme
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Grabs post data and inserts that data into the internal
-	 * handler_vars array, which eventually gets extracted into 
+	 * handler_vars array, which eventually gets extracted into
 	 * the theme's ( and thereby the template_engine's ) local
 	 * symbol table for use in the theme's templates
-	 * 
-	 * This is the default, generic function to grab posts.  To 
-	 * "filter" the posts retrieved, simply pass any filters to 
+	 *
+	 * This is the default, generic function to grab posts.  To
+	 * "filter" the posts retrieved, simply pass any filters to
 	 * the handler_vars variables associated with the post retrieval.
 	 * For instance, to filter by tag, ensure that handler_vars['tag']
 	 * contains the tag to filter by.  Simple as that.
@@ -151,12 +151,14 @@ class Theme
 			$where_filters['status']= Post::status('published');
 		}
 
-		$user_filters= Plugins::filter( 'template_user_filters', $user_filters );
-		$user_filters= array_intersect_key( $user_filters, array_flip( $this->valid_filters ) );
-		$where_filters= array_merge( $where_filters, $user_filters );
+		if( !isset( $posts ) ) {
+			$user_filters= Plugins::filter( 'template_user_filters', $user_filters );
+			$user_filters= array_intersect_key( $user_filters, array_flip( $this->valid_filters ) );
+			$where_filters= array_merge( $where_filters, $user_filters );
 
-		$posts= Posts::get( $where_filters );
-		
+			$posts= Posts::get( $where_filters );
+		}
+
 		$this->assign( 'posts', $posts );
 		$this->assign( 'page', isset( $page ) ? $page : 1 );
 
@@ -164,13 +166,13 @@ class Theme
 			$post= ( count( $posts ) > 1 ) ? $posts[0] : $posts;
 			$this->assign( 'post', $post );
 			$types= array_flip( Post::list_active_post_types() );
-			$type= $types[$post->content_type]; 
+			$type= $types[$post->content_type];
 		}
 		elseif( $posts === false ) {
 			$fallback= array('404');
 			header( 'HTTP/1.0 404 Not Found' );
 		}
-		
+
 		extract( $where_filters );
 
 		if ( !isset( $fallback ) ) {
@@ -178,23 +180,23 @@ class Theme
 			$fallback= array( '{$type}.{$id}', '{$type}.{$slug}', '{$type}.tag.{$posttag}' );
 			if( count( $posts ) > 1 ) {
 				$fallback[]= '{$type}.multiple';
-				$fallback[]= 'multiple'; 
+				$fallback[]= 'multiple';
 			}
 			else {
 				$fallback[]= '{$type}.single';
-				$fallback[]= 'single'; 
+				$fallback[]= 'single';
 			}
 			$fallback[]= 'home';
 		}
-		
+
 		$searches= array('{$id}','{$slug}','{$year}','{$month}','{$day}','{$type}','{$tag}',);
 		$replacements= array(
 			(isset($post) && $post instanceof Post)?$post->id:'-',
-			(isset($post) && $post instanceof Post)?$post->slug:'-',  
-			isset($year)?$year:'-', 
-			isset($month)?$month:'-', 
-			isset($day)?$day:'-', 
-			isset($type)?$type:'-', 
+			(isset($post) && $post instanceof Post)?$post->slug:'-',
+			isset($year)?$year:'-',
+			isset($month)?$month:'-',
+			isset($day)?$day:'-',
+			isset($type)?$type:'-',
 			isset($tag)?$tag:'-',
 		);
 		$fallback= Plugins::filter( 'template_fallback', $fallback );
@@ -216,42 +218,42 @@ class Theme
 
 		return $this->display_fallback( $fallback );
 	}
-		
+
 	/**
 	 * Helper function: Displays the home page
 	 * @param array $user_filters Additional arguments used to get the page content
-	 */	 	 	
+	 */
 	public function act_display_home( $user_filters= array() )
-	{	
+	{
 		$paramarray['fallback']= array(
 			'home',
 			'multiple',
 		);
-		
+
 		// Makes sure home displays only entries
 		$default_filters= array(
 		 'content_type' => Post::type('entry'),
 		);
-		
+
 		$paramarray['user_filters']= array_merge( $default_filters, $user_filters );
-				
+
 		return $this->act_display( $paramarray );
 	}
-	
+
 	/**
 	 * Helper function: Display a post
 	 * @param array $user_filters Additional arguments used to get the page content
-	 */	 	 	
+	 */
 	public function act_display_post( $user_filters= array() )
 	{
 		$paramarray['fallback']= array(
-		 '{$type}.{$id}', 
+		 '{$type}.{$id}',
 		 '{$type}.{$slug}',
-		 '{$type}.tag.{$posttag}', 
-		 '{$type}.single', 
+		 '{$type}.tag.{$posttag}',
+		 '{$type}.single',
 		 '{$type}.multiple',
 		 'single',
-		 'multiple', 
+		 'multiple',
 		 'home',
 		);
 
@@ -264,30 +266,30 @@ class Theme
 		// Remove the page from filters.
 		$page_key= array_search( 'page', $this->valid_filters );
 		unset( $this->valid_filters[$page_key] );
-		
+
 		$paramarray['user_filters']= array_merge( $default_filters, $user_filters );
-		
+
 		return $this->act_display( $paramarray );
 	}
 
 	/**
 	 * Helper function: Display the posts for a tag
 	 * @param array $user_filters Additional arguments used to get the page content
-	 */	 	 	
+	 */
 	public function act_display_tag( $user_filters= array() )
-	{	
+	{
 		$paramarray['fallback']= array(
 			'tag.{$tag}',
 			'tag',
-			'multiple', 
+			'multiple',
 			'home',
 		);
-		
+
 		// Makes sure home displays only entries
 		$default_filters= array(
 		 'content_type' => Post::type('entry'),
 		);
-		
+
 		$paramarray['user_filters']= array_merge( $default_filters, $user_filters );
 
 		return $this->act_display( $paramarray );
@@ -296,14 +298,14 @@ class Theme
 	/**
 	 * Helper function: Display the posts for a specific date
 	 * @param array $user_filters Additional arguments used to get the page content
-	 */	 	 	
+	 */
 	public function act_display_date( $user_filters= array() )
 	{
 		$handler_vars= Controller::get_handler()->handler_vars;
 		$y = isset( $handler_vars['year'] );
 		$m = isset( $handler_vars['month'] );
 		$d = isset( $handler_vars['day'] );
-		
+
 		if($y&&$m&&$d) $paramarray['fallback'][]= 'year.{$year}.month.{$month}.day.{$day}';
 		if($y&&$m&&$d) $paramarray['fallback'][]= 'year.month.day';
 		if($m&&$d) $paramarray['fallback'][]= 'month.{$month}.day.{$day}';
@@ -318,9 +320,9 @@ class Theme
 		if($y) $paramarray['fallback'][]= 'year';
 		if($m) $paramarray['fallback'][]= 'month';
 		if($d) $paramarray['fallback'][]= 'day';
-		$paramarray['fallback'][]= 'multiple'; 
+		$paramarray['fallback'][]= 'multiple';
 		$paramarray['fallback'][]= 'home';
-				
+
 		$paramarray['user_filters']= $user_filters;
 		if ( !isset( $paramarray['user_filters']['content_type'] ) ) {
 			$paramarray['user_filters']['content_type']= 'entry';
@@ -328,7 +330,7 @@ class Theme
 
 		return $this->act_display( $paramarray );
 	}
-	
+
 	/**
 		* Helper function: Display the posts for a specific criteria
 		* @param array $user_filters Additional arguments used to get the page content
@@ -340,15 +342,15 @@ class Theme
 			'multiple',
 			'home',
 		);
-		
+
 		$paramarray['user_filters']= $user_filters;
 
 		return $this->act_display( $paramarray );
 	}
-	
+
 	/**
 	 * Helper function: Display a 404 template
-	 * 
+	 *
 	 * @param array $user_filters Additional arguments user to get the page content
 	 */
 	public function act_display_404( $user_filters= array() )
@@ -360,7 +362,7 @@ class Theme
 		$paramarray['user_filters']= $user_filters;
 		return $this->act_display( $paramarray );
 	}
-	
+
 	/**
 	 * Helper function: Avoids having to call $theme->template_engine->display( 'template_name' );
 	 * @param string $template_name The name of the template to display
@@ -374,7 +376,7 @@ class Theme
 				$this->assign( $key, $value );
 			}
 		}
-		
+
 		$this->template_engine->display( $template_name );
 	}
 
@@ -385,7 +387,7 @@ class Theme
 	{
 		$this->template_engine->$key= $value;
 	}
- 	
+
  	/**
 	 * Aggregates and echos the additional header code by combining Plugins and Stack calls.
 	 */
@@ -400,7 +402,7 @@ class Theme
 		Stack::out( 'template_stylesheet', '<link rel="stylesheet" type="text/css" href="%s" media="%s">'."\r\n" );
 		Stack::out( 'template_header_javascript', '<script src="%s" type="text/javascript"></script>'."\r\n" );
 	}
-	
+
 	/**
 	 * Aggregates and echos the additional footer code by combining Plugins and Stack calls.
 	 */
@@ -414,11 +416,11 @@ class Theme
 		}
 		Stack::out( 'template_footer_javascript', ' <script src="%s" type="text/javascript"></script>'."\r\n" );
 	}
-	
-	/** 
-	 * Detects if a variable is assigned to the template engine for use in 
+
+	/**
+	 * Detects if a variable is assigned to the template engine for use in
 	 * constructing the template's output.
-	 * 
+	 *
 	 * @param key name of variable
 	 * @returns boolean true if name is set, false if not set
 	 */
@@ -426,21 +428,21 @@ class Theme
 	{
 		return isset( $this->template_engine->$key );
 	}
-	
+
 	/**
 	 * Set a template variable, a property alias for assign()
-	 * 
+	 *
 	 * @param string $key The template variable to set
-	 * @param mixed $value The value of the variable 
+	 * @param mixed $value The value of the variable
 	 */
 	public function __set( $key, $value )
 	{
 		$this->assign( $key, $value );
 	}
-	
+
 	/**
 	 * Get a template variable value
-	 * 
+	 *
 	 * @param string $key The template variable name to get
 	 * @return mixed The value of the variable
 	 */
@@ -450,12 +452,12 @@ class Theme
 	}
 
 	/**
-	 * Handle methods called on this class or its descendants that are not defined by this class.	
+	 * Handle methods called on this class or its descendants that are not defined by this class.
 	 * Allow plugins to provide additional theme actions, like a custom act_display_*()
-	 * 
+	 *
 	 * @param string $function The method that was called.
 	 * @param array $params An array of parameters passed to the method
-	 **/	 	 
+	 **/
 	public function __call( $function, $params )
 	{
 		if(strpos($function, 'act_') === 0) {
