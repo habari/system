6a25dcc9177b5a5807efdca8502a87a0b1141a4b
Owen Winkler <epithet@gmail.com>
2007-12-11 05:33:39 +0000

    Log theme exceptions to the log and don't display them to the public site.
    Creating log entries against modules and types that don't exist cause the module and type to be created.
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1193 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/userthemehandler.php
+++ classes/userthemehandler.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Class for handling default user theme actions.
- * 
+ *
  * @note Any theme will be able to override these default actions by registering an override function with Theme->act_some_function()
  */
 class UserThemeHandler extends ActionHandler
@@ -9,8 +9,8 @@ class UserThemeHandler extends ActionHandler
 	private $theme= null;
 
 	/**
-	 * Constructor for the default theme handler.  Here, we 
-	 * automatically load the active theme for the installation, 
+	 * Constructor for the default theme handler.  Here, we
+	 * automatically load the active theme for the installation,
 	 * and create a new Theme instance.
 	 */
 	public function __construct()
@@ -22,40 +22,50 @@ class UserThemeHandler extends ActionHandler
 	 * The UserThemeHandler's act() method differs from ActionHandler's
 	 * act() method in one distinct way: if the Handler's theme variable
 	 * registers an override action via Theme->register_action(), then
-	 * that function is called instead of the default handler action. 
+	 * that function is called instead of the default handler action.
 	 *
 	 * @param   action  the action that was in the URL rule
 	 * @return  bool    did the action succeed?
-	 */	 	 	 	 
+	 */
 	public function act( $action )
 	{
 		$this->action= $action;
-		
+
 		$action_method= 'act_' . $action;
 		$before_action_method= 'before_' . $action_method;
 		$after_action_method= 'after_' . $action_method;
-		
+
 		$this->theme->$before_action_method();
-		$this->theme->$action_method();
+		try {
+			$this->theme->$action_method();
+		}
+		catch(exception $e) {
+			EventLog::log($e->getMessage() . ' in ' . $e->getFile() . ' line ' . $e->getLine() , 'error', 'theme', 'habari', print_r($e, 1) );
+		}
 		$this->theme->$after_action_method();
 	}
 
 	/**
 	 * Helper function which automatically assigns all handler_vars
 	 * into the theme and displays a theme template
-	 * 
+	 *
 	 * @param template_name Name of template to display ( note: not the filename )
 	 */
 	protected function display( $template_name )
 	{
-		/* 
+		/*
 		 * Assign internal variables into the theme ( and therefore into the theme's template
 		 * engine.  See Theme::assign().
 		 */
 		foreach ( $this->handler_vars as $key => $value ) {
 			$this->theme->assign( $key, $value );
 		}
-		$this->theme->display( $template_name );
+		try {
+			$this->theme->display( $template_name );
+		}
+		catch(exception $e) {
+			EventLog::log($e->getMessage() . ' in ' . $e->getFile() . ' line ' . $e->getLine() , 'error', 'theme', 'habari', print_r($e, 1) );
+		}
 	}
 }
 
