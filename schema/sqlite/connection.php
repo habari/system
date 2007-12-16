d4799c6c7bc7f34118fe07d28526415a4bbceabe
Owen Winkler <epithet@gmail.com>
2007-12-16 17:38:49 +0000

    Added two plugin hooks, 'query' and 'query_postprocess' that allow you to alter a query before it is executed.  Also included sql_t() in the translation chain, and added a filter_tables() method to DatabaseConnection so that queries can reference table names with braces (like: "SELECT * FROM {tablename}") instead of calling unwieldy DB::table('tablename').  Fixes #17.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1211 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/sqlite/connection.php
+++ schema/sqlite/connection.php
@@ -5,25 +5,26 @@
  * @package Habari
  */
 
-class SQLiteConnection extends DatabaseConnection 
+class SQLiteConnection extends DatabaseConnection
 {
-	 /** 
-	 * database specific SQL translation function, loosely modelled on the 
+	 /**
+	 * database specific SQL translation function, loosely modelled on the
 	 * internationalization _t()  function
 	 * Call with a database independent SQL string and it will be translated
 	 * to a SQLite specific SQL string
-	 * 
+	 *
 	 * @param sql database independent SQL
 	 * @return  string	translated SQL string
 	 */
-	function sql_t( $sql ) 
+	function sql_t( $sql )
 	{
+		return $sql;
 	}
-	
+
 	/**
 	 * Connect to SQLite
 	 * Overrides the DatabaseConnection to return false if the SQLite file doesn't exist.
-	 * 	 
+	 *
 	 * @param connection_string string a PDO connection string
 	 * @param db_user string the database user name
 	 * @param db_pass string the database user password
@@ -33,9 +34,9 @@ class SQLiteConnection extends DatabaseConnection
 	{
 		list($type, $file) = explode(':', $connect_string, 2);
 		return parent::connect( $connect_string, $db_user, $db_pass );
-	}	 	 	 	
+	}
 
-		/** 
+		/**
 	 * automatic diffing function - used for determining required database upgrades
 	 * based on Owen Winkler's microwiki upgrade function
 	 *
@@ -45,7 +46,7 @@ class SQLiteConnection extends DatabaseConnection
 	 * @return  string			translated SQL string
 	 *** FIXME: SQLite diffing is horribly terribly broken. There is varying support for alter table and mucking with columns
 	 */
-	function dbdelta( $queries, $execute = true, $silent = true, $doinserts= false ) 
+	function dbdelta( $queries, $execute = true, $silent = true, $doinserts= false )
 	{
 		if( !is_array($queries) ) {
 			$queries = explode( ';', $queries );
@@ -71,7 +72,7 @@ class SQLiteConnection extends DatabaseConnection
 				// Unrecognized query type
 			}
 		}
-		
+
 		if( $execute ) {
 			$allqueries = $cqueries;
 			if( $doinserts ) {
