9922f96db40e0b84472f007e14bc47a6d77f64ae
Michael Bishop <miklb@contrib.habariproject.org>
2008-01-05 21:12:17 +0000

    changing links to manual in admin
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1245 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/footer.php
+++ admin/footer.php
@@ -1,6 +1,6 @@
 
 <div id="footer">
-	<p class="left"><a href="<?php Site::out_url( 'habari' ); ?>/manual/index.html" onclick="popUp(this.href); return false;" title="Read the user manual">Manual</a> - 
+	<p class="left"><a href="<?php Site::out_url( 'habari' ); ?>/doc/manual/index.html" onclick="popUp(this.href); return false;" title="Read the user manual">Manual</a> - 
 		<a href="http://wiki.habariproject.org/" title="Read the Habari wiki">Wiki</a> - 
 		<a href="http://groups.google.com/group/habari-users" title="Ask the community">Mailing List</a>
 	</p>
