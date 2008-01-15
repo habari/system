b7bee2787f071a2274e2b75c8bac8e0b1ed0a8da
Michael Bishop <miklb@contrib.habariproject.org>
2008-01-15 17:21:49 +0000

    adding conditional to note which theme is active and fix float when multiple themes exist
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1285 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/themes.php
+++ admin/themes.php
@@ -9,17 +9,21 @@
 	<div class="column prepend-1 span-22 append-1">
 		<?php foreach( $all_themes as $theme ) : ?>
 			<div class="screenshot">
-				<img src="<?php echo $theme['screenshot']; ?>" width="200" height="150" /><br />
-				<b><?php echo $theme['info']->name; ?> <?php echo $theme['info']->version; ?></b><br />
+				<img src="<?php echo $theme['screenshot']; ?>" width="200" height="150" /><br>
+				<b><?php echo $theme['info']->name; ?> <?php echo $theme['info']->version; ?></b><br>
 				 by <a href="<?php echo $theme['info']->url; ?>"><?php echo $theme['info']->author; ?></a>
 
-				<?php if ( $theme['dir'] != $active_theme ) : ?>
+				<?php if ( $theme['dir'] != $active_theme ) { ?>
 				<form method='post' action='<?php URL::out('admin', 'page=activate_theme'); ?>'>
 				<input type='hidden' name='theme_name' value='<?php echo $theme['info']->name; ?>'>
 				<input type='hidden' name='theme_dir' value='<?php echo $theme['dir']; ?>'>
 				<input type='submit' name='submit' value='activate'>
 				</form>	
-				<?php endif; ?>
+				<?php } else {
+					echo "<br><strong>Currently Active Theme</strong><br><br>";
+				  }
+				 ?>
+				
 			</div>
 		<?php endforeach; ?>
 	</div>
