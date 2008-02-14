3dc0f2a6932d11bd7e4463be7e5615396bd0a619
Michael Bishop <miklb@contrib.habariproject.org>
2008-02-14 19:07:15 +0000

    Separate active and inactive plugins in admin - props to itrebal.  Closes #143
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1349 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/plugins.php
+++ admin/plugins.php
@@ -7,6 +7,7 @@
 		<p class="center">Activate, deactivate and configure plugins through this interface.</p>
 	</div>
 	<div class="column prepend-2 span-20 append-2">
+		<h3>Active Plugins</h3>
 		<table cellspacing="0" width="100%">
 			<thead>
 				<tr>
@@ -19,7 +20,8 @@
 			</thead>
 			<tbody>
 			<?php
-			foreach($plugins as $plugin) {
+			foreach($active_plugins as $plugin) {
+
 				if($plugin['debug']) {
 				?>
 					<tr>
@@ -82,6 +84,61 @@
 			<?php } ?>
 			</tbody>
 		</table>
+		<h3>Inactive Plugins</h3>
+        <table cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th align="left">Plugin Name</th>
+                    <th align="left">Author Name</th>
+                    <th align="left">Version</th>
+                    <th align="left">Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach($inactive_plugins as $plugin) {
+                
+                if($plugin['debug']) {
+                ?>  
+                    <tr>
+                        <td colspan="3" class="error"><p>The plugin file '<?php echo $plugin['file']; ?>' had syntax errors and could not load.</p>
+                        <div style="display:none;" id="error_<?php echo $plugin['plugin_id']; ?>"><?php echo $plugin['error']; ?></div>
+                        </td>
+                        <td><button onclick="$('#error_<?php echo $plugin['plugin_id']; ?>').show();">Show Error</button></td>
+                    </tr>
+                <?php
+                }   
+                else {
+                ?>  
+                    <tr id="plugin_<?php echo $plugin['plugin_id']; ?>">
+                        <td><?php echo '<a href="' . $plugin['info']->url . '">' . $plugin['info']->name . '</a>'; ?>
+                        </td>
+                        <td><?php echo empty( $plugin['info']->authorurl ) ? $plugin['info']->author : '<a href="' . $plugin['info']->authorurl . '">' . $plugin['info']->author . '</a>'; ?></td>         
+                        <td><?php echo $plugin['info']->version; ?></td>
+                        <td><?php echo $plugin['info']->description; ?></td>
+                        <td>
+                        <form method='POST' action='<?php URL::out( 'admin', 'page=plugin_toggle' ); ?>'>
+                        <p><input type='hidden' name='plugin' value='<?php echo $plugin['file']; ?>'>
+                        <input type='hidden' name='action' value='Activate'>
+                        <button name='submit' type='submit'><?php echo $plugin['verb']; ?></button>
+                        </p>
+                        </form>
+                        </td>
+                    </tr>
+                    <?php if ( isset( $this->engine_vars['configure'] ) && ( $configure == $plugin['plugin_id'] ) ) { ?>
+                    </tbody></table></div></div>
+                    <div class="wrapper">
+                    <div class="container">
+                    <div class="column prepend-1 span-22 append-1">
+                    <table cellspacing="0" width="100%"><tbody>
+                    <?php } ?>
+                <?php } ?>
+            <?php } ?>
+            </tbody>
+        </table>
+
+
 	</div>
 </div>
 <?php include( 'footer.php' ); ?>
