05c9692a2457d7d828f46908d16f1e982a2d2184
ringmaster <epithet@gmail.com>
2008-02-23 04:36:06 +0000

    Changes to the Version class to include comments, the svn revision number, and a function is_devel() that returns true if the running code was checked out from the trunk or a branch.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1375 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/version.php
+++ classes/version.php
@@ -15,30 +15,66 @@ class Version
 	const API_VERSION = 1366;
 
 	const HABARI_VERSION = '0.5-alpha';
-	
-	// Experimental detection of whether this checkout was from trunk or a tag/branch
-	const HABARI_SVN_HEAD_URL = '$HeadURL$';
 
+	// This string contains the URL to the Habari SVN repository used for this working copy or export
+	const HABARI_SVN_HEAD_URL = '$HeadURL$';
+	// This string contains the SVN revision used for this working copy or export
+	const HABARI_SVN_REV = '$Revision$';
+
+	/**
+	 * Get the database version
+	 *
+	 * @return integer The revision in which the most recent database change took place
+	 */
 	public static function get_dbversion()
 	{
 		return Version::DB_VERSION;
 	}
 
+	/**
+	 * Get the API version
+	 *
+	 * @return integer The revision in which the most recent API change took place
+	 */
 	public static function get_apiversion()
 	{
 		return Version::API_VERSION;
 	}
 
+	/**
+	 * Get the Habari version
+	 *
+	 * @return string A version_compare()-compatible string of this version of Habari
+	 * @see version_compare
+	 */
 	public static function get_habariversion()
 	{
 		return Version::HABARI_VERSION;
 	}
 
+	/**
+	 * Determine whether this working copy or export was created from a subversion development branch
+	 *
+	 * @return boolean True if this is a development version, false if not
+	 */
+	public static function is_devel()
+	{
+		return strpos(HABARI_SVN_HEAD_URL, '/trunk/') !== false && strpos(HABARI_SVN_HEAD_URL, '/branches/') !== false;
+	}
+
+	/**
+	 * Store the current database version in the options table
+	 */
 	public static function save_dbversion()
 	{
 		Options::set('db_version', Version::DB_VERSION);
 	}
 
+	/**
+	 * Determine if the database needs to be updated based on the source database version being newer than the schema last applied to the database
+	 *
+	 * @return boolean True if an update is needed
+	 */
 	public static function requires_upgrade()
 	{
 		if (Options::get('db_version') < Version::DB_VERSION){
