ae00d45ee72110c79fcfc64e6de93ff5ca9461a7
Rich Bowen <rbowen@contrib.habariproject.org>
2008-02-24 02:55:21 +0000

    Patch submitted by Alan, A Humble User. Previously committed to the wrong repos. I have no witty excuse to offer.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1380 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/k2/footer.php
+++ themes/k2/footer.php
@@ -6,7 +6,7 @@
 <hr>
 
 <p id="footer">
- <small><?php Options::out('title'); _e(' is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a> <?php _e('and'); ?> <a href="http://i-want-a-pony.com/" title="A Pony" rel="nofollow">A Pony</a>.</small><br>
+ <small><?php Options::out('title'); _e(' is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a> <?php _e('and'); ?> <a href="http://en.wikipedia.org/wiki/Earl_Grey_tea" title="A cup of Earl Grey" rel="nofollow">A cup of Earl Grey</a>.</small><br>
  <small><a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>">Atom Entries</a> and <a href="<?php URL::out( 'atom_feed_comments' ); ?>">Atom Comments</a></small>
 </p>
 
