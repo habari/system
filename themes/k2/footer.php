da52a086c925ada7e4d043396260dca8ebba5bbf
Christian Mohn <h0bbel@contrib.habariproject.org>
2008-03-26 20:42:38 +0000

    Forgot <br>. Doh.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1522 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/k2/footer.php
+++ themes/k2/footer.php
@@ -7,7 +7,7 @@
 
 <p id="footer">
  <small><?php Options::out('title'); _e(' is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a> <?php _e('and'); ?> a huge amount of 
- <a href="http://en.wikipedia.org/wiki/Caffeine" title="Caffeine" rel="nofollow">C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub></a></small>
+ <a href="http://en.wikipedia.org/wiki/Caffeine" title="Caffeine" rel="nofollow">C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub></a></small><br>
  <small><a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>">Atom Entries</a> and <a href="<?php URL::out( 'atom_feed_comments' ); ?>">Atom Comments</a></small>
 </p>
 
