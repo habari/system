4c9d5b804d1e40fdacbb1f6c9a581c4d705887e8
Andrew da Silva <freakerz@contrib.habariproject.org>
2008-04-11 01:58:21 +0000

    Add support for "multiple" <select>.
    To enable, use:
    $control= $formui->add( 'select', ... );
    $control->multiple= true;
    
    Default size is 5, to change, change $control->size.
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1550 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_select.php
+++ admin/formcontrols/formcontrol_select.php
@@ -1,14 +1,14 @@
 <div class="<?php echo $class ?>">
-<label><?php echo $this->caption; ?><select name="<?php echo $field ?>">
+<label><?php echo $this->caption; ?><select name="<?php echo $field . ( $multiple ? '[]' : '' ); ?>"<?php echo ( $multiple ? ' multiple="multiple" size="' . intval($size) . '"' : '' ) ?>>
 <?php foreach($options as $opts_key => $opts_val) : ?>
 	<?php if (is_array($opts_val)) : ?>
 		<optgroup label="<?php echo $opts_key; ?>">
 		<?php foreach($opts_val as $opt_key => $opt_val) : ?>
-			<option value="<?php echo $opt_key; ?>" <?php echo ( ( $value == $opt_key ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($opt_val); ?></option>
+			<option value="<?php echo $opt_key; ?>"<?php echo ( in_array( $opt_key, (array) $value ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($opt_val); ?></option>
 		<?php endforeach; ?>
 		</optgroup>
 	<?php else : ?>
-		<option value="<?php echo $opts_key; ?>" <?php echo ( ( $value == $opts_key ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($opts_val); ?></option>
+		<option value="<?php echo $opts_key; ?>"<?php echo ( in_array( $opts_key, (array) $value ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($opts_val); ?></option>
 	<?php endif; ?>
 <?php endforeach; ?>
 </select></label>
