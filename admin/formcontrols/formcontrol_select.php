674aaaf841fee87bc9982e387d3b0564e800481b
Andrew da Silva <freakerz@contrib.habariproject.org>
2008-04-11 01:17:25 +0000

    Add single-level <optgroup> support.
    To use, simply pass a options array looking like:
    array( 'Supported Ratings' => array( 'G', 'R', 'X' ) )
    
    I'ld like to support multi-level, but for now this takes care of at least 99% demands. :)
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1549 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_select.php
+++ admin/formcontrols/formcontrol_select.php
@@ -1,7 +1,15 @@
 <div class="<?php echo $class ?>">
 <label><?php echo $this->caption; ?><select name="<?php echo $field ?>">
-<?php foreach($options as $key => $text) : ?>
-	<option value="<?php echo $key; ?>" <?php echo ( ( $value == $key ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($text); ?></option>
+<?php foreach($options as $opts_key => $opts_val) : ?>
+	<?php if (is_array($opts_val)) : ?>
+		<optgroup label="<?php echo $opts_key; ?>">
+		<?php foreach($opts_val as $opt_key => $opt_val) : ?>
+			<option value="<?php echo $opt_key; ?>" <?php echo ( ( $value == $opt_key ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($opt_val); ?></option>
+		<?php endforeach; ?>
+		</optgroup>
+	<?php else : ?>
+		<option value="<?php echo $opts_key; ?>" <?php echo ( ( $value == $opts_key ) ? ' selected' : '' ); ?>><?php echo htmlspecialchars($opts_val); ?></option>
+	<?php endif; ?>
 <?php endforeach; ?>
 </select></label>
 <?php if($message != '') : ?>
