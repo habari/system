176c4e3336e35347d3ad0bd332375b5a65f7af5e
Rich Bowen <rbowen@contrib.habariproject.org>
2008-04-27 15:30:29 +0000

    Add a note about scheduled posts to the UI. If there's a better way to insert this into the CSS, please go right ahead.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1579 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/publish_settings.php
+++ admin/publish_settings.php
@@ -7,7 +7,7 @@
 				<hr>
 				<div class="container"><p class="column span-5"><?php _e('Comments Allowed'); ?></p> 	<p class="column span-14 last"><input type="checkbox" name="comments_enabled" class="styledformelement" value="1" <?php echo ( $post->info->comments_disabled == 1 ) ? '' : 'checked'; ?>></p></div>
 				<hr>
-				<div class="container"><p class="column span-5"><?php _e('Publication Time'); ?></p>	<p class="column span-14 last"><input type="text" name="pubdate" id="pubdate" class="styledformelement" value="<?php echo $post->pubdate; ?>"></p></div>
+				<div class="container"><p class="column span-5"><?php _e('Publication Time'); ?></p>	<p class="column span-14 last"><input type="text" name="pubdate" id="pubdate" class="styledformelement" value="<?php echo $post->pubdate; ?>"> <em>To schedule a post, enter a future date</em></p></div>
 				<hr>
 				<div class="container"><p class="column span-5"><?php _e('Content Address'); ?></p>		<p class="column span-14 last"><input type="text" name="newslug" id="newslug" class="styledformelement" value="<?php echo $post->slug; ?>"></p></div>
 
