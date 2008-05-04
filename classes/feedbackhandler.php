75315bb9d24c25b21ca7630d25bf78c894282af6
Michael C. Harris <michaeltwofish@contrib.habariproject.org>
2008-05-04 12:36:58 +0000

    If a comment isn't saved don't set a session message saying the comment is in moderation.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1624 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/feedbackhandler.php
+++ classes/feedbackhandler.php
@@ -27,7 +27,7 @@ class FeedbackHandler extends ActionHandler
 		if ( ( 1 == Options::get( 'comments_require_id' ) ) && ( empty( $this->handler_vars['name'] ) || empty( $this->handler_vars['email'] ) ) ) {
 			Session::error(_t( 'Both name and e-mail address must be provided.' ) );
 		}
-		
+
 		if ( empty( $this->handler_vars['content'] ) ) {
 			Session::error( _t('You did not provide any content for your comment!') );
 		}
@@ -84,9 +84,9 @@ class FeedbackHandler extends ActionHandler
 		}
 		$cleaned_content= preg_replace( '/^\s+/', '', $this->handler_vars['content'] );
 		if ( $cleaned_content === '' ) {
-		    Session::error( _t( 'Comment contains only whitespace/empty comment' ) );
+			Session::error( _t( 'Comment contains only whitespace/empty comment' ) );
 			Utils::redirect( $post->permalink );
-	        exit();
+			exit();
 		}
 
 		/* Create comment object*/
@@ -100,7 +100,7 @@ class FeedbackHandler extends ActionHandler
 			'status' =>	Comment::STATUS_UNAPPROVED,
 			'date' =>	date( 'Y-m-d H:i:s' ),
 			'type' => Comment::COMMENT,
-	 	) );
+		) );
 
 		// Should this really be here or in a default filter?
 		// In any case, we should let plugins modify the status after we set it here.
@@ -115,33 +115,33 @@ class FeedbackHandler extends ActionHandler
 		$spam_rating= Plugins::filter('spam_filter', $spam_rating, $comment, $this->handler_vars);
 
 		$comment->insert();
+		$anchor= '';
+
+		// If the comment was saved
 		if ( $comment->id ) {
 			$anchor= '#comment-' . $comment->id;
-		} else {
-			$anchor= '';
-		}
 
-		// store in the user's session that this comment is pending moderation
-		if ( $comment->status == Comment::STATUS_UNAPPROVED ) {
-			Session::notice(_t('Your comment is pending moderation.'), 'comment_' . $comment->id );
-		}
+			// store in the user's session that this comment is pending moderation
+			if ( $comment->status == Comment::STATUS_UNAPPROVED ) {
+				Session::notice(_t('Your comment is pending moderation.'), 'comment_' . $comment->id );
+			}
 
-		// if no cookie exists, we should set one
-		// but only if the user provided some details
-		// and the comment was actually saved
-		$cookie= 'comment_' . Options::get('GUID');
-		if ( ( ! User::identify() )
-			&& ( $comment->id )
-			&& ( ! isset( $_COOKIE[$cookie] ) )
-			&& ( ! empty( $this->handler_vars['name'] )
-				|| ! empty( $this->handler_vars['email'] )
-				|| ! empty( $this->handler_vars['url'] )
+			// if no cookie exists, we should set one
+			// but only if the user provided some details
+			$cookie= 'comment_' . Options::get('GUID');
+			if ( ( ! User::identify() )
+				&& ( ! isset( $_COOKIE[$cookie] ) )
+				&& ( ! empty( $this->handler_vars['name'] )
+					|| ! empty( $this->handler_vars['email'] )
+					|| ! empty( $this->handler_vars['url'] )
+				)
 			)
-		)
-		{
-			$cookie_content = $comment->name . '#' . $comment->email . '#' . $comment->url;
-			$site_url= Site::get_path('base',true);
-			setcookie( $cookie, $cookie_content, time() + 31536000, $site_url );
+			{
+				Session::notice(_t('Setting the cookie.'), 'comment_' . $comment->id );
+				$cookie_content = $comment->name . '#' . $comment->email . '#' . $comment->url;
+				$site_url= Site::get_path('base',true);
+				setcookie( $cookie, $cookie_content, time() + 31536000, $site_url );
+			}
 		}
 
 		// Return the commenter to the original page.
