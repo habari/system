89cafc571815d0ba481135c1918889aae454eb61
Michael Bishop <miklb@contrib.habariproject.org>
2008-05-05 03:18:02 +0000

    adding charset - props rick_c
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1639 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/header.php
+++ admin/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title><?php Options::out('title'); ?> : <?php echo $admin_page; ?></title>
-
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 	<link rel="stylesheet" href="<?php Site::out_url('habari'); ?>/3rdparty/blueprint/screen.css" type="text/css" media="screen">
 	<link rel="stylesheet" href="<?php Site::out_url('habari'); ?>/3rdparty/blueprint/print.css" type="text/css" media="print">
 	<link rel="stylesheet" href="<?php Site::out_url('admin_theme'); ?>/css/admin.css" type="text/css" media="screen">
