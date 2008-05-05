15a13af14e09ba5b9c905a2b1a27f6a63c7637ae
Michael C. Harris <michaeltwofish@contrib.habariproject.org>
2008-05-05 10:58:12 +0000

    First pass at tags support. Needs further JS work. Thanks dmondark.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1650 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/tags.php
+++ classes/tags.php
@@ -8,7 +8,7 @@
 class Tags extends ArrayObject
 {
     public static function get() {
-		$tags= DB::get_results( 'SELECT tag_text as tag FROM ' . DB::table('tags') . ' ORDER BY tag_text ASC' );
+		$tags= DB::get_results( 'SELECT t.tag_text AS tag, t.tag_slug AS slug , COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id GROUP BY t.tag_slug ORDER BY tag_text ASC' );
 		return $tags;
 	}
 }
