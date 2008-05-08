9d91848f337fc528a7eb5bcdea2180914e5220b9
ringmaster <epithet@gmail.com>
2008-05-08 05:56:27 +0000

    Display proper recent comments. Obtain the 5 posts with the most recent comments.  Of those posts, display any comment more recent than the newest comment on the post with the oldest comments, up to a limit of 5 per post.  Fade out older comments.  Attempt to fix #309.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1673 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/dash_latestcomments.php
+++ admin/dash_latestcomments.php
@@ -7,13 +7,25 @@
 
 	<ul class="items">
 
-		<?php foreach( Comments::get( array( 'status' => Comment::STATUS_APPROVED, 'limit' => 10, 'orderby' => 'date DESC' ) ) as $comment ): ?>
+		<?php
+		$mindate= DB::get_value('select min(`date`) from (select `date` FROM {comments} where status = ? and `type` = ? group by post_id order by `date` desc limit 5) as date_foo;', array(Comment::STATUS_APPROVED, Comment::COMMENT));
+		foreach( DB::get_results('select p.* from {posts} p, {comments} c where p.id = c.post_id AND c.status = ? and c.`type` = ? group by p.id order by `date` desc limit 5;', array(Comment::STATUS_APPROVED, Comment::COMMENT), 'Post') as $post ):
+		?>
 		<li class="item clear">
-			<span class="titleanddate pct85"><a href="<?php echo $comment->post->permalink; ?>" class="title"><?php echo $comment->post->title; ?></a> <a href="#" class="date minor"><?php echo date('M j', strtotime($comment->post->pubdate)); ?></a></span>
-			<span class="comments pct15"><a href="<?php echo $comment->post->permalink; ?>#comments" title="<?php printf(_n('%1$d comment', '%1$d comments', $comment->post->comments->approved->comments->count), $comment->post->comments->approved->comments->count); ?>"><?php echo $comment->post->comments->approved->comments->count; ?></a></span>
+			<span class="titleanddate pct85"><a href="<?php echo $post->permalink; ?>" class="title"><?php echo $post->title; ?></a> <a href="#" class="date minor"><?php echo date('M j', strtotime($post->pubdate)); ?></a></span>
+			<span class="comments pct15"><a href="<?php echo $post->permalink; ?>#comments" title="<?php printf(_n('%1$d comment', '%1$d comments', $post->comments->approved->comments->count), $post->comments->approved->comments->count); ?>"><?php echo $post->comments->approved->comments->count; ?></a></span>
 			<ul class="commentauthors pct85 minor">
-				<?php foreach($comment->post->comments->comments->approved as $comment): ?>
-				<li><a href="<?php echo $comment->post->permalink; ?>#comment-<?php echo $comment->id; ?>" title="<?php printf(_t('Posted at %1$s'), date('g:m a \o\n F jS, Y', strtotime($comment->date))); ?>" class="opa100"><?php echo $comment->name; ?></a></li>
+				<?php
+				$comment_count= 0;
+				$comments = DB::get_results('SELECT * FROM {comments} WHERE post_id = ? AND status = ? AND `date` >= ? AND `type` = ?', array($post->id, Comment::STATUS_APPROVED, $mindate, Comment::COMMENT), 'Comment');
+				foreach($comments as $comment):
+					$comment_count++;
+					if($comment_count > 5) {
+					 	break;
+					}
+					$opa = 'opa' . (100 - $comment_count * 15);
+				?>
+				<li><a href="<?php echo $comment->post->permalink; ?>#comment-<?php echo $comment->id; ?>" title="<?php printf(_t('Posted at %1$s'), date('g:m a \o\n F jS, Y', strtotime($comment->date))); ?>" class="<?php echo $opa; ?>"><?php echo $comment->name; ?></a></li>
 				<?php endforeach; ?>
 			</ul>
 		</li>
