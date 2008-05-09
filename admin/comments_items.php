ad3b44872450e6b041d96a5480302a948fcc243e
Owen Winkler <epithet@gmail.com>
2008-05-09 02:29:39 +0000

    You might be looking for this missing admin template from my last commit.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1680 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ admin/comments_items.php
@@ -0,0 +1,36 @@
+<?php foreach( $comments as $comment ) : ?>
+
+<div class="item clear" id="comment_<?php echo $comment->id; ?>">
+	<div class="head clear">
+		<span class="checkboxandtitle pct25">
+			<input type="checkbox" class="checkbox" name="comment_ids[<?php echo $comment->id; ?>]" id="comments_ids[<?php echo $comment->id; ?>]" value="1"></input>
+			<?php if($comment->url != ''): ?>
+			<a href="#" class="author"><?php echo $comment->name; ?></a>
+			<?php else: ?>
+			<?php echo $comment->name; ?>
+			<?php endif; ?>
+		</span>
+		<span class="entry pct30"><a href="<?php echo $comment->post->permalink ?>#comment-<?php echo $comment->id; ?>"><?php echo $comment->post->title; ?></a></span>
+    <span class="time pct10"><a href="#"><span class="dim">at</span> <?php echo date('H.i', strtotime($comment->date));?></a></span>
+    <span class="date pct15"><a href="#"><span class="dim">on</span> <?php echo date('M d, Y', strtotime($comment->date));?></a></span>
+		<ul class="dropbutton">
+			<li><a href="#">Delete</a></li>
+			<li><a href="#">Spam</a></li>
+			<li><a href="#">Approve</a></li>
+			<li><a href="#">Unapprove</a></li>
+			<li><a href="#">Edit</a></li>
+		</ul>
+	</div>
+
+	<div class="infoandcontent clear">
+		<span class="authorinfo pct25 minor">
+			<?php if ($comment->url != '')
+				echo '<a href="' . $comment->url . '">' . $comment->url . '</a>'."\r\n"; ?>
+			<?php if ( $comment->email != '' )
+				echo '<a href="mailto:' . $comment->email . '">' . $comment->email . '</a>'."\r\n"; ?>
+		</span>
+		<span class="content pct75"><?php echo $comment->content ?></span>
+	</div>
+</div>
+
+<?php endforeach; ?>
