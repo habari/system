682ec0fabb326a19a34833328a4a5381544780e8
Owen Winkler <epithet@gmail.com>
2008-05-10 04:05:44 +0000

    Put nonce check back into post and comment deletion.  Thanks, bjohnson.  Fixes #316.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1690 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/entries.php
+++ admin/entries.php
@@ -39,6 +39,9 @@
 <div class="container transparent">
 
 	<div class="item controls">
+		<input type="hidden" name="nonce" id="nonce" value="<?php echo $wsse['nonce']; ?>"></input>
+		<input type="hidden" name="timestamp" id="timestamp" value="<?php echo $wsse['timestamp']; ?>"></input>
+		<input type="hidden" name="PasswordDigest" id="PasswordDigest" value="<?php echo $wsse['digest']; ?>"></input>
 		<span class="pct25">
 			<input type="checkbox"></input>
 			<span class="selectedtext minor none">None selected</span>
