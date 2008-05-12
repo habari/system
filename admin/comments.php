26b233935f7fdeaf92b485423e1ce98b70105ccf
Ali B <dmondark@contrib.habariproject.org>
2008-05-12 01:03:18 +0000

    Committing first patch submitted to #323. fixes a couple of issues and add a little more functionally and doesn't break anything that's not broken. Still work to be done on the subject though. Props to bjohnson.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1697 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/comments.php
+++ admin/comments.php
@@ -1,11 +1,11 @@
 <?php include('header.php'); ?>
 
 <div class="container navigator">
-	<span class="older pct10"><a href="#">&laquo; Older</a></span>
+	<span class="older pct10"><a href="#" onclick="timeline.skipLoupeLeft();return false">&laquo; Older</a></span>
 	<span class="currentposition pct15 minor">0-0 of 0</span>
 	<span class="search pct50"><input type="search" placeholder="Type and wait to search for any entry component" autosave="habaricontent" results="10"></span>
 	<span class="nothing pct15">&nbsp;</span>
-	<span class="newer pct10"><a href="#">Newer &raquo;</a></span>
+	<span class="newer pct10"><a href="#" onclick="timeline.skipLoupeRight();return false">Newer &raquo;</a></span>
 
 	<div class="timeline">
 		<div class="years">
