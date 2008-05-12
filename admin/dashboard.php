b7fd5e8f24413ceba5774c90380a3ac51392c494
michaeltwofish <michaeltwofish@contrib.habariproject.org>
2008-05-12 03:26:21 +0000

    Attempt to incorporate pluralisation (as per r1689) without breaking i18n. I've tried to keep the strings for translation as long as possible to avoid translation issues, but these still may be a problem for some locales. If anyone has ideas of a better way to do this, please speak up.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1699 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,16 +3,22 @@
 <div class="container dashboardinfo transparent">
 		<p>
 		<?php
-		printf(_t(Options::get('title'). ' has been active for %1$d years, %2$d months and %3$d days.'), $active_time['years'], $active_time['months'], $active_time['days']);
+		printf(
+			Options::get('title') .
+			_n(' has been active for %1$d year, ', ' has been active for %1$d years, ', $active_time['years']) .
+			_n('%2$d month ', '%2$d months ', $active_time['months']) .
+			_n('and %3$d day.', 'and %3$d days.', $active_time['days']),
+			$active_time['years'], $active_time['months'], $active_time['days']
+		 );
 		?>
 		<br>
 		<?php
 		printf(
-			_t('The <a href="%6$s">%1$d author' . ($stats['author_count'] == 1 ? '' : 's' ) . '</a> ' .
-			   ($stats['author_count'] == 1 ? 'has' : 'have' ) . 
-			   ' published <a href="%7$s">%2$d page' . ($stats['page_count'] == 1 ? '' : 's' ) . 
-			    '</a> and <a href="%8$s">%3$d entr' . ($stats['entry_count'] == 1 ? 'y' : 'ies') . 
-	                    '</a> with <a href="%9$s">%4$d comments</a> and <a href="%10$s">%5$d tags</a>'),
+			_n('The <a href="%6$s">%1$d author </a> has ', 'The <a href="%6$s">%1$d authors </a> have ', $stats['author_count']) .
+			_n('published <a href="%7$s">%2$d page</a> ', 'published <a href="%7$s">%2$d pages</a> ', $stats['page_count']) .
+			_n('and <a href="%8$s">%3$d entry</a> ', 'and <a href="%8$s">%3$d entries</a> ', $stats['entry_count']) .
+			_n('with <a href="%9$s">%4$d comment</a> ', 'with <a href="%9$s">%4$d comments</a> ', $stats['comment_count']) .
+			_n('and <a href="%10$s">%5$d tag</a>', 'and <a href="%10$s">%5$d tags</a>', $stats['tag_count']),
 			$stats['author_count'],
 			$stats['page_count'],
 			$stats['entry_count'],
@@ -27,7 +33,10 @@
 		?></p>
 		<p><?php
 		printf(
-			_t('You currently have <a href="%5$s">%1$d entry drafts</a>, <a href="%6$s">%2$d scheduled entries</a>, <a href="%7$s">%3$d page drafts</a>, and <a href="%8$s">%4$d comments awaiting approval</a>'),
+			_n('You currently have <a href="%5$s">%1$d entry draft</a>, ', 'You currently have <a href="%5$s">%1$d entry drafts</a>, ', $stats['entry_draft_count']) .
+			_n('<a href="%6$s">%2$d scheduled entry</a>, ', '<a href="%6$s">%2$d scheduled entries</a>, ', $stats['user_entry_scheduled_count']) .
+			_n('<a href="%7$s">%3$d page draft</a>, ', '<a href="%7$s">%3$d page drafts</a>, ', $stats['page_draft_count']) .
+			_n('and <a href="%8$s">%4$d comment awaiting approval</a>', 'and <a href="%8$s">%4$d comments awaiting approval</a>', $stats['unapproved_comment_count']),
 			$stats['entry_draft_count'],
 			$stats['user_entry_scheduled_count'],
 			$stats['page_draft_count'],
