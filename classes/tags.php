719276b06467ebb7f836257a1ea85f5af851ed20
michaeltwofish <michaeltwofish@contrib.habariproject.org>
2008-05-15 01:38:43 +0000

    Implement tag deletion backend and wire it in to admin tags page. Rename to come. Thanks itrebal for initial work on this.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1711 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/tags.php
+++ classes/tags.php
@@ -1,15 +1,41 @@
 <?php
 /**
- * Habari Tag Class
+ * Habari Tags Class
  *
- * Requires PHP 5.0.4 or later
  * @package Habari
  */
 class Tags extends ArrayObject
 {
-    public static function get() {
-		$tags= DB::get_results( 'SELECT t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id GROUP BY tag, slug ORDER BY tag ASC' );
+	/**
+	 * Returns all tags
+	 * <b>THIS CLASS SHOULD CACHE QUERY RESULTS!</b>
+	 *
+	 * @return array An array of Tag objects
+	 **/
+	public static function get() {
+		$tags= DB::get_results( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id GROUP BY tag, slug ORDER BY tag ASC' );
 		return $tags;
+	}
+
+	public static function delete($tag)
+	{
+		DB::query( 'DELETE FROM {tag2post} WHERE tag_id = ?', array($tag->id) );
+		DB::query( 'DELETE FROM {tags} WHERE id = ?', array($tag->id) );
+	}
+
+	public static function get_by_text($tag)
+	{
+		return DB::get_row( 'SELECT id, tag_text, tag_slug FROM {tags} WHERE tag_text = ?', array($tag) );
+	}
+
+	public static function get_by_slug($tag)
+	{
+		return DB::get_row( 'SELECT id, tag_text, tag_slug FROM {tags} WHERE tag_slug = ?', array($tag) );
+	}
+
+	public static function get_by_id($tag)
+	{
+		return DB::get_row( 'SELECT id, tag_text, tag_slug FROM {tags} WHERE id = ?', array($tag) );
 	}
 }
 ?>
