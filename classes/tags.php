08716df77765d52cbce52fb86792e3763b8aa4ae
Raman Ng <tinyau@contrib.habariproject.org>
2008-05-17 01:30:23 +0000

    Modified the SQL statements which valid for MySQL/SQLite/PostgreSQL.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1735 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/tags.php
+++ classes/tags.php
@@ -14,7 +14,7 @@ class Tags extends ArrayObject
 	 **/
 	public static function get()
 	{
-		$tags= DB::get_results( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id GROUP BY tag, slug ORDER BY tag ASC' );
+		$tags= DB::get_results( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id GROUP BY id, tag, slug ORDER BY tag ASC' );
 		return $tags;
 	}
 
@@ -25,7 +25,7 @@ class Tags extends ArrayObject
 	 **/
 	public static function get_one($tag)
 	{
- 		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t LEFT JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_slug = ? OR t.id = ? GROUP BY id', array( Utils::slugify( $tag ), $tag ) );
+ 		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t LEFT JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_slug = ? OR t.id = ? GROUP BY id, tag, slug', array( Utils::slugify( $tag ), $tag ) );
 	}
 
 	/**
@@ -107,7 +107,7 @@ class Tags extends ArrayObject
 	 * @return int The number of times the most used tag is used.
 	 **/
 	public static function max_count() {
-		return DB::get_value( 'SELECT count( t2.post_id ) AS max FROM {tags} t, {tag2post} t2 WHERE t2.tag_id = t.id GROUP BY t.id ORDER BY count( t2.post_id ) DESC LIMIT 0, 1' );
+		return DB::get_value( 'SELECT count( t2.post_id ) AS max FROM {tags} t, {tag2post} t2 WHERE t2.tag_id = t.id GROUP BY t.id ORDER BY count( t2.post_id ) DESC LIMIT 1' );
 	}
 
 	/**
@@ -129,17 +129,17 @@ class Tags extends ArrayObject
 
 	public static function get_by_text($tag)
 	{
-		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_text = ? GROUP BY id', array($tag) );
+		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_text = ? GROUP BY id, tag, slug', array($tag) );
 	}
 
 	public static function get_by_slug($tag)
 	{
-		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_slug = ? GROUP BY id', array($tag) );
+		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_slug = ? GROUP BY id, tag, slug', array($tag) );
 	}
 
 	public static function get_by_id($tag)
 	{
-		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id WHERE id = ? GROUP BY id', array($tag) );
+		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t INNER JOIN {tag2post} tp ON t.id=tp.tag_id WHERE id = ? GROUP BY id, tag, slug', array($tag) );
 	}
 }
 ?>
