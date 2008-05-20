4d01769697b999707a67654f087b5245c4f4750a
Jay Pipes <jaypipes@contrib.habariproject.org>
2008-05-20 16:59:35 +0000

    This changeset fixes Issue #375:  "Dashboard says 1 Tag, but Tags Admin shows No Tags"
    The problem was that tags could exist that were not related
    to any post (for instance, the "habari" tag that the default
    installation creates.  However, the queries used in both
    retrieving all the tags in the tag set and in grabbing a single
    tag for change or deletion used an INNER JOIN to the tag2post
    table, resulting in the inability to remove or see tags not
    related to a post but existing in the database.
    
    M    admin/tag_collection.php
    M    classes/tags.php
    M    classes/adminhandler.php
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1771 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/tag_collection.php
+++ admin/tag_collection.php
@@ -2,5 +2,7 @@
 	$tags= Tags::get();
 	$max= Tags::max_count();
 	foreach ($tags as $tag) : ?>
-		<a href="#" id="<?php echo 'tag_' . $tag->id ?>" class="tag wt<?php echo round(($tag->count * 10)/$max); ?>"><span><?php echo $tag->tag; ?></span><sup><?php echo $tag->count; ?></sup></a>
+		<a href="#" id="<?php echo 'tag_' . $tag->id ?>" class="tag wt<?php echo $max > 0 ? round(($tag->count * 10)/$max) : 0; ?>">
+			<span><?php echo $tag->tag; ?></span><sup><?php echo $tag->count; ?></sup>
+		</a>
 <?php endforeach; ?>
