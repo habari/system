57ca1a3e0b681dea45952f51a372eb3a87a27d1d
Michael Heilemann <heilemann@contrib.habariproject.org>
2008-05-20 12:57:57 +0000

    Monolith: Added missing 'in' to entry title on Comments page.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1764 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/comments_items.php
+++ admin/comments_items.php
@@ -10,7 +10,7 @@
 			<?php echo $comment->name; ?>
 			<?php endif; ?>
 		</span>
-		<span class="entry pct30"><a href="<?php echo $comment->post->permalink ?>#comment-<?php echo $comment->id; ?>"><?php echo $comment->post->title; ?></a></span>
+		<span class="entry pct30"><span class="dim">in '</span><a href="<?php echo $comment->post->permalink ?>#comment-<?php echo $comment->id; ?>"><?php echo $comment->post->title; ?></a><span class="dim">'</span></span>
     <span class="time pct10"><a href="#"><span class="dim">at</span> <?php echo date('H:i', strtotime($comment->date));?></a></span>
     <span class="date pct15"><a href="#"><span class="dim">on</span> <?php echo date('M d, Y', strtotime($comment->date));?></a></span>
 		<ul class="dropbutton">
