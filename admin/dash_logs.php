e89b04652ddec9c1970db5eadcd9921811ea58d4
Michael Heilemann <heilemann@contrib.habariproject.org>
2008-05-31 09:07:52 +0000

    Changed name of Log dashboard module from 'Activity Logs' to 'Latest Log Activity', as it seems more descriptive of the limited scope of the module.
    Yeah, I'll stop spamming nano-commits soon :)
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1914 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/dash_logs.php
+++ admin/dash_logs.php
@@ -1,6 +1,6 @@
 <div class="options">&nbsp;</div>
 <div class="modulecore">
-	<h2><a href="<?php Site::out_url('admin'); ?>/logs"><?php _e('Activity Logs'); ?></a></h2>
+	<h2><a href="<?php Site::out_url('admin'); ?>/logs"><?php _e('Latest Log Activity'); ?></a></h2>
 	<div class="handle">&nbsp;</div>
 	<ul class="items">
 		<?php foreach( eventlog::get( array( 'limit' => 8 ) ) as $log ) { ?>
