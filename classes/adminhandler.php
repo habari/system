021ccd504818ccdb4c1f4054b8ce9e1f1e347955
Ali B <dmondark@contrib.habariproject.org>
2008-05-31 18:32:17 +0000

    Display 0 days activity if no post exists in the database. fixes #415.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1918 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/adminhandler.php
+++ classes/adminhandler.php
@@ -164,7 +164,7 @@ class AdminHandler extends ActionHandler
 	{
 		// Not sure how best to determine this yet, maybe set an option on install, maybe do this:
 		$firstpostdate= strtotime(DB::get_value('SELECT min(pubdate) FROM {posts} WHERE status = ?', array(Post::status('published'))));
-		$firstpostdate= time() - $firstpostdate;
+		if ( intval( $firstpostdate ) !== 0 ) $firstpostdate= time() - $firstpostdate;
 		$this->theme->active_time= array(
 			'years' => floor($firstpostdate / 31556736),
 			'months' => floor(($firstpostdate % 31556736) / 2629728),
