b19ad0ce363650030f8cdb6c8e4a324e0443fc8a
Ali B <dmondark@contrib.habariproject.org>
2008-05-31 00:47:38 +0000

    Moving comments from single templates to a seperate template file, added a moderation note and removed some unused styling.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1901 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/page.single.php
+++ themes/charcoal/page.single.php
@@ -34,39 +34,13 @@
 <div id="page-bottom">
 	<div id="wrapper-bottom">
 		<div id="bottom-primary">
-		<?php if ( !$post->info->comments_disabled || $post->comments->moderated->count ) :?>
-			<div id="post-comments">
-			<?php 
-				if ( $post->comments->moderated->count ) :
-					foreach ( $post->comments->moderated as $comment ) :
-						$class= '"post-comment';
-						if ( $comment->status == Comment::STATUS_UNAPPROVED ) {
-							$class.= '-u';
-						}
-				$class.= '"';
-			?>
-				<div id="comment-<?php echo $comment->id; ?>" class=<?php echo $class; ?>>
-					<div class="post-comment-commentor">
-						<h2>
-							<a href="<?php echo $comment->url; ?>" rel="external"><?php echo $comment->name; ?></a>
-						</h2>
-				</div>
-				<div class="post-comment-body">
-					<?php echo $comment->content_out; ?>
-					<p class="post-comment-link"><a href="#comment-<?php echo $comment->id; ?>" title="<?php _e( "Time of this comment" ); ?>"><?php echo $comment->date; ?></a></p>
-				</div>
-			</div>
-			<?php endforeach; ?>
-			<?php else : ?>
-			<h2><?php _e( "Be the first to write a comment!" ); ?></h2>
-			<?php endif; ?>
-			<div id="post-comments-footer">
-				<!-- TODO: A hook can be placed here-->
-			</div>
-			</div>
-		<?php endif; ?>
+		
+		<?php include 'comments.php'; ?>
+		
 		<!-- comment form -->
+		
 		<?php include 'commentform.php'; ?>
+		
 		<!-- /comment form -->
 
 <?php include 'footer.php'; ?>
