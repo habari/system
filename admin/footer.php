1ac2e53b4002f517d1680d92404b60eb3e84c877
Michael Heilemann <heilemann@contrib.habariproject.org>
2008-06-01 08:25:14 +0000

    Added descriptive titles to the footer links.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1919 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/footer.php
+++ admin/footer.php
@@ -1,14 +1,8 @@
 <div id="footer">
 	<p>
-		<span><a href="http://habariproject.org/">Habari <?php echo version::get_habariversion() ?></a></span> <span class="middot">&middot;</span>
-		<span><?php _e('Logged in as'); ?></span> <a href="<?php Site::out_url( 'habari' ); ?>/admin/user"><?php echo User::identify()->displayname ?></a> <span class="middot">&middot;</span>
-		<span><a href="<?php Site::out_url( 'habari' ); ?>/doc/manual/index.html" onclick="popUp(this.href); return false;" title="<?php _e('Read the user manual'); ?>"><?php _e('Manual'); ?></a></span> 
-
-	<!--
-		<a href="<?php Site::out_url( 'habari' ); ?>/doc/manual/index.html" onclick="popUp(this.href); return false;" title="<?php _e('Read the user manual'); ?>"><?php _e('Manual'); ?></a> &middot; 
-		<a href="http://wiki.habariproject.org/" title="<?php _e('Read the Habari wiki'); ?>"><?php _e('Wiki'); ?></a> &middot; 
-		<a href="http://groups.google.com/group/habari-users" title="<?php _e('Ask the community'); ?>"><?php _e('Mailing List'); ?></a>
-	-->
+		<span><a href="http://habariproject.org/" title="<?php _e('Go to the Habari site'); ?>">Habari <?php echo version::get_habariversion() ?></a></span> <span class="middot">&middot;</span>
+		<span><?php _e('Logged in as'); ?></span> <a href="<?php Site::out_url( 'habari' ); ?>/admin/user" title="<?php _e('Go to your user page'); ?>"><?php echo User::identify()->displayname ?></a> <span class="middot">&middot;</span>
+		<span><a href="<?php Site::out_url( 'habari' ); ?>/doc/manual/index.html" onclick="popUp(this.href); return false;" title="<?php _e('Open the Habari manual in a new window'); ?>"><?php _e('Manual'); ?></a></span> 
 	</p>
 </div>
 
@@ -17,7 +11,9 @@
 	Stack::out( 'admin_footer_javascript', ' <script src="%s" type="text/javascript"></script>'."\r\n" );
 	include ('db_profiling.php');
 ?>
+
 </div>
+
 <?php if(Session::has_messages()): ?>
 	<script type="text/javascript">
 	jQuery(document).ready(function() {
@@ -25,5 +21,6 @@
 	})
   </script>
 <?php endif; ?>
+
 </body>
 </html>
