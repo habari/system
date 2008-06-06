6b42560553ddbd51df8e848a16169c78003cb0fd
Michael Heilemann <heilemann@contrib.habariproject.org>
2008-06-06 21:50:56 +0000

    Hiding the 'Upload Plugin Package' block, for future generations to apply functionality to... In the post .5 world :)
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1963 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/plugins.php
+++ admin/plugins.php
@@ -122,12 +122,12 @@
 </div>
 <?php endif; ?>
 
-<div class="container uploadpackage">
+<!--<div class="container uploadpackage">
 	<h2><?php _e('Upload Plugin Package'); ?></h2>
 	<div class="uploadform">
 		<input type="file">
 		<input type="submit" value="<?php _e('Upload'); ?>">
 	</div>
-</div>
+</div>-->
 
 <?php include('footer.php'); ?>
