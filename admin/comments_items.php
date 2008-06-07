dfdb10020512920dae2afb1ecebde9c3d0d1de2b
Michael Heilemann <heilemann@contrib.habariproject.org>
2008-06-07 13:38:43 +0000

    Monolith: Added titles and semantic classes to entries and comments manage pages, and added an experimental coloring of comments marked as  unapproved and spam.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@1969 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/comments_items.php
+++ admin/comments_items.php
@@ -1,6 +1,6 @@
 <?php foreach( $comments as $comment ) : ?>
 
-<div class="item clear" id="comment_<?php echo $comment->id; ?>">
+<div class="item clear <?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) { echo 'unapproved'; } ?> <?php if ( $comment->status == Comment::STATUS_APPROVED ) { echo 'approved'; } ?> <?php if ( $comment->status == Comment::STATUS_SPAM ) { echo 'spam'; } ?>" id="comment_<?php echo $comment->id; ?>">
 	<div class="head clear">
 		<span class="checkboxandtitle pct25">
 			<input type="checkbox" class="checkbox" name="comment_ids[<?php echo $comment->id; ?>]" id="comments_ids[<?php echo $comment->id; ?>]" value="1">
@@ -10,21 +10,21 @@
 			<?php echo $comment->name; ?>
 			<?php endif; ?>
 		</span>
-		<span class="title pct40"><span class="dim"><?php _e('in'); ?> '</span><a href="<?php echo $comment->post->permalink ?>#comment-<?php echo $comment->id; ?>"><?php echo $comment->post->title; ?></a><span class="dim">'</span></span>
-	    <span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="#"><?php echo date('M d, Y', strtotime($comment->date));?></a></span>
+		<span class="title pct40"><span class="dim"><?php _e('in'); ?> '</span><a href="<?php echo $comment->post->permalink ?>#comment-<?php echo $comment->id; ?>" title="<?php _e('Go to \''.$comment->post->title.'\'') ?>"><?php echo $comment->post->title; ?></a><span class="dim">'</span></span>
+	    <span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="#" title="<?php _e('Search for other comments from '.date('M, Y', strtotime($comment->date))) ?>"><?php echo date('M d, Y', strtotime($comment->date));?></a></span>
 	    <span class="time pct10 dim"><?php _e('at'); ?> <?php echo date('H:i', strtotime($comment->date));?></span>
 
 		<ul class="dropbutton">
 		<?php if ( $comment->status != Comment::STATUS_APPROVED ) : ?>
-			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'approve');return false;"><?php _e('Approve'); ?></a></li>
+			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'approve');return false;" title="<?php _e('Approve this comment') ?>"><?php _e('Approve'); ?></a></li>
 		<?php endif; ?>
 		<?php if ( $comment->status != Comment::STATUS_UNAPPROVED ) : ?>
-			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'unapprove');return false;"><?php _e('Unapprove'); ?></a></li>
+			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'unapprove');return false;" title="<?php _e('Unapprove this comment') ?>"><?php _e('Unapprove'); ?></a></li>
 		<?php endif; ?>
 		<?php if ( $comment->status != Comment::STATUS_SPAM ) :?>
-			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'spam');return false;"><?php _e('Spam'); ?></a></li>
+			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'spam');return false;" title="<?php _e('Spam this comment') ?>"><?php _e('Spam'); ?></a></li>
 		<?php endif; ?>
-			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'delete');return false;"><?php _e('Delete'); ?></a></li>
+			<li><a href="#" onclick="itemManage.update(<?php echo $comment->id; ?>, 'delete');return false;" title="<?php _e('Delete this Comment') ?>"><?php _e('Delete'); ?></a></li>
 			<li><a href="#"><?php _e('Edit'); ?></a></li>
 		</ul>
 	</div>
@@ -35,6 +35,11 @@
 				echo '<a href="' . $comment->url . '">' . $comment->url . '</a>'."\r\n"; ?>
 			<?php if ( $comment->email != '' )
 				echo '<a href="mailto:' . $comment->email . '">' . $comment->email . '</a>'."\r\n"; ?>
+
+			<?php if ( $comment->status == Comment::STATUS_SPAM ) :?>
+				<p>Marked as spam</p>
+			<?php endif; ?>
+
 		</span>
 		<span class="content pct75"><?php echo $comment->content ?></span>
 	</div>
--- admin/entries_items.php
+++ admin/entries_items.php
@@ -1,19 +1,19 @@
 <?php	foreach($posts as $post) : ?>
 
-<div class="item clear" id="post_<?php echo $post->id; ?>">
+<div class="item clear <?php echo $post->statusname; ?>" id="post_<?php echo $post->id; ?>">
 	<div class="head clear">
-		<span class="checkboxandtitle pct30">
+		<span class="checkboxandtitle pct35">
 			<input type="checkbox" class="checkbox" name="checkbox_ids[<?php echo $post->id; ?>]" id="checkbox_ids[<?php echo $post->id; ?>]">
-			<a href="<?php echo $post->permalink; ?>" class="title"><?php echo $post->title; ?></a>
+			<a href="<?php echo $post->permalink; ?>" class="title" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php echo $post->title; ?></a>
 		</span>
-		<span class="state pct10"><a href="<?php URL::out('admin', array('page' => 'entries', 'type' => $post->content_type, 'status' => $post->status ) ); ?>"><?php echo $post->statusname; ?></a></span>
-		<span class="author pct20"><span class="dim"><?php _e('by'); ?></span> <a href="<?php URL::out('admin', array('page' => 'entries', 'user_id' => $post->user_id, 'type' => $post->content_type, 'status' => 'any') ); ?>"><?php echo $post->author->displayname; ?></a></span>
-		<span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="<?php URL::out('admin', array('page' => 'entries', 'type' => $post->content_type, 'year_month' => date( 'Y-m', strtotime( $post->pubdate ) ) ) ); ?>"><?php echo date('M j, Y', strtotime($post->pubdate)); ?></a></span>
+		<span class="state pct10"><a href="<?php URL::out('admin', array('page' => 'entries', 'type' => $post->content_type, 'status' => $post->status ) ); ?>" title="<?php _e('Search for other '.$post->statusname.' items'); ?>"><?php echo $post->statusname; ?></a></span>
+		<span class="author pct20"><span class="dim"><?php _e('by'); ?></span> <a href="<?php URL::out('admin', array('page' => 'entries', 'user_id' => $post->user_id, 'type' => $post->content_type, 'status' => 'any') ); ?>" title="<?php _e('Search for other items by '.$post->author->displayname) ?>"><?php echo $post->author->displayname; ?></a></span>
+		<span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="<?php URL::out('admin', array('page' => 'entries', 'type' => $post->content_type, 'year_month' => date( 'Y-m', strtotime( $post->pubdate ) ) ) ); ?>" title="<?php _e('Search for other items from '.date('M, Y', strtotime($post->pubdate))) ?>"><?php echo date('M j, Y', strtotime($post->pubdate)); ?></a></span>
 		<span class="time pct10"><span class="dim"><?php _e('at'); ?> <?php echo date('H:i', strtotime($post->pubdate)); ?></span></span>
 
 		<ul class="dropbutton">
-			<li><a href="<?php URL::out('admin', 'page=publish&slug=' . $post->slug); ?>"><?php _e('Edit'); ?></a></li>
-			<li><a href="#" onclick="itemManage.remove(<?php echo $post->id; ?>, 'post');return false;"><?php _e('Delete'); ?></a></li>
+			<li><a href="<?php URL::out('admin', 'page=publish&slug=' . $post->slug); ?>" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php _e('Edit'); ?></a></li>
+			<li><a href="#" onclick="itemManage.remove(<?php echo $post->id; ?>, 'post');return false;" title="<?php _e('Delete this item') ?>"><?php _e('Delete'); ?></a></li>
 		</ul>
 	</div>
 
