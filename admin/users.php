98474d88c138d1ae27dc712d9408b929e530eae5
Michael Heilemann <heilemann@contrib.habariproject.org>
2008-06-08 16:06:33 +0000

    Redesigned the Users page. It ain't perfect, but it's a step in the right direction. Note that because of the way the labeless JS works at the moment, the password fields in the 'add new user' container don't display their inline label properly. My JS-foo is weak, but I'll see what I can do; but feel free to step in and take a crack at it if you feel like it.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@1979 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/users.php
+++ admin/users.php
@@ -1,41 +1,92 @@
 <?php include('header.php');?>
-<div class="container">
-<hr>
-<div class="column prepend-1 span-22 append-1">
-<?php
-$currentuser = User::identify();
-?>
-<h3><?php _e('User Management'); ?></h3>
-<p><?php _e('Add, edit and remove users from your site from this interface.'); ?></p>
-<h3><strong><?php _e('Users'); ?></strong></h3>
-<ul>
-<?php
-foreach ( Users::get_all() as $user )
-{
-	if ( $user->username == $currentuser->username ) {
-		$url = Url::get( 'admin', 'page=user' );
-	}
-	else {
-		$url = Url::get( 'user_profile', array( 'page' => 'user', 'user' => $user->username ) );
-	}
-	echo '<li>';
-	echo '<a href="' . $url . '">' . $user->username . '</a> <em>(' . _t('Last login:') . $user->info->authenticate_time . ')</em><br>';
-	echo Posts::count_by_author( $user->id, Post::status('published') ) . _t(' published posts, ') . Posts::count_by_author( $user->id, Post::status('draft') ) . _t(' pending drafts, and ') . Posts::count_by_author( $user->id, Post::status('private') ) . _t(' private posts.');
-	echo '</li>';
-}
-?>
-</ul>
 
-<form method="post" action="">
-<h3><strong><?php _e('Add a new user'); ?></strong></h3>
-<p><?php _e('Username:'); ?> <input type="text" size="40" name="username" value="<?php echo ( isset( $settings['username'] ) ) ? $settings['username'] : ''; ?>"></p>
-<p><?php _e('Email:'); ?> <input type="text" size="40" name="email" value="<?php echo ( isset( $settings['email'] ) ) ? $settings['email'] : ''; ?>"></p>
-<p><?php _e('Password (twice to confirm):'); ?></p>
-<p><input type="password" size="40" name="pass1"></p>
-<p><input type="password" size="40" name="pass2"></p>
-<p><input type="hidden" name="action" value="newuser"></p>
-<p><input type="submit" value="<?php _e('Add User'); ?>"></p>
-</form>
+<?php $currentuser = User::identify(); ?>
+
+
+<div class="container navigation">
+	<span class="pct40">
+
+		<form>
+		<select class="navigationdropdown" onChange="navigationDropdown.changePage(this.form.navigationdropdown)" name="navigationdropdown">
+			<?php /*
+			foreach ( Users::get_all() as $user ) {
+				if ( $user->username == $currentuser->username ) {
+					$url = Url::get( 'admin', 'page=user' );
+				}
+				else {
+					$url = Url::get( 'user_profile', array( 'page' => 'user', 'user' => $user->username ) );
+				}
+				echo '<option id="' . $user->id . '" value="' . $url . '">' . $user->displayname . '</option>';
+			} */ ?>
+			<option value=""><?php _e('Complete User List'); ?></option>
+		</select>
+		</form>
+	</span>
+	<span class="or pct20">
+		<?php _e('or'); ?>
+	</span>
+	<span class="pct40">
+		<input type="search" placeholder="<?php _e('search users'); ?>" autosave="habarisettings" results="10">
+	</span>
 </div>
+
+
+<div class="container users">
+
+	<!-- a b c d e f g h i j k l m n o p q r s t u v x y z æ å ø -->
+
+	<ul>
+		<?php foreach (Users::get_all() as $user) {
+			if ( $user->username == $currentuser->username )
+				$url = Url::get( 'admin', 'page=user' );
+			else
+				$url = Url::get( 'user_profile', array( 'page' => 'user', 'user' => $user->username ) );
+		?>
+
+		<li class="item clear">
+			<span class="user pct100"><a href="<?php echo $url ?>" title="<?php _e('Open '. $user->displayname .'\'s user page') ?>"><?php echo $user->displayname ?></a></span><br>
+
+			<span class="aka pct 100">
+				<strong><?php echo $user->username ?></strong> 
+				<?php _e('was last seen'); ?> <strong><?php echo date('M j, Y', strtotime($user->info->authenticate_time)) ?> at <?php echo date('H:i', strtotime($user->info->authenticate_time)) ?></strong>
+				<?php _e('and currently has') ?> <strong><?php echo Posts::count_by_author( $user->id, Post::status('published') ) . _t(' published posts, ') . Posts::count_by_author( $user->id, Post::status('draft') ) . _t(' pending drafts, and ') . Posts::count_by_author( $user->id, Post::status('private') ) . _t(' private posts'); ?></strong>
+			</span>
+		</li>
+
+		<?php } ?>
+	</ul>
+
 </div>
+
+
+<div class="container users addnewuser settings">
+
+	<h2><?php _e('Add New User'); ?></h2>
+
+	<form method="post" action="">
+
+	<span class="pct25">
+		<label for="username" class="incontent">Username</label>
+		<input type="text" size="40" name="username" id="username" value="<?php echo ( isset( $settings['username'] ) ) ? $settings['username'] : ''; ?>" class="styledformelement">
+	</span>
+
+	<span class="pct25">
+		<label for="email" class="incontent">E-Mail</label>
+		<input type="text" size="40" id="email" name="email" value="<?php echo ( isset( $settings['email'] ) ) ? $settings['email'] : ''; ?>" class="styledformelement">
+	</span>
+	<span class="pct25">
+		<label for="pass1" class="incontent">Password</label>
+		<input type="password" size="40" name="pass1" id="pass1" class="styledformelement">
+	</span>
+	<span class="pct25 last-child">
+		<label for="pass2" class="incontent">Password Again</label>
+		<input type="password" size="40" name="pass2" id="pass2" class="styledformelement">
+	</span>
+
+	<input type="hidden" name="action" value="newuser">
+	<input type="submit" value="<?php _e('Add User'); ?>">
+	</form>
+
+</div>
+
 <?php include('footer.php');?>
