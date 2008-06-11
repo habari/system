f790f0e6d557ea0c30b2e7a26f9553a60744f5aa
Matt Read <mattread@contrib.habariproject.org>
2008-06-11 04:04:53 +0000

    changing the way dashboard modules behave as disscussed on the list. You now populate your available module with the dash_modules filter.
    Also changing the way the dash_module_* filter behaves. Now you filter 
    the $modules array instead of just the content, which allows you to 
    change the title, content, and options keys. This method also simplifies 
    the template used to generate the content of the modules. See the 
    coredashmodules plugin for examples..
    
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2017 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/dashboard_modules.php
+++ admin/dashboard_modules.php
@@ -1,7 +1,36 @@
 <ul class="modules">
 	<?php foreach($modules as $moduleid => $module): ?>
 	<li class="module <?php echo Utils::slugify( $module['name'] ); ?>-module" id="<?php echo $moduleid . ':' . $module['name']; ?>">
-		<?php echo $module['content']; ?>
+		
+		<div class="close">&nbsp;</div> 
+		<?php if ( $module['options'] ) : ?>
+			<div class="options">&nbsp;</div>
+		<?php endif; ?>
+		
+		<div class="modulecore">
+			<h2><?php echo $module['title']; ?></h2>
+			
+			<?php if ( $moduleid != 'nosort' ) : ?>
+				<div class="handle">&nbsp;</div>
+			<?php endif; ?>
+			
+			<?php echo $module['content']; ?>
+		</div>
+		
+		<?php if ( $module['options'] ) : ?>
+		<div class="optionswindow">
+			<h2><?php echo $module['title']; ?></h2>
+			
+			<?php if ( $moduleid != 'nosort' ) : ?>
+				<div class="handle">&nbsp;</div>
+			<?php endif; ?>
+			
+			<div class="optionscontent"> 
+				<?php echo $module['options']; ?>
+			</div>
+		</div>
+		<?php endif; ?>
+	
 	</li>
 	<?php endforeach; ?>
 </ul>
