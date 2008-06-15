00b4712a142b643889382389ea9b786f07ae6f1f
Owen Winkler <epithet@gmail.com>
2008-06-15 05:36:51 +0000

    Add checkboxes control.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2059 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ admin/formcontrols/formcontrol_checkboxes.php
@@ -0,0 +1,11 @@
+<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
+<ul>
+<?php foreach($options as $key => $text) : ?>
+	<li><label><input type="checkbox" name="<?php echo $field; ?>[]" value="<?php echo $key; ?>"<?php echo ( in_array( $key, (array) $value ) ? ' checked' : '' ); ?>><?php echo htmlspecialchars($text); ?></label></li>
+<?php endforeach; ?>
+</ul>
+<input type="hidden" name="<?php echo $field; ?>_submitted" value="1">
+<?php if($message != '') : ?>
+<p class="error"><?php echo $message; ?></p>
+<?php endif; ?>
+</div>
