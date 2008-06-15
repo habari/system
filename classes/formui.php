bf520001f0865c55dd1f0a42f0e88894fb942108
ringmaster <epithet@gmail.com>
2008-06-15 05:36:51 +0000

    Add checkboxes control.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2059 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/formui.php
+++ classes/formui.php
@@ -1284,7 +1284,7 @@ class FormControlTextMulti extends FormControl
 }
 
 /**
- * A text control based on FormControl for output via a FormUI.
+ * A select control based on FormControl for output via a FormUI.
  */
 class FormControlSelect extends FormControl
 {
@@ -1331,6 +1331,55 @@ class FormControlSelect extends FormControl
 		return $theme->fetch( $this->get_template() );
 	}
 }
+
+/**
+ * A set of checkbox controls based on FormControl for output via a FormUI.
+ */
+class FormControlCheckboxes extends FormControlSelect
+{
+	/**
+	 * Produce HTML output for this text control.
+	 *
+	 * @param boolean $forvalidation True if this control should render error information based on validation.
+	 * @return string HTML that will render this control in the form
+	 */
+	public function get($forvalidation)
+	{
+		$theme= $this->get_theme($forvalidation);
+		$theme->options = $this->options;
+		$theme->id = $this->name;
+
+		return $theme->fetch( $this->get_template() );
+	}
+	
+	/**
+	 * Magic __get method for returning property values
+	 * Override the handling of the value property to properly return the setting of the checkbox.
+	 *
+	 * @param string $name The name of the property
+	 * @return mixed The value of the requested property
+	 */
+	public function __get($name)
+	{
+		switch($name) {
+		case 'value':
+			if(isset($_POST[$this->field . '_submitted'])) {
+				if(isset($_POST[$this->field])) {
+					return $_POST[$this->field];
+				}
+				else {
+					return array();
+				}
+			}
+			else {
+				return $this->get_default();
+			}
+		}
+		return parent::__get($name);
+	}
+	
+}
+
 
 /**
  * A textarea control based on FormControl for output via a FormUI.
