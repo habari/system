fa0bb5cb8e4e7ac92418d32dbf29832bf2b2a77d
Ali B <dmondark@contrib.habariproject.org>
2008-06-26 17:27:06 +0000

    Escaping displayed html in the admin's text and textarea fields. Fixes #495. Thanks laserlight.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2108 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/admincontrol_text.php
+++ admin/formcontrols/admincontrol_text.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<p>
 			<label for="<?php echo $id; ?>" class="incontent"><?php echo $caption; ?></label>
-			<input type="text" name="<?php echo $field; ?>" id="<?php echo $id; ?>" class="styledformelement text <?php echo $class; ?>" size="100%" value="<?php echo $value; ?>" <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>>
+			<input type="text" name="<?php echo $field; ?>" id="<?php echo $id; ?>" class="styledformelement text <?php echo $class; ?>" size="100%" value="<?php echo htmlspecialchars($value); ?>" <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>>
 		</p>
 		<?php if($message != '') : ?>
 		<p class="error"><?php echo $message; ?></p>
--- admin/formcontrols/admincontrol_textarea.php
+++ admin/formcontrols/admincontrol_textarea.php
@@ -1,7 +1,7 @@
 	<div class="container">
 		<p>
 			<label for="<?php echo $id; ?>" class="incontent"><?php echo $caption; ?></label>
-			<textarea name="<?php echo $field; ?>" id="<?php echo $id; ?>" class="styledformelement <?php echo $class; ?>" rows="20" cols="114" <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>><?php echo $value; ?></textarea>
+			<textarea name="<?php echo $field; ?>" id="<?php echo $id; ?>" class="styledformelement <?php echo $class; ?>" rows="20" cols="114" <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>><?php echo htmlspecialchars($value); ?></textarea>
 		</p>
 		<?php if($message != '') : ?>
 		<p class="error"><?php echo $message; ?></p>
