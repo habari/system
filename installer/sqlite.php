ef837cf54d2f9d3910c9d3e9611aae2fff851d4e
Rick Cockrum <rickc@contrib.habariproject.org>
2008-06-30 21:32:32 +0000

    Incorporate an attempt to write a Files section to the .htaccess file to prevent unwanted access to the SQLite database file if SQLite is chosen as the type of database backend to use. Fixes ticket #239.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2128 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ installer/sqlite.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Language" content="en"/>
+    <meta name="robots" content="noindex,nofollow" />
+    <link href="system/installer/style.css" rel="stylesheet" type="text/css" />
+    
+	  <title><?php _e('Install Habari'); ?></title>
+		<script type="text/javascript" src="/scripts/jquery.js"></script>
+		<script type="text/javascript" src="/scripts/jquery.form.js"></script>
+	<script type="text/javascript">
+	
+	$(document).ready(function() {
+		$('.help-me').click(function(){$(this).parents('.installstep').find('.help').slideToggle();return false;})
+		$('.help').hide();
+	});
+	</script>
+		
+  </head>
+  <body id="installer">
+
+<div id="wrapper">
+
+<form action="" method="get">
+
+<div id="masthead">
+	<h1>Habari</h1>
+	<p><?php _e('Developer Review'); ?></p>
+</div>
+<div class="installstep ready">
+	<h2>.htaccess<a href="#" class="help-me">(<?php _e('help'); ?>)</a></h2>
+	<div class="options">
+		<div class="inputfield">
+			<?php _e('Your <strong>.htaccess</strong> file is not writable. In order to secure your SQLite database, please paste the following into'); ?> <strong><?php echo HABARI_PATH . '/.htaccess'; ?></strong>:<br />
+			<textarea class="config"><?php echo $sqlite_contents; ?></textarea>
+			<div class="help">
+				<?php _e('Your SQLite database is a file on your server like
+				any other file. You can enhance it\'s security by including a section
+				in your .htaccess file that disallows access to it by readers on the web.
+				Habari is not able to write this section in your .htaccess file
+				automatically, so you must add this section yourself to gain the enhanced
+				security it offers you.'); ?>  <a onclick="this.target='_blank';" href="<?php Site::out_url( 'habari' ); ?>/manual/index.html#Installation"><?php _e('Learn More...'); ?></a>
+			</div>
+    </div>
+	</div>
+	<div class="bottom"></div>
+</div>
+
+
+<div class="next-section"></div>
+
+<div class="installstep ready">
+	<h2><?php _e('Install'); ?></h2>
+	<div class="options">
+		
+		<div class="inputfield submit">
+			<div><?php _e('When you have successfully placed the .htaccess file, run the installer again.'); ?></div>
+			<input type="submit" name="submit" value="<?php _e('Restart Installer'); ?>" />
+		</div>
+		
+	</div>
+		
+	<div class="bottom"></div>
+</div>
+</form>
+</div>
+  </body>
+</html>
