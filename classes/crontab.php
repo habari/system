c5131ca05708cbfa31d73688790385b6de8957c8
Matt Read <mattread@contrib.habariproject.org>
2008-07-04 04:11:56 +0000

    fixing crons, hopefully.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2136 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/crontab.php
+++ classes/crontab.php
@@ -12,48 +12,86 @@ class CronTab extends ActionHandler
 	/**
 	 * Executes all cron jobs in the DB.
 	 * @param boolean $async If true, allows execution to continue by making an asynchronous request to a cron URL
-	 * @todo Change the query for crons to use a stored value of when the next cron should happen, rather than executing a non-cacheable query on every pass.
 	 */
-	static function run_cron($async = false)
+	static function run_cron( $async = false )
 	{
-		if($async) {
-			$poll_time = doubleval(Options::get('poll_cron'));
-			if($poll_time > microtime(true)) {
-				return;
-			}
-			//Wait 30 second between trial requests
-			$poll_time = microtime(true) + 30;
-			Options::set('poll_cron', $poll_time);
+		// check if it's time to run crons, and if crons are already running.
+		$next_cron = doubleval( Options::get('next_cron') );
+		if ( ( $next_cron > time() )
+			|| ( Options::get('cron_running') && Options::get('cron_running') > microtime(true) )
+			) {
+			return;
+		}
+		
+		// cron_running will timeout in 10 minutes
+		$run_time = microtime(true) + 600;
+		Options::set('cron_running', $run_time);
+		
+		if ( $async ) {
 			// Timeout is really low so that it doesn't wait for the request to finish
 			$cronurl = URL::get('cron',
 				array(
-					'time' => sprintf('%.6F', $poll_time),
+					'time' => $run_time,
 					'asyncronous' => Utils::crypt(Options::get('guid')) )
 				);
-			$request= new RemoteRequest($cronurl, 'GET', 1);
+			$request = new RemoteRequest($cronurl, 'GET', 1);
 			$request->execute();
 		}
 		else {
-			if( Options::get('cron_running') ) {
-				return;
-			}
-			$hash = md5(microtime(true));
-			Options::set('cron_running', $hash);
 			usleep(5000);
-			if( Options::get('cron_running') != $hash ) {
+			if( Options::get('cron_running') != $run_time ) {
 				return;
 			}
-			$crons = DB::get_results( 'SELECT * FROM ' . DB::table('crontab') . ' WHERE start_time <= ? AND next_run <= ?', array( time(), time() ), 'CronJob' );
+			
+			$crons = DB::get_results(
+				'SELECT * FROM {crontab} WHERE start_time <= ? AND next_run <= ?',
+				array( time(), time() ),
+				'CronJob'
+				);
 			if ( $crons ) {
 				foreach( $crons as $cron ) {
 					$cron->execute();
 				}
 			}
-			Options::set('poll_cron', 0);
+			
+			// set the next run time to the lowest next_run OR a max of one day.
+			$next_cron = DB::get_value( 'SELECT next_run FROM {crontab} ORDER BY next_run ASC LIMIT 1', array() );
+			Options::set('next_cron', min( (int) $next_cron, time() + 86400 ) );
 			Options::set('cron_running', false);
 		}
 	}
-
+	
+	/**
+	 * Handles asyncronous cron calls
+	 */
+	function act_poll_cron()
+	{
+		$time = $this->handler_vars['time'];
+		if ( $time != Options::get('cron_running') ) {
+			return;
+		}
+		
+		// allow script to run for 10 minutes
+		set_time_limit(600);
+		
+		$crons = DB::get_results(
+			'SELECT * FROM {crontab} WHERE start_time <= ? AND next_run <= ?',
+			array( time(), time() ),
+			'CronJob'
+			);
+		
+		if ( $crons ) {
+			foreach( $crons as $cron ) {
+				$cron->execute();
+			}
+		}
+		
+		// set the next run time to the lowest next_run OR a max of one day.
+		$next_cron = DB::get_value( 'SELECT next_run FROM {crontab} ORDER BY next_run ASC LIMIT 1', array() );
+		Options::set('next_cron', min( (int) $next_cron, time() + 86400 ) );
+		Options::set('cron_running', false);
+	}
+	
 	/**
 	 * Get a Cron Job by name from the Database.
 	 * @param string $name Document the functions.
@@ -177,21 +215,6 @@ class CronTab extends ActionHandler
 			'description' => $description
 		);
 		return self::add_cron( $paramarray );
-	}
-
-	function act_poll_cron()
-	{
-		$time = $this->handler_vars['time'];
-		if($time != Options::get('poll_cron')) {
-			return;
-		}
-		$crons = DB::get_results( 'SELECT * FROM ' . DB::table('crontab') . ' WHERE start_time <= ? AND next_run <= ?', array( time(), time() ), 'CronJob' );
-		if ( $crons ) {
-			foreach( $crons as $cron ) {
-				$cron->execute();
-			}
-		}
-		Options::set('poll_cron', 0);
 	}
 }
 
