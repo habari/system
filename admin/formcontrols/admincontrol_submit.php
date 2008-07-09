f6f644464d067417659afc9bdf8a09c029e0d990
Michael Bishop <miklb@contrib.habariproject.org>
2008-07-09 15:46:02 +0000

    placing apply button in formcontrols in container - props arthus and msi. Fixes #515
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2152 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/admincontrol_submit.php
+++ admin/formcontrols/admincontrol_submit.php
@@ -1,4 +1,4 @@
-<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>><input type="submit" name="<?php echo $field; ?>" class="button" value="<?php echo htmlspecialchars($caption); ?>">
+<div class="container transparent <?php echo ($class) ? $class : ''?>" <?php echo ($id) ? ' id="' . $id . '"' : ''?>><input type="submit" name="<?php echo $field; ?>" class="button" value="<?php echo htmlspecialchars($caption); ?>">
 <?php if($message != '') : ?>
 <p class="error"><?php echo $message; ?></p>
 <?php endif; ?>
