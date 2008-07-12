0d539557dc1f3e46fe203e422a5dcde636e269d0
Rick Cockrum <rickc@contrib.habariproject.org>
2008-07-12 04:59:27 +0000

    _e() wasn't passing the domain on to _t().
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2163 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/locale.php
+++ classes/locale.php
@@ -18,17 +18,17 @@ class Locale
 	 * Sets the locale for Habari.
 	 * 
 	 * @param string $locale A language code like 'en' or 'en-us' or 'x-klingon', will be lowercased
-	 **/	 
+	 **/
 	public static function set( $locale= NULL )
 	{
 		if ( $locale == NULL ) {
 			return;
 		}
-		
+
 		self::$locale= strtolower( $locale );
 		self::$uselocale= self::load_domain( 'habari' );
 	}
-	
+
 	/**
 	 * Load translations for a given domain and base directory for a pluggable object.
 	 * Translations are stored in gettext-style .mo files.
@@ -38,13 +38,13 @@ class Locale
 	 * @param string $domain the domain to load
 	 * @param string $base_dir the base directory in which to find the translation files
 	 * @return boolean TRUE if data was successfully loaded, FALSE otherwise
-	 **/	
+	 **/
 	public static function load_pluggable_domain( $domain, $base_dir )
 	{
 		$file= $base_dir . '/locale/' . self::$locale . '/LC_MESSAGES/' . $domain . '.mo';
 		return self::load_file( $domain, $file );
 	}
-	
+
 	/**
 	 * Load translations for a given domain.
 	 * Translations are stored in gettext-style .mo files.
@@ -53,14 +53,14 @@ class Locale
 	 * @link http://www.gnu.org/software/gettext/manual/html_node/gettext_136.html GNU Gettext Manual: Description of the MO file format
 	 * @param string $domain the domain to load
 	 * @return boolean TRUE if data was successfully loaded, FALSE otherwise
-	 **/	 	 	 	
+	 **/
 	private static function load_domain( $domain )
 	{
 		$file= HABARI_PATH . '/system/locale/' . self::$locale . '/LC_MESSAGES/' . $domain . '.mo';
-		
+
 		return self::load_file( $domain, $file );
 	}
-	
+
 	/**
 	 * Load translations from a given file.
 	 * 
@@ -77,17 +77,17 @@ class Locale
 			Error::raise( sprintf( _t('Invalid .MO file for locale %s, domain %s!'), self::$locale, $domain ) );
 			return FALSE;
 		}
-		
+
 		$fp= fopen( $file, 'rb' );
 		$data= fread( $fp, filesize( $file ) );
 		fclose( $fp );
-		
+
 		// determine endianness
 		$little_endian= TRUE;
-		
+
 		list(,$magic)= unpack( 'V1', substr( $data, 0, 4 ) );
 		switch ( $magic & 0xFFFFFFFF ) {
-			case (int)0x950412de:	
+			case (int)0x950412de:
 				$little_endian= TRUE;
 				break;
 			case (int)0xde120495:
@@ -97,26 +97,26 @@ class Locale
 				Error::raise( sprintf( _t('Invalid magic number 0x%08x in %s!'), $magic, $file ) );
 				return FALSE;
 		}
-		
+
 		$revision= substr( $data, 4, 4 );
 		if ( $revision != 0 ) {
 			Error::raise( sprintf( _t('Unknown revision number %d in %s!'), $revision, $file ) );
 			return FALSE;
 		}
-		
+
 		$l= $little_endian ? 'V' : 'N';
-		
+
 		if ( $data && strlen( $data ) >= 20 ) {
 			$header= substr( $data, 8, 12 );
 			$header= unpack( "{$l}1msgcount/{$l}1msgblock/{$l}1transblock", $header );
-			
+
 			if ( $header['msgblock'] + ($header['msgcount'] - 1 ) * 8 > filesize( $file ) ) {
 				Error::raise( sprintf( _t('Message count (%d) out of bounds in %s!'), $header['msgcount'], $file ) );
 				return FALSE;
 			}
-			
+
 			$lo= "{$l}1length/{$l}1offset";
-		
+
 			for ( $msgindex= 0; $msgindex < $header['msgcount']; $msgindex++ ) {
 				$msginfo= unpack( $lo, substr( $data, $header['msgblock'] + $msgindex * 8, 8 ) );
 				$msgids= explode( "\0", substr( $data, $msginfo['offset'], $msginfo['length'] ) );
@@ -128,27 +128,27 @@ class Locale
 				);
 			}
 		}
-		
+
 		// setup plural functionality
 		self::$plural_function= self::get_plural_function( self::$messages[$domain][''][1][0] );
 
 		// only use locale if we actually read something
 		return ( count( self::$messages ) > 0 );
 	}
-	
+
 	private static function get_plural_function( $header )
 	{
 		if (preg_match('/plural-forms: (.*?)$/i', $header, $matches) && preg_match('/^\s*nplurals\s*=\s*(\d+)\s*;\s*plural=(.*)$/', $matches[1], $matches)) {
 			// sanitize
 			$nplurals= preg_replace( '@[^0-9]@', '', $matches[1] );
 			$plural= preg_replace( '@[^n0-9:\(\)\?\|\&=!<>+*/\%-]@', '', $matches[2] );
-			
+
 			$body= str_replace(
 				array('plural',  'n',  '$n$plurals', ),
 				array('$plural', '$n', '$nplurals', ),
 				'nplurals='. $nplurals . '; plural=' . $plural
 			);
-			
+
 			// add parens
 			// important since PHP's ternary evaluates from left to right
 			$body.= ';';
@@ -172,7 +172,7 @@ class Locale
 						$res.= $ch;
 				}
 			}
-			
+
 			$body= $res . 'return ($plural>=$nplurals?$nplurals-1:$plural);';
 			$fn= create_function(
 				'$n',
@@ -186,10 +186,10 @@ class Locale
 				'$nplurals=2;$plural=($n==1?0:1);return ($plural>=$nplurals?$nplurals-1:$plural);'
 			);
 		}
-		
+
 		return $fn;
 	}
-	
+
 	/**
 	 * DO NOT USE THIS FUNCTION.
 	 * This function is only to be used by the test case for the Locale class!
@@ -201,10 +201,10 @@ class Locale
 		for ($n= 0; $n < 200; $n++) {
 			$res.= $fn($n);
 		}
-		
+
 		return $res;
 	}
-	
+
 	/**
 	 * DO NOT USE THIS FUNCTION.
 	 * This function is only to be used by the test case for the Locale class!
@@ -213,24 +213,24 @@ class Locale
 	{
 		return self::load_file( 'test', $filename );
 	}
-	
+
 	/**
 	 * Echo a version of the string translated into the current locale
 	 * @param string $text The text to echo translated
 	 * @param string $domain (optional) The domain to search for the message	 
-	 **/	 	 	 	
+	 **/
 	public static function _e( $text, $domain= 'habari' )
 	{
-		echo self::_t( $text );
+		echo self::_t( $text, $domain );
 	}
-	
+
 	/**
 	 * Return a version of the string translated into the current locale
 	 * 
 	 * @param string $text The text to echo translated
 	 * @param string $domain (optional) The domain to search for the message	 
 	 * @return string The translated string
-	 **/	 	 	 	 	 	
+	 **/
 	public static function _t( $text, $domain= 'habari' )
 	{
 		if ( isset( self::$messages[$domain][$text] ) ) {
@@ -240,7 +240,7 @@ class Locale
 			return $text;
 		}
 	}
-	
+
 	/**
 	 * Echo singular or plural version of the string, translated into the current locale, based on the count provided
 	 * 
@@ -248,12 +248,12 @@ class Locale
 	 * @param string $plural The plural form
 	 * @param string $count The count
 	 * @param string $domain (optional) The domain to search for the message	 
-	 **/	 	 	 	
+	 **/
 	public static function _ne( $singular, $plural, $count, $domain= 'habari' )
 	{
 		echo self::_n( $singular, $plural, $count, $domain );
 	}
-	
+
 	/**
 	 * Return a singular or plural string translated into the current locale based on the count provided
 	 * 
@@ -282,7 +282,7 @@ class Locale
  * Echo a version of the string translated into the current locale, alias for Locale::_e() 
  * 
  * @param string $text The text to translate
- **/	 	 	 	
+ **/
 function _e( $text, $domain= 'habari' )
 {
 	return Locale::_e( $text, $domain );
@@ -295,7 +295,7 @@ function _e( $text, $domain= 'habari' )
  * @param string $singular The singular form
  * @param string $plural The plural form
  * @param string $count The count
- **/	 	
+ **/
 function _ne( $singular, $plural, $count, $domain= 'habari' )
 {
 	return Locale::_ne( $singular, $plural, $count, $domain );
@@ -306,7 +306,7 @@ function _ne( $singular, $plural, $count, $domain= 'habari' )
  *  
  * @param string $text The text to translate
  * @return string The translated string
- **/	 	 	 	 	 	
+ **/
 function _t( $text, $domain= 'habari' )
 {
 	return Locale::_t( $text, $domain );
