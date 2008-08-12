d81bbdd772124724228f583886e2bb63cc539d6c
Rick Cockrum <rickc@contrib.habariproject.org>
2008-08-12 23:58:34 +0000

    Improved translatability of K2's search page. Thanks, msi!
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2283 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/k2/search.php
+++ themes/k2/search.php
@@ -3,14 +3,14 @@
   <div class="content">
    <div id="primary">
     <div id="primarycontent" class="hfeed">
-     <h2><?php _e('Search results for'); ?> <?php echo htmlspecialchars( $criteria ); ?></h2>
+     <h2><?php printf( _t("Search results for '%s'"), htmlspecialchars( $criteria ) ); ?></h2>
 <?php foreach ( $posts as $post ) { ?>
      <div id="post-<?php echo $post->id; ?>" class="<?php echo $post->statusname; ?>">
 
       <div class="entry-head">
        <h3 class="entry-title"><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h3>
        <small class="entry-meta">
-        <span class="chronodata"><abbr class="published"><?php echo $post->pubdate_out; ?></abbr></span><?php if ( $show_author ) { ?> by <?php echo $post->author->displayname; ?> <?php } ?>
+        <span class="chronodata"><abbr class="published"><?php echo $post->pubdate_out; ?></abbr></span><?php if ( $show_author ) { ?> <?php _e('by'); ?> <?php echo $post->author->displayname; ?> <?php } ?>
         <span class="commentslink"><a href="<?php echo $post->permalink; ?>#comments" title="<?php _e('Comments to this post'); ?>"><?php echo $post->comments->approved->count; ?>
 		<?php echo _n( 'Comment', 'Comments', $post->comments->approved->count ); ?></a></span>
 <?php if ( $user ) { ?>
