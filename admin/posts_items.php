1d8da1b361b69655d4dcf2a7872c2d23b5ad7905
Michael Bishop <miklb@contrib.habariproject.org>
2008-08-18 18:24:50 +0000

    Changing behavior of Mange Entry page to reflect assumed original intent of clicking post title to actually go to edit the post, not view it.   Leaving fixed Edit link in dropdown as well, and adding a View link between edit and delete in dropdown.  Fixes #590
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2299 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/posts_items.php
+++ admin/posts_items.php
@@ -5,15 +5,16 @@
 	<div class="head clear">
 		<span class="checkbox title pct35">
 			<input type="checkbox" class="checkbox" name="checkbox_ids[<?php echo $post->id; ?>]" id="checkbox_ids[<?php echo $post->id; ?>]">
-			<a href="<?php echo $post->permalink; ?>" class="title" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php echo $post->title; ?></a>
+			<a href="<?php URL::out('admin', 'page=publish&slug=' . $post->slug); ?>" class="title" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php echo $post->title; ?></a>
 		</span>
 		<span class="state pct10"><a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'status' => $post->status ) ); ?>" title="<?php _e('Search for other '.$post->statusname.' items'); ?>"><?php echo $post->statusname; ?></a></span>
 		<span class="author pct20"><span class="dim"><?php _e('by'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'user_id' => $post->user_id, 'type' => $post->content_type, 'status' => 'any') ); ?>" title="<?php _e('Search for other items by '.$post->author->displayname) ?>"><?php echo $post->author->displayname; ?></a></span>
 		<span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'year_month' => date( 'Y-m', strtotime( $post->pubdate ) ) ) ); ?>" title="<?php _e('Search for other items from '.date('M, Y', strtotime($post->pubdate))) ?>"><?php echo date('M j, Y', strtotime($post->pubdate)); ?></a></span>
 		<span class="time pct10"><span class="dim"><?php _e('at'); ?> <?php echo date('H:i', strtotime($post->pubdate)); ?></span></span>
 
-		<ul class="dropbutton">
+		<ul class="dropbutton">	
 			<li><a href="<?php URL::out('admin', 'page=publish&slug=' . $post->slug); ?>" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php _e('Edit'); ?></a></li>
+			<li><a href="<?php echo $post->permalink; ?>" title="<?php _e('View \''.$post->title.'\'') ?>"><?php _e('View'); ?></a></li>
 			<li><a href="#" onclick="itemManage.remove(<?php echo $post->id; ?>, 'post');return false;" title="<?php _e('Delete this item') ?>"><?php _e('Delete'); ?></a></li>
 		</ul>
 	</div>
