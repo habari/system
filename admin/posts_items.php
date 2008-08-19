8b91ea658905227193c8e162026553d5cc0ce641
Ali B <dmondark@contrib.habariproject.org>
2008-08-19 17:41:45 +0000

    Applying 2nd patch for #595. Thanks again msi!
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2304 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/posts_items.php
+++ admin/posts_items.php
@@ -5,11 +5,11 @@
 	<div class="head clear">
 		<span class="checkbox title pct35">
 			<input type="checkbox" class="checkbox" name="checkbox_ids[<?php echo $post->id; ?>]" id="checkbox_ids[<?php echo $post->id; ?>]">
-			<a href="<?php URL::out('admin', 'page=publish&slug=' . $post->slug); ?>" class="title" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php echo $post->title; ?></a>
+			<a href="<?php URL::out('admin', 'page=publish&slug=' . $post->slug); ?>" class="title" title="<?php printf( _t('Edit \'%s\''), $post->title ); ?>"><?php echo $post->title; ?></a>
 		</span>
-		<span class="state pct10"><a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'status' => $post->status ) ); ?>" title="<?php _e('Search for other '.$post->statusname.' items'); ?>"><?php echo $post->statusname; ?></a></span>
-		<span class="author pct20"><span class="dim"><?php _e('by'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'user_id' => $post->user_id, 'type' => $post->content_type, 'status' => 'any') ); ?>" title="<?php _e('Search for other items by '.$post->author->displayname) ?>"><?php echo $post->author->displayname; ?></a></span>
-		<span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'year_month' => date( 'Y-m', strtotime( $post->pubdate ) ) ) ); ?>" title="<?php _e('Search for other items from '.date('M, Y', strtotime($post->pubdate))) ?>"><?php echo date('M j, Y', strtotime($post->pubdate)); ?></a></span>
+		<span class="state pct10"><a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'status' => $post->status ) ); ?>" title="<?php printf( _t('Search for other %s items'), $post->statusname ); ?>"><?php echo $post->statusname; ?></a></span>
+		<span class="author pct20"><span class="dim"><?php _e('by'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'user_id' => $post->user_id, 'type' => $post->content_type, 'status' => 'any') ); ?>" title="<?php printf( _t('Search for other items by %s'), $post->author->displayname ); ?>"><?php echo $post->author->displayname; ?></a></span>
+		<span class="date pct15"><span class="dim"><?php _e('on'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'year_month' => date( 'Y-m', strtotime( $post->pubdate ) ) ) ); ?>" title="<?php printf( _t('Search for other items from %s'), date('M, Y', strtotime($post->pubdate)) ) ; ?>"><?php echo date('M j, Y', strtotime($post->pubdate)); ?></a></span>
 		<span class="time pct10"><span class="dim"><?php _e('at'); ?> <?php echo date('H:i', strtotime($post->pubdate)); ?></span></span>
 
 		<ul class="dropbutton">	
