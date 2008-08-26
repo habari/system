a06329c66421de74945ae1297cad956bcbf28262
Chris Meller <chris@doesnthaveone.com>
2008-08-26 03:18:44 +0000

    Changed the 'description' of the user to include their number of posts, even if the user has never logged in. Helpful for imported users and things like the Lipsum plugin, which generates a 'fake' user.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2374 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/users_items.php
+++ admin/users_items.php
@@ -17,38 +17,51 @@
 		<div class="clear">
 			<span class="nothing pct5">&nbsp;</span>
 			<span class="aka pct90">
+			
 			<?php
+			
 				if ( !$user->info->authenticate_time ) {
-					_e( "has not logged in yet.");
+					$last_login_message = _t( 'has not logged in yet' );
 				}
 				else {
-					$message_bits = array();
-					$post_statuses= Post::list_post_statuses();
-					unset( $post_statuses[array_search( 'any', $post_statuses )] );
-					foreach ( $post_statuses as $status_name => $status_id ) {
-						$count= Posts::count_by_author( $user->id, $status_id );
-						if ( $count > 0 ) {
-							$message = '<strong><a href="' . URL::get( 'admin', array( 'page' => 'posts', 'user_id' => $user->id, 'type' => Post::type( 'any' ), 'status' => $status_id ) ) . '">';
-							$message.= sprintf( '%d ' . _n( _t( $status_name . ' post' ), _t( $status_name . ' posts' ), $count ), $count ) ;
-							$message.= '</a></strong>';
-							$message_bits[]= $message;
-						}
-					}
-
-					if ( Posts::count_by_author( $user->id, Post::status('any') ) != 0 ) {
-						$string = _t('was last seen %1$s at %2$s and currently has %3$s');
-					}
-					else {
-						$string = _t('was last seen %1$s at %2$s');
-					}
-
-					printf( $string,
-						"<strong>" . date('M j, Y', strtotime($user->info->authenticate_time)) . "</strong>",
-						"<strong>" . date('H:i', strtotime($user->info->authenticate_time)) . "</strong>",
-						Format::and_list( $message_bits )
+					
+					$last_login_message = _t( 'was last seen %1$s at %2$s' );
+					$last_login_message = sprintf( $last_login_message,
+						'<strong>' . date('M j, Y', strtotime( $user->info->authenticate_time ) ) . '</strong>',
+						'<strong>' . date('H:i', strtotime( $user->info->authenticate_time ) ) . '</strong>'
 					);
+					
 				}
+				
+				$message_bits = array();
+				
+				$post_statuses = Post::list_post_statuses();
+				unset( $post_statuses[ array_search( 'any', $post_statuses ) ] );
+				
+				foreach( $post_statuses as $status_name => $status_id ) {
+					
+					$count = Posts::count_by_author( $user->id, $status_id );
+					
+					if ( $count > 0 ) {
+						$message = '<strong><a href="' . URL::get( 'admin', array( 'page' => 'posts', 'user_id' => $user->id, 'type' => Post::type( 'any' ), 'status' => $status_id ) ) . '">';
+						$message.= sprintf( '%d ' . _n( _t( $status_name . ' post' ), _t( $status_name . ' posts' ), $count ), $count ) ;
+						$message.= '</a></strong>';
+						$message_bits[]= $message;
+					}
+					
+				}
+				
+				if ( !empty( $message_bits ) ) {
+					$string = sprintf( _t( '%1$s and currently has %2$s' ), $last_login_message, Format::and_list( $message_bits ) );
+				}
+				else {
+					$string = $last_login_message;
+				}
+				
+				echo $string;
+				
 			?>
+			
 			</span>
 		</div>
 	</li>
