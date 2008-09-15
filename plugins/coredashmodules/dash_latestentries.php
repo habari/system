66d55f07faa37463dff6c3f586684c7a76992014
Ali B <dmondark@contrib.habariproject.org>
2008-09-15 23:44:58 +0000

    Fixing dates displayed in the core dash modules.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2569 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/coredashmodules/dash_latestentries.php
+++ plugins/coredashmodules/dash_latestentries.php
@@ -2,7 +2,7 @@
 
 		<?php foreach($recent_posts as $post): ?>
 		<li class="item clear">
-			<span class="date pct15 minor"><a href="<?php echo URL::get('display_entries_by_date', array('year' => date('Y', strtotime($post->pubdate)), 'month' => date('m', strtotime($post->pubdate)), 'day' => date('d', strtotime($post->pubdate)))); ?>" title="<?php printf(_t('Posted at %1$s'), date('g:m a \o\n F jS, Y', strtotime($post->pubdate))); ?>"><?php echo date('M j', strtotime($post->pubdate)); ?></a></span>
+			<span class="date pct15 minor"><a href="<?php echo URL::get('display_entries_by_date', array('year' => $post->pubdate->get( 'Y' ), 'month' => $post->pubdate->get( 'm' ), 'day' => $post->pubdate->get( 'd' ) ) ); ?>" title="<?php printf(_t('Posted at %1$s'), $post->pubdate->get( 'g:m a \o\n F jS, Y' ) ); ?>"><?php $post->pubdate->out( 'M j' ); ?></a></span>
 			<span class="title pct75"><a href="<?php echo $post->permalink; ?>"><?php echo $post->title; ?></a> <a class="minor" href="<?php Site::out_url('habari'); ?>/admin/user/<?php echo $post->author->username; ?>"> <?php _e('by'); ?> <?php echo $post->author->displayname; ?></a></span>
 			<span class="comments pct10"><a href="<?php echo $post->permalink; ?>#comments"><?php echo $post->comments->approved->count; ?></a></span>
 		</li>
