36993cc9e715842186d3fdfaa340f9fc824f444b
Ali B <dmondark@gmail.com>
2008-09-15 23:44:58 +0000

    Fixing dates displayed in the core dash modules.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2569 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/coredashmodules/dash_latestcomments.php
+++ plugins/coredashmodules/dash_latestcomments.php
@@ -2,7 +2,7 @@
 
 		<?php foreach ( $latestcomments_posts as $post ): ?>
 		<li class="item clear">
-			<span class="titleanddate pct85"><a href="<?php echo $post->permalink; ?>" class="title"><?php echo $post->title; ?></a> <a href="#" class="date minor"><?php echo date('M j', strtotime($post->pubdate)); ?></a></span>
+			<span class="titleanddate pct85"><a href="<?php echo $post->permalink; ?>" class="title"><?php echo $post->title; ?></a> <a href="#" class="date minor"><?php $post->pubdate->out( 'M j' ); ?></a></span>
 			<span class="comments pct15"><a href="<?php echo $post->permalink; ?>#comments" title="<?php printf(_n('%1$d comment', '%1$d comments', $post->comments->approved->comments->count), $post->comments->approved->comments->count); ?>"><?php echo $post->comments->approved->comments->count; ?></a></span>
 			<ul class="commentauthors pct85 minor">
 				<?php
