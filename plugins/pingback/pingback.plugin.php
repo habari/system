8fe017bf2122b35405b7704d371f3e8b9eec8f84
Ali B <dmondark@gmail.com>
2008-09-21 19:06:39 +0000

    Fixing dates of created pingbacks.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2602 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/pingback/pingback.plugin.php
+++ plugins/pingback/pingback.plugin.php
@@ -171,12 +171,12 @@ class Pingback extends Plugin
 			if ( !preg_match( '%.{0,100}?<a[^>]*?href\\s*=\\s*("|\'|)' . $target_uri . '\\1[^>]*?'.'>(.+?)</a>.{0,100}%s', $source_contents_filtered, $source_excerpt ) ) {
 				throw new XMLRPCException( 17 );
 			}
-			
+
 			/** Sanitize Data */
 			$source_excerpt = '...' . InputFilter::filter( $source_excerpt[0] ) . '...';
 			$source_title = InputFilter::filter($source_title);
 			$source_uri = InputFilter::filter($source_uri);
-			
+
 			/* Sanitize the URL */
 			if (!empty($source_uri)) {
 				$parsed = InputFilter::parse_url( $source_uri );
@@ -211,7 +211,7 @@ class Pingback extends Plugin
 				'ip'		=>	sprintf( "%u", ip2long( $_SERVER['REMOTE_ADDR'] ) ),
 				'content'	=>	$source_excerpt,
 				'status'	=>	Comment::STATUS_UNAPPROVED,
-				'date'		=>	date( 'Y-m-d H:i:s' ),
+				'date'		=>	HabariDateTime::date_create(),
 				'type' 		=> 	Comment::PINGBACK,
 				) );
 
