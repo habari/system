5d7c3712444e7db1cb6606327f8f5baaea60b063
Ali B <dmondark@contrib.habariproject.org>
2008-10-01 05:15:09 +0000

    Recommitting the part of r2299 that was lost in the latest merge. This fixes #680. Please see ticket for details. Thanks fesja for reporting.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2623 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/posts_items.php
+++ admin/posts_items.php
@@ -4,7 +4,7 @@
 	<div class="head clear">
 		<span class="checkbox title pct35">
 			<input type="checkbox" class="checkbox" name="checkbox_ids[<?php echo $post->id; ?>]" id="checkbox_ids[<?php echo $post->id; ?>]">
-			<a href="<?php echo $post->permalink; ?>" class="title" title="<?php _e('Edit \'$s\'', $post->title ) ?>"><?php echo $post->title; ?></a>
+			<a href="<?php echo URL::out('admin', 'page=publish&slug=' . $post->slug); ?>" class="title" title="<?php _e('Edit \'$s\'', $post->title ) ?>"><?php echo $post->title; ?></a>
 		</span>
 		<span class="state pct10"><a href="<?php URL::out('admin', array('page' => 'posts', 'type' => $post->content_type, 'status' => $post->status ) ); ?>" title="<?php _e('Search for other $s items', $post->statusname); ?>"><?php echo $post->statusname; ?></a></span>
 		<span class="author pct20"><span class="dim"><?php _e('by'); ?></span> <a href="<?php URL::out('admin', array('page' => 'posts', 'user_id' => $post->user_id, 'type' => $post->content_type, 'status' => 'any') ); ?>" title="<?php _e('Search for other items by $s', $post->author->displayname ) ?>"><?php echo $post->author->displayname; ?></a></span>
