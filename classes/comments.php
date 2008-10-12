2cd5733d48cd5f38306e3d9e6f3e3463766f9b9c
Chris Meller <chrismeller@contrib.habariproject.org>
2008-10-12 22:00:25 +0000

    Fixed a typo in create_function() that rick_c apparently made. :P
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2663 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/comments.php
+++ classes/comments.php
@@ -75,7 +75,7 @@ class Comments extends ArrayObject
 				if ( isset( $paramset['status'] ) && FALSE !== $paramset['status'] ) {
 					if(is_array( $paramset['status'] )) {
 						$paramset['status'] = array_diff( $paramset['status'], array( 'any' ) );
-						array_walk( $paramset['status'], createfunction( '&$a,$b', '$a = Comment::status( $a );' ) );
+						array_walk( $paramset['status'], create_function( '&$a,$b', '$a = Comment::status( $a );' ) );
 						$where[]= "type IN (" . Utils::placeholder_string(count($paramset['status'])) . ")";
 						$params = array_merge( $params, $paramset['status'] );
 					}
