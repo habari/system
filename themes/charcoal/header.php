4c37d828ca8093e2a3acec7b06df2a5dcf0dd3c8
Chris Meller <chris@doesnthaveone.com>
2008-10-15 01:52:07 +0000

    Moving the Content-Type meta tags above the title (the first of the content) in the headers of all the default themes. Having the title first could, potentially, cause encoding issues in some scenarios.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2667 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/header.php
+++ themes/charcoal/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title><?php Options::out( 'title' ) ?><?php if($request->display_entry && isset($post)) { echo " :: {$post->title}"; } ?></title>
 	<meta http-equiv="Content-Type" content="text/html">
+	<title><?php Options::out( 'title' ) ?><?php if($request->display_entry && isset($post)) { echo " :: {$post->title}"; } ?></title>
 	<meta name="generator" content="Habari">
 	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php $theme->feed_alternate(); ?>">
 	<link rel="edit" type="application/atom+xml" title="Atom Publishing Protocol" href="<?php URL::out( 'atompub_servicedocument' ); ?>">
