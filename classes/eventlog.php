d1d7836a9020b0538f1b84fb1889b7963bf00643
Rick Cockrum <rickc@contrib.habariproject.org>
2008-10-27 02:26:20 +0000

    Moving a wild brace that had somehow escaped it's stall.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2721 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/eventlog.php
+++ classes/eventlog.php
@@ -244,6 +244,7 @@ class EventLog extends ArrayObject
 
 				$wheres[]= ' (' . implode( ' AND ', $where ) . ') ';
 			}
+		}
 
 		if ( isset( $page ) && is_numeric( $page ) ) {
 			$offset = ( intval( $page ) - 1 ) * intval( $limit );
@@ -304,7 +305,6 @@ class EventLog extends ArrayObject
 			$return_value->get_param_cache = $paramarray;
 			return $return_value;
 		}
-	}
 	}
 
 }
