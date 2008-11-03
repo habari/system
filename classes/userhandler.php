c541d7ae628160fd040113b62a480ca9f4478122
Matt Read <mattread@contrib.habariproject.org>
2008-11-03 20:48:42 +0000

    sanitize username for html output
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2752 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/userhandler.php
+++ classes/userhandler.php
@@ -78,7 +78,7 @@ class UserHandler extends ActionHandler
 			$request->{$rule->name}= ( $rule->name == URL::get_matched_rule()->name );
 		}
 		$this->theme->assign( 'request', $request );
-		$this->theme->assign( 'habari_username', $name );
+		$this->theme->assign( 'habari_username', htmlentities($name, ENT_QUOTES, 'UTF-8') );
 		$this->display( 'login' );
 		return TRUE;
 	}
