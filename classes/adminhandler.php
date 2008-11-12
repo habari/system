0474bae8c2642154df894ba1abab23dd4b09dcdc
Sean Coates <sean@contrib.habariproject.org>
2008-11-12 00:50:34 +0000

    whitespace before I attack this file
    git-svn-id: http://svn.habariproject.org/habari/branches/sginput/htdocs/system@2797 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/adminhandler.php
+++ classes/adminhandler.php
@@ -26,15 +26,15 @@ class AdminHandler extends ActionHandler
 				echo '{callback: function(){location.href="'.$_SERVER['HTTP_REFERER'].'"} }';
 			}
 			else {
-			if ( !empty( $_POST ) ) {
-				Session::add_to_set( 'last_form_data', $_POST, 'post' );
-				Session::error( _t('We saved the last form you posted. Log back in to continue its submission.'), 'expired_form_submission' );
-			}
-			if ( !empty( $_GET ) ) {
-				Session::add_to_set( 'last_form_data', $_GET, 'get' );
-				Session::error( _t('We saved the last form you posted. Log back in to continue its submission.'), 'expired_form_submission' );
-			}
-			Utils::redirect( URL::get( 'user', array( 'page' => 'login' ) ) );
+				if ( !empty( $_POST ) ) {
+					Session::add_to_set( 'last_form_data', $_POST, 'post' );
+					Session::error( _t('We saved the last form you posted. Log back in to continue its submission.'), 'expired_form_submission' );
+				}
+				if ( !empty( $_GET ) ) {
+					Session::add_to_set( 'last_form_data', $_GET, 'get' );
+					Session::error( _t('We saved the last form you posted. Log back in to continue its submission.'), 'expired_form_submission' );
+				}
+				Utils::redirect( URL::get( 'user', array( 'page' => 'login' ) ) );
 			}
 			exit;
 		}
