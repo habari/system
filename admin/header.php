5868f25075d71b1b510f00978a1b7aef405b8955
Chris Meller <chrismeller@contrib.habariproject.org>
2008-11-14 16:38:53 +0000

    Moving the admin_header plugin hook below stylesheet and javascript output so that plugins can properly execute things that require or override a default include.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2811 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/header.php
+++ admin/header.php
@@ -17,13 +17,16 @@
 	};
 	</script>
 	<?php
-		Plugins::act( 'admin_header', $this );
 		Stack::out( 'admin_header_javascript', '<script src="%s" type="text/javascript"></script>'."\r\n" );
 		Stack::out( 'admin_stylesheet', '<link rel="stylesheet" type="text/css" href="%s" media="%s">'."\r\n" );
 	?>
 	<!--[if IE 7]>
 	<link rel="stylesheet" type="text/css" href="<?php Site::out_url('admin_theme'); ?>/css/ie.css" media="screen">
 	<![endif]-->
+
+	<?php
+		Plugins::act( 'admin_header', $this );
+	?>
 
 </head>
 <body class="page-<?php echo $admin_page; ?>">
@@ -36,16 +39,16 @@
 		<div id="menulist" class="dropbuttonlist">
 			<ul>
 			<?php foreach($mainmenu as $menu_id => $menu): ?>
-				<li id="link-<?php echo $menu_id ?>" class="<?php if($menu['selected'] == TRUE) { echo 'selected'; } ?><?php if(isset($menu['submenu'])): ?> submenu<?php endif; ?>" title="<?php echo $menu['title']; ?>"><a class="top" href="<?php echo $menu['url']; ?>"><?php echo $menu['text']; ?> 
+				<li id="link-<?php echo $menu_id ?>" class="<?php if($menu['selected'] == TRUE) { echo 'selected'; } ?><?php if(isset($menu['submenu'])): ?> submenu<?php endif; ?>" title="<?php echo $menu['title']; ?>"><a class="top" href="<?php echo $menu['url']; ?>"><?php echo $menu['text']; ?>
 				<?php if(isset($menu['hotkey']) && $menu['hotkey'] != ''): ?><span class="hotkey"><?php echo $menu['hotkey']; ?></span><?php endif; ?>
 				</a>
 				<?php if(isset($menu['submenu'])): ?>
 				<ul class="submenu">
-				 <?php foreach($menu['submenu'] as $submenu_id => $submenu_item): ?> 
-				 	<li id="link-<?php echo $submenu_id ?>" title="<?php echo $submenu_item['title']; ?>" class="sub<?php if(isset($submenu_item['hotkey']) && $submenu_item['hotkey'] != ''): ?> hotkey-<?php echo $submenu_item['hotkey']; ?><?php endif; ?>"><a href="<?php echo $submenu_item['url']; ?>"><?php echo $submenu_item['text']; ?> 
-				 	<?php if(isset($submenu_item['hotkey']) && $submenu_item['hotkey'] != ''): ?><span class="hotkey"><?php echo $submenu_item['hotkey']; ?></span><?php endif; ?> 
-				 	</a></li> 
-				 <?php endforeach; ?> 
+				 <?php foreach($menu['submenu'] as $submenu_id => $submenu_item): ?>
+				 	<li id="link-<?php echo $submenu_id ?>" title="<?php echo $submenu_item['title']; ?>" class="sub<?php if(isset($submenu_item['hotkey']) && $submenu_item['hotkey'] != ''): ?> hotkey-<?php echo $submenu_item['hotkey']; ?><?php endif; ?>"><a href="<?php echo $submenu_item['url']; ?>"><?php echo $submenu_item['text']; ?>
+				 	<?php if(isset($submenu_item['hotkey']) && $submenu_item['hotkey'] != ''): ?><span class="hotkey"><?php echo $submenu_item['hotkey']; ?></span><?php endif; ?>
+				 	</a></li>
+				 <?php endforeach; ?>
 				 </ul>
 				<?php endif; ?>
 				</li>
