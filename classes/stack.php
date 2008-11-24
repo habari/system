b7d321b49e6126ca5cd4cca9c432d1a30d9e4bca
Owen Winkler <epithet@gmail.com>
2008-11-24 05:24:59 +0000

    Stacks now support the $after parameter.   Setting the $after parameter lets you name one or more named stack entries after which a new stack entry will appear.  So if you add a new stack with 	Stack::add('admin_header_javascript', $output, 'podcast', array('jquery', 'ui.tabs')); then the 'podcast' entry will be inserted after both the 'jquery' and the 'ui.tabs' items.
    Also, Stack::scripts() and Stack::styles() have been added which allow Stack::add() to be called to add both URL-referenced and inline styles.  For example, you can now call this to add an inline script after jquery is loaded:  Stack::add('admin_header_javascript', '$(function(){alert("on load");});', 'my alert', 'jquery');
    
    Referenced URLs added to these stacks must be absolute URLs, starting with 'http://'
    
    This should eliminate the need for the 'admin_header_after' hook.
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2865 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/stack.php
+++ classes/stack.php
@@ -6,20 +6,20 @@
  * can be output using a specific formatting string.
  * This is useful for collecting a set of unique javascript references to output
  * and then insert them at a specific point on the page.
- * 
+ *
  * <code>
- * // Add jquery to the javascript stack: 
+ * // Add jquery to the javascript stack:
  * Stack::add( 'template_header_javascript', Site::get_url('scripts') . '/jquery.js', 'jquery' );
  *
  * // Add stylesheet to theme_stylesheet stack with media type
  * Stack::add( 'template_stylesheet', array( Site::get_url('theme') . '/style.css', 'screen' ), 'style' );
- *  
+ *
  * // Output the javascript stack:
  * Stack::out( 'template_header_javascript', '<script src="%s" type="text/javascript"></script>' );
  *
  * // Output the theme_stylesheet stack:
  * Stack::out( 'template_stylesheet', '<link rel="stylesheet" type="text/css"  href="%s" media="%s">' );
- * </code>   
+ * </code>
  *
  * @package Habari
  */
@@ -27,14 +27,14 @@
 class Stack
 {
 	private static $stacks = array();
-	
+
 	/**
 	 * Private constructor for Stack.
-	 * Stack objects should only be created using the static 
+	 * Stack objects should only be created using the static
 	 * method Static::create_stack(), or one of the methods that
-	 * adds a value directly to a stack.  This prevents multiple Stack 
+	 * adds a value directly to a stack.  This prevents multiple Stack
 	 * objects from being created with the same name.
-	 * 
+	 *
 	 * @param mixed $input An array or ArrayObject to create the stack from.
 	 * @return array The created stack
 	 **/
@@ -47,7 +47,7 @@ class Stack
 	 * Retreive a named stack instance
 	 * @param string $stack_name The name of the stack to return
 	 * @return Stack The requested stack
-	 **/	  	 	 	
+	 **/
 	public static function get_named_stack( $stack_name )
 	{
 		if ( isset( self::$stacks[$stack_name] ) ) {
@@ -57,7 +57,7 @@ class Stack
 			return self::create_stack( $stack_name );
 		}
 	}
-	
+
 	/**
 	 * Check for the existence of a given stack item.
 	 *
@@ -66,19 +66,19 @@ class Stack
 	 * @return boolean TRUE if the item exists, FALSE otherwise.
 	 */
 	public static function has ( $stack_name, $value_name ) {
-		
+
 		// get the stack
 		$stack = self::get_named_stack( $stack_name );
-		
+
 		if ( isset( $stack[ $value_name ] ) ) {
 			return true;
 		}
 		else {
 			return false;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Get a single item from a given stack.
 	 *
@@ -88,24 +88,24 @@ class Stack
 	 * @return mixed The item, or $default_value if it does not exist.
 	 */
 	public static function get_item ( $stack_name, $value_name, $default_value = null ) {
-		
+
 		// get the stack
 		$stack = self::get_named_stack( $stack_name );
-		
+
 		if ( isset( $stack[ $value_name ] ) ) {
 			return $stack[ $value_name ];
 		}
 		else {
 			return $default_value;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Creates and retreives a named stack instance
 	 * @param string $stack_name The name of the stack to create and return
 	 * @return array The created stack
-	 **/	  	 	 	
+	 **/
 	public static function create_stack( $stack_name )
 	{
 		if ( empty( self::$stacks[$stack_name] ) ) {
@@ -114,29 +114,52 @@ class Stack
 		}
 		return self::$stacks[$stack_name];
 	}
-	
+
 	/**
 	 * Add a value to a stack
 	 * @param string $stack_name The name of the stack
 	 * @param mixed $value The value to add
 	 * @param string $value_name The name of the value to add
-	 * @return array The stack that was added to	 
-	 **/	 
-	public static function add( $stack_name, $value, $value_name = null )
+	 * @return array The stack that was added to
+	 **/
+	public static function add( $stack_name, $value, $value_name = null, $after = null )
 	{
 		$stack = self::get_named_stack( $stack_name );
 		$value_name = $value_name ? $value_name : md5( serialize( $value ) );
-		$stack[$value_name]= $value;
+		if(!is_null($after)) {
+			if(!is_array($after)) {
+				$after = array($after);
+			}
+			$newstack = array();
+			foreach($stack as $k => $v) {
+				$newstack[$k] = $v;
+				$inserted = false;
+				if(in_array($k, $after)) {
+					unset($after[array_search($k, $after)]);
+					if(count($after) == 0) {
+						$newstack[$value_name] = $value;
+						$inserted = true;
+					}
+				}
+			}
+			if(!$inserted) {
+				$newstack[$value_name] = $value;
+			}
+			$stack = $newstack;
+		}
+		else {
+			$stack[$value_name]= $value;
+		}
 		self::$stacks[$stack_name]= $stack;
 		return $stack;
 	}
-	
+
 	/**
 	 * Remove a value to a stack
 	 * @param string $stack_name The name of the stack
 	 * @param string $value_name The name of the value to remove
-	 * @return array The rest of the stack, post-remove	 
-	 **/	 
+	 * @return array The rest of the stack, post-remove
+	 **/
 	public static function remove( $stack_name, $value_name )
 	{
 		$stack = self::get_named_stack( $stack_name );
@@ -146,12 +169,12 @@ class Stack
 		self::$stacks[$stack_name]= $stack;
 		return $stack;
 	}
-	
+
 	/**
 	 * Returns all of the values of the stack
 	 * @param string $stack_name The name of the stack to output
 	 * @param mixed $format A printf-style formatting string or callback used to output each stack element
-	 **/	   	 
+	 **/
 	public static function get( $stack_name, $format = null)
 	{
 		$out = '';
@@ -159,7 +182,7 @@ class Stack
 		$stack = Plugins::filter( 'stack_out', $stack, $stack_name );
 		foreach( $stack as $element ) {
 			if ( is_callable($format) ) {
-				$out.= call_user_func_array( $format, (array) $element ); 
+				$out.= call_user_func_array( $format, (array) $element );
 			}
 			elseif ( is_string( $format ) ) {
 				$out.= vsprintf( $format, (array) $element );
@@ -170,15 +193,37 @@ class Stack
 		}
 		return $out;
 	}
-	
+
 	/**
 	 * Outputs all of the values of the stack
 	 * @param string $stack_name The name of the stack to output
 	 * @param mixed $format A printf-style formatting string or callback used to output each stack element
-	 **/	   	 
+	 **/
 	public static function out( $stack_name, $format = null)
 	{
 		echo self::get( $stack_name, $format );
+	}
+
+	public static function scripts( $element )
+	{
+		if(strpos($element, 'http://') === 0 && strpos($element, "\n") === FALSE) {
+			$output = sprintf( '<script src="%s" type="text/javascript"></script>'."\r\n", $element);
+		}
+		else {
+			$output = sprintf( '<script type="text/javascript">%s</script>'."\r\n", $element);
+		}
+		return $output;
+	}
+
+	public static function styles( $element, $typename )
+	{
+		if(strpos($element, 'http://') === 0 && strpos($element, "\n") === FALSE) {
+			$output = sprintf( '<link rel="stylesheet" type="text/css" href="%s" media="%s">'."\r\n", $element, $typename);
+		}
+		else {
+			$output = sprintf( '<style type="text/stylesheet" media="%s">%s</style>'."\r\n", $typename, $element);
+		}
+		return $output;
 	}
 }
 
