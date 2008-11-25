c003c0a2b98b46ba4afa4816a287f717f88d0de0
Caius Durling <caius@contrib.habariproject.org>
2008-11-25 00:46:15 +0000

    Formatting a couple of SQL queries
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2875 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/tags.php
+++ classes/tags.php
@@ -21,7 +21,14 @@ class Tags extends ArrayObject
 		 * which are not related (yet) to any post itself.  These
 		 * tags are essentially lost to the world.
 		 */
-		$tags = DB::get_results( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t LEFT JOIN {tag2post} tp ON t.id=tp.tag_id GROUP BY id, tag, slug ORDER BY tag ASC' );
+		$tags = DB::get_results( 'SELECT t.id AS id, 
+			t.tag_text AS tag, 
+			t.tag_slug AS slug, 
+			COUNT(tp.tag_id) AS count 
+			FROM {tags} t 
+			LEFT JOIN {tag2post} tp ON t.id=tp.tag_id 
+			GROUP BY id, tag, slug 
+			ORDER BY tag ASC' );
 		return $tags;
 	}
 
@@ -32,7 +39,14 @@ class Tags extends ArrayObject
 	 **/
 	public static function get_one($tag)
 	{
- 		return DB::get_row( 'SELECT t.id AS id, t.tag_text AS tag, t.tag_slug AS slug, COUNT(tp.tag_id) AS count FROM {tags} t LEFT JOIN {tag2post} tp ON t.id=tp.tag_id WHERE tag_slug = ? OR t.id = ? GROUP BY id, tag, slug', array( Utils::slugify( $tag ), $tag ) );
+ 		return DB::get_row( 'SELECT t.id AS id, 
+			t.tag_text AS tag, 
+			t.tag_slug AS slug,
+			COUNT(tp.tag_id) AS count 
+			FROM {tags} t 
+			LEFT JOIN {tag2post} tp ON t.id=tp.tag_id 
+			WHERE tag_slug = ? OR t.id = ? 
+			GROUP BY id, tag, slug', array( Utils::slugify( $tag ), $tag ) );
 	}
 
 	/**
