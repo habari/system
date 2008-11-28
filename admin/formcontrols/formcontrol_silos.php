0a3641a618577d9eaa914014a451bc33114cfd0d
Rick Cockrum <rickc@contrib.habariproject.org>
2008-11-28 04:38:44 +0000

    Remove most of the annoying flicker with the silos when the publish page loads in the admin when using FireFox. Some is left. Better fixes are welcome. Fixes ticket #576.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2899 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_silos.php
+++ admin/formcontrols/formcontrol_silos.php
@@ -1,10 +1,10 @@
 <div class="container pagesplitter">
-	<ul id="mediatabs" class="tabs">
+	<ul id="mediatabs" class="tabs hidden">
 		<?php foreach($silos as $ct => $silodir): ?><li><a href="#silo_<?php echo $ct; ?>"<?php if($silodir->icon != NULL): ?> style="background-image: url(<?php echo $silodir->icon; ?>)"<?php endif; ?>><?php echo $silodir->path; ?></a></li><?php endforeach; ?>
 	</ul>
 
 	<?php foreach($silos as $ct => $silodir): ?>
-		<div id="silo_<?php echo $ct; ?>" class="splitter mediasplitter">
+		<div id="silo_<?php echo $ct; ?>" class="splitter mediasplitter hidden">
 			<div class="toload pathstore" style="display:none;"><?php echo $silodir->path; ?></div>
 			<div class="splitterinside">
 				<div id="mediaspinner"></div>
