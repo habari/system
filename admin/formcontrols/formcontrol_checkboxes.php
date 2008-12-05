20d397becb6d0b51a5723090a8a61be7983ba931
Chris Meller <chrismeller@contrib.habariproject.org>
2008-12-05 15:17:51 +0000

    Moving the actual admin input elements outside the <label> tag that currently contains them to be a little more semantic. Fixes #791.
    Form UI needs to be updated to add for="" attributes to all labels so that they actually function.
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@2920 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_checkboxes.php
+++ admin/formcontrols/formcontrol_checkboxes.php
@@ -1,7 +1,9 @@
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
 <ul>
 <?php foreach($options as $key => $text) : ?>
-	<li><label><input type="checkbox" name="<?php echo $field; ?>[]" value="<?php echo $key; ?>"<?php echo ( in_array( $key, (array) $value ) ? ' checked' : '' ); ?>><?php echo htmlspecialchars($text); ?></label></li>
+	<li>
+		<input type="checkbox" name="<?php echo $field; ?>[]" value="<?php echo $key; ?>"<?php echo ( in_array( $key, (array) $value ) ? ' checked' : '' ); ?>><label><?php echo htmlspecialchars($text); ?></label>
+	</li>
 <?php endforeach; ?>
 </ul>
 <input type="hidden" name="<?php echo $field; ?>_submitted" value="1">
--- admin/formcontrols/formcontrol_password.php
+++ admin/formcontrols/formcontrol_password.php
@@ -1,5 +1,6 @@
-<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>><label><?php echo $this->caption; ?><input type="password" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars($outvalue); ?>"></label>
-<?php if($message != '') : ?>
-<p class="error"><?php echo $message; ?></p>
-<?php endif; ?>
+<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
+	<label><?php echo $this->caption; ?></label><input type="password" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars($outvalue); ?>">
+	<?php if($message != '') : ?>
+		<p class="error"><?php echo $message; ?></p>
+	<?php endif; ?>
 </div>
