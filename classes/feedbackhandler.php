3c27da09691d5704bfd3c55d4398d1382e325e26
Rick Cockrum <rickc@contrib.habariproject.org>
2008-12-21 05:38:01 +0000

    User::identify() always returns an object now, so test whether they're logged in or not.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2957 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/feedbackhandler.php
+++ classes/feedbackhandler.php
@@ -141,7 +141,7 @@ class FeedbackHandler extends ActionHandler
 			// if no cookie exists, we should set one
 			// but only if the user provided some details
 			$cookie = 'comment_' . Options::get('GUID');
-			if ( ( ! User::identify() )
+			if ( ( ! $user->loggedin )
 				&& ( ! isset( $_COOKIE[$cookie] ) )
 				&& ( ! empty( $this->handler_vars['name'] )
 					|| ! empty( $this->handler_vars['email'] )
