6de61d3a714625fc2b06bed1024d53c81eacf627
Ali B <dmondark@contrib.habariproject.org>
2009-01-17 12:48:21 +0000

    'ISNULL' is a function and cannot be used as a condition. The correct is 'IS NULL'. This fixes the db displayed everywhere error when logged in (MySQL).
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@2996 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/posts.php
+++ classes/posts.php
@@ -402,7 +402,7 @@ class Posts extends ArrayObject implements IsContent
 
 					// If a user can read posts without tokens, let him
 					if(User::identify()->can('null_token', 'read')) {
-						$perm_where[] = '{post_tokens}.token_id ISNULL';
+						$perm_where[] = '{post_tokens}.token_id IS NULL';
 					}
 
 					// If there are granted permissions to check, add them to the where clause
@@ -517,9 +517,9 @@ class Posts extends ArrayObject implements IsContent
 		/**
 		 * DEBUG: Uncomment the following line to display everything that happens in this function
 		 */
-		//print_R('<pre>'.$query.'</pre>');
-		//Utils::debug( $paramarray, $fetch_fn, $query, $params );
-		// Session::notice($query);
+		print_R('<pre>'.$query.'</pre>');
+		Utils::debug( $paramarray, $fetch_fn, $query, $params );
+		Session::notice($query);
 
 		/**
 		 * Execute the SQL statement using the PDO extension
