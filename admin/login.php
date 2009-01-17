4255bd2e6038f4ddbe1497c99f2cf97df14fa3eb
ringmaster <epithet@gmail.com>
2009-01-17 22:10:58 +0000

    Allow users to request a reset and then reset their passwords.  Fixes #184.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3022 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/login.php
+++ admin/login.php
@@ -47,6 +47,9 @@
 					<p>
 						<input class="submit" type="submit" value="<?php _e('Login'); ?>">
 					</p>
+					<p id="password_utils">
+						<a href="#" onclick="if($('#habari_username').val() ==''){alert('Please fill in the name field, and click again.');}else{window.location='<?php URL::out('user', array('page'=>'password_request')); ?>?username=' +$('#habari_username').val();}"><?php _e('Forgot your password?'); ?></a>
+					</p>
 
 				</form>
      			<?php Plugins::act( 'theme_loginform_after' ); ?>
