bdc5e75be7db0a84b6dd00bdfa82a9f46408cf04
michaeltwofish <michaeltwofish@contrib.habariproject.org>
2009-01-19 03:36:36 +0000

    Make save always the default, so that when one hits enter in the title or tags field the post is saved (not deleted or published). Fixes #555.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3037 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/publish.php
+++ admin/publish.php
@@ -9,21 +9,29 @@
 <script type="text/javascript">
 $(document).ready(function(){
 	$('.container').addClass('transparent');
+	// If this post has been saved, add a delete button and a nonce for authorising deletes
 	<?php if(isset($post->id) && ($post->id != '')) : ?>
-	$('.container.buttons').prepend($('<input type="submit" name="submit" id="delete" class="button delete" tabindex="6" value="<?php _e('Delete'); ?>">'));
+	$('.container.buttons').prepend($('<input type="button" id="delete" class="button delete" tabindex="6" value="<?php _e('Delete'); ?>">'));
 	$('#delete').click(function(){
 		$('#create-content')
 			.append($('<input type="hidden" name="nonce" value="<?php echo $wsse['nonce']; ?>"><input type="hidden" name="timestamp" value="<?php echo $wsse['timestamp']; ?>"><input type="hidden" name="digest" value="<?php echo $wsse['digest']; ?>">'))
-			.attr('action', '<?php URL::out( 'admin', array('page' => 'delete_post', 'id' => $post->id )); ?>');
+			.attr('action', '<?php URL::out( 'admin', array('page' => 'delete_post', 'id' => $post->id )); ?>')
+			.submit();
 	});
 	<?php endif; ?>
 
+	// If the post hasn't been published, add a publish button
 	<?php if(isset($statuses['published']) && $post->status != $statuses['published']) : ?>
-	$('.container.buttons').prepend($('<input type="submit" name="submit" id="publish" class="button publish" tabindex="5" value="<?php _e('Publish'); ?>">'));
+	$('.container.buttons').prepend($('<input type="button" id="publish" class="button publish" tabindex="5" value="<?php _e('Publish'); ?>">'));
 	$('#publish').click( function() {
 		$('#status').val(<?php echo $statuses['published']; ?>);
 	});
 	<?php endif; ?>
+
+	// Submit when the publish button is clicked.
+	$('#publish').click( function() {
+		$('#create-content').submit();
+	});
 });
 </script>
 
