52117ce49d6ef471a05f8d253c30bdab14a8c4d4
Michael Bishop <miklb@contrib.habariproject.org>
2009-01-31 04:53:28 +0000

    removing pct classes on th and td, moving margin on table to padding on th.  Better allows for additional columns to be added to items down the road
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3092 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/dash_status.php
+++ admin/dash_status.php
@@ -1,5 +1,5 @@
 <table id="dash_status">
 <?php foreach($status_data as $label => $value): ?>
-<tr class="status_item"><th scope="row" class="label pct70"><?php echo $label; ?></th><td class="pct25"><?php echo $value; ?></td></tr>
+<tr class="status_item"><th scope="row" class="label"><?php echo $label; ?></th><td><?php echo $value; ?></td></tr>
 <?php endforeach; ?>
 </table>
