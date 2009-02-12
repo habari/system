2f885d869a13c50360f6e25a3ee0a15e665dda2d
Matthias Bauer <moeffju@contrib.habariproject.org>
2009-02-12 13:25:06 +0000

    Add license boilerplate to all system files, as per http://www.apache.org/dev/apply-license.html#new
    Maybe this will help ohloh grok our licensing, too.
    
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3184 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/searchform.php
+++ themes/charcoal/searchform.php
@@ -1,3 +1,20 @@
+<?php /*
+
+  Copyright 2007-2009 The Habari Project <http://www.habariproject.org/>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/ ?>
 <form method="get" id="search-form" action="<?php URL::out('display_search'); ?>">
 	<input type="text" name="criteria" id="search-box" value="<?php if ( isset( $criteria ) ) { echo htmlentities($criteria, ENT_COMPAT, 'UTF-8');} else { printf( _t("Search %s"), Options::get("title")) ;}  ?>" onfocus="if (this.value == '<?php printf( _t("Search %s"), str_replace("'", "\'", Options::get("title")) ); ?>') {this.value = '';}" onblur="if (this.value == '') {this.value = '<?php printf( _t("Search %s"), str_replace("'", "\'", Options::get("title")) ); ?>';}" >
 	<input type="submit" id="search-btn" value="" title="<?php _e( "Go" ); ?>">
