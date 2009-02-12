14ecc8d3b88c4fa56aca6d385f13c0036773c70c
Matt Read <mattread@contrib.habariproject.org>
2009-02-12 23:58:26 +0000

    add restore action to posts management page
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3187 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/undelete/undelete.plugin.php
+++ plugins/undelete/undelete.plugin.php
@@ -6,6 +6,8 @@
  * This class provides undelete functionality for posts and comments, and
  * provides a trashcan interface for restoring items.
  *
+ * @todo Document methods
+ * @todo Provide an undo popup link like in gmail.
  **/
 
 class Undelete extends Plugin
@@ -22,7 +24,7 @@ class Undelete extends Plugin
 			'url' => 'http://habariproject.org/',
 			'author' => 'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
-			'version' => '1.0',
+			'version' => '1.1.alpha',
 			'description' => 'Stores deleted items in a virtual trashcan to support undelete functionality.',
 			'license' => 'Apache License 2.0',
 		);
@@ -42,7 +44,6 @@ class Undelete extends Plugin
 	}
 
 	/**
-	 * function filter_allow_post_delete
 	 * This function is executed when the filter "before_post_delete" is
 	 * called just before a post is to be deleted.
 	 * This filter should return a boolean value to indicate whether
@@ -57,9 +58,6 @@ class Undelete extends Plugin
 		// and then return false.  The Post::delete() method will
 		// see the false return value, and simply return, leaving
 		// the post in the database.
-		// However, we should capture the current status and save
-		// it in a postinfo record, so that undelete can restore
-		// it to that status
 		if( $post->status != Post::status( 'deleted' ) && is_object( User::get_by_id( $post->user_id ) ) ) {
 			$post->info->prior_status = $post->status;
 			$post->status = Post::status( 'deleted' );
@@ -71,12 +69,19 @@ class Undelete extends Plugin
 		}
 	}
 
-	function filter_post_actions($actions, $post) {
-		if($post->status == Post::status('deleted')) {
+	public function filter_post_actions($actions, $post)
+	{
+		if ( $post->status == Post::status('deleted') ) {
 			$actions['remove']['label']= _t('Delete forever');
 			$actions['remove']['title']= _t('Permanently delete this post');
+			$remove = array_pop($actions);
+			$restore = array(
+				'url' => 'javascript:unDelete.post('. $post->id . ');',
+				'title' => _t('Restore this item'),
+				'label' => _t('Restore')
+			);
+			array_push($actions, $restore, $remove);
 		}
-
 		return $actions;
 	}
 
@@ -85,19 +90,38 @@ class Undelete extends Plugin
 	 * This function reverts a post's status from 'deleted' to whatever
 	 * it previously was.
 	**/
-	function undelete_post( $post_id )
+	private function undelete_post( $post_id )
 	{
-		$post = Post::get( array( 'id' => $post_id ) );
-		$post->status = $post->info->prior_status;
-		unset( $post->info->prior_status );
-		$post->update();
+		$post = Post::get( array( 'id' => $post_id, 'status' => Post::status('any') ) );
+		if ( $post->status == Post::status('deleted') ) {
+			$post->status = $post->info->prior_status ? $post->info->prior_status : Post::status( 'draft' );
+			unset( $post->info->prior_status );
+			$post->update();
+
+			EventLog::log(
+				sprintf(_t('Post %1$s (%2$s) restored.'), $post->id, $post->slug),
+				'info', 'content', 'habari'
+			);
+			//scheduled post
+			if( $post->status == Post::status( 'scheduled' ) ) {
+				Posts::update_scheduled_posts_cronjob();
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
-/*
-	public function filter_rewrite_rules()
+	public function action_auth_ajax_undelete()
 	{
+		if ( $this->undelete_post($_POST['id']) ) {
+			_e('Restored post %d', array($_POST['id']) );
+		}
+		else {
+			_e( 'Could not restore post %d', array($_POST['id']) );
+		}
 	}
-*/
 
 	public function filter_plugin_config( $actions, $plugin_id )
 	{
@@ -112,21 +136,21 @@ class Undelete extends Plugin
 	{
 		if ( $plugin_id == $this->plugin_id() ) {
 			switch ( $action ) {
-			case _t( 'Configure' ):
-				$ui = new FormUI( strtolower( get_class( $this ) ) );
-				$ui->append( 'textarea', 'style', 'option:undelete__style', _t( 'Style declaration for deleted content:' ) );
-				$ui->append( 'submit', 'save', 'Save' );
-				$ui->on_success( array( $this, 'updated_config' ) );
-				$ui->out();
-				break;
+				case _t( 'Configure' ):
+					$ui = new FormUI( strtolower( get_class( $this ) ) );
+					$ui->append( 'textarea', 'style', 'option:undelete__style', _t( 'Style declaration for deleted content:' ) );
+					$ui->append( 'submit', 'save', 'Save' );
+					$ui->on_success( array( $this, 'updated_config' ) );
+					$ui->out();
+					break;
 
-			case _t( 'Empty Trash' ):
-				$ui = new FormUI( strtolower( get_class( $this ) ) );
-				$ui->append( 'static', 'explanation', _t('Pressing this button will permanently delete all posts from the virtual trash can. You cannot undo this.') );
-				$ui->append( 'submit', 'delete', 'Delete All' );
-				$ui->on_success( array( $this, 'deleted_all' ) );
-				$ui->out();
-				break;
+				case _t( 'Empty Trash' ):
+					$ui = new FormUI( strtolower( get_class( $this ) ) );
+					$ui->append( 'static', 'explanation', _t('Pressing this button will permanently delete all posts from the virtual trash can. You cannot undo this.') );
+					$ui->append( 'submit', 'delete', 'Delete All' );
+					$ui->on_success( array( $this, 'deleted_all' ) );
+					$ui->out();
+					break;
 			}
 
 		}
@@ -147,7 +171,8 @@ class Undelete extends Plugin
 	}
 
 	// This method will permanently delete all posts stored in the trash can
-	function delete_all() {
+	private function delete_all()
+	{
 		$posts = Posts::get(array('status' => Post::status('deleted'), 'nolimit' => TRUE));
 
 		$count = 0;
@@ -160,9 +185,12 @@ class Undelete extends Plugin
 		return $count;
 	}
 
-	// this method will inject some CSS into the <head>
-	// so that deleted posts will show up differently
-	function action_template_header()
+	/**
+	 * this method will inject some CSS into the <head>
+	 * of the public facing theme so that deleted posts
+	 * will show up differently
+	 */
+	public function action_template_header()
 	{
 		// only show the style to logged in users
 		if ( User::identify()->loggedin ) {
@@ -176,6 +204,31 @@ class Undelete extends Plugin
 	{
 		if ( $theme->page == 'posts' ) {
 			Stack::add( 'admin_stylesheet', array($this->get_url() . '/undelete.css', 'screen') );
+			$url = URL::get( 'auth_ajax', array('context' => 'undelete') );
+			$script = <<<JS
+var unDelete = {
+	post : function(id) {
+		spinner.start();
+		\$.post(
+			'$url',
+			{'id':id},
+			function( result ) {
+				spinner.stop();
+				humanMsg.displayMsg( result );
+				if ( $('.timeline').length ) {
+					loupeInfo = timelineHandle.getLoupeInfo();
+					itemManage.fetch( 0, loupeInfo.limit, true );
+					timelineHandle.updateLoupeInfo();
+				}
+				else {
+					itemManage.fetch( 0, 20, false );
+				}
+			}
+		);
+	}
+}
+JS;
+			Stack::add( 'admin_header_javascript', $script, 'undelete', 'admin' );
 		}
 	}
 }
