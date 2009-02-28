feb9d9a8d30aa6e0da3fe57d760e1a1eb40a379f
Michael Bishop <miklb@contrib.habariproject.org>
2009-02-28 05:44:39 +0000

    fixing a translate string thanks msi fixes #881
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3268 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/coredashmodules/dash_latestentries.php
+++ plugins/coredashmodules/dash_latestentries.php
@@ -3,7 +3,7 @@
 		<?php foreach($recent_posts as $post): ?>
 		<li class="item clear">
 			<span class="date pct15 minor"><a href="<?php echo URL::get('display_entries_by_date', array('year' => $post->pubdate->get( 'Y' ), 'month' => $post->pubdate->get( 'm' ), 'day' => $post->pubdate->get( 'd' ) ) ); ?>" title="<?php printf(_t('Posted at %1$s'), $post->pubdate->get( 'g:m a \o\n F jS, Y' ) ); ?>"><?php $post->pubdate->out( 'M j' ); ?></a></span>
-			<span class="title pct75"><a href="<?php URL::out('admin', 'page=publish&id=' . $post->id); ?>" title="<?php _e('Edit \''.$post->title.'\'') ?>"><?php echo $post->title; ?></a> <a class="minor" href="<?php Site::out_url('habari'); ?>/admin/user/<?php echo $post->author->username; ?>"> <?php _e('by'); ?> <?php echo $post->author->displayname; ?></a></span>
+			<span class="title pct75"><a href="<?php URL::out('admin', 'page=publish&id=' . $post->id); ?>" title="<?php printf( _t('Edit \'%s\''), $post->title ); ?>"><?php echo $post->title; ?></a> <a class="minor" href="<?php Site::out_url('habari'); ?>/admin/user/<?php echo $post->author->username; ?>"> <?php _e('by'); ?> <?php echo $post->author->displayname; ?></a></span>
 			<span class="comments pct10"><a href="<?php echo $post->permalink; ?>#comments"><?php echo $post->comments->approved->count; ?></a></span>
 		</li>
 		<?php endforeach; ?>
