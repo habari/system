b7f7158791320e3ad7e225405bf4eab185f66cfb
Michael Bishop <miklb@contrib.habariproject.org>
2009-02-28 04:48:04 +0000

    fixing the labels in formcontrol and optionscontrol fixes #791
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3266 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_select.php
+++ admin/formcontrols/formcontrol_select.php
@@ -1,5 +1,5 @@
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
-<label><?php echo $this->caption; ?></label>
+<label for="<?php echo $id; ?>"><?php echo $this->caption; ?></label>
 <select name="<?php echo $field . ( $multiple ? '[]' : '' ); ?>"<?php echo ( $multiple ? ' multiple="multiple" size="' . intval($size) . '"' : '' ) ?>>
 <?php foreach($options as $opts_key => $opts_val) : ?>
 	<?php if (is_array($opts_val)) : ?>
