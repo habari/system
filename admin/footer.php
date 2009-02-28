a0cecfff8a0e68e2b7e77bb9aa8d25e2bd73024c
Rick Cockrum <rickc@contrib.habariproject.org>
2009-02-28 19:49:56 +0000

    Don't give logged in users a link to edit their user info page if they don't have permission to edit it. Fixes ticket #879.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3272 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/footer.php
+++ admin/footer.php
@@ -1,23 +1,30 @@
 <div id="footer">
 	<p>
 		<span><a href="http://habariproject.org/" title="<?php _e('Go to the Habari site'); ?>">Habari
-<?php
-echo Version::get_habariversion();
-if(Version::is_devel()) {
-	echo ' r' . Version::get_svn_revision();
-}
-?> </a></span> <span class="middot">&middot;</span>
-		<span><?php _e('Logged in as'); ?></span> <a href="<?php Site::out_url( 'habari' ); ?>/admin/user" title="<?php _e('Go to your user page'); ?>"><?php echo User::identify()->displayname ?></a> <span class="middot">&middot;</span>
-		<span><a href="<?php Site::out_url( 'habari' ); ?>/doc/manual/index.html" onclick="popUp(this.href); return false;" title="<?php _e('Open the Habari manual in a new window'); ?>"><?php _e('Manual'); ?></a></span>
-		<?php
-			if ( User::identify()->can('super_user') ) {
-				?>
-					<span class="middot">&middot;</span>
-					<span><a href="<?php Site::out_url( 'habari' ); ?>/admin/sysinfo"> <?php _e( 'System Information'); ?></a>
-				<?php
-			}
-		?>
- 
+	<?php
+	echo Version::get_habariversion();
+	if(Version::is_devel()) {
+		echo ' r' . Version::get_svn_revision();
+	}
+	?> </a></span>
+	 <span class="middot">&middot;</span>
+	 <span><?php _e('Logged in as'); ?></span>
+	 <?php if( User::identify()->can( 'manage_users' ) ) { ?>
+			 <a href="<?php Site::out_url( 'habari' ); ?>/admin/user" title="<?php _e('Go to your user page'); ?>"><?php echo User::identify()->displayname ?></a>
+	<?php } else { ?>
+			 <span><?php echo User::identify()->displayname ?></span>
+	<?php } ?>
+	 <span class="middot">&middot;</span>
+	 <span><a href="<?php Site::out_url( 'habari' ); ?>/doc/manual/index.html" onclick="popUp(this.href); return false;" title="<?php _e('Open the Habari manual in a new window'); ?>"><?php _e('Manual'); ?></a></span>
+	<?php
+		if ( User::identify()->can('super_user') ) {
+			?>
+				<span class="middot">&middot;</span>
+				<span><a href="<?php Site::out_url( 'habari' ); ?>/admin/sysinfo"> <?php _e( 'System Information'); ?></a>
+			<?php
+		}
+	?>
+
 	</p>
 </div>
 
