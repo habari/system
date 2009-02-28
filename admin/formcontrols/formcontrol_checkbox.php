07e548e0a88db83f0b012fd553e3f39231fcdb75
Michael Bishop <miklb@contrib.habariproject.org>
2009-02-28 04:48:04 +0000

    fixing the labels in formcontrol and optionscontrol fixes #791
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3266 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_checkbox.php
+++ admin/formcontrols/formcontrol_checkbox.php
@@ -1,5 +1,5 @@
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
-	<label><?php echo $this->caption; ?></label>
+	<label for="<?php echo $id; ?>"><?php echo $this->caption; ?></label>
 	<input type="checkbox" name="<?php echo $field; ?>" value="1" <?php echo $value ? 'checked' : ''; ?>>
 	<input type="hidden" name="<?php echo $field; ?>_submitted" value="1" >
 	<?php $control->errors_out('<li>%s</li>', '<ul class="error">%s</ul>'); ?>
