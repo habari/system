f5c89344a96ad1884f6514b8dc17ab5855c2ff94
Michael C. Harris <michaeltwofish@contrib.habariproject.org>
2009-03-01 07:43:04 +0000

    Change spaces to tabs, as per coding standard.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3278 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/k2/404.php
+++ themes/k2/404.php
@@ -1,29 +1,29 @@
 <?php $theme->display ( 'header' ); ?>
 <!-- error -->
-  <div class="content">
-   <div id="primary">
-    <div id="post" class="error">
+	<div class="content">
+	<div id="primary">
+		<div id="post" class="error">
 
-     <div class="entry-head">
-      <h3 class="entry-title"><?php _e('Error!'); ?></h3>
-     </div>
+		<div class="entry-head">
+			<h3 class="entry-title"><?php _e('Error!'); ?></h3>
+		</div>
 
-     <div class="entry-content">
-      <p><?php _e('The requested post was not found.'); ?></p>
-     </div>
+		<div class="entry-content">
+			<p><?php _e('The requested post was not found.'); ?></p>
+		</div>
 
-    </div>
-   </div>
+		</div>
+	</div>
 
-   <hr>
+	<hr>
 
-   <div class="secondary">
+	<div class="secondary">
 
 <?php $theme->display ( 'sidebar' ); ?>
 
-   </div>
+	</div>
 
-   <div class="clear"></div>
-  </div>
+	<div class="clear"></div>
+	</div>
 <!-- /error -->
 <?php $theme->display ('footer'); ?>
--- themes/k2/comments.php
+++ themes/k2/comments.php
@@ -2,15 +2,15 @@
 <?php // Do not delete these lines
 if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly. Thanks!') ); }
 ?>
-    <hr>
+		<hr>
 
-    <div class="comments">
-     <h4><span id="comments"><?php echo $post->comments->moderated->count; ?> <?php _e('Responses to'); ?> <?php echo $post->title; ?></span></h4>
-     <div class="metalinks">
-      <span class="commentsrsslink"><a href="<?php echo $post->comment_feed_link; ?>"><?php _e('Feed for this Entry'); ?></a></span>
-     </div>
+		<div class="comments">
+		<h4><span id="comments"><?php echo $post->comments->moderated->count; ?> <?php _e('Responses to'); ?> <?php echo $post->title; ?></span></h4>
+		<div class="metalinks">
+			<span class="commentsrsslink"><a href="<?php echo $post->comment_feed_link; ?>"><?php _e('Feed for this Entry'); ?></a></span>
+		</div>
 
-     <ol id="commentlist">
+		<ol id="commentlist">
 <?php
 if ( $post->comments->moderated->count ) {
 	foreach ( $post->comments->moderated as $comment ) {
@@ -23,28 +23,28 @@ if ( $post->comments->moderated->count ) {
 		}
 
 ?>
-      <li id="comment-<?php echo $comment->id; ?>" <?php echo $theme->k2_comment_class( $comment, $post ); ?>>
-       <a href="#comment-<?php echo $comment->id; ?>" class="counter" title="<?php _e('Permanent Link to this Comment'); ?>"><?php echo $comment->id; ?></a>
-       <span class="commentauthor"><?php echo $comment_url; ?></span>
-       <small class="comment-meta"><a href="#comment-<?php echo $comment->id; ?>" title="<?php _e('Time of this Comment'); ?>"><?php $comment->date->out(); ?></a><?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) : ?> <em><?php _e('In moderation'); ?></em><?php endif; ?></small>
+			<li id="comment-<?php echo $comment->id; ?>" <?php echo $theme->k2_comment_class( $comment, $post ); ?>>
+			<a href="#comment-<?php echo $comment->id; ?>" class="counter" title="<?php _e('Permanent Link to this Comment'); ?>"><?php echo $comment->id; ?></a>
+			<span class="commentauthor"><?php echo $comment_url; ?></span>
+			<small class="comment-meta"><a href="#comment-<?php echo $comment->id; ?>" title="<?php _e('Time of this Comment'); ?>"><?php $comment->date->out(); ?></a><?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) : ?> <em><?php _e('In moderation'); ?></em><?php endif; ?></small>
 
-       <div class="comment-content">
-        <?php echo $comment->content_out; ?>
+			<div class="comment-content">
+				<?php echo $comment->content_out; ?>
 
-       </div>
-      </li>
+			</div>
+			</li>
 
 <?php
 	}
 }
 else { ?>
-      <li><?php _e('There are currently no comments.'); ?></li>
+			<li><?php _e('There are currently no comments.'); ?></li>
 <?php } ?>
-     </ol>
+		</ol>
 
 <?php if ( ! $post->info->comments_disabled ) { include_once( 'commentform.php' ); } ?>
 
-     <hr>
+		<hr>
 
-    </div>
+		</div>
 <!-- /comments -->
