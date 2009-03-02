07c9d20cca04a638bf24665062918776303cb0a0
Rick Cockrum <rickc@contrib.habariproject.org>
2009-03-02 04:32:47 +0000

    Fix comment count display in the latest comments dashboard module in IE7. Props to nathanhammond for the patch. Thank you! Fixes ticket #647.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3283 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/coredashmodules/dash_latestcomments.php
+++ plugins/coredashmodules/dash_latestcomments.php
@@ -2,8 +2,8 @@
 
 		<?php foreach ( $latestcomments_posts as $post ): ?>
 		<li class="item clear">
+			<span class="comments pct15" style="float: right;"><a href="<?php echo $post->permalink; ?>#comments" title="<?php printf(_n('%1$d comment', '%1$d comments', $post->comments->approved->comments->count), $post->comments->approved->comments->count); ?>"><?php echo $post->comments->approved->comments->count; ?></a></span>
 			<span class="titleanddate pct85"><a href="<?php echo $post->permalink; ?>" class="title"><?php echo $post->title; ?></a> <a href="#" class="date minor"><?php $post->pubdate->out( 'M j' ); ?></a></span>
-			<span class="comments pct15"><a href="<?php echo $post->permalink; ?>#comments" title="<?php printf(_n('%1$d comment', '%1$d comments', $post->comments->approved->comments->count), $post->comments->approved->comments->count); ?>"><?php echo $post->comments->approved->comments->count; ?></a></span>
 			<ul class="commentauthors pct85 minor">
 				<?php
 				$comment_count = 0;
