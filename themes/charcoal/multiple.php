5d20d91a6a3527e5aed756b9d15f9b4b92d2215b
Ali B <dmondark@contrib.habariproject.org>
2009-03-14 19:33:58 +0000

    More tweaks. All theme pages now validate as HTML 4.01 Transitional.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3312 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/multiple.php
+++ themes/charcoal/multiple.php
@@ -37,8 +37,8 @@
 	<div id="wrapper-bottom">
 		<div id="bottom-primary">
 			<div id="prev-posts-footer">
-				<span class="nav-next"><?php $theme->prev_page_link( _t('Newer Posts') ); ?><img class="hidden" src="<?php Site::out_url('theme'); ?>/images/nav-right.png" /></span>
-				<span class="nav-prev"><img class="hidden" src="<?php Site::out_url('theme'); ?>/images/nav-left.png" /><?php $theme->next_page_link( _t('Older Posts') ); ?></span>
+				<span class="nav-next"><?php $theme->prev_page_link( _t('Newer Posts') ); ?><img class="hidden" src="<?php Site::out_url('theme'); ?>/images/nav-right.png" alt=""></span>
+				<span class="nav-prev"><img class="hidden" src="<?php Site::out_url('theme'); ?>/images/nav-left.png" alt=""><?php $theme->next_page_link( _t('Older Posts') ); ?></span>
 				<br class="clear">
 			</div>
 			<?php $theme->display_archives() ;?>
