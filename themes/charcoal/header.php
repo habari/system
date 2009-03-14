8275f18933fba6bfc8d065d024bc2d22c3d92bc4
Ali B <dmondark@gmail.com>
2009-03-14 19:33:58 +0000

    More tweaks. All theme pages now validate as HTML 4.01 Transitional.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3312 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/header.php
+++ themes/charcoal/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php Options::out( 'title' ) ?><?php if($request->display_entry && isset($post)) { echo " :: {$post->title}"; } ?></title>
 	<meta name="generator" content="Habari">
 	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php $theme->feed_alternate(); ?>">
