17d8b46cef8f0e74048f8fd3f8eb9e3970f6a641
Mike Lietz <mikelietz@contrib.habariproject.org>
2009-04-06 16:06:54 +0000

    Attempting to correct #930, but I may be overstepping by removing the wrapping <div> from formcontrol_textarea. Did every form element needs its own div? If not, the other default formcontrol_ templates probably also need to be updated. The main change is moving class and id from that (now nonexistent) div to the form element itself, as is done in admincontrol_textarea which does, however, have a wrapping div, and also a wrapper <p> as well.
    I'll soon be committing an updated simpleblacklist to take advantage of this.
    
    
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3448 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_textarea.php
+++ admin/formcontrols/formcontrol_textarea.php
@@ -1,5 +1,5 @@
-<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>><label><?php echo $this->caption; ?><textarea name="<?php echo $field; ?>"><?php echo htmlspecialchars($value); ?></textarea></label>
+<label><?php echo $this->caption; ?><textarea name="<?php echo $field; ?>"<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : 
+''?><?php echo isset( $rows ) ? " rows=\"$rows\"" : ''; ?><?php echo isset( $cols ) ? " cols=\"$cols\"" : ''; ?>><?php echo htmlspecialchars($value); ?></textarea></label>
 <?php if($message != '') : ?>
-<p class="error"><?php echo $message; ?></p>
+	<p class="error"><?php echo $message; ?></p>
 <?php endif; ?>
-</div>
