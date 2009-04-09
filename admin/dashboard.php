c89f6746bdfeb8f8607005d46d9823a246cf11f2
Christian Mohn <h0bbel@contrib.habariproject.org>
2009-04-09 22:06:04 +0000

    Adding first pass as IE6 (and earlier) warning to dashboard. Text probably needs massaging, and to be turned into something translateable but I don't really know how to do that. I guess it should possibly also be moved into it's own file and included on demand only, instead of the hacky html comment mode it currently uses, but this is at least a quick fix for ticket #942
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3462 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,16 @@
 <?php include( 'header.php' ); ?>
 
 <div class="container dashboardinfo transparent">
+        <!--[if lte IE 6]>
+        <p>
+            Oh, great!  You're using IE6!  I've finally found someone I can pawn this old betamax player off on!<br />
+            If you're reading this, you're surfing using Internet Explorer 6, an eight-year-old browser that cannot cope with the demands of the modern internet. 
+            For the best web experience, we strongly recommend upgrading to <a href="http://getfirefox.com">Firefox</a>, <a href="http://www.opera.com/">Opera</a>, 
+            <a href="http://www.apple.com/safari/download/">Safari</a>, <a href="http://www.google.com/chrome">Google Chrome</a>, or a more recent version of 
+            <a href="http://www.microsoft.com/windows/Internet-explorer/default.aspx">Internet Explorer</a>.
+        </p>
+        <![endif]-->  
+        
 		<p>
 		<?php
 		$active_msg = array();
