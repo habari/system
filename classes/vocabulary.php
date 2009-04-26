43e80b1cc8d7b5f5fa3c0716fa0ab28305915520
Michael C. Harris <michaeltwofish@contrib.habariproject.org>
2009-04-26 04:51:09 +0000

    Add some vocabulary tests, and expand the vocabulary skeleton. Tests don't run yet, let alone pass.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3508 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/vocabulary.php
+++ classes/vocabulary.php
@@ -2,7 +2,7 @@
 
 /**
  * Vocabulary Class
- * 
+ *
  *
  * @version $Id$
  * @copyright 2009
@@ -11,8 +11,8 @@
 class Vocabulary extends QueryRecord
 {
 	/**
-	 * Return the defined database columns for a Post.
-	 * @return array Array of columns in the Post table
+	 * Return the defined database columns for a Vocabulary.
+	 * @return array Array of columns in the Vocabulary table
 	 **/
 	public static function default_fields()
 	{
@@ -24,13 +24,12 @@ class Vocabulary extends QueryRecord
 		);
 	}
 
-		
 	/**
 	 * Vocabulary constructor
 	 * Creates a Vocabulary instance
-	 * 
+	 *
 	 * @param string $name The name of the Vocabulary
-	 * @param string $description A description of the Vocabulary 
+	 * @param string $description A description of the Vocabulary
 	 * @param BitMask $properties Properties of this Vocabulary
 	 */
 	public function __construct($name, $description, BitMask $properties)
@@ -41,7 +40,7 @@ class Vocabulary extends QueryRecord
 			$this->fields,
 			$this->newfields
 		);
-		
+
 		$paramarray = array(
 			'name'=> $name,
 			'description' => $description,
@@ -52,7 +51,87 @@ class Vocabulary extends QueryRecord
 
 		$this->exclude_fields( 'id' );
 	}
-	
+
+	/**
+	 * Return a Vocabulary by name.
+	 * @return Vocabulary The requested vocabulary
+	 **/
+	public static function get($name)
+	{
+	}
+
+	/**
+	 * Delete a Vocabulary by name.
+	 * @return boolean true if the Vocabulary was deleted, false otherwise
+	 **/
+	public static function delete($name)
+	{
+	}
+
+	/**
+	 * Rename a Vocabulary.
+	 * @return boolean true if the Vocabulary was renamed, false otherwise
+	 **/
+	public static function rename($name, $newname)
+	{
+	}
+
+	/**
+	 * Return the names of all vocabularies
+	 * @return array Array of Vocabulary names
+	 **/
+	public static function names()
+	{
+	}
+
+	/**
+	 * Return the Term objects associated to that type of object with that id in any vocabulary.
+	 * @return array Array of Vocabulary names
+	 **/
+	public static function get_all_object_terms($object_type, $id)
+	{
+	}
+
+	/**
+	 * Produce a BitMask for a feature mask. Convenience method for use when creating a Vocabulary.
+	 * @return array Array of Vocabulary names
+	 **/
+	public static function feature_mask($hierarchical, $required, $multiple, $free)
+	{
+	}
+
+	/**
+	 * Adds a term to the vocabulary. Returns a Term object. null parameters append the term to the end of any hierarchies.
+	 * @return Term The Term object added
+	 **/
+	public function add_term($term, $parent_term = null, $before_term = null)
+	{
+	}
+
+	/**
+	 * Gets the term object for that string. No parameter returns the root Term object.
+	 * @return Term The Term object requested
+	 **/
+	public function get_term($term)
+	{
+	}
+
+	/**
+	 * Gets the Term objects associated to that type of object with that id.
+	 * @return Array The Term objects requested
+	 **/
+	public function get_object_terms($object_type, $id)
+	{
+	}
+
+	/**
+	 * Remove the term from the vocabulary.  Convenience method to ->get_term('foo')->delete().
+	 * @return Array The Term objects requested
+	 **/
+	public function delete_term($term)
+	{
+	}
+
 }
 
 ?>
