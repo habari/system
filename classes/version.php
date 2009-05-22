88e47ad9b71383a3d0ac74d7fb45f3d2c02eb633
Scott Merrill <skippy@contrib.habariproject.org>
2009-05-22 13:35:37 +0000

    merging r3577 for 0.6.2 release
    git-svn-id: https://svn.habariproject.org/habari/makaanga/0.x/htdocs/system@3595 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/version.php
+++ classes/version.php
@@ -17,7 +17,7 @@ class Version
 	const DB_VERSION = 3249;
 	const API_VERSION = 3124;
 
-	const HABARI_VERSION = '0.6.1';
+	const HABARI_VERSION = '0.6.2';
 
 	// This string contains the URL to the Habari SVN repository used for this working copy or export
 	const HABARI_SVN_HEAD_URL = '$HeadURL$';
