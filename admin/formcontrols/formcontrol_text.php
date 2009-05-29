1730724745939977613b414085f11d350d8dc2b2
Michael Bishop <miklb@contrib.habariproject.org>
2009-05-29 18:59:25 +0000

    adding optional titles to labels and textarea/text input. Props rick_c and mikelietz
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3607 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_text.php
+++ admin/formcontrols/formcontrol_text.php
@@ -1,5 +1,5 @@
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
-	<label><?php echo $this->caption; ?></label>
-	<input type="text" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars($value); ?>">
+	<label <?php echo isset( $title ) ? " title=\"$title\"" : ''; ?>><?php echo $this->caption; ?></label>
+	<input <?php echo isset( $title ) ? " title=\"$title\"" : ''; ?> type="text" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars($value); ?>" >
 	<?php $control->errors_out('<li>%s</li>', '<ul class="error">%s</ul>'); ?>
 </div>
