14bca8185172b9fa58e2b3944f27c4361c4fba74
Ali B <dmondark@contrib.habariproject.org>
2009-06-01 17:53:38 +0000

    Wrap the textarea form control with a div like every other control and assign id attribute to the div element instead of the textarea field.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3614 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_textarea.php
+++ admin/formcontrols/formcontrol_textarea.php
@@ -1,9 +1,11 @@
+<div<?php echo( isset( $id ) ? " id=\"$id\"" : '' ); ?>>
 <label<?php if ( isset( $label_title ) ) { ?> title="<?php echo $label_title; ?>"<?php $label_title = ''; } else { echo ( isset( $title ) ? "  title=\"$title\"" : '' ); } ?> for="<?php echo $id; ?>">
 <?php echo $this->caption; ?></label>
 <textarea name="<?php echo $field; ?>"<?php echo ( isset( $class ) ? " class=\"$class\"" : '' ) . 
-	( isset( $id ) ? " id=\"$id\"" : '' ) . " rows=\"" . ( isset( $rows ) ? $rows : 10 ) . "\" cols=\"" . ( isset( $cols ) ? $cols : 100 );
+	" rows=\"" . ( isset( $rows ) ? $rows : 10 ) . "\" cols=\"" . ( isset( $cols ) ? $cols : 100 );
 	if ( isset( $control_title ) ) { echo "\" title=\"$control_title\""; } else { echo ( isset( $title ) ? "\" title=\"$title\"" : '"' ); } ?>><?php 
 echo htmlspecialchars( $value ); ?></textarea>
 <?php if ( $message != '' ) : ?>
 	<p class="error"><?php echo $message; ?></p>
 <?php endif; ?>
+</div>
