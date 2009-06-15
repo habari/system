f02ddbbe77d19f649bf9e6309208837f88d877db
Michael C. Harris <michaeltwofish@contrib.habariproject.org>
2009-06-15 11:41:51 +0000

    Make FormUI text fields output their classes properly. As the formcontrol class is always set, we can probably take out the test entirely, which would make the templates much more readable.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3638 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_text.php
+++ admin/formcontrols/formcontrol_text.php
@@ -1,4 +1,4 @@
-<div<?php echo isset( $class ) ? " class=\"$class\"" : '' . isset( $id ) ? " id=\"$id\"" : ''; ?>>
+<div<?php echo isset( $class ) ? " class=\"$class\"" : ''; ?><? echo isset( $id ) ? " id=\"$id\"" : ''; ?>>
 	<label<?php if ( isset( $label_title ) ) { ?> title="<?php echo $label_title; ?>"<?php } else { echo ( isset( $title ) ? " title=\"$title\"" : '' ); } ?> for="<?php echo $field; ?>"><?php echo $this->caption; ?></label>
 	<input<?php if ( isset( $control_title ) ) { ?> title="<?php echo $control_title; ?>"<?php } else { echo ( isset( $title ) ? " title=\"$title\"" : '' ); } ?> type="text" id="<?php echo $field; ?>" name="<?php echo $field; ?>" value="<?php echo htmlspecialchars( $value ); ?>">
 	<?php $control->errors_out( '<li>%s</li>', '<ul class="error">%s</ul>' ); ?>
