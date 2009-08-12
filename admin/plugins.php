d2935c1433a8148ad074e48a89d70cfab608db32
michaeltwofish <michaeltwofish@contrib.habariproject.org>
2009-08-12 04:34:38 +0000

    List all plugin authors when a plugin is being configured, rather than just one. Also some DRY and translation. Thanks arthus for the patch. Closes #1031.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3684 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/plugins.php
+++ admin/plugins.php
@@ -4,11 +4,14 @@
 	<span class="pct40">
 		<select name="navigationdropdown" onchange="navigationDropdown.filter();" tabindex="1">
 			<option value="all"><?php _e('All plugins'); ?></option>
+			<?php if ( isset($config_plugin) ): ?>
+				<option value="configureplugin"><?php echo $config_plugin_caption; ?></option>
+			<?php endif; ?>
 			<?php if ( count($active_plugins) > 0 ): ?>
-				<option value="activeplugins">Active Plugins</option>
+				<option value="activeplugins"><?php _e('Active Plugins'); ?></option>
 			<?php endif; ?>
 			<?php if ( count($inactive_plugins) > 0 ): ?>
-				<option value="inactiveplugins">Inactive Plugins</option>
+				<option value="inactiveplugins"><?php _e('Inactive Plugins'); ?></option>
 			<?php endif; ?>
 		</select>
 	</span>
@@ -22,50 +25,24 @@
 
 
 <?php
-if(isset($config_plugin)):
-?>
+if ( isset($config_plugin) ): ?>
 
-<div class="container plugins activeplugins" id="configureplugin">
+<div class="container plugins configureplugin" id="configureplugin">
 
 	<h2><?php echo $config_plugin['info']->name; ?> &middot; <?php echo $config_plugin_caption; ?></h2>
 
-	<div class="item plugin clear" id="plugin_<?php echo $config_plugin['plugin_id']; ?>">
-		<div class="head">
-			<a href="<?php echo $config_plugin['info']->url; ?>" class="plugin"><?php echo $config_plugin['info']->name; ?> <span class="version"><?php echo $config_plugin['info']->version; ?></span></a> <span class="dim"><?php _e('by'); ?></span> <?php echo empty( $config_plugin['info']->authorurl ) ? $config_plugin['info']->author : '<a href="' . $config_plugin['info']->authorurl . '">' . $config_plugin['info']->author . '</a>'; ?>
-			<?php if( isset($config_plugin['help']) ): ?>
-			<a class="help" href="<?php echo $config_plugin['help']['url']; ?>">?</a>
-			<?php endif; ?>
-			<ul class="dropbutton">
+	<?php
 
-<?php foreach( $config_plugin['actions'] as $plugin_action => $action ) : ?>
-						<li><a href="<?php echo htmlspecialchars( $action['url'], ENT_COMPAT, 'UTF-8' ); ?>"><?php echo $action['caption']; ?></a></li>
-<?php endforeach; ?>
+	$theme->config = true;
+	$theme->plugin = $config_plugin;
+	$theme->display('plugin');
 
-			</ul>
+	$theme->config = false;
 
+	?>
 
-			<?php if( isset( $config_plugin['update'] ) ) { ?>
-			<ul class="dropbutton alert">
-				<li><a href="#"><?php _e('v1.1 Update Available Now'); ?></a></li>
-			</ul>
-			<?php } ?>
-
-		</div>
-
-		<p class="description"><?php echo $config_plugin['info']->description; ?></p>
-		
-		<div class="pluginhelp"<?php if($helpaction == '_help'): ?> class="active"<?php endif; ?>>
-			<?php Plugins::act( 'plugin_ui', $config_plugin['plugin_id'], '_help' ); ?>
-		</div>
-		
-		<div id="pluginconfigure">
-			<?php Plugins::act( 'plugin_ui', $configure, $configaction ); ?>
-			<a class="link_as_button" href="<?php URL::out( 'admin', 'page=plugins' ); ?>"><?php _e('Close'); ?></a>
-		</div>
-
-	</div>
-	
 </div>
+
 <?php endif; ?>
 
 
@@ -75,76 +52,12 @@ if(isset($config_plugin)):
 
 	<h2><?php _e('Active Plugins'); ?></h2>
 
-	<?php foreach($active_plugins as $plugin) : ?>
-	<?php if($plugin['debug']) : ?>
-	<div class="item plugin clear">
-		<div class="head">
-  			<p><?php printf( _t('The plugin file %s had syntax errors and could not load.'), $plugin['file'] ); ?></p>
-			<div style="display:none;" id="error_<?php echo $plugin['plugin_id']; ?>"><?php echo $plugin['error']; ?></div>
-				<ul class="dropbutton">
-					<li><a href="#" onclick="$('#error_<?php echo $plugin['plugin_id']; ?>').show();"><?php _e('Show Error'); ?></a></li>
-				</ul>
-		</div>
-	</div>
-
-	<?php elseif(!isset($plugin['info'])): ?>
-	<div class="item plugin clear">
-		<div class="head">
-			<p><?php printf( _t('The plugin file %s is a legacy plugin, and does not include an XML info file.'), $plugin['file'] ); ?></p>
-		</div>
-	</div>
-	<?php else: ?>
-
-	<div class="item plugin clear" id="plugin_<?php echo $plugin['plugin_id']; ?>">
-		<div class="head">
-			<a href="<?php echo $plugin['info']->url; ?>" class="plugin"><?php echo $plugin['info']->name; ?> <span class="version"><?php echo $plugin['info']->version; ?></span></a> <span class="dim"><?php _e('by'); ?></span> 
-<?php 
-$comma = ' '; 
-foreach($plugin['info']->author as $author) { 
-	echo $comma;
-	echo isset( $author['url'] ) ? '<a href="' . $author['url'] . '">' . $author . '</a>' : $author;
-	$comma = ' &middot; ';
-}
-?>
-			<?php if( isset($plugin['help']) ): ?>
-			<a class="help" href="<?php echo $plugin['help']['url']; ?>">?</a>
-			<?php endif; ?>
-			<ul class="dropbutton">
-
-<?php foreach( $plugin['actions'] as $plugin_action => $action ) : ?>
-						<li><a href="<?php echo htmlspecialchars( $action['url'], ENT_COMPAT, 'UTF-8' ); ?>"><?php echo $action['caption']; ?></a></li>
-<?php endforeach; ?>
-
-
-			</ul>
-
-
-			<?php if( isset( $plugin['update'] ) ) : ?>
-			<ul class="dropbutton alert">
-				<li><a href="#"><?php _e('v1.1 Update Available Now'); ?></a></li>
-			</ul>
-			<?php endif; ?>
-			
-
-		</div>
-
-		<p class="description"><?php echo $plugin['info']->description; ?></p>
-
-		<div class="pluginhelp"<?php if($helpaction == '_help'): ?> class="active"<?php endif; ?>>
-			<?php Plugins::act( 'plugin_ui', $plugin['plugin_id'], '_help' ); ?>
-		</div>
-
-		<?php if ( isset( $this->engine_vars['configure'] ) && ( $configure == $plugin['plugin_id'] ) ): ?>
-		<div id="pluginconfigure">
-			<?php Plugins::act( 'plugin_ui', $configure, $configaction ); ?>
-			<a class="link_as_button" href="<?php URL::out( 'admin', 'page=plugins' ); ?>"><?php _e('Close'); ?></a>
-		</div>
-		<?php endif; ?>
-	</div>
-
-	<?php endif; ?>
-
-	<?php endforeach; ?>
+	<?php
+	foreach ( $active_plugins as $plugin ) {
+		$theme->plugin = $plugin;
+		$theme->display('plugin');
+	}
+	?>
 
 </div>
 <?php endif; ?>
@@ -153,56 +66,13 @@ foreach($plugin['info']->author as $author) {
 <div class="container plugins inactiveplugins" id="inactiveplugins">
 
 	<h2><?php _e('Inactive Plugins'); ?></h2>
-
-	<?php foreach($inactive_plugins as $plugin) : ?> 
-	<?php	if($plugin['debug']) : ?>
-	<div class="item plugin clear">
-		<div class="head">
-			<p><?php printf( _t('The plugin file %s had syntax errors and could not load.'), $plugin['file'] ); ?></p>
-			<div style="display:none;" id="error_<?php echo $plugin['plugin_id']; ?>"><?php echo $plugin['error']; ?></div>
-				<ul class="dropbutton">
-					<li><a href="#" onclick="$('#error_<?php echo $plugin['plugin_id']; ?>').show();"><?php _e('Show Error'); ?></a></li>
-				</ul>
-		</div>
-	</div>
-	<?php elseif(!isset($plugin['info'])): ?>
-	<div class="item plugin clear">
-		<div class="head">
-			<p><?php printf( _t('The plugin file %s is missing an XML info file and could not load.'), $plugin['file'] ); ?></p>
-		</div>
-	</div>
-	<?php else: ?>
-	<div class="item plugin clear" id="plugin_<?php echo $plugin['plugin_id']; ?>">
-		<div class="head">
-			<a href="<?php echo $plugin['info']->url; ?>" class="plugin"><?php echo $plugin['info']->name; ?> <span class="version"><?php echo $plugin['info']->version; ?></span></a> <span class="dim"><?php _e('by'); ?></span> <?php echo empty( $plugin['info']->authorurl ) ? $plugin['info']->author : '<a href="' . $plugin['info']->authorurl . '">' . $plugin['info']->author . '</a>'; ?>
-
-			<ul class="dropbutton">
-<?php foreach( $plugin['actions'] as $plugin_action => $action ) : ?>
-						<li><a href="<?php echo htmlspecialchars( $action['url'], ENT_COMPAT, 'UTF-8' ); ?>"><?php echo $action['caption']; ?></a></li>
-<?php endforeach; ?>
-			</ul>
-
-			<?php if( isset( $updates ) ) { ?>
-			<ul class="dropbutton alert">
-				<li><a href="#"><?php _e('v1.1 Update Available Now'); ?></a></li>
-			</ul>
-			<?php } ?>
-			<?php if( isset( $plugin['missing'] ) ): ?>
-			<p><?php _e('This plugin cannot be activated because the following features were not present:'); ?></p>
-			<ul>
-			<?php foreach($plugin['missing'] as $feature => $url): ?>
-				<li><?php echo ($url != '') ? sprintf('<a href="%s">%s</a>', $url, $feature) : $feature; ?></li>
-			<?php endforeach; ?>
-			</ul>
-			<?php endif; ?>
-
-		</div>
-
-		<p class="description"><?php echo $plugin['info']->description; ?></p>
-
-	</div>
-	<?php endif; ?>
-<?php endforeach; ?>
+	
+	<?php
+	foreach ( $inactive_plugins as $plugin) {
+		$theme->plugin = $plugin;
+		$theme->display('plugin');
+	}
+	?>
 
 </div>
 <?php endif; ?>
