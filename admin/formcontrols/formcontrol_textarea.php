70ef737d5978b6b6ff261320302df074e940b408
Mike Lietz <codger+git@gmail.com>
2009-08-13 03:00:17 +0000

    update formcontrol templates so that tabindexes appear on the formui comment form. All of the templates probably should get this, or perhaps a more generalized method (i.e. if ( isset ( $whatever ) { .... whatever="$whatever" ....) is called for.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@3688 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_textarea.php
+++ admin/formcontrols/formcontrol_textarea.php
@@ -3,7 +3,7 @@
 <?php echo $this->caption; ?></label>
 <textarea name="<?php echo $field; ?>" id="<?php echo $field; ?>"<?php echo ( isset( $class ) ? " class=\"$class\"" : '' ) . 
 	" rows=\"" . ( isset( $rows ) ? $rows : 10 ) . "\" cols=\"" . ( isset( $cols ) ? $cols : 100 );
-	if ( isset( $control_title ) ) { echo "\" title=\"$control_title\""; } else { echo ( isset( $title ) ? "\" title=\"$title\"" : '"' ); } ?>><?php 
+	if ( isset( $control_title ) ) { echo "\" title=\"$control_title\""; } else { echo ( isset( $title ) ? "\" title=\"$title\"" : '"' ); } if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } ?>><?php 
 echo htmlspecialchars( $value ); ?></textarea>
 <?php if ( $message != '' ) : ?>
 	<p class="error"><?php echo $message; ?></p>
