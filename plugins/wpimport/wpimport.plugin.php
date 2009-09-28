habari/system

commit 263c9163d30171fa0bce5870b1e085c1dfdf8859

Clean up the html.
git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3739 653ae4dd-d31e-0410-96ef-6bf7bf53c507

plugins/wpimport/wpimport.plugin.php
@@ -131,9 +131,7 @@ class WPImport extends Plugin implements Importer
 				<span class="pct40"><label for="utw_import">Import tags from Ultimate Tag Warrior</label></span>
 				<span class="pct25"><input type="checkbox" name="utw_import" value="1" tab index="6"></span>
 			</div>
-			</div>
-			<div
-			<div class="container transparent"
+			<div class="container transparent">
 			<input type="submit" class="button" name="import" value="Import" />
 			</div>
 WP_IMPORT_STAGE1;
@@ -240,8 +238,7 @@ WP_IMPORT_STAGE2;
 			$max = min( ( $postindex + 1 ) * IMPORT_BATCH, $postcount );
 
 			$user_map = array();
-			$userinfo = DB::table( 'userinfo' );
-			$user_info = DB::get_results( "SELECT user_id, value FROM {$userinfo} WHERE name= 'wp_id';" );
+			$user_info = DB::get_results( "SELECT user_id, value FROM {userinfo WHERE name= 'wp_id';" );
 			foreach( $user_info as $info ) {
 				$user_map[$info->value]= $info->user_id;
 			}
@@ -561,8 +558,7 @@ WP_IMPORT_USERS1;
 
 			echo "<p>Importing comments {$min}-{$max} of {$commentcount}.</p>";
 
-			$postinfo = DB::table( 'postinfo' );
-			$post_info = DB::get_results( "SELECT post_id, value FROM {$postinfo} WHERE name= 'wp_id';" );
+			$post_info = DB::get_results( "SELECT post_id, value FROM {postinfo} WHERE name= 'wp_id';" );
 			foreach( $post_info as $info ) {
 				$post_map[$info->value]= $info->post_id;
 			}

commit 62d2dd37cb86c9923ebcba73af7ccc5080764914

Fix the typo introduced in r3739.
git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3741 653ae4dd-d31e-0410-96ef-6bf7bf53c507

plugins/wpimport/wpimport.plugin.php
@@ -238,7 +238,7 @@ WP_IMPORT_STAGE2;
 			$max = min( ( $postindex + 1 ) * IMPORT_BATCH, $postcount );
 
 			$user_map = array();
-			$user_info = DB::get_results( "SELECT user_id, value FROM {userinfo WHERE name= 'wp_id';" );
+			$user_info = DB::get_results( "SELECT user_id, value FROM {userinfo} WHERE name= 'wp_id';" );
 			foreach( $user_info as $info ) {
 				$user_map[$info->value]= $info->user_id;
 			}