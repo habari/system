a0e710f6f8d3da4f2152186c7a0035f2b9b0086c
Rick Cockrum <rickc@contrib.habariproject.org>
2009-11-14 05:37:15 +0000

    Make pgsql do case-insensitive LIKEs to match MySql and SQLite. props to BigJibby for the regex. Fixes ticket #1079.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3801 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/pgsql/connection.php
+++ schema/pgsql/connection.php
@@ -48,6 +48,7 @@ class PGSQLConnection extends DatabaseConnection
 		$sql = preg_replace( '%YEAR\s*\(\s*FROM_UNIXTIME\s*\(\s*([^ ]*)\s*\)\s*\)%ims', 'date_part(\'year\', \'epoch\'::timestamptz + ${1} * \'1 second\'::interval)', $sql );
 		$sql = preg_replace( '%MONTH\s*\(\s*FROM_UNIXTIME\s*\(\s*([^ ]*)\s*\)\s*\)%ims', 'date_part(\'month\',  \'epoch\'::timestamptz + ${1} * \'1 second\'::interval)', $sql );
 		$sql = preg_replace( '%DAY\s*\(\s*FROM_UNIXTIME\s*\(\s*([^ ]*)\s*\)\s*\)%ims', 'date_part(\'day\',  \'epoch\'::timestamptz + ${1} * \'1 second\'::interval)', $sql );
+		$sql = preg_replace('%LIKE\s+((\'|").*\2)%iUms', 'ILIKE \1', $sql);
 		return $sql;
 	}
 
