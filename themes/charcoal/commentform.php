68ce52cf6ff477effd9d8c52b4a88a768f984aa4
Mike Lietz <mikelietz@contrib.habariproject.org>
2009-11-29 05:22:37 +0000

    These lines can be deleted.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3841 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/commentform.php
+++ themes/charcoal/commentform.php
@@ -1,6 +1,3 @@
-<?php // Do not delete these lines
-if ( ! defined('HABARI_PATH' ) ) { die( 'Please do not load this page directly. Thanks!' ); }
-?>
 <?php if ( !$post->info->comments_disabled ) : ?>
 <div id="comment-form">
 <?php 	if ( Session::has_messages() ) Session::messages_out(); ?>
