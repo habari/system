4796483722d60b6bf1d9ba9592d0a1868c980459
Mike Lietz <mikelietz@contrib.habariproject.org>
2010-01-04 16:38:59 +0000

    Add optionscontrol_textarea template for site options needing a textarea, for example, the 'About' option. Which may well be being removed, I realize. Plugins may later add options that could use this template.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3945 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ admin/formcontrols/optionscontrol_textarea.php
@@ -0,0 +1,8 @@
+<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
+	<span class="pct25"><label for="<?php echo $field; ?>"><?php echo $caption; ?></label></span>
+	<span class="pct50"><textarea name="<?php echo $field; ?>" id="<?php echo $field; ?>" class="resizable"<?php 
+		echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>><?php echo htmlspecialchars( $value ); ?></textarea></span>
+	<?php if($message != '') : ?>
+	<p class="error"><?php echo $message; ?></p>
+	<?php endif; ?>
+</div>
