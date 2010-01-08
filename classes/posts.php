515b78440f8322022c100124dbbe896a15924465
Matt Read <mattread@contrib.habariproject.org>
2010-01-08 05:54:33 +0000

    we better make it countable too
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@3954 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/posts.php
+++ classes/posts.php
@@ -238,7 +238,7 @@ class Posts extends ArrayObject implements IsContent
 					// merge the two possibile calls together
 					$infos = array_merge( isset( $paramset['all:info'] ) ? $paramset['all:info'] : array(), isset( $paramset['info'] ) ? $paramset['info'] : array() );
 
-					if ( is_array( $infos ) || $infos instanceof Traversable ) {
+					if ( is_array( $infos ) || ($infos instanceof Traversable && $infos instanceof Countable) ) {
 						$pi_count = 0;
 						foreach ( $infos as $info_key => $info_value ) {
 							$pi_count++;
@@ -254,7 +254,8 @@ class Posts extends ArrayObject implements IsContent
 				}
 
 				if ( isset( $paramset['any:info'] ) ) {
-					if ( is_array( $paramset['any:info'] ) || $paramset['any:info'] instanceof Traversable ) {
+					if ( is_array( $paramset['any:info'] ) ||
+						($paramset['any:info'] instanceof Traversable && $paramset['any:info'] instanceof Countable) ) {
 						$pi_count = 0;
 						$pi_where = array();
 						foreach ( $paramset['any:info'] as $info_key => $info_value ) {
@@ -292,7 +293,7 @@ class Posts extends ArrayObject implements IsContent
 					// merge the two possible calls together
 					$infos = array_merge( isset( $paramset['not:all:info'] ) ? $paramset['not:all:info'] : array(), isset( $paramset['not:info'] ) ? $paramset['not:info'] : array() );
 
-					if ( is_array( $infos ) ) {
+					if ( is_array( $infos ) || ($infos instanceof Traversable && $infos instanceof Countable) ) {
 						$the_ins = array();
 
 						foreach ( $infos as $info_key => $info_value ) {
@@ -318,7 +319,8 @@ class Posts extends ArrayObject implements IsContent
 
 				if ( isset( $paramset['not:any:info'] ) ) {
 
-					if ( is_array( $paramset['not:any:info'] ) ) {
+					if ( is_array( $paramset['not:any:info'] ) ||
+						($paramset['not:any:info'] instanceof Traversable && $paramset['not:any:info'] instanceof Countable) ) {
 
 						foreach ( $paramset['not:any:info'] as $info_key => $info_value ) {
 
