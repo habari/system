f05f6b2dea844f5029e6574d71c641a206e06727
Chris Meller <chrismeller@contrib.habariproject.org>
2010-01-31 19:51:45 +0000

    Just a quick indentation fix, because I'm anal... and because I can.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4018 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/logs_items.php
+++ admin/logs_items.php
@@ -1,5 +1,5 @@
 <?php foreach( $logs as $log ): ?>
-<div class="item clear">
+	<div class="item clear">
 		<span class="checkbox pct5"><span><input type="checkbox" class="checkbox" name="checkbox_ids[<?php echo $log->id; ?>]" id="checkbox_ids[<?php echo $log->id; ?>]"></span></span>
 		<span class="time pct15 minor"><span><?php $log->timestamp->out( "M j, Y" ); ?> &middot; <?php $log->timestamp->out( "H:i" ); ?></span></span>
 		<span class="user pct15 minor"><span>
@@ -17,5 +17,5 @@
 		<span class="severity pct10 minor"><span><?php echo $log->severity; ?></span></span>
 		<span class="message pct25 minor less"><span><?php echo Utils::truncate(htmlspecialchars($log->message), 40, false); ?></span></span>
 		<span class="message pct25 minor more"><span><?php echo htmlspecialchars($log->message); ?></span></span>
-</div>
+	</div>
 <?php endforeach; ?>
