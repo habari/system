807094c8b7768e870b08a68832f88dbef29163a5
Rick Cockrum <rickc@contrib.habariproject.org>
2010-03-17 14:24:36 +0000

    Use Utils::htmlspecialchars() in the form controls.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@4125 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_submit.php
+++ admin/formcontrols/formcontrol_submit.php
@@ -1,4 +1,4 @@
-<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>><input type="submit"<?php if(isset($disabled) && $disabled) { ?>disabled <?php } if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } ?> name="<?php echo $field; ?>" value="<?php echo htmlspecialchars($caption, ENT_COMPAT, 'UTF-8'); ?>">
+<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>><input type="submit"<?php if(isset($disabled) && $disabled) { ?>disabled <?php } if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } ?> name="<?php echo $field; ?>" value="<?php echo Utils::htmlspecialchars($caption); ?>">
 <?php if($message != '') : ?>
 <p class="error"><?php echo $message; ?></p>
 <?php endif; ?>
--- admin/formcontrols/formcontrol_textmulti.php
+++ admin/formcontrols/formcontrol_textmulti.php
@@ -11,7 +11,7 @@ $i = 0;
 foreach($value as $value_1) :
 $i++;
 ?>
-	<input type="text" name="<?php echo $field; ?>[]" id="<?php echo $field . '_' . $i; ?>" value="<?php echo htmlspecialchars($value_1, ENT_COMPAT, 'UTF-8'); ?>"> <label for="<?php echo $field . '_' . $i; ?>"><a href="#" onclick="return controls.textmulti.remove(this);">[<?php _e('remove'); ?>]</a></label>
+	<input type="text" name="<?php echo $field; ?>[]" id="<?php echo $field . '_' . $i; ?>" value="<?php echo Utils::htmlspecialchars($value_1); ?>"> <label for="<?php echo $field . '_' . $i; ?>"><a href="#" onclick="return controls.textmulti.remove(this);">[<?php _e('remove'); ?>]</a></label>
 <?php
 endforeach;
 ?>
