8221d40bab0c7b7d31e768c9016981a3e12f4eeb
Michael Bishop <miklb@contrib.habariproject.org>
2010-03-20 01:29:02 +0000

    adding themehelper functions to core, removing plugin from system/plugins. Added example usage of comment functions to mzingi. Fixes #524
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@4137 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/theme.php
+++ classes/theme.php
@@ -788,6 +788,110 @@ class Theme extends Pluggable
 	}
 
 	/**
+	 * Returns a full qualified URL of the specified post based on the comments count, and links to the post.
+ 	 *
+	 * Passed strings are localized prior to parsing therefore to localize "%d Comments" in french, it would be "%d Commentaires".
+	 *
+	 * Since we use sprintf() in the final concatenation, you must format passed strings accordingly.
+	 *
+	 * @param Theme $theme The current theme object
+	 * @param Post $post Post object used to build the comments link
+	 * @param string $zero String to return when there are no comments
+	 * @param string $one String to return when there is one comment
+	 * @param string $many String to return when there are more than one comment
+	 * @return string Linked string to display for comment count
+	 * @see Theme::theme_comments_count()
+	 */
+	public function theme_comments_link( $theme, $post, $zero, $one, $many )
+	{
+		$count = $theme->comments_count_return( $post, $zero, $one, $many );
+		return '<a href="' . $post->permalink . '#comments" title="' . _t( 'Read Comments' ) . '">' . end( $count ) . '</a>';
+	}
+
+	/**
+	 * Returns a full qualified URL of the specified post based on the comments count.
+ 	 *
+	 * Passed strings are localized prior to parsing therefore to localize "%d Comments" in french, it would be "%d Commentaires".
+	 *
+	 * Since we use sprintf() in the final concatenation, you must format passed strings accordingly.
+	 *
+	 * @param Theme $theme The current theme object
+	 * @param Post $post Post object used to build the comments link
+	 * @param string $zero String to return when there are no comments
+	 * @param string $one String to return when there is one comment
+	 * @param string $many String to return when there are more than one comment
+	 * @return string String to display for comment count
+	 */
+	public function theme_comments_count( $theme, $post, $zero = '', $one = '', $many = '' )
+	{
+		$count = $post->comments->approved->count;
+		if( empty( $zero ) ) {
+			$zero = _t( '%d Comments' );
+		}
+		if( empty( $one ) ) {
+			$one = _t( '%d Comment' );
+		}
+		if( empty( $many ) ) {
+			$many = _t( '%d Comments' );
+		}
+
+		if ($count >= 1) {
+			$text = _n( $one, $many, $count );
+		}
+		else {
+			$text = $zero;
+		}
+		return sprintf( $text, $count );
+	}
+
+	/**
+	 * Returns the count of queries executed
+	 *
+	 * @return integer The query count
+	 */
+	public function theme_query_count()
+	{
+		return count(DB::get_profiles());
+	}
+
+	/**
+	 * Returns total query execution time in seconds
+	 *
+	 * @return float Query execution time in seconds, with fractions.
+	 */
+	public function theme_query_time( )
+	{
+		return array_sum(array_map(create_function('$a', 'return $a->total_time;'), DB::get_profiles()));
+	}
+
+	/**
+	 * Returns a humane commenter's link for a comment if a URL is supplied, or just display the comment author's name
+	 *
+	 * @param Theme $theme The current theme
+	 * @param Comment $comment The comment object
+	 * @return string A link to the comment author or the comment author's name with no link
+	 */
+	public function theme_comment_author_link( $theme, $comment )
+	{
+		$url = $comment->url;
+		if($url != '') {
+			$parsed_url = InputFilter::parse_url($url);
+			if($parsed_url['host'] == '') {
+				$url = '';
+			}
+			else {
+				$url = InputFilter::glue_url($parsed_url);
+			}
+		}
+		if($url != '') {
+			return '<a href="'.$url.'">' . $comment->name . '</a>';
+		}
+		else {
+			return $comment->name;
+		}
+	}
+
+	/**
 	 * Detects if a variable is assigned to the template engine for use in
 	 * constructing the template's output.
 	 *
