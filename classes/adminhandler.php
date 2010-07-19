c471b903d474bdf8f69d158c9399394ac212baf7
Colin Seymour <lildude@contrib.habariproject.org>
2010-07-19 10:19:10 +0000

    Deleting slug now automatically regenerates new slug based on post title when post not in published state. Posts in published state retain old slug unless the slug itself is manually changed. Resolves #979
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@4283 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/adminhandler.php
+++ classes/adminhandler.php
@@ -511,7 +511,7 @@ class AdminHandler extends ActionHandler
 					$post->$field = $form->$field->value;
 				}
 			}
-			if ( $form->newslug->value == '' ) {
+			if ( $form->newslug->value == '' && $post->status == Post::status( 'published' ) ) {
 				Session::notice( _t( 'A post slug cannot be empty. Keeping old slug.' ) );
 			}
 			elseif ( $form->newslug->value != $form->slug->value ) {
@@ -598,7 +598,7 @@ class AdminHandler extends ActionHandler
 
 		$permalink = ( $post->status != Post::status( 'published' ) ) ? $post->permalink . '?preview=1' : $post->permalink;
 		Session::notice( sprintf( _t( 'The post %1$s has been saved as %2$s.' ), sprintf('<a href="%1$s">\'%2$s\'</a>', $permalink, Utils::htmlspecialchars( $post->title ) ), Post::status_name( $post->status ) ) );
-		if ( $post->slug != Utils::slugify( $post->title ) ) {
+		if ( $post->slug != Utils::slugify( $post->title ) || $post->slug != $form->slug->value ) {
 			Session::notice( sprintf( _t( 'The content address is \'%1$s\'.'), $post->slug ));
 		}
 		Utils::redirect( URL::get( 'admin', 'page=publish&id=' . $post->id ) );
