626731f5d850489001d987f7db4c274b71880d70
Colin Seymour <lildood@gmail.com>
2010-08-10 12:08:48 +0000

    Only show block "configure" option if the block actually provides configuration controls.  Also made "configure" and "delete" translatable.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4295 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/block_instances.php
+++ admin/block_instances.php
@@ -11,8 +11,10 @@
 	<div class="block_instance">
 		<h3 class="block_instance_<?php echo $instance->id; ?>"><?php echo Utils::htmlspecialchars($instance->title); ?><small><?php echo Utils::htmlspecialchars($instance->type); ?></small></h3>
 		<ul>
-			<li><a href="#" onclick="var i = $('<iframe src=\'<?php echo URL::get('admin', array('page' => 'configure_block', 'blockid' => $instance->id)); ?>\' style=\'width:600px;height:300px;\'></iframe>'); i.dialog({bgiframe:true,height:300,width:778,modal:true,dialogClass:'jqueryui',draggable:false,title:'Configure Block: <?php echo Utils::htmlspecialchars($instance->title); ?> (<?php echo Utils::htmlspecialchars($instance->type); ?>)'});i.css('width','768px');return false;">configure</a></li>
-			<li><a href="#" onclick="delete_block(<?php echo $instance->id; ?>);return false;">delete</a></li>
+		<?php if ( $instance->get_form()->controls ) { ?>
+			<li><a href="#" onclick="var i = $('<iframe src=\'<?php echo URL::get('admin', array('page' => 'configure_block', 'blockid' => $instance->id)); ?>\' style=\'width:600px;height:300px;\'></iframe>'); i.dialog({bgiframe:true,height:300,width:778,modal:true,dialogClass:'jqueryui',draggable:false,title:'Configure Block: <?php echo Utils::htmlspecialchars($instance->title); ?> (<?php echo Utils::htmlspecialchars($instance->type); ?>)'});i.css('width','768px');return false;"><?php _e( 'Configure'); ?></a></li>
+		<?php } ?>
+			<li><a href="#" onclick="delete_block(<?php echo $instance->id; ?>);return false;"><?php _e('Delete'); ?></a></li>
 		</ul>
 	</div>
 	<?php endforeach; ?>
