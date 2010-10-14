31f6271dcef9a7dd080f26f83b25f484625c1220
Chris Meller <chrismeller@contrib.habariproject.org>
2010-10-14 03:17:58 +0000

    Totally rewrite all the code for handling the main admin logs page. It's now clean, makes sense, and has been seriously modularized and prettified. This may break some ajax down the line, we'll deal with that later.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4410 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/adminhandler.php
+++ classes/adminhandler.php
@@ -2453,172 +2453,207 @@ class AdminHandler extends ActionHandler
 	 */
 	public function post_logs()
 	{
-		$this->fetch_logs();
-		$this->display( 'logs' );
-	}
-
-	/**
-	 * Assign values needed to display the logs page to the theme based on handlervars and parameters.
-	 *
-	 */
-	private function fetch_logs($params = NULL)
-	{
-		$locals = array(
-			'do_delete' => false,
-			'log_ids' => null,
-			'nonce' => '',
-			'timestamp' => '',
-			'PasswordDigest' => '',
-			'change' => '',
-			'limit' => 20,
-			'offset' => 0,
-			'user' => 0,
-			'date' => 'any',
-			'module' => '0',
-			'type' => '0',
-			'severity' => 'any',
-			'address' => '0',
-			'search' => '',
-			'do_search' => false,
-			'index' => 1,
-		);
-
-		foreach ( $locals as $varname => $default ) {
-			$$varname = isset( $this->handler_vars[$varname] ) ? $this->handler_vars[$varname] : $default;
-			$this->theme->{$varname} = $$varname;
-		}
-
-		if ( $do_delete && isset( $log_ids ) ) {
-			$okay = true;
-
-			if ( empty( $nonce ) || empty( $timestamp ) ||  empty( $PasswordDigest ) ) {
-				$okay = false;
-			}
-
-			$wsse = Utils::WSSE( $nonce, $timestamp );
-
-			if ( $PasswordDigest != $wsse['digest'] ) {
-				$okay = false;
-			}
-
-			if ( $okay ) {
-				foreach ( $log_ids as $id ) {
-					$ids[] = array( 'id' => $id );
-				}
-
-				$to_delete = EventLog::get( array( 'nolimit' => 1 ) );
-				$count = 0;
-				foreach ( $to_delete as $log ) {
-					$log->delete();
-					$count++;
-				}
-				Session::notice( _t( 'Deleted %d logs', array( $count ) ) );
-			}
-
-			Utils::redirect();
-		}
-
-		$this->theme->severities = LogEntry::list_severities();
-		$any = array( '0' => 'Any' );
-
-		$modulelist = LogEntry::list_logentry_types();
-		$modules = array();
-		$types = array();
-		$addresses = $any;
-		$ips = DB::get_column( 'SELECT DISTINCT(ip) FROM {log}' );
-		foreach ( $ips as $ip ) {
-			$addresses[$ip] = long2ip( $ip );
-		}
-		$this->theme->addresses = $addresses;
-		foreach ( $modulelist as $modulename => $typearray ) {
-			$modules['0,'.implode( ',', $typearray )] = $modulename;
-			foreach ( $typearray as $typename => $typevalue ) {
-				if ( !isset( $types[$typename] ) ) {
-					$types[$typename] = '0';
-				}
-				$types[$typename] .= ',' . $typevalue;
-			}
-		}
-		$types = array_flip( $types );
-		$this->theme->types = array_merge( $any, $types );
-		$this->theme->modules = array_merge( $any, $modules );
-
-		// set up the users
-		$users_temp = DB::get_results( 'SELECT DISTINCT username, user_id FROM {users} JOIN {log} ON {users}.id = {log}.user_id ORDER BY username ASC' );
-		array_unshift( $users_temp, new QueryRecord( array( 'username' => 'All', 'user_id' => 0 ) ) );
-		foreach ( $users_temp as $user_temp ) {
-			$users[$user_temp->user_id] = $user_temp->username;
-		}
-		$this->theme->users = $users;
-
-		// set up dates.
-		$dates = DB::get_column( 'SELECT timestamp FROM {log} ORDER BY timestamp DESC' );
-		$dates = array_map( create_function( '$date', 'return HabariDateTime::date_create( $date )->get(\'Y-m\');' ), $dates );
-		array_unshift( $dates, 'Any' );
-		$dates = array_combine( $dates, $dates );
-		$this->theme->dates = $dates;
-
-		// prepare the WSSE tokens
-		$this->theme->wsse = Utils::WSSE();
-
+		
+		// load all the values for our filter drop-downs
+		$dates = $this->fetch_log_dates();
+		$users = $this->fetch_log_users();
+		$ips = $this->fetch_log_ips();
+		extract( $this->fetch_log_modules_types() );		// $modules and $types
+		$severities = LogEntry::list_severities();
+		
+		
+		// parse out the arguments we'll fetch logs for
+		
+		// the initial arguments
 		$arguments = array(
-			'severity' => LogEntry::severity( $severity ),
-			'limit' => $limit,
-			'offset' => $offset,
+			'limit' => Controller::get_var( 'limit', 20 ),
+			'offset' => Controller::get_var( 'offset', 0 ),
 		);
+		
+		
+		// filter for the search field
+		$search = Controller::get_var( 'search', '' );
 
-		// deduce type_id from module and type
-		$r_type = explode( ',', substr( $type, 2 ) );
-		$r_module = explode( ',', substr( $module, 2 ) );
-		if ( $type != '0' && $module != '0' ) {
-			$arguments['type_id'] = array_intersect( $r_type, $r_module );
-		}
-		elseif ( $type == '0' ) {
-			$arguments['type_id'] = $r_module;
-		}
-		elseif ( $module == '0' ) {
-			$arguments['type_id'] = $r_type;
-		}
-
-		if ( '0' != $address ) {
-			$arguments['ip'] = $address;
-		}
-
-		if ( 'any' != strtolower( $date ) ) {
-			list( $arguments['year'], $arguments['month'] ) = explode( '-', $date );
-		}
-		if ( '' != $search ) {
+		if ( $search != '' ) {
 			$arguments['criteria'] = $search;
 		}
-		if ( '0' != $user ) {
+		
+		
+		// filter by date
+		$date = Controller::get_var( 'date', 'any' );
+		
+		if ( $date != 'any' ) {
+			$d = DateTime::createFromFormat( '!Y-m', $date );	// ! means fill any non-specified pieces with default Unix Epoch ones
+			$arguments['year'] = $d->format('Y');
+			$arguments['month'] = $d->format('m');
+		}
+		
+		
+		// filter by user
+		$user = Controller::get_var( 'user', 'any' );
+		
+		if ( $user != 'any' ) {
 			$arguments['user_id'] = $user;
 		}
-
-		if ( is_array($params) ) {
-			$arguments = array_merge($arguments, $params);
+		
+		
+		// filter by ip
+		$ip = Controller::get_var( 'address', 'any' );
+		
+		if ( $ip != 'any' ) {
+			$arguments['ip'] = $ip;
 		}
-
-		$this->theme->logs = EventLog::get( $arguments );
-
-		$monthcts = EventLog::get( array_merge( $arguments, array( 'month_cts' => true ) ) );
-		foreach ( $monthcts as $month ) {
-
-			if ( isset($years[$month->year]) ) {
-				$years[$month->year][] = $month;
+		
+		
+		// filter modules and types
+		// @todo get events of a specific type in a specific module, instead of either of the two
+		// the interface doesn't currently make any link between module and type, so we won't worry about it for now
+		
+		$module = Controller::get_var( 'module', 'any' );
+		$type = Controller::get_var( 'type', 'any' );
+		
+		if ( $module != 'any' ) {
+			// we get a slugified key back, get the actual module name
+			$arguments['module'] = $modules[ $module ];
+		}
+		
+		if ( $type != 'any' ) {
+			// we get a slugified key back, get the actual type name
+			$arguments['type'] = $types[ $type ];
+		}
+		
+		
+		// filter by severity
+		$severity = Controller::get_var( 'severity', 'any' );
+		
+		if ( $severity != 'any' ) {
+			$arguments['severity'] = $severity;
+		}
+		
+		
+		// get the logs!
+		$logs = EventLog::get( $arguments );
+		
+		
+		// last, but not least, generate the list of years used for the timeline
+		$months = EventLog::get( array_merge( $arguments, array( 'month_cts' => true ) ) );
+		
+		$years = array();
+		foreach ( $months as $m ) {
+			
+			$years[ $m->year ][] = $m;
+			
+		}
+		
+		
+		// assign all our theme values in one spot
+		
+		// first the filter options
+		$this->theme->dates = $dates;
+		$this->theme->users = $users;
+		$this->theme->addresses = $ips;
+		$this->theme->modules = $modules;
+		$this->theme->types = $types;
+		$this->theme->severities = $severities;
+		
+		// next the filter criteria we used
+		$this->theme->search = $search;
+		$this->theme->date = $date;
+		$this->theme->user = $user;
+		$this->theme->address = $ip;
+		$this->theme->module = $module;
+		$this->theme->type = $type;
+		$this->theme->severity = $severity;
+		
+		$this->theme->logs = $logs;
+		
+		$this->theme->years = $years;
+		
+		$this->theme->wsse = Utils::WSSE();		// prepare a WSSE token for any ajax calls
+		
+		//$this->fetch_logs();
+		$this->display( 'logs' );
+	}
+	
+	private function fetch_log_dates ( ) {
+		
+		$db_dates = DB::get_column( 'SELECT timestamp FROM {log} ORDER BY timestamp DESC' );
+		
+		$dates = array(
+			'any' => 'Any'
+		);
+		
+		foreach ( $db_dates as $db_date ) {
+			
+			$date = HabariDateTime::date_create( $db_date )->format('Y-m');
+			
+			$dates[ $date ] = $date;
+			
+		}
+		
+		return $dates;
+		
+	}
+	
+	private function fetch_log_users ( ) {
+		
+		$db_users = DB::get_results( 'SELECT DISTINCT username, user_id FROM {users} JOIN {log} ON {users}.id = {log}.user_id ORDER BY username ASC' );
+		
+		$users = array(
+			'any' => 'Any'
+		);
+		
+		foreach ( $db_users as $db_user ) {
+			
+			$users[ $db_user->user_id ] = $db_user->username;
+			
+		}
+		
+		return $users;
+		
+	}
+	
+	private function fetch_log_ips ( ) {
+		
+		$db_ips = DB::get_column( 'SELECT DISTINCT(ip) FROM {log}' );
+		
+		$ips = array(
+			'any' => 'Any'
+		);
+		
+		foreach ( $db_ips as $db_ip ) {
+			
+			$ips[ $db_ip ] = long2ip( $db_ip );
+			
+		}
+		
+		return $ips;
+		
+	}
+	
+	private function fetch_log_modules_types ( ) {
+		
+		$module_list = LogEntry::list_logentry_types();
+		
+		$modules = $types = array(
+			'any' => 'Any',
+		);
+		
+		foreach ( $module_list as $module_name => $type_array ) {
+			
+			// Utils::slugify() gives us a safe key to use - this is what will be handed to the filter after a POST as well
+			$modules[ Utils::slugify( $module_name ) ] = $module_name;
+			
+			foreach ( $type_array as $type_name => $type_value ) {
+				
+				$types[ Utils::slugify( $type_name ) ] = $type_name;
+				
 			}
-			else {
-				$years[$month->year] = array( $month );
-			}
-
+			
 		}
-
-		if ( isset($years) ) {
-			$this->theme->years = $years;
-		}
-		else {
-			$this->theme->years = array();
-		}
-
+				
+		return array( 'modules' => $modules, 'types' => $types );
+		
 	}
 
 	/**
