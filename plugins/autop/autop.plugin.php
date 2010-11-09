3890e357e415c23b828589dff6fe774fdef99947
Chris Meller <chris@doesnthaveone.com>
2010-11-09 02:16:09 +0000

    Remove the update beacon from the AutoP plugin. There's no reason for core plugins to have their own beacons, they're covered by the main Habari updates.
    git-svn-id: http://svn.habariproject.org/habari/trunk/htdocs/system@4531 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/autop/autop.plugin.php
+++ plugins/autop/autop.plugin.php
@@ -3,11 +3,6 @@
 class AutoP extends Plugin
 {
 	
-	public function action_update_check()
-	{
-		Update::add( $this->info->name, '21b481a3-9000-41f5-a906-2a80b3d5eb50', $this->info->version );
-	}
-	
 	public function action_init_atom() {
 		Format::apply( 'autop', 'post_content_atom' );
 	}
