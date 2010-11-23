4b88d2216f16f6f197fb54f71378429ee1e9dd17
Owen Winkler <epithet@gmail.com>
2010-11-23 17:55:23 +0000

    Output an ol for the tree and add a handle.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4615 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/formcontrol_tree.php
+++ admin/formcontrols/formcontrol_tree.php
@@ -1,10 +1,22 @@
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
 <ol class="tree">
-<?php foreach($options as $key => $text) : ?>
-	<li id="tree_item_<?php echo $key; ?>">
-		<div><?php echo Utils::htmlspecialchars($text); ?></div>
-	</li>
-<?php endforeach; ?>
+<?php
+if($firstnode = reset($options)) {
+	$lastright = $lastleft = reset($options)->mptt_left;
+	$indent = 0;
+	$stack = array();
+	foreach ( $options as $key => $term ) {
+		while ( count( $stack ) > 0 && end( $stack )->mptt_right < $term->mptt_left ) {
+			array_pop( $stack );
+			echo '</ol>';
+		}
+		echo '<ol><li id="tree_item_' . $key .'">
+			<div>' .  Utils::htmlspecialchars($term->term_display) . '</div>
+		</li>';
+		$stack[] = $term;
+	}
+}
+?>
 </ol>
 <input type="hidden" name="<?php echo $field; ?>_submitted" value="1">
 <?php $control->errors_out('<li>%s</li>', '<ul class="error">%s</ul>'); ?>
