e74e440188ae3cc198929251c3f7ee5a21b25d0d
Chris Meller <chrismeller@contrib.habariproject.org>
2010-12-03 17:27:20 +0000

    And finally Mzingi for 0.6.5. The bad man made me do it... *mumbles to himself incoherently*
    git-svn-id: https://svn.habariproject.org/habari/makaanga/0.x/htdocs/system@4662 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/mzingi/footer.php
+++ themes/mzingi/footer.php
@@ -1,3 +1,4 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
 	<!--begin footer-->
 	<div id="footer" class="clear">
 	<p><?php Options::out('title'); ?> <?php _e('is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a></p>
