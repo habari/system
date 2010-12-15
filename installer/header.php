cdd15c19f082a1959acfc27e4d7d958e9f4fcd2d
Chris Meller <chrismeller@contrib.habariproject.org>
2010-12-15 23:34:53 +0000

    Update a path I missed in the installer that pointed to the now-changed scripts directory manually.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4702 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- installer/header.php
+++ installer/header.php
@@ -7,7 +7,7 @@
 	<meta name="robots" content="no index,no follow" />
 	<link rel="shortcut icon" href="<?php Site::out_url('habari'); ?>/favicon.ico" />
 	<link href="<?php Site::out_url('habari'); ?>/system/installer/style.css" rel="stylesheet" type="text/css" />
-	<script type="text/javascript" src="<?php Site::out_url('habari'); ?>/scripts/jquery.js"></script>
+	<script type="text/javascript" src="<?php Site::out_url('vendor'); ?>/jquery.js"></script>
 	<title><?php _e('Install Habari'); ?></title>
 </head>
 <body id="installer">
