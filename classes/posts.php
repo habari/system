879828b48db86781bedecffcb120fc3c6cd52b5a
Rick Cockrum <rickc@contrib.habariproject.org>
2010-12-21 12:36:16 +0000

    Do what the comment says and account for the 'month_cts' param being set in deciding how to set the limit string for the query. Fixes the total counts displayed by the timeline not being correct.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4718 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/posts.php
+++ classes/posts.php
@@ -596,7 +596,7 @@ class Posts extends ArrayObject implements IsContent
 
 		// Remove the LIMIT if 'nolimit' or 'month_cts' is set
 		// Doing this first should allow OFFSET to work
-		if ( isset( $nolimit ) ) {
+		if ( isset( $nolimit ) || isset( $paramset['month_cts'] ) ) {
 			$limit = '';
 		}
 
