dd010f804513a653477ca4e2f6ffc29056139d28
Chris Meller <chrismeller@contrib.habariproject.org>
2010-12-24 02:11:48 +0000

    I committed the wrong control in r4730. This fixes my mistake.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4736 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/formcontrols/optionscontrol_text.php
+++ admin/formcontrols/optionscontrol_text.php
@@ -1,7 +1,15 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
+<?php
+	if ( isset( $autocomplete ) ) {
+		$autocomplete = 'autocomplete="' . $autocomplete . '"';
+	}
+	else {
+		$autocomplete = '';
+	}
+?>
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
 	<span class="pct25"><label for="<?php echo $field; ?>"><?php echo $caption; ?></label></span>
-	<span class="pct25"><input type="<?php echo (isset($this->type)) ? $type : 'text'?>" name="<?php echo $field; ?>" id="<?php echo $field; ?>" value="<?php echo Utils::htmlspecialchars( $value ); ?>" <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>></span>
+	<span class="pct25"><input type="<?php echo (isset($this->type)) ? $type : 'text'?>" name="<?php echo $field; ?>" id="<?php echo $field; ?>" value="<?php echo Utils::htmlspecialchars( $value ); ?>" <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?> <?php echo $autocomplete; ?>></span>
 	<?php if (!empty($helptext)) : ?>
 	<span class="pct40 helptext"><?php echo $helptext; ?></span>
 	<?php endif; ?>
