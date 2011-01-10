208d48a307d354d6839a636ec3aae538df874d50
Chris Meller <chrismeller@contrib.habariproject.org>
2011-01-10 03:57:36 +0000

    Related to r4774, this allows you to add on _out or any other _filter to the end of the post info value and hook a plugin filter just for that. Props to Konzertheld.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4776 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/postinfo.php
+++ classes/postinfo.php
@@ -16,9 +16,26 @@ class PostInfo extends InfoRecords
 	
 	public function __get ( $name ) {
 		
+		// if there is a _ in the name, there is a filter at the end
+		if ( strpos( $name, '_' ) !== false ) {
+			// pick off the last _'d piece
+			preg_match( '/^(.*)_([^_]+)$/', $name, $matches );
+			list( $junk, $name, $filter ) = $matches;
+		}
+		else {
+			$filter = false;
+		}
+		
+		// get the value by calling our parent function directly
 		$value = parent::__get( $name );
 		
+		// apply the main filter so values can be altered regardless of any _filter
 		$value = Plugins::filter( "post_info_{$name}", $value );
+		
+		// if there is a filter, apply that specific one too
+		if ( $filter ) {
+			$value = Plugins::filter( "post_info_{$name}_{$filter}", $value );
+		}
 		
 		return $value;
 		
