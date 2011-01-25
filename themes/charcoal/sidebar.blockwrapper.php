98acb35955b4d8ffa000d867457e83f0e6d003c8
Colin Seymour <lildood@gmail.com>
2011-01-25 14:48:01 +0000

    Added sidebar.blockwrapper to Charcoal so the block option to show the title actually takes effect. Have not added a nav.blockwrapper as this area does not seem to be used anywhere in the theme and I can not work out where it is intended to be used.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4810 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ themes/charcoal/sidebar.blockwrapper.php
@@ -0,0 +1,6 @@
+<div class="block<?php if( $block->_first ): ?> first<?php endif; if( $block->_last ):?> last<?php endif; echo ' index_' . $block->_area_index; ?>">
+<?php if ( $block->_show_title ) :?>
+	<h2><?php echo $block->title; ?></h2>
+<?php endif; ?>
+<?php echo $content; ?>
+</div>
