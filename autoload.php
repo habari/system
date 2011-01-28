1e043c55c5191588b6f9384d42f442decde5b145
Colin Seymour <lildood@gmail.com>
2011-01-28 10:14:40 +0000

    Coding standards
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4812 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- autoload.php
+++ autoload.php
@@ -11,47 +11,48 @@
  *
  * @param string $class_name Class called by the user
  */
-function habari_autoload($class_name) {
+function habari_autoload( $class_name )
+{
 	static $files = null;
 
 	$success = false;
-	$class_file = strtolower($class_name) . '.php';
+	$class_file = strtolower( $class_name ) . '.php';
 
-	if ( empty($files) ) {
+	if ( empty( $files ) ) {
 		$files = array();
 		$dirs = array( HABARI_PATH . '/system', HABARI_PATH . '/user' );
 
 		// For each directory, save the available files in the $files array.
-		foreach ($dirs as $dir) {
+		foreach ( $dirs as $dir ) {
 			$glob = glob( $dir . '/classes/*.php' );
 			if ( $glob === false || empty( $glob ) ) continue;
-			$fnames = array_map(create_function('$a', 'return strtolower(basename($a));'), $glob);
-			$files = array_merge($files, array_combine($fnames, $glob));
+			$fnames = array_map( create_function( '$a', 'return strtolower(basename($a));' ), $glob );
+			$files = array_merge( $files, array_combine( $fnames, $glob ) );
 		}
 
 		// Load the Site class, a requirement to get files from a multisite directory.
-		if ( isset($files['site.php']) ) {
-			require $files['site.php'];
+		if ( isset( $files['site.php'] ) ) {
+			require( $files['site.php'] );
 		}
 
 		// Verify if this Habari instance is a multisite.
-		if ( ($site_user_dir = Site::get_dir('user')) != HABARI_PATH . '/user' ) {
+		if ( ( $site_user_dir = Site::get_dir( 'user' ) ) != HABARI_PATH . '/user' ) {
 			// We are dealing with a site defined in /user/sites/x.y.z
 			// Add the available files in that directory in the $files array.
 			$glob = glob( $site_user_dir . '/classes/*.php' );
 			if ( $glob !== false && !empty( $glob ) ) {
-				$fnames = array_map(create_function('$a', 'return strtolower(basename($a));'), $glob);
-				$files = array_merge($files, array_combine($fnames, $glob));
+				$fnames = array_map( create_function( '$a', 'return strtolower(basename($a));' ), $glob );
+				$files = array_merge( $files, array_combine( $fnames, $glob ) );
 			}
 		}
 	}
 
 	// Search in the available files for the undefined class file.
-	if ( isset($files[$class_file]) ) {
-		require $files[$class_file];
+	if ( isset( $files[$class_file] ) ) {
+		require( $files[$class_file] );
 		// If the class has a static method named __static(), execute it now, on initial load.
-		if ( class_exists($class_name, false) && method_exists($class_name, '__static') ) {
-			call_user_func(array($class_name, '__static'));
+		if ( class_exists( $class_name, false ) && method_exists( $class_name, '__static' ) ) {
+			call_user_func( array( $class_name, '__static' ) );
 		}
 		$success = true;
 	}
