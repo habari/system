b4f6969c906bb3203b8d1e8cbff461f28541d990
Michael Bishop <miklb@contrib.habariproject.org>
2011-01-30 19:08:27 +0000

    updating blockwrapper in Mzingi to use new css logic added in r4844
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4845 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/mzingi/sidebar.blockwrapper.php
+++ themes/mzingi/sidebar.blockwrapper.php
@@ -1,6 +1,4 @@
-<div class="block<?php if( $block->_first ): ?> first<?php endif; if( $block->_last ):?> last<?php endif; echo ' index_' . $block->_area_index; ?>">
-<?php if ( $block->_show_title ) :?>
-	<h2><?php echo $block->title; ?></h2>
-<?php endif; ?>
+<div class="<?php echo $block->css_classes; ?>">
+<?php if($block->_show_title):?><h2><?php echo $block->title; ?></h2><?php endif; ?>
 <?php echo $content; ?>
 </div>
