212ea071de270e5de442fc185d06465bad5b7203
Owen Winkler <epithet@gmail.com>
2011-02-08 03:32:52 +0000

    Update theme block config display.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4859 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- admin/block_instances.php
+++ admin/block_instances.php
@@ -13,7 +13,7 @@
 		<h3 class="block_instance_<?php echo $instance->id; ?>"><?php echo Utils::htmlspecialchars($instance->title); ?><small><?php echo Utils::htmlspecialchars($instance->type); ?></small></h3>
 		<ul>
 		<?php if ( $instance->get_form()->controls ) { ?>
-			<li><a href="#" onclick="var i = $('<iframe src=\'<?php echo URL::get('admin', array('page' => 'configure_block', 'blockid' => $instance->id)); ?>\' style=\'width:600px;height:300px;\'></iframe>'); i.dialog({bgiframe:true,height:300,width:778,modal:true,dialogClass:'jqueryui',draggable:false,title:'Configure Block: <?php echo Utils::htmlspecialchars($instance->title); ?> (<?php echo Utils::htmlspecialchars($instance->type); ?>)'});i.css('width','768px');return false;"><?php _e( 'Configure'); ?></a></li>
+			<li><a href="#" onclick="var i = $('<iframe src=\'<?php echo URL::get('admin', array('page' => 'configure_block', 'blockid' => $instance->id)); ?>\' style=\'width:600px;height:300px;\'></iframe>'); i.dialog({bgiframe:true,width:778,modal:true,dialogClass:'jqueryui',title:'<?php _e('Configure Block: %1s (%2s)', array(Utils::htmlspecialchars($instance->title), Utils::htmlspecialchars($instance->type))); ?>'});i.css('width','768px');return false;"><?php _e( 'Configure'); ?></a></li>
 		<?php } ?>
 			<li><a href="#" onclick="delete_block(<?php echo $instance->id; ?>);return false;"><?php _e('Delete'); ?></a></li>
 		</ul>
