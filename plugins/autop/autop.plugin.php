393fe60b1dd2ab8b7dedd734d0ffa4b7c2527481
Rick Cockrum <rickc@contrib.habariproject.org>
2011-02-16 03:00:58 +0000

    init_theme is theme-specific now. autoparagraph needs to use init_theme_any. Fix formatting and parameters passed to the actions while I'm at it.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4887 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- plugins/autop/autop.plugin.php
+++ plugins/autop/autop.plugin.php
@@ -3,11 +3,13 @@
 class AutoP extends Plugin
 {
 	
-	public function action_init_atom() {
+	public function action_init_atom_any( $theme )
+	{
 		Format::apply( 'autop', 'post_content_atom' );
 	}
 	
-	public function action_init_theme() {
+	public function action_init_theme_any( $theme )
+	{
 		Format::apply( 'autop', 'post_content_out' );
 	}
 	
