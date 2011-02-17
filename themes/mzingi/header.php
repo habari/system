4b3f81001eab840d87209a01583b32601f7aedb7
Mike Lietz <mikelietz@contrib.habariproject.org>
2011-02-17 03:01:15 +0000

    Remove atom links - $theme->header() will add them.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4891 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/mzingi/header.php
+++ themes/mzingi/header.php
@@ -7,9 +7,6 @@
 	<title><?php if ($request->display_entry && isset($post)) { echo "{$post->title} - "; } ?><?php Options::out( 'title' ) ?></title>
 	<meta name="generator" content="Habari">
 
-	<link rel="edit" type="application/atom+xml" title="Atom Publishing Protocol" href="<?php URL::out( 'atompub_servicedocument' ); ?>">
-	<link rel="alternate" type="application/atom+xml" title="Atom" href="<?php $theme->feed_alternate(); ?>">
-	<link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php URL::out( 'rsd' ); ?>">
 	<link rel="stylesheet" type="text/css"  media="print" href="<?php Site::out_url( 'vendor'); ?>/blueprint/print.css">
 	<link rel="stylesheet" type="text/css" media ="screen" href="<?php Site::out_url( 'vendor'); ?>/blueprint/screen.css">
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php Site::out_url( 'theme' ); ?>/style.css">
