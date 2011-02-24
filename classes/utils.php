c68f0aceeb8c342497ebb8d243bf952a287c4f38
Chris Meller <chrismeller@contrib.habariproject.org>
2011-02-24 03:04:22 +0000

    Replace some double quotes with single quotes.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4911 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/utils.php
+++ classes/utils.php
@@ -1050,19 +1050,19 @@ class Utils
 	public static function get_ip()
 	{
 		if ( isset( $_SERVER['HTTP_CLIENT_IP'] ) ) {
-			return $_SERVER["HTTP_CLIENT_IP"];
+			return $_SERVER['HTTP_CLIENT_IP'];
 		}
 		else if ( isset( $_SERVER['HTTP_FORWARDED'] ) ) {
-			return $_SERVER["HTTP_FORWARDED"];
+			return $_SERVER['HTTP_FORWARDED'];
 		}
 		else if ( isset( $_SERVER['HTTP_X_FORWARDED'] ) ) {
-			return $_SERVER["HTTP_X_FORWARDED"];
+			return $_SERVER['HTTP_X_FORWARDED'];
 		}
 		else if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-			return $_SERVER["HTTP_X_FORWARDED_FOR"];
+			return $_SERVER['HTTP_X_FORWARDED_FOR'];
 		}
 		else if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
-			return $_SERVER["REMOTE_ADDR"];
+			return $_SERVER['REMOTE_ADDR'];
 		}
 		else {
 			return '0.0.0.0';
