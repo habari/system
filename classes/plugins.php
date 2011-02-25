5096771e2cefa6720935cbbfa6be8a3c572ebaf0
Rick Cockrum <rickc@contrib.habariproject.org>
2011-02-25 04:48:44 +0000

    Don't even mess with the active plugins option if the plugin is already installed. Fixes the $class variable being empty.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@4923 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- classes/plugins.php
+++ classes/plugins.php
@@ -482,6 +482,9 @@ class Plugins
 			$plugin = Plugins::load( $class );
 			$ok = Plugins::filter( 'activate_plugin', $ok, $file ); // Allow plugins to reject activation
 		}
+		else if ( in_array( $short_file, $activated ) ) {
+			$ok = false;
+		}
 		if ( $ok ) {
 			$activated[$class] = $short_file;
 			Options::set( 'active_plugins', $activated );
