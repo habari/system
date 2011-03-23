c4b306f26b00e678ba59a19841f989ca30fc340a
Chris Meller <chrismeller@contrib.habariproject.org>
2011-03-23 22:51:31 +0000

    Remove the atom-related headers from k2 and charcoal, they're added automagically by the theme class.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/system@5016 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- themes/charcoal/header.php
+++ themes/charcoal/header.php
@@ -5,9 +5,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php Options::out( 'title' ) ?><?php if ($request->display_entry && isset($post)) { echo " :: {$post->title}"; } ?></title>
 	<meta name="generator" content="Habari">
-	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php $theme->feed_alternate(); ?>">
-	<link rel="edit" type="application/atom+xml" title="Atom Publishing Protocol" href="<?php URL::out( 'atompub_servicedocument' ); ?>">
-	<link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php URL::out( 'rsd' ); ?>">
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php Site::out_url( 'theme' ); ?>/style.css">
 	<?php if ($localized_css): ?>
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php echo Site::get_url( 'theme', true ) . $localized_css; ?>">
--- themes/k2/header.php
+++ themes/k2/header.php
@@ -6,10 +6,6 @@
 <title><?php if ($request->display_entry && isset($post)) { echo "{$post->title} - "; } ?><?php Options::out( 'title' ) ?></title>
 <meta name="generator" content="Habari">
 
-<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php $theme->feed_alternate(); ?>">
-<link rel="edit" type="application/atom+xml" title="Atom Publishing Protocol" href="<?php URL::out( 'atompub_servicedocument' ); ?>">
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php URL::out( 'rsd' ); ?>">
-
 <link rel="stylesheet" type="text/css" media="screen" href="<?php Site::out_url( 'theme' ); ?>/style.css">
 
 <?php $theme->header(); ?>
