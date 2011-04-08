a4e9f47a7ef4d54a6d80e47d6cf3ec57178b0f09
Michael C. Harris <michaeltwofish@contrib.habariproject.org>
2011-04-08 06:37:21 +0000

    The media panels are related to publishing and updating posts, so put them in the admin posts handler too.
    git-svn-id: https://svn.habariproject.org/habari/branches/adminhandlers/htdocs/system@5074 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- handlers/adminpostshandler.php
+++ handlers/adminpostshandler.php
@@ -398,6 +398,82 @@ class AdminPostsHandler extends AdminHandler
 	}
 
 	/**
+	 * Handles AJAX requests from media silos.
+	 */
+	public function ajax_media( $handler_vars )
+	{
+		Utils::check_request_method( array( 'POST' ) );
+
+		$path = $handler_vars['path'];
+		$rpath = $path;
+		$silo = Media::get_silo( $rpath, true );  // get_silo sets $rpath by reference to the path inside the silo
+		$assets = Media::dir( $path );
+		$output = array(
+			'ok' => 1,
+			'dirs' => array(),
+			'files' => array(),
+			'path' => $path,
+		);
+		foreach ( $assets as $asset ) {
+			if ( $asset->is_dir ) {
+				$output['dirs'][$asset->basename] = $asset->get_props();
+			}
+			else {
+				$output['files'][$asset->basename] = $asset->get_props();
+			}
+		}
+		$rootpath = MultiByte::strpos( $path, '/' ) !== false ? MultiByte::substr( $path, 0, MultiByte::strpos( $path, '/' ) ) : $path;
+		$controls = array( 'root' => '<a href="#" onclick="habari.media.fullReload();habari.media.showdir(\''. $rootpath . '\');return false;">' . _t( 'Root' ) . '</a>' );
+		$controls = Plugins::filter( 'media_controls', $controls, $silo, $rpath, '' );
+		$controls_out = '';
+		foreach ( $controls as $k => $v ) {
+			if ( is_numeric( $k ) ) {
+				$controls_out .= "<li>{$v}</li>";
+			}
+			else {
+				$controls_out .= "<li class=\"{$k}\">{$v}</li>";
+			}
+		}
+		$output['controls'] = $controls_out;
+
+		echo json_encode( $output );
+	}
+
+	/**
+	 * Handles AJAX requests from media panels.
+	 */
+	public function ajax_media_panel( $handler_vars )
+	{
+		Utils::check_request_method( array( 'POST' ) );
+
+		$path = $handler_vars['path'];
+		$panelname = $handler_vars['panel'];
+		$rpath = $path;
+		$silo = Media::get_silo( $rpath, true );  // get_silo sets $rpath by reference to the path inside the silo
+
+		$panel = '';
+		$panel = Plugins::filter( 'media_panels', $panel, $silo, $rpath, $panelname );
+
+		$controls = array();
+		$controls = Plugins::filter( 'media_controls', $controls, $silo, $rpath, $panelname );
+		$controls_out = '';
+		foreach ( $controls as $k => $v ) {
+			if ( is_numeric( $k ) ) {
+				$controls_out .= "<li>{$v}</li>";
+			}
+			else {
+				$controls_out .= "<li class=\"{$k}\">{$v}</li>";
+			}
+		}
+		$output = array(
+			'controls' => $controls_out,
+			'panel' => $panel,
+		);
+
+		echo json_encode( $output );
+	}
+
+	/**
 	 * Handles AJAX requests from the manage posts page.
 	 */
 	public function ajax_posts()
