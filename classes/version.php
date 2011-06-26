96ba8337ef60a6157b17b70f786d4c9a5faf79c9
ringmaster <epithet@gmail.com>
2011-06-26 04:29:50 +0000

    Bumping the API version number.

--- classes/version.php
+++ classes/version.php
@@ -15,7 +15,7 @@ class Version
 	// DB and API versions are aligned with the SVN revision
 	// number in which they last changed.
 	const DB_VERSION = 5097;
-	const API_VERSION = 4955;
+	const API_VERSION = 4956;
 
 	const HABARI_VERSION = '0.8-alpha';
 
