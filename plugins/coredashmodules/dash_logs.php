4174308e1be4d88938373b73043182b0ad39955d
Richard Cockrum <rickcockrum@gmail.com>
2011-08-27 16:20:34 -0400

    Allow times and dates to be localized in proper order. Applies to habari/habari#79

--- plugins/coredashmodules/dash_logs.php
+++ plugins/coredashmodules/dash_logs.php
@@ -2,7 +2,7 @@
 	<ul class="items">
 		<?php foreach( $logs as $log ) { ?>
 			<li class="item clear">
-				<span class="date pct15 minor"><?php $log->timestamp->out( 'M j' ); ?></span>
+				<span class="date pct15 minor"><?php /* @locale Date formats according to http://php.net/manual/en/function.date.php */ $log->timestamp->out( _t( 'M j' ) ); ?></span>
 				<span class="message pct85 minor"><?php echo Utils::htmlspecialchars( $log->message ); ?></span>
 			</li>
 		<?php } ?>
