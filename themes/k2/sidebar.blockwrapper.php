d295a2f97bc0bf22eac662128f823d52aae2e80d
ringmaster <a_github@midnightcircus.com>
2011-11-22 21:47:58 -0500

    Hoping nobody notices this. Removed k2 as a system theme.

--- themes/k2/sidebar.blockwrapper.php
+++
@@ -1,5 +0,0 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
-<div class="<?php echo $block->css_classes; ?>">
-<?php if($block->_show_title):?><h2><?php echo $block->title; ?></h2><?php endif; ?>
-<?php echo $content; ?>
-</div>
