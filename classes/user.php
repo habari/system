5bdacc5b008124ac0fbb1c775a3ad2ca38317ec6
ringmaster <a_github@midnightcircus.com>
2012-04-12 13:37:44 -0400

    Make the User class implement FormStorage

--- classes/user.php
+++ classes/user.php
@@ -21,7 +21,7 @@
  * </code>
  *
  */
-class User extends QueryRecord
+class User extends QueryRecord implements FormStorage
 {
 	/**
 	 * Static storage for the currently logged-in User record
@@ -693,6 +693,28 @@ class User extends QueryRecord
 		return $this->url_args;
 	}
 
+	/**
+	 * Stores a form value into the object
+	 *
+	 * @param string $key The name of a form component that will be stored
+	 * @param mixed $value The value of the form component to store
+	 */
+	function field_save($key, $value)
+	{
+		$this->info->$key = $value;
+		$this->info->commit();
+	}
+
+	/**
+	 * Loads form values from an object
+	 *
+	 * @param string $key The name of a form component that will be loaded
+	 * @return mixed The stored value returned
+	 */
+	function field_load($key)
+	{
+		return $this->info->$key;
+	}
 }
 
 ?>
