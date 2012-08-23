0eb5a310c819bd581ae9de768f77291df3c795be
helpy <dev@helpy.de>
2012-08-23 10:53:22 +0200

    Corrected/Extended the comments of the class methods

--- classes/site.php
+++ classes/site.php
@@ -116,9 +116,13 @@ class Site
 	 *		http://www.habariproject.org/user/sites/x.y.z/themes/theme_name
 	 *	'admin' returns http://www.habariproject.org/admin
 	 *	'admin_theme' returns http://www.habariproject.org/system/admin
+	 *  'login' returns http://www.habariproject.org/auth/login
+	 *  'logout' returns http://www.habariproject.org/auth/logout
 	 *	'system' returns http://www.habariproject.org/system
-	 *	'scripts' returns http://www.habariproject.org/scripts
+	 *	'vendor' returns http://www.habariproject.org/system/vendor
+	 *	'scripts' returns http://www.habariproject.org/system/vendor
 	 *	'3rdparty' returns http://www.habariproject.org/3rdparty
+	 *     if /3rdparty does not exists, /system/vendor will be returned
 	 *	'hostname' returns www.habariproject.org
 	 * @param string the name of the URL to return
 	 * @param bool whether to include a trailing slash.  Default: No
@@ -226,6 +230,7 @@ class Site
 	/**
 	 * get_path returns a relative URL path, without leading protocol or host
 	 *	'base' returns the URL sub-directory in which Habari is installed, if any.
+	 *  'habari' returns the same as 'base'
 	 *	'user' returns one of the following:
 	 *		user
 	 *		user/sites/x.y.z
@@ -278,6 +283,8 @@ class Site
 	 *	'config' returns the path of the directory containing config.php
 	 *	'user' returns the path of the user directory
 	 *	'theme' returns the path of the site's active theme
+	 *  'admin_theme' returns the path to the admin directory
+	 *  'vendor' returns the path to the vendor directory
 	 * @param string the name of the path item to return
 	 * @param bool whether to include a trailing slash.  Default: No
 	 * @return string Path
