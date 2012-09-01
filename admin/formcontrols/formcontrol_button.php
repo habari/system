237264344eba5eec8255f19d687cfe13212851e7
Colin Seymour <lildood@gmail.com>
2012-09-01 10:21:45 +0100

    Clean up and fix formcontrol_button. Part of implementing habari/habari#275

--- admin/formcontrols/formcontrol_button.php
+++ admin/formcontrols/formcontrol_button.php
@@ -1,21 +1,33 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>><input type="button" <?php if (isset($disabled) && $disabled) { ?>disabled <?php } 	if ( isset( $tabindex ) ) { ?> tabindex="<?php echo $tabindex; ?>"<?php } ?>	name="<?php echo $field; ?>" value="<?php echo Utils::htmlspecialchars($caption); ?>"
-<?php if (isset($onclick) && $onclick != ''): ?>
-<script type="text/javascript">function onclick_<?php echo Utils::slugify($id, '_') ?>() {
-<?php echo $onclick; ?>
-}</script>
-<?php endif; ?>
->
-<?php 
-
-	if ( isset( $helptext ) && !empty( $helptext ) ) {
+<div<?php
+		echo $control->parameter_map(
+			array(
+				'class', 'id' => 'name'
+			)
+		); ?>>
+		<input type="button" <?php
+		echo $control->parameter_map(
+			array(
+				'title' => array( 'control_title', 'title' ),
+				'tabindex', 'disabled',
+				'id' => 'field',
+				'name' => 'field',
+			),
+			array(
+				'value' => Utils::htmlspecialchars( $caption ),
+				'onclick' => 'onclick_'.Utils::slugify( $id, '_' ).'()',
+			)
+		);
+		?>>
+		<?php if ( isset( $onclick ) && $onclick != '' ): ?>
+		<script type="text/javascript">function onclick_<?php echo Utils::slugify( $id, '_' ) ?>() {
+		<?php echo $onclick; ?>
+		}</script>
+		<?php endif; ?>
+		<?php 
+			if ( isset( $helptext ) && !empty( $helptext ) ) { ?>
+				<span class="helptext"><?php echo $helptext; ?></span>
+				<?php
+			}
 		?>
-			<span class="helptext"><?php echo $helptext; ?></span>
-		<?php
-	}
-
-?>
-<?php if ($message != '') : ?>
-<p class="error"><?php echo $message; ?></p>
-<?php endif; ?>
 </div>
