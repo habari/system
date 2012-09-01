2249e610092d1d2498744b5db2d69b028f55064d
Colin Seymour <lildood@gmail.com>
2012-09-01 10:51:36 +0100

    Clean up and fix formcontrol_checkbox. Part of implementing habari/habari#275 and habari/habari#340

--- admin/formcontrols/formcontrol_checkbox.php
+++ admin/formcontrols/formcontrol_checkbox.php
@@ -5,11 +5,28 @@
 				'class', 'id' => 'name'
 			)
 		); ?>>
-	<label for="<?php echo $field; ?>"><?php echo $this->caption; ?></label>
-	<input type="checkbox" name="<?php echo $field; ?>" id="<?php echo $field; ?>" value="1" <?php echo $value ? 'checked' : ''; ?>>
+	<label <?php
+		echo $control->parameter_map(
+			array(
+				'title' => array( 'label_title', 'title' ),
+				'for' => 'field',
+			)
+		); ?>><?php echo $this->caption; ?></label>
+	<input type="checkbox" <?php
+		echo $control->parameter_map(
+			array(
+				'title' => array( 'control_title', 'title' ),
+				'tabindex', 'disabled', 'readonly',
+				'id' => 'field',
+				'name' => 'field',
+			),
+			array(
+				'value' => '1',
+			)
+		);
+		?> <?php echo $value ? 'checked' : ''; ?>>
 	<input type="hidden" name="<?php echo $field; ?>_submitted" value="1" >
 	<?php 
-	
 		if ( isset( $helptext ) && !empty( $helptext ) ) {
 			?>
 				<span class="helptext"><?php echo $helptext; ?></span>
