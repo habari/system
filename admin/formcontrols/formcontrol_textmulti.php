3fe6fa2d0f617e939980c7b27fb922f87bf2817a
Colin Seymour <lildood@gmail.com>
2012-09-04 16:47:24 +0100

    Clean up formcontrol_textmulti. Part of implementing habari/habari#275 and habari/habari#340

--- admin/formcontrols/formcontrol_textmulti.php
+++ admin/formcontrols/formcontrol_textmulti.php
@@ -1,25 +1,39 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?>>
-<?php if ($message != '') : ?>
-<p class="error"><?php echo $message; ?></p>
-<?php endif; ?>
+<div<?php
+		echo $control->parameter_map(
+			array(
+				'class', 'id' => 'name'
+			)
+		); ?>
 <p><?php echo $this->caption; ?></p>
 <?php
-if (!is_array($value)) {
-	$value = array($value);
+if ( !is_array( $value ) ) {
+	$value = array( $value );
 }
 $i = 0;
-foreach($value as $value_1) :
+foreach( $value as $value_1 ) :
 $i++;
 	if ( $value_1 ) :
  ?>
-	<span class="textmulti_item"><input type="text" name="<?php echo $field; ?>[]" id="<?php echo $field . '_' . $i; ?>" value="<?php echo Utils::htmlspecialchars($value_1); ?>"> <a href="#" onclick="return controls.textmulti.remove(this);" title="<?php _e( 'remove' ); ?>" class="textmulti_remove opa50">[<?php _e( 'remove' ); ?>]</a></span>
+	<span class="textmulti_item">
+		<input <?php
+		echo $control->parameter_map(
+			array(
+				'tabindex', 'size', 'maxlength', 'autocomplete', 'disabled', 'readonly',
+			),
+			array(
+				'name' => $control->field . '[]',
+				'id' => $control->field . '_' . $i,
+				'value' => Utils::htmlspecialchars( $value_1 ),
+			)
+		);
+		?>>	<a href="#" onclick="return controls.textmulti.remove( this );" title="<?php _e( 'remove' ); ?>" class="textmulti_remove opa50">[<?php _e( 'remove' ); ?>]</a></span>
 <?php
 	endif;
 endforeach;
 ?>
 <a href="#" onclick="return controls.textmulti.add(this, '<?php echo $field; ?>');" class="textmulti_add opa50" title="<?php _e( 'add' ); ?>">[<?php _e( 'add' ); ?>]</a>
-<?php 
+<?php
 
 	if ( isset( $helptext ) && !empty( $helptext ) ) {
 		?>
@@ -28,4 +42,5 @@ endforeach;
 	}
 
 ?>
+	<?php $control->errors_out( '<li>%s</li>', '<ul class="error">%s</ul>' ); ?>
 </div>
