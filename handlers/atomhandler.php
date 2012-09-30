021a19fca80496687f5b4142cad9a709c62ed1f9
Richard Cockrum <rick@ubuntu.ubuntu-domain>
2012-09-30 11:20:03 -0400

    Allow admincontrol_textarea to display un-decoded data when used in raw mode. Make accompanying changes to the atom feed content.
    First attempt at fixing #216. Proper escaping is a pain.

--- handlers/atomhandler.php
+++ handlers/atomhandler.php
@@ -178,8 +178,7 @@ class AtomHandler extends ActionHandler
 		foreach ( $posts as $post ) {
 			$user = User::get_by_id( $post->user_id );
 			$title = ( $this->is_auth() ) ? Utils::htmlspecialchars( $post->title ) : Utils::htmlspecialchars( $post->title_atom );
-			$content = ( $this->is_auth() ) ? Utils::htmlspecialchars( $post->content ) : Utils::htmlspecialchars( $post->content_atom );
-
+			$content = ( $this->is_auth() ) ? Utils::htmlspecialchars( $post->content, ENT_COMPAT, 'UTF-8', false ) : Utils::htmlspecialchars( $post->content_atom, ENT_COMPAT, 'UTF-8', false );
 			$content = Plugins::filter( 'atom_add_post', $content, $post );
 
 			$feed_entry = $xml->addChild( 'entry' );
@@ -553,7 +552,7 @@ class AtomHandler extends ActionHandler
 
 			$user = User::get_by_id( $post->user_id );
 			$title = ( $this->is_auth() ) ? $post->title : $post->title_atom;
-			$content = ( $this->is_auth() ) ? Utils::htmlspecialchars( $post->content ) : Utils::htmlspecialchars( $post->content_atom );
+			$content = ( $this->is_auth() ) ? Utils::htmlspecialchars( $post->content, ENT_COMPAT, 'UTF-8', false ) : Utils::htmlspecialchars( $post->content_atom, ENT_COMPAT, 'UTF-8', false );
 
 			// Build the namespaces, plugins can alter it to override or insert their own.
 			$namespaces = array( 'default' => 'http://www.w3.org/2005/Atom' );
