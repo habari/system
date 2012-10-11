5143877f56e6c6b1d17fcc5ac96f24529a619848
Chris J. Davis <chrisjdavis@contrib.habariproject.org>
2012-10-11 09:26:51 -0500

    Add a method for verified REST requests
    Most APIs have two types of endpoints, those that require verification
    and those that do not. Create a method for verified requests and add a
    plugin hook to sink when verifying requests.

--- handlers/resthandler.php
+++ handlers/resthandler.php
@@ -13,6 +13,7 @@ class RestHandler extends ActionHandler
 
 	/**
 	 * Handles incoming REST requests for which the user need not be authenticated.
+	 * Used for requests where no verification/authentication is required.
 	 * Forwards the request to plugin hook registered for the RewriteRule.
 	 *
 	 */
@@ -22,12 +23,34 @@ class RestHandler extends ActionHandler
 		$hookfn = $matched_rule->parameters['hook'];
 		$result = call_user_func_array($hookfn, array($matched_rule->named_arg_values));
 		//Utils::debug($result);
+		
 		if(!$result instanceof RestResponse) {
 			$result = new RestResponse($result);
 		}
+		
 		$result->out();
 	}
 
+	/**
+	 * Handles incoming REST requests for which the user must be authenticated.
+	 * Used for requests where tokens are provided for verification.
+	 * Forwards the request to plugin hook registered for the RewriteRule.
+	 *
+	 * @see act_rest()
+	 */
+	public function act_verified_rest()
+	{
+		Plugins::act( 'auth_rest_verify',  $this );
+		
+		$matched_rule = Controller::get_matched_rule();
+		$hookfn = $matched_rule->parameters['hook'];
+		$result = call_user_func_array($hookfn, array($matched_rule->named_arg_values));
+		
+		if(!$result instanceof RestResponse) {
+			$result = new RestResponse($result);
+		}
+		
+		$result->out();
+	}
 }
-
 ?>
