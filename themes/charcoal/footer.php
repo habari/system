bdbaccec45f3edd2a067a5eda939cd81b7fc2511
Chris Meller <chris@doesnthaveone.com>
2012-11-12 12:59:19 -0600

    Make the Charcoal footer text more easily translatable.

--- themes/charcoal/footer.php
+++ themes/charcoal/footer.php
@@ -5,12 +5,17 @@
 	</div>
 	<div id="footer">
 		<p>
-			<?php _e( '%1$s is powered by %2$s', array(
+			<?php
+
+				_e( '%1$s is powered by %2$s', array(
 					Options::get('title'),
 					'<a href="http://www.habariproject.org/" title="Habari">Habari ' . Version::get_habariversion() . '</a>'
-				)); ?> - 
-			<a href="<?php URL::out( 'atom_feed', array( 'index' => '1' ) ); ?>"><?php _e( 'Atom Entries' ); ?></a><?php _e( ' and ' ); ?>
-			<a href="<?php URL::out( 'atom_feed_comments' ); ?>"><?php _e( 'Atom Comments' ); ?></a>
+				));
+
+			?> - 
+			<?php
+				_e( '<a href="%1$s">Atom Entries</a> and <a href="%2$s">Atom Comments</a>', array( URL::get( 'atom_feed', array( 'index' => 1 ) ), URL::get( 'atom_feed_comments' ) ) );
+			?>
 		</p>
 	</div>
 	<div class="clear"></div>
