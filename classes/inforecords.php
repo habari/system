9404c47795f6a8e5ee03161c147ef37207251268
Chris Meller <chris@doesnthaveone.com>
2013-02-10 13:12:47 -0600

    Add a method to InfoRecords to get all the records as an array.
    This would have been native if we had extended ArrayObject… Why didn't we do this, again?

--- classes/inforecords.php
+++ classes/inforecords.php
@@ -120,6 +120,17 @@ abstract class InfoRecords implements URLProperties
 		return $this->__inforecord_array[$name]['value'];
 	}
 
+	public function getArrayCopy ( ) {
+		$this->_load();
+
+		$result = array();
+		foreach ( $this->__inforecord_array as $k => $v ) {
+			$result[ $k ] = $v['value'];
+		}
+
+		return $result;
+	}
+
 	/**
 	 * Update the info record.
 	 * The value will not be stored in the database until calling $this->commit();
