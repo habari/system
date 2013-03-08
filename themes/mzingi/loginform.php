b33592e5ce0bff0007f79f2ef5baa6a6f3744020
Konzertheld <christian.gredig@gmail.com>
2013-03-08 06:48:51 +0100

    New FormUI login form for Mzingi. Yay! With working password reset! Double-Yay! Plus bonus PHP notice fixes.

--- themes/mzingi/loginform.php
+++ themes/mzingi/loginform.php
@@ -6,7 +6,7 @@ if ( isset( $error ) ) {
 <p><?php _e('That login is incorrect.'); ?></p>
 <?php
 }
-if ( $loggedin ) {
+if ( isset( $loggedin ) && $loggedin ) {
 ?>
 <p><?php _e('You are logged in as'); ?> <?php echo $user->username; ?>.</p>
 <p><?php _e('Want to'); ?> <a href="<?php Site::out_url( 'habari' ); ?>/auth/logout"><?php _e('log out'); ?></a>?</p>
@@ -15,23 +15,39 @@ if ( $loggedin ) {
 else {
 ?>
 <?php Plugins::act( 'theme_loginform_before' ); ?>
-<form method="post" action="<?php URL::out( 'auth', array( 'page' => 'login' ) ); ?>">
-	<p>
-		<label for="habari_username"><small><strong><?php _e('Name:'); ?></strong></small></label>
-		<input type="text" size="25" name="habari_username" id="habari_username">
-	</p>
-	<p>
-		<label for="habari_password"><small><strong><?php _e('Password:'); ?></strong></small></label>
-		<input type="password" size="25" name="habari_password" id="habari_password">
-	</p>
-	<?php Plugins::act( 'theme_loginform_controls' ); ?>
-	<p>
-		<input type="submit" value="<?php _e('GO!'); ?>">
-	</p>
-	<p id="password_utils">
-		<input class="submit" type="submit" name="submit_button" value="<?php _e('Reset password'); ?>">
-	</p>
-</form>
+<?php if ( Session::has_messages() ) Session::messages_out(); ?>
+<?php echo $form; ?>
+<script type="text/javascript">
+	var password_label;
+	$(document).ready( function() {
+		// No session output via JS, just hide the stuff we don't need
+		$("#reset_message").hide();
+		$("#reset_button").hide();
+		password_label = $('label[for=habari_password]');
+		// to fix autofill issues, we need to check the password field on every keyup
+		$('#habari_username').keyup( function() {
+			setTimeout( "labeler.check( password_label );", 10 );
+		} ).click( function() {
+			setTimeout( "labeler.check( password_label );", 50 );
+		} );
+		// for autofill without user input
+		setTimeout( function(){ labeler.check( password_label ); }, 10 );
+		
+		// Make the login form a bit more intuitive when requesting a password reset
+		$("#reset_link").click(function() {
+			// Hide password box (and surrounding container)
+			$("#habari_password").parent().hide();
+			// Hide Login button and link
+			$("#submit_button").hide();
+			$("#reset_link").hide();
+			// Show message that explains things a bit better
+			$("p#reset_message").fadeIn();
+			// Show real button
+			$("#reset_button").fadeIn();
+			return false;
+		});
+	});
+</script>
 <?php Plugins::act( 'theme_loginform_after' ); ?>
 <?php
 }
