2db5b4728a082b8807aa85885d1c02cd91a9df6f
Peter Wu <lekensteyn@gmail.com>
2013-04-15 19:51:47 +0200

    Added Plugin namespace for plugin/coreblocks/block.search_form

--- plugins/coreblocks/block.search_form.php
+++ plugins/coreblocks/block.search_form.php
@@ -1,4 +1,6 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
+<?php
+namespace Habari;
+if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
 	Plugins::act( 'theme_searchform_before' );
 	echo $content->form;
 	Plugins::act( 'theme_searchform_after' );
