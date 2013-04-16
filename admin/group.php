d75bb00a14a35e417aff2138b9701ee78b3f7650
mikelietz <codger+github@gmail.com>
2013-04-16 17:45:07 +0000

    Get rid of all the classes (pctX and item and clear)\!

--- admin/group.php
+++ admin/group.php
@@ -4,7 +4,7 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 ?>
 <?php include('header.php'); ?>
 <div class="container navigation">
-	<span class="pct40">
+	<span>
 		<select name="navigationdropdown" onchange="navigationDropdown.changePage();" tabindex="1">
 			<option value="<?php echo URL::get('admin', 'page=groups'); ?>"><?php _e('All Groups'); ?></option>
 			<?php foreach ( $groups as $group_nav ): ?>
@@ -12,10 +12,10 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 			<?php endforeach; ?>
 		</select>
 	</span>
-	<span class="or pct20">
+	<span class="or">
 		<?php _e('or'); ?>
 	</span>
-	<span class="pct40">
+	<span>
 		<input type="search" id="search" placeholder="<?php _e('search settings'); ?>" tabindex="2" autofocus="autofocus">
 	</span>
 </div>
@@ -28,40 +28,40 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 <div class="container settings group groupmembers" id="groupmembers">
 
 	<h2><?php _e('Group Members'); ?></h2>
-	
-	<div class="item clear" id="assignedusers">
-		<span class="pct100" id="currentusers">
-			<span class="pct20">
+
+	<div id="assignedusers">
+		<span id="currentusers">
+			<span>
 				<label><strong><?php _e('Members'); ?></strong></label>
 			</span>
-			<span class="pct80 memberlist"></span>
+			<span class="memberlist"></span>
 		</span>
-		<span class="pct100" id="newusers">
-			<span class="pct20">
+		<span id="newusers">
+			<span>
 				<label><strong><?php _e('Members To Add'); ?></strong></label>
 			</span>
-			<span class="pct80 memberlist"></span>
+			<span class="memberlist"></span>
 		</span>
 	</div>
-	<div class="item clear">
-		<span class="pct100">
-			<span class="pct20">&nbsp;</span>
-			<span class="pct80" id="add_users" >
-				<span class="pct40"><select name="assign_user" id="assign_user"></select></span>
-				<span class="pct60"><input type="button" id="add_user" value="<?php _e('Add'); ?>" class="button add"></span>
+	<div>
+		<span>
+			<span>&nbsp;</span>
+			<span id="add_users" >
+				<span><select name="assign_user" id="assign_user"></select></span>
+				<span><input type="button" id="add_user" value="<?php _e('Add'); ?>" class="button add"></span>
 			</span>
 		</span>
-		<span class="pct100" id="removedusers">
-			<span class="pct20">
+		<span id="removedusers">
+			<span>
 				<label><strong><?php _e('Members To Remove'); ?></strong></label>
 			</span>
-			<span class="pct80 memberlist"></span>
+			<span class="memberlist"></span>
 		</span>
 	</div>
 		<?php foreach ( $users as $user ): ?>
 			<input type="hidden" name="user[<?php echo $user->id; ?>]" value="<?php echo ($user->membership) ? '1' : 0; ?>" id="user_<?php echo $user->id; ?>">
 		<?php endforeach; ?>
-	
+
 </div>
 
 <div class="container settings group groupacl" id="groupacl">
@@ -73,24 +73,24 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 		$crud_tokens = ( isset($token_group['crud']) ) ? $token_group['crud'] : array();
 		$bool_tokens = ( isset($token_group['bool']) ) ? $token_group['bool'] : array();
 	?>
-	<div class="item clear permission-group">
+	<div class="permission-group">
 		<h3><?php echo $group_name; ?></h3>
 		<?php if ( !empty( $crud_tokens ) ): ?>
-			<table id="<?php echo $group_name; ?>-crud-permissions" class="pct100 crud-permissions">
+			<table id="<?php echo $group_name; ?>-crud-permissions" class="crud-permissions">
 				<tr class="head">
-					<th class="pct40"><?php _e( 'Token Description' ); ?></th>
+					<th><?php _e( 'Token Description' ); ?></th>
 					<?php foreach ( $access_names as $name ): ?>
-					<th class="pct10"><?php echo $access_display[$name]; ?></th>
+					<th><?php echo $access_display[$name]; ?></th>
 					<?php endforeach; ?>
 				</tr>
 				<?php foreach ( $crud_tokens as $token ): ?>
 				<tr>
-					<td class="token_description pct40"><strong><?php _e($token->description); ?></strong></td>
-					<?php 
+					<td class="token_description"><strong><?php _e($token->description); ?></strong></td>
+					<?php
 					foreach ( $access_names as $name ):
 						$checked = ( isset($token->access) && ACL::access_check( $token->access, $name ) ) ? ' checked' : '';
 					?>
-						<td class="token_access pct10">
+						<td class="token_access">
 							<input type="checkbox" id="token_<?php echo $token->id . '_' . $name; ?>" class="bitflag-<?php echo $name; ?>" name="tokens[<?php echo $token->id . '][' . $name; ?>]" <?php echo $checked; ?>>
 						</td>
 					<?php endforeach; ?>
@@ -99,21 +99,21 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 			</table>
 		<?php endif; ?>
 		<?php if ( !empty( $bool_tokens ) ): ?>
-			<table id="<?php echo $group_name; ?>-bool-permissions" class="pct100 bool-permissions">
+			<table id="<?php echo $group_name; ?>-bool-permissions" class="bool-permissions">
 				<tr class="head">
-					<th class="pct40"><?php _e( 'Token Description' ); ?></th>
-					<th class="pct10"><?php echo $bool_access_display['allow']; ?></th>
-					<th class="pct10"><?php echo $bool_access_display['deny']; ?></th>
+					<th><?php _e( 'Token Description' ); ?></th>
+					<th><?php echo $bool_access_display['allow']; ?></th>
+					<th><?php echo $bool_access_display['deny']; ?></th>
 				</tr>
 				<?php foreach ( $bool_tokens as $token ): ?>
 				<tr>
-					<td class="token_description pct40"><strong><?php echo $token->description; ?></strong></td>
+					<td class="token_description"><strong><?php echo $token->description; ?></strong></td>
 					<?php $checked = ( isset($token->access) && ACL::access_check( $token->access, 'any' ) ) ? ' checked' : '';?>
-					<td class="token_access pct10">
+					<td class="token_access">
 						<input type="checkbox" id="token_<?php echo $token->id . '_full'; ?>" class="bitflag-full" name="tokens[<?php echo $token->id; ?>][full]" <?php echo $checked; ?>>
 					</td>
 					<?php $checked = ( isset($token->access) && ACL::access_check( $token->access, 'deny' ) ) ? ' checked' : '';?>
-					<td class="token_access pct10">
+					<td class="token_access">
 						<input type="checkbox" id="token_<?php echo $token->id . '_deny'; ?>" class="bitflag-deny" name="tokens[<?php echo $token->id; ?>][deny]" <?php echo $checked; ?>>
 					</td>
 				</tr>
@@ -126,10 +126,10 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 </div>
 
 <div class="container controls transparent">
-	<span class="pct50">
+	<span>
 		<input type="submit" name="delete" value="<?php _e('Delete'); ?>" class="delete button">
 	</span>
-	<span class="pct50">
+	<span>
 		<input type="submit" value="<?php _e('Apply'); ?>" class="button save">
 	</span>
 
