40b8e09baea115e499c79f742ba68943cd6e2745
mikelietz <codger+github@gmail.com>
2013-04-16 18:09:49 +0000

    Change the logs page from being a fake table to being a real table. The more/less messages functionality will need to be fixed. Also, the number of columns should probably be fixed, as well as making the data pretty and the line displayed when there's nothing there too.

--- admin/logs.php
+++ admin/logs.php
@@ -8,49 +8,48 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 
 <?php echo $form; ?>
 
-<div class="container">
+<table class="container">
 
-	<div class="head clear">
+	<thead>
+		<tr>
+			<th class="checkbox">&nbsp;</th>
+			<th class="time"><?php _e('Date &amp; Time'); ?></th>
+			<th class="user"><?php _e('User'); ?></th>
+			<th class="ip"><?php _e('IP'); ?></th>
+			<th class="module"><?php _e('Module'); ?></th>
+			<th class="type"><?php _e('Type'); ?></th>
+			<th class="severity"><?php _e('Severity'); ?></th>
+			<th class="message"><?php _e('Message'); ?></th>
+		</tr>
+		<tr>
+			<th>&nbsp;</th>
+			<th><?php echo Utils::html_select('date', $dates, $date, array( 'class'=>'')); ?></th>
+			<th><?php echo Utils::html_select('user', $users, $user, array( 'class'=>'')); ?></th>
+			<th><?php echo Utils::html_select('address', $addresses, $address, array( 'class'=>'')); ?></th>
+			<th><?php echo Utils::html_select('module', $modules, $module, array( 'class'=>'')); ?></th>
+			<th><?php echo Utils::html_select('type', $types, $type, array( 'class'=>'')); ?></th>
+			<th><?php echo Utils::html_select('severity', $severities, $severity, array( 'class'=>'')); ?></th>
+			<th><input type="submit" name="filter" value="<?php _e('Filter'); ?>"></th>
+		</tr>
+	</thead>
 
-		<span class="checkbox pct5">&nbsp;</span>
-		<span class="time pct15"><?php _e('Date &amp; Time'); ?></span>
-		<span class="user pct15"><?php _e('User'); ?></span>
-		<span class="ip pct10"><?php _e('IP'); ?></span>
-		<span class="module pct10"><?php _e('Module'); ?></span>
-		<span class="type pct10"><?php _e('Type'); ?></span>
-		<span class="severity pct10"><?php _e('Severity'); ?></span>
-		<span class="message pct25"><?php _e('Message'); ?></span>
-
-	</div>
-
-	<div class="item clear">
-		<span class="pct5">&nbsp;</span>
-		<span class="pct15"><?php echo Utils::html_select('date', $dates, $date, array( 'class'=>'pct90')); ?></span>
-		<span class="pct15"><?php echo Utils::html_select('user', $users, $user, array( 'class'=>'pct90')); ?></span>
-		<span class="pct10"><?php echo Utils::html_select('address', $addresses, $address, array( 'class'=>'pct90')); ?></span>
-		<span class="pct10"><?php echo Utils::html_select('module', $modules, $module, array( 'class'=>'pct90')); ?></span>
-		<span class="pct10"><?php echo Utils::html_select('type', $types, $type, array( 'class'=>'pct90')); ?></span>
-		<span class="pct10"><?php echo Utils::html_select('severity', $severities, $severity, array( 'class'=>'pct90')); ?></span>
-		<span class="pct25"><input type="submit" name="filter" value="<?php _e('Filter'); ?>"></span>
-	</div>
-	
 	<?php if (isset($years)) { ?>
-	<div class="manage logs">
+	<tbody class="manage logs">
 
 		<?php $theme->display('logs_items'); ?>
 
-	</div>
+	</tbody>
 
 	<?php } else { ?>
 
-	<div class="item clear">
+	<tbody><tr><td colspan="9">
 
-	<span class="pct5">&nbsp;</span><span class="pct90"><?php _e('There are no logs to be displayed at this time.'); ?></span><span class="pct5">&nbsp;</span>
+		<?php _e('There are no logs to be displayed at this time.'); ?>
 
-	</div>
+	</td></tr></tbody>
 	<?php } ?>
 
-</div>
+</table>
 
 <?php echo $form->dupe(); ?>
 
--- admin/logs_items.php
+++ admin/logs_items.php
@@ -3,24 +3,24 @@ namespace Habari;
 if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 ?>
 <?php foreach ( $logs as $log ): ?>
-	<div class="item clear">
-		<span class="checkbox pct5"><span><input type="checkbox" class="checkbox log_entry" value="<?php echo $log->id; ?>"></span></span>
-		<span class="time pct15 minor"><span><?php $log->timestamp->out( "Y-m-d H:i:s" ); ?></span></span>
-		<span class="user pct15 minor"><span>
-			<?php if ( $log->user_id ) { 
+	<tr>
+		<td class="checkbox"><input type="checkbox" class="checkbox log_entry" value="<?php echo $log->id; ?>">
+		<td class="time"><?php $log->timestamp->out( "Y-m-d H:i:s" ); ?>
+		<td class="user">
+			<?php if ( $log->user_id ) {
 				if ( $user = User::get_by_id( $log->user_id ) ) {
 					echo $user->displayname;
 				}
 				else {
 					echo $log->user_id;
 				}
-			} ?>&nbsp;
-		</span></span>
-		<span class="ip pct10 minor"><span><?php echo $log->ip; ?></span></span>
-		<span class="module pct10 minor"><span><?php echo $log->module; ?></span></span>
-		<span class="type pct10 minor"><span><?php echo $log->type; ?></span></span>
-		<span class="severity pct10 minor"><span><?php echo $log->severity; ?></span></span>
-		<span class="message pct25 minor<?php if( MultiByte::strlen( $log->message ) > 40 ) { echo ' less'; } ?>"><span><?php echo Utils::truncate(Utils::htmlspecialchars($log->message), 40, false); ?></span></span>
-		<span class="message pct25 minor more"><span><?php echo Utils::htmlspecialchars($log->message); ?></span></span>
-	</div>
+			} ?>
+		</td>
+		<td class="ip"><?php echo $log->ip; ?></td>
+		<td class="module pct10 minor"><span><?php echo $log->module; ?></td>
+		<td class="type pct10 minor"><span><?php echo $log->type; ?></td>
+		<td class="severity"><?php echo $log->severity; ?></td>
+		<td class="message <?php if( MultiByte::strlen( $log->message ) > 40 ) { echo ' less'; } ?>"><?php echo Utils::truncate(Utils::htmlspecialchars($log->message), 40, false); ?>
+		<td class="message more"><?php echo Utils::htmlspecialchars($log->message); ?>
+	</tr>
 <?php endforeach; ?>
