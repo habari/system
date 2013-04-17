7a540b5bd4e6a920f63f111b1d284304617e9d29
Michael Bishop <miklb@miklb.com>
2013-04-17 10:55:46 -0400

    begin aligning admin/themes with admin/plugins markup

--- admin/themes.php
+++ admin/themes.php
@@ -6,16 +6,17 @@
 	<h2 class="lead"><?php _e( 'Current Theme' ); ?></h2>
 	<div class="item">
 		<div class="head">
-
-			<a href="<?php echo $active_theme['info']->url; ?>" class="plugin"><?php echo $active_theme['info']->name; ?></a> <?php echo $active_theme['info']->version; ?> <?php _e('by'); ?>
-			<?php
-			$authors = array();
-			foreach ( $active_theme['info']->author as $author ) {
-				$authors[] = isset( $author['url'] ) ? '<a href="' . $author['url'] . '">' . $author . '</a>' : $author;
-			}
-			// @locale The string used between the last two items in the list of authors of a theme on the admin page (one, two, three *and* four).
-			echo Format::and_list( $authors,  _t( ' and ' ));
-			?>
+			<div class="title">
+				<a href="<?php echo $active_theme['info']->url; ?>" class="plugin"><?php echo $active_theme['info']->name; ?></a> <?php echo $active_theme['info']->version; ?> <?php _e('by'); ?>
+				<?php
+				$authors = array();
+				foreach ( $active_theme['info']->author as $author ) {
+					$authors[] = isset( $author['url'] ) ? '<a href="' . $author['url'] . '">' . $author . '</a>' : $author;
+				}
+				// @locale The string used between the last two items in the list of authors of a theme on the admin page (one, two, three *and* four).
+				echo Format::and_list( $authors,  _t( ' and ' ));
+				?>
+			</div>	
 			<?php
 				if ( isset( $active_theme['info']->help ) ):
 					if( Controller::get_var('help') == $active_theme['dir'] ):
@@ -42,24 +43,26 @@
 
 		</div>
 
-		<div>
-			<div class="thumb"><img src="<?php echo $active_theme['screenshot']; ?>"></div>
-
-			<p class="description"><?php echo $active_theme['info']->description; ?></p>
-			<?php if ( $active_theme['info']->license != '' ): ?>
-			<p class="description"><?php printf( _t('%1$s is licensed under the %2$s'), $active_theme['info']->name, '<a href="' . $active_theme['info']->license['url'] . '">' . $active_theme['info']->license . '</a>' ); ?></p>
-			<?php endif; ?>
-
-			<?php if ( isset( $active_theme['info']->help ) ): ?>
-			<div id="themehelp" class="<?php if( Controller::get_var('help') == $active_theme['dir'] ): ?>active<?php endif; ?>">
-				<div class="help">
-					<?php echo Pluggable::get_xml_text($active_theme['info']['filename'], $active_theme['info']->help); ?>
-				</div>
+		<div class="container">
+			<div class="thumb columns four"><img src="<?php echo $active_theme['screenshot']; ?>">
 			</div>
-			<?php endif; ?>
+			<div class="themeinfo columns ten">
+				<p class="description"><?php echo $active_theme['info']->description; ?></p>
+				<?php if ( $active_theme['info']->license != '' ): ?>
+				<p class="description"><?php printf( _t('%1$s is licensed under the %2$s'), $active_theme['info']->name, '<a href="' . $active_theme['info']->license['url'] . '">' . $active_theme['info']->license . '</a>' ); ?></p>
+				<?php endif; ?>
 
+				<?php if ( isset( $active_theme['info']->help ) ): ?>
+				<div id="themehelp" class="<?php if( Controller::get_var('help') == $active_theme['dir'] ): ?>active<?php endif; ?>">
+					<div class="help">
+						<?php echo Pluggable::get_xml_text($active_theme['info']['filename'], $active_theme['info']->help); ?>
+					</div>
+				</div>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
+</div>
 
 	<?php
 	// Capture the admin config output.  If nothing is there, don't output the section
@@ -77,21 +80,22 @@
 	<?php endif; ?>
 
 	<?php if ( isset($active_theme['info']->areas) ): ?>
-	<div id="blocksconfigure" class="item">
-		<h3><?php _e( "Areas" ); ?></h3>
-				<div>
-					<div id="block_add">
-						<?php $this->display('block_instances'); ?>
-					</div>
-
+	<div id="blocksconfigure" class="item container">
+		
+			<h3><?php _e( "Areas" ); ?></h3>
+				<div id="block_add" class="columns ten">
+					<?php $this->display('block_instances'); ?>
+				</div>
+				<div class="columns four">
 					<div id="scope_container">
 					<?php $this->display('block_areas'); ?>
 					</div>
 					<div class="formcontrol"><button id="save_areas" disabled="disabled"><?php _e('Save'); ?></button>
 					</div>
 				</div>
-			</div>
 			<?php endif; ?>
+
+
 
 <?php /* hide this until Scope is implemented
 		<div class="item">
@@ -113,45 +117,48 @@
 foreach ( $all_themes as $inactive_theme ):
 	if ( $inactive_theme['path'] != $active_theme_dir ) : ?>
 	<div class="item <?php if ($previewed == $inactive_theme['dir']) echo " previewing"; ?>"> 
-		<div class="head theme_credits"> 
-			<a href="<?php echo $inactive_theme['info']->url; ?>"><?php echo $inactive_theme['info']->name; ?> <?php echo $inactive_theme['info']->version; ?></a> <?php _e('by'); ?> 
-			<?php
-			$authors = array();
-			foreach ( $inactive_theme['info']->author as $author ) {
-				$authors[] = isset( $author['url'] ) ? '<a href="' . $author['url'] . '">' . $author . '</a>' : $author;
-			}
-			// @locale The string used between the last two items in the list of authors of a theme on the admin page (one, two, three *and* four).
-			echo Format::and_list( $authors,  _t( ' and ' ));
-			?>
-		</div> 
- 
-		<div class="thumb">
-			<img src="<?php echo $inactive_theme['screenshot']; ?>" class="themethumb">
-			<div class="themeinfo">
+		<div class="head">
+			<div class="title">
+				<a href="<?php echo $inactive_theme['info']->url; ?>"><?php echo $inactive_theme['info']->name; ?> <?php echo $inactive_theme['info']->version; ?></a> <?php _e('by'); ?> 
+				<?php
+				$authors = array();
+				foreach ( $inactive_theme['info']->author as $author ) {
+					$authors[] = isset( $author['url'] ) ? '<a href="' . $author['url'] . '">' . $author . '</a>' : $author;
+				}
+				// @locale The string used between the last two items in the list of authors of a theme on the admin page (one, two, three *and* four).
+				echo Format::and_list( $authors,  _t( ' and ' ));
+				?>
+			</div>
+
+		    <?php
+		    if ( $inactive_theme['info']->getName() != 'pluggable' || (string) $inactive_theme['info']->attributes()->type != 'theme' ) : ?>
+			<p class="legacy"><?php _e( 'Legacy theme.' ); ?></p>
+	      <?php elseif(isset($inactive_theme['req_parent'])): ?>
+			<p class="legacy"><?php _e( 'This theme requires the parent theme named "%s".', array($inactive_theme['req_parent']) ); ?></p>
+		    <?php else: ?>
+			<ul class="dropbutton"> 
+				<?php if ($previewed == $inactive_theme['dir']): ?>
+				<li><a href="<?php URL::out( 'admin', 'page=preview_theme&theme_dir=' . $inactive_theme['dir'] . '&theme_name=' . $inactive_theme['info']->name ); ?>"><?php _e('End Preview'); ?></a></li>
+				<?php else: ?>
+				<li><a href="<?php URL::out( 'admin', 'page=preview_theme&theme_dir=' . $inactive_theme['dir'] . '&theme_name=' . $inactive_theme['info']->name ); ?>"><?php _e('Preview'); ?></a></li>
+				<?php endif; ?>
+				<li><a href="<?php URL::out( 'admin', 'page=activate_theme&theme_dir=' . $inactive_theme['dir'] . '&theme_name=' . $inactive_theme['info']->name ); ?>"><?php _e('Activate'); ?></a></li>
+			</ul>
+		    <?php endif; ?>
+		</div>
+		<div class="container">
+		    <div class="thumb columns four">
+				<img src="<?php echo $inactive_theme['screenshot']; ?>" class="themethumb">
+			</div>
+			<div class="themeinfo columns ten">
 				<div class="pointer"></div>
 				<p class="description"><?php echo $inactive_theme['info']->description; ?></p>
 				<?php if ( $inactive_theme['info']->license != '' ): ?>
 				<p class="description"><?php printf( _t('%1$s is licensed under the %2$s'), $inactive_theme['info']->name, '<a href="' . $inactive_theme['info']->license['url'] . '">' . $inactive_theme['info']->license . '</a>' ); ?></p>
 				<?php endif; ?>
 			</div>
-		</div>
-	    <?php
-	    if ( $inactive_theme['info']->getName() != 'pluggable' || (string) $inactive_theme['info']->attributes()->type != 'theme' ) : ?>
-		<p class="legacy"><?php _e( 'Legacy theme.' ); ?></p>
-      <?php elseif(isset($inactive_theme['req_parent'])): ?>
-		<p class="legacy"><?php _e( 'This theme requires the parent theme named "%s".', array($inactive_theme['req_parent']) ); ?></p>
-	    <?php else: ?>
-		<ul class="dropbutton"> 
-			<?php if ($previewed == $inactive_theme['dir']): ?>
-			<li><a href="<?php URL::out( 'admin', 'page=preview_theme&theme_dir=' . $inactive_theme['dir'] . '&theme_name=' . $inactive_theme['info']->name ); ?>"><?php _e('End Preview'); ?></a></li>
-			<?php else: ?>
-			<li><a href="<?php URL::out( 'admin', 'page=preview_theme&theme_dir=' . $inactive_theme['dir'] . '&theme_name=' . $inactive_theme['info']->name ); ?>"><?php _e('Preview'); ?></a></li>
-			<?php endif; ?>
-			<li><a href="<?php URL::out( 'admin', 'page=activate_theme&theme_dir=' . $inactive_theme['dir'] . '&theme_name=' . $inactive_theme['info']->name ); ?>"><?php _e('Activate'); ?></a></li>
-		</ul>
-	    <?php endif; ?>
-	</div> 	
-
+	</div>	
+</div>
 <?php
 	endif;
 endforeach;
