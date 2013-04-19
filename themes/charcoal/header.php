ade1d0689ef16827a7374d07a7349f90be434370
mikelietz <codger+github@gmail.com>
2013-04-19 13:22:03 +0000

    Use HTML5 doctypes for Charcoal and Mzingi. Now all three of the core themes, as well as the admin, are served as HTML5. Closes habari/habari#274.

--- themes/charcoal/header.php
+++ themes/charcoal/header.php
@@ -2,7 +2,7 @@
 namespace Habari;
 if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML>
 <html lang="<?php echo $locale; ?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
--- themes/mzingi/header.php
+++ themes/mzingi/header.php
@@ -1,7 +1,6 @@
 <?php namespace Habari; ?>
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-"http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE HTML>
 <html <?php if(isset($locale)): ?>lang="<?php echo $locale; endif; ?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; <?php if(isset($charset)): ?>charset=<?php echo $charset; endif; ?>">
