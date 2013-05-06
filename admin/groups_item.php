b2ea371c50c72ff2ceddb1f4284e0a6aa9537f47
Michael C. Harris <michael@twofishcreative.com>
2013-05-06 12:41:51 +0000

    Start to convert groups page to use per-item dropbuttons.
    There's currently an unexpected javascript error, `Uncaught ReferenceError:
    controls is not defined `. The ability for plugins to filter actions on the
    dropbuttons has also been temporarily removed.

--- admin/groups_item.php
+++ admin/groups_item.php
@@ -5,17 +5,29 @@ if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 <div class="item group" id="item-<?php echo $group->id; ?>">
 	<div>
 		<h4><a href="<?php echo URL::get('display_group', 'id=' . $group->id) ?>" title="<?php _e('Edit group'); ?>"><?php echo $group->name; ?></a></h4>
-		<ul class="dropbutton">
-			<?php $actions = array(
-				'edit' => array('url' => URL::get('display_group', 'id=' . $group->id), 'title' => _t('Edit group'), 'label' => _t('Edit')),
-				'remove' => array('url' => 'javascript:itemManage.remove('. $group->id . ', \'group\');', 'title' => _t('Delete this group'), 'label' => _t('Delete'))
-			);
-			$actions = Plugins::filter('group_actions', $actions);
-			foreach ( $actions as $action ):
-			?>
-				<li><a href="<?php echo $action['url']; ?>" title="<?php echo $action['title']; ?>"><?php echo $action['label']; ?></a></li>
-			<?php endforeach; ?>
-		</ul>
+		<?php
+		$group_actions = FormControlDropbutton::create('group_actions');
+		$group_actions->append(
+			FormControlSubmit::create('edit')->set_caption(_t('Edit'))
+				->set_url(URL::get( 'display_group', 'id=' . $group->id ))
+				->set_property('title', _t( 'Edit \'%s\'', array( $group->title ) ) )
+				->set_enable(function($control) {
+					return User::identify()->can('manage_groups');
+				})
+		);
+		$group_actions->append(
+			FormControlSubmit::create('delete')->set_caption(_t('Delete'))
+				->set_url('javascript:itemManage.remove('. $group->id . ', \'group\');')
+				->set_property('title', _t( 'Delete \'%s\'', array( $group->title ) ) )
+				->set_enable(function($control) {
+					return User::identify()->can('manage_groups');
+				})
+		);
+		echo $group_actions->pre_out();
+		echo $group_actions->get($theme);
+		// @todo Add back in filtering of group actions
+		// $actions = Plugins::filter('group_actions', $actions);
+		?>
 	</div>
 	<?php if ( count($users) > 0 ): ?>
 		<p class="users"><?php echo _t('Members: ') . Format::and_list($users); ?></p>
