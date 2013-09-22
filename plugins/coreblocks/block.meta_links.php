60623a8d621b485b81c89a8cdb44ff279fea6d3a
Konzertheld <christian.gredig@gmail.com>
2013-09-22 04:00:10 +0200

    Remove all plugins before re-adding them as submodules. We will all be doomed.

--- plugins/coreblocks/block.meta_links.php
+++
@@ -1,6 +0,0 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
-<ul id="meta_links">
-	<?php $links = $content->list; foreach( $links as $label => $href): ?>
-	<li><a href="<?php echo $label; ?>"><?php echo $href; ?></a></li>
-	<?php endforeach; ?>
-</ul>
