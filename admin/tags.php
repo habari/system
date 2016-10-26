3540564ad027ea63f1a5493ac884a0a2db1498fb
Christian Gredig <konzertheld@users.noreply.github.com>
2016-10-26 03:12:06 +0200

    Add a possibility to add a function to manager that is executed after updating. Use that to fix behaviour of selected tags when filtering the tags page. This should be improved later so that selection does not get lost when filtering. ref habari/habari#596

--- admin/tags.php
+++ admin/tags.php
@@ -12,24 +12,32 @@
 
 <script type="text/javascript">
 // Tell the item manager what to update and from where
-$('#tag_collection').manager({updateURL: "<?php echo URL::get('admin_ajax', array('context' => 'tags')) ?>"});
+$('#tag_collection').manager({updateURL: "<?php echo URL::get('admin_ajax', array('context' => 'tags')) ?>", after_update: "visual_hooks"});
 
 // this is just visual effect stuff, the actual selecting and processing is done with FormUI
-$('#tag_collection li.tag input').change(function() {
-	var listitem = $(this).parent();
-	if(!$(this).attr('checked')) { // "checked" is set afterwards, therefore the "not"
-		// remove item from selected list (again, visually, the actual removing is done by FormUI)
-		var regex = /tag_[0-9]+/;
-		var idclass = regex.exec(listitem.attr('class'));
-		$('#selected_tags .' + idclass).remove();
-	}
-	else {
-		// keep all the properties and the input so we can click the item in both lists, but get rid of id to avoid conflicts
-		var cloneli = listitem.clone();
-		cloneli.find('input').removeAttr('id');
-		cloneli.appendTo('#selected_tags');
-	}
-});
+function visual_hooks() {
+	// Cleanup as we call this after filtering
+	// @TODO Do not lose selection when filtering
+	$("#selected_tags li").remove();
+	$("#tags_selected_items").val("")
+
+	$('#tag_collection li.tag input').change(function() {
+		var listitem = $(this).parent();
+		if(!$(this).attr('checked')) { // "checked" is set afterwards, therefore the "not"
+			// remove item from selected list (again, visually, the actual removing is done by FormUI)
+			var regex = /tag_[0-9]+/;
+			var idclass = regex.exec(listitem.attr('class'));
+			$('#selected_tags .' + idclass).remove();
+		}
+		else {
+			// keep all the properties and the input so we can click the item in both lists, but get rid of id to avoid conflicts
+			var cloneli = listitem.clone();
+			cloneli.find('input').removeAttr('id');
+			cloneli.appendTo('#selected_tags');
+		}
+	});
+}
+visual_hooks();
 
 // we need additional code when all of the tags are selected with the FormUI aggregate control
 // that is because programmatical changes of the checked state do not trigger events
